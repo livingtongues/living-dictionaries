f875bd4a9b10ae773a75c71908088c4334c233fa
Diego Córdova Nieto <gatesofsilent@hotmail.com>
2021-09-13 11:24:22 -0500

    Creating a new page for export

---
+++ src/routes/[dictionaryId]/export.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import { _ } from 'svelte-i18n';
+  import { dictionary } from '$lib/stores';
+  import Button from '$svelteui/ui/Button.svelte';
+  import ShowHide from '$svelteui/functions/ShowHide.svelte';
+  import ExportingButton from './_ExportingButton.svelte';
+
+  //Testing
+  let imgs = [
+    'https://i.imgur.com/0LVyDUY.jpeg',
+    'https://i.imgur.com/4AA1jC4.jpeg',
+    'https://firebasestorage.googleapis.com/v0/b/talking-dictionaries-alpha.appspot.com/o/images%2Fmandarin-practice%2FmogAtD3lTCtkuwj7tLDD_1630105898118.jpg?alt=media',
+  ];
+</script>
+
+<svelte:head>
+  <title>
+    {$dictionary.name}
+    {$_('misc.export', { default: 'export' })}
+  </title>
+</svelte:head>
+
+<h3 class="text-xl font-semibold">{$_('misc.export', { default: 'export' })}</h3>
+
+<ShowHide let:show let:toggle>
+  <ExportingButton images={imgs} />
+</ShowHide>
