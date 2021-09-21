a5ebccaf228fe56dd960de2cbea434ed79490594
Diego Córdova Nieto <gatesofsilent@hotmail.com>
2021-09-20 19:04:54 -0500

    adding UI exporting options

--- src/routes/[dictionaryId]/export.svelte
+++ src/routes/[dictionaryId]/export.svelte
@@ -10,6 +10,9 @@
     'https://i.imgur.com/4AA1jC4.jpeg',
     'https://firebasestorage.googleapis.com/v0/b/talking-dictionaries-alpha.appspot.com/o/images%2Fmandarin-practice%2FmogAtD3lTCtkuwj7tLDD_1630105898118.jpg?alt=media',
   ];
+  let CSV = false;
+  let images = false;
+  let audio = false;
 </script>
 
 <svelte:head>
@@ -20,6 +23,29 @@
 </svelte:head>
 
 <h3 class="text-xl font-semibold">{$_('misc.export', { default: 'export' })}</h3>
+
+<h3 class="font-semibold text-lg mt-3">
+  <!-- {$_('dictionary.contributors', { default: 'Contributors' })} -->
+  Options
+</h3>
+
+<div class="flex items-center mt-2 mb-6">
+  <input id="public" type="checkbox" bind:checked={CSV} />
+  <label for="public" class="mx-2 block leading-5 text-gray-900">
+    <!-- {$_('create.visible_to_public', { default: 'Visible to Public' })} -->
+    CSV
+  </label>
+  <input id="public" type="checkbox" bind:checked={images} />
+  <label for="public" class="mx-2 block leading-5 text-gray-900">
+    <!-- {$_('create.visible_to_public', { default: 'Visible to Public' })} -->
+    Images
+  </label>
+  <input id="public" type="checkbox" bind:checked={audio} />
+  <label for="public" class="mx-2 block leading-5 text-gray-900">
+    <!-- {$_('create.visible_to_public', { default: 'Visible to Public' })} -->
+    Audio
+  </label>
+</div>
 
 <ShowHide let:show let:toggle>
   <ExportingButton images={imgs} />
