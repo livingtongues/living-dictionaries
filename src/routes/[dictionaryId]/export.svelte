c68a4ab8767bf852657a9c99e94e49e28618331f
Diego Córdova Nieto <gatesofsilent@hotmail.com>
2021-09-22 13:36:17 -0500

    adding radio buttons

--- src/routes/[dictionaryId]/export.svelte
+++ src/routes/[dictionaryId]/export.svelte
@@ -10,7 +10,9 @@
     'https://i.imgur.com/4AA1jC4.jpeg',
     'https://firebasestorage.googleapis.com/v0/b/talking-dictionaries-alpha.appspot.com/o/images%2Fmandarin-practice%2FmogAtD3lTCtkuwj7tLDD_1630105898118.jpg?alt=media',
   ];
-  let CSV = false;
+  let Data = false;
+  let CSV = true;
+  let xlsx = false;
   let images = false;
   let audio = false;
 </script>
@@ -29,12 +31,22 @@
   Options
 </h3>
 
-<div class="flex items-center mt-2 mb-6">
-  <input id="public" type="checkbox" bind:checked={CSV} />
+<div class="items-center mt-2 mb-6">
+  <input id="public" type="checkbox" bind:checked={Data} />
   <label for="public" class="mx-2 block leading-5 text-gray-900">
     <!-- {$_('create.visible_to_public', { default: 'Visible to Public' })} -->
-    CSV
+    Data
   </label>
+  <div class="ml-6 mt-2">
+    <label class="inline-flex items-center">
+      <input type="radio" class="form-radio" name="accountType" />
+      <span class="ml-2">CSV</span>
+    </label>
+    <label class="inline-flex items-center ml-6">
+      <input type="radio" class="form-radio" name="accountType" />
+      <span class="ml-2">xlsx</span>
+    </label>
+  </div>
   <input id="public" type="checkbox" bind:checked={images} />
   <label for="public" class="mx-2 block leading-5 text-gray-900">
     <!-- {$_('create.visible_to_public', { default: 'Visible to Public' })} -->
