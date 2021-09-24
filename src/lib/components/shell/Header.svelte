5c87fa10c1b7ad581725c5e7b18178478a027f21
Diego Córdova Nieto <gatesofsilent@hotmail.com>
2021-09-24 12:26:00 -0500

    Fixing duplicate contact us icons on prod

--- src/lib/components/shell/Header.svelte
+++ src/lib/components/shell/Header.svelte
@@ -59,9 +59,14 @@
   </Button>
 
   <ShowHide let:show let:toggle>
-    <Button form="text" class="print:hidden" onclick={toggle}>
-      <i class="hidden lg:inline far fa-comment" />
-      <i class="lg:hidden far fa-question-circle" />
+    <Button form="text" class="hidden lg:inline print:hidden" onclick={toggle}>
+      <i class="far fa-comment" />
+      <span class="ml-1 hidden sm:inline">
+        {$_('header.contact_us', { default: 'Contact Us' })}
+      </span>
+    </Button>
+    <Button form="text" class="lg:hidden print:hidden" onclick={toggle}>
+      <i class="far fa-question-circle" />
       <span class="ml-1 hidden sm:inline">
         {$_('header.contact_us', { default: 'Contact Us' })}
       </span>
