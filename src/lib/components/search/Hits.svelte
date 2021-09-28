bd6b708f929e75e72d8c23ccf008af897a11261d
jacobbowdoin <jwrunner7@gmail.com>
2021-09-27 22:48:11 -0700

    fetch recently added entries

--- src/lib/components/search/Hits.svelte
+++ src/lib/components/search/Hits.svelte
@@ -1,13 +1,19 @@
 <script lang="ts">
-  import type { InstantSearch } from 'instantsearch.js';
-  export let search: InstantSearch;
-
-  import type { IEntry } from '$lib/interfaces';
-  let hits: IEntry[] = [];
-
   import { onMount } from 'svelte';
   // import { connectHits } from 'instantsearch.js/es/connectors';
   import { connectHits } from 'instantsearch.js/cjs/connectors/index.js';
+  import { limit, orderBy, where, Timestamp } from 'firebase/firestore';
+  import { user } from '$sveltefire/user';
+  import { canEdit, dictionary } from '$lib/stores';
+  import { mergeBy } from '$lib/helpers/array';
+  import type { IEntry } from '$lib/interfaces';
+  import type { InstantSearch } from 'instantsearch.js';
+
+  export let search: InstantSearch;
+
+  let hits: IEntry[] = [];
+  let recentlyUpdatedEntries: IEntry[] = [];
+  $: entries = mergeBy<IEntry>(hits, recentlyUpdatedEntries, 'id');
 
   onMount(() => {
     const customHits = connectHits((params) => {
@@ -19,6 +25,25 @@
 
     search.addWidgets([customHits({})]);
   });
+
+  function minutesAgo(minutes: number) {
+    return Timestamp.fromMillis(Date.now() - minutes * 1000 * 60);
+  }
 </script>
 
-<slot entries={hits}>Loading...</slot>
+<slot {entries}>Loading...</slot>
+
+{#if $canEdit}
+  {#await import('$sveltefire/components/Collection.svelte') then { default: Collection }}
+    <Collection
+      path={`dictionaries/${$dictionary.id}/words`}
+      queryConstraints={[
+        where('ub', '==', $user.uid),
+        where('ua', '>', minutesAgo(10)),
+        orderBy('ua', 'desc'),
+        limit(4),
+      ]}
+      startWith={recentlyUpdatedEntries}
+      on:data={(e) => (recentlyUpdatedEntries = e.detail.data)} />
+  {/await}
+{/if}
---
+++ src/lib/helpers/array.ts
@@ -0,0 +1,12 @@
+export function mergeBy<T>(sourceArray: T[], updatingArray: T[], field: string) {
+  const mergedArr = sourceArray;
+  for (const value of updatingArray.reverse()) {
+    const matchedIndex = sourceArray.findIndex((x) => x[field] === value[field]);
+    if (matchedIndex >= 0) {
+      sourceArray[matchedIndex] = value;
+    } else {
+      mergedArr.unshift(value);
+    }
+  }
+  return mergedArr;
+}
