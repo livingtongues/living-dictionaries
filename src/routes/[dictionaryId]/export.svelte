cc2b7d88dadec68ae17b12389a4811653b421fed
jacobbowdoin <jwrunner7@gmail.com>
2021-10-27 12:21:06 -0500

    visual cleanup

--- src/routes/[dictionaryId]/export.svelte
+++ src/routes/[dictionaryId]/export.svelte
@@ -36,20 +36,16 @@
 <h3 class="font-semibold text-lg mt-3">Options</h3>
 
 <div class="items-center mt-2 mb-6 ml-3">
-  <input disabled id="data" type="checkbox" bind:checked={downloadData} class="opacity-50" />
-  <label for="data" class="mx-2 block leading-5 text-gray-900"> Data </label>
+  <div class="flex items-center">
+    <input disabled id="data" type="checkbox" bind:checked={downloadData} class="opacity-50" />
+    <label for="data" class="mx-2 block leading-5 text-gray-900"> Data </label>
+  </div>
   <div
-    class={`ml-8 mt-2 py-2 px-4 opacity-50 ${downloadData ? '' : 'opacity-50 cursor-not-allowed'}`}>
-    <label class="inline-flex items-center">
-      <input
-        disabled={!downloadData}
-        type="radio"
-        class="form-radio"
-        name="accountType"
-        bind:group={dataType}
-        value={'CSV'} />
-      <span class="ml-2">CSV</span>
-    </label>
+    class="ml-4 my-2 py-2 px-4 flex items-center {downloadData
+      ? ''
+      : 'opacity-50 cursor-not-allowed'}">
+    <input disabled={!downloadData} type="radio" name="csv" bind:group={dataType} value={'CSV'} />
+    <label for="csv" class="mx-2 block leading-5 text-gray-900"> CSV </label>
     <!--TODO xlsx option-->
     <!-- <label class="inline-flex items-center ml-6">
       <input
@@ -62,28 +58,24 @@
       <span class="ml-2">xlsx</span>
     </label> -->
   </div>
-  <div>
-    <div class={`${hasImages ? '' : 'opacity-50 cursor-not-allowed'}`}>
-      <input disabled={!hasImages} id="images" type="checkbox" bind:checked={includeImages} />
-      <label for="images" class="mx-2 block leading-5 text-gray-900"> Images </label>
-    </div>
-    {#if !mounted}
-      <p class="text-xs italic text-orange-400 p-2">Checking if images are available</p>
-    {:else if !hasImages}
-      <p class="text-sm text-red-700 p-3">There are no images</p>
-    {/if}
+  <div class="flex items-center {hasImages ? '' : 'opacity-50 cursor-not-allowed'}">
+    <input disabled={!hasImages} id="images" type="checkbox" bind:checked={includeImages} />
+    <label for="images" class="mx-2 block leading-5 text-gray-900"> Images </label>
   </div>
-  <div>
-    <div class={`${hasAudio ? '' : 'opacity-50 cursor-not-allowed'}`}>
-      <input id="audio" type="checkbox" bind:checked={includeAudio} />
-      <label for="audio" class="mx-2 block leading-5 text-gray-900"> Audio </label>
-    </div>
-    {#if !mounted}
-      <p class="text-xs italic text-orange-400 p-2">Checking if audio is available</p>
-    {:else if !hasAudio}
-      <p class="text-sm text-red-700 p-3">There are no audio files</p>
-    {/if}
+  {#if !mounted}
+    <p class="text-xs italic text-orange-400 p-2">Checking if images are available</p>
+  {:else if !hasImages}
+    <p class="text-sm text-red-700 p-3">There are no images</p>
+  {/if}
+  <div class="flex items-center {hasAudio ? '' : 'opacity-50 cursor-not-allowed'}">
+    <input id="audio" type="checkbox" bind:checked={includeAudio} />
+    <label for="audio" class="mx-2 block leading-5 text-gray-900"> Audio </label>
   </div>
+  {#if !mounted}
+    <p class="text-xs italic text-orange-400 p-2">Checking if audio is available</p>
+  {:else if !hasAudio}
+    <p class="text-sm text-red-700 p-3">There are no audio files</p>
+  {/if}
 </div>
 
 <Button
