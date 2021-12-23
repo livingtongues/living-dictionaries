9338cbd6c2c6dcc7a251063b90230a3644162e96
jacobbowdoin <jwrunner7@gmail.com>
2021-12-24 07:05:59 +0800

    prepare import script to handle media

---
+++ functions/scripts/import/getImageServingUrl.ts
@@ -0,0 +1,14 @@
+import { fetchAsText } from 'fetch-as';
+const p = 'anet';
+const processImageUrl = `https://${p}-photo.appspot.com/urlfull`;
+
+export const getImageServingUrl = async (imageStoragePath: string, environment: string) => {
+  try {
+    const storagePath = `${processImageUrl}/talking-dictionaries-${environment}.appspot.com/${imageStoragePath}`;
+    const imageServingUrl = await fetchAsText(storagePath);
+    return imageServingUrl.data.replace('http://lh3.googleusercontent.com/', '');
+  } catch (error) {
+    console.log(`Error getting serving url for ${imageStoragePath} on ${environment}`);
+    throw new Error(error);
+  }
+};
