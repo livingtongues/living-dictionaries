8e9ce3ff7659a058d4e1c8b767eb95e2b610b5e0
jacobbowdoin <jwrunner7@gmail.com>
2021-12-28 14:22:17 +0800

    set url to alpha when prod

--- functions/scripts/import/getImageServingUrl.ts
+++ functions/scripts/import/getImageServingUrl.ts
@@ -4,7 +4,9 @@ const processImageUrl = `https://${p}-photo.appspot.com/urlfull`;
 
 export const getImageServingUrl = async (imageStoragePath: string, environment: string) => {
   try {
-    const storagePath = `${processImageUrl}/talking-dictionaries-${environment}.appspot.com/${imageStoragePath}`;
+    const storagePath = `${processImageUrl}/talking-dictionaries-${
+      environment == 'prod' ? 'alpha' : 'dev'
+    }.appspot.com/${imageStoragePath}`;
     const imageServingUrl = await fetchAsText(storagePath);
     return imageServingUrl.data.replace('http://lh3.googleusercontent.com/', '');
   } catch (error) {
