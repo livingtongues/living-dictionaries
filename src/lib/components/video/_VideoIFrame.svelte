69481da666286fbdb16678d763e4c1ad59ea7c50
Diego Córdova Nieto <gatesofsilent@hotmail.com>
2022-01-21 14:00:25 -0600

    improve iframe video responsivness

--- src/lib/components/video/_VideoIFrame.svelte
+++ src/lib/components/video/_VideoIFrame.svelte
@@ -14,11 +14,12 @@
 
 <section class="flex justify-center">
   {#if whereSource(videoFile.path) === 'www.youtube'}
+    <!-- different iframes could have different width and height measurements -->
     <iframe
       id="player"
       type="text/html"
-      width={screenWidth >= 540 ? '456' : screenWidth >= 425 ? '342' : '247'}
-      height={screenWidth >= 540 ? '342' : screenWidth >= 425 ? '256' : '200'}
+      width={screenWidth >= 540 ? '456' : screenWidth * 0.8444}
+      height={screenWidth >= 540 ? '342' : screenWidth * 0.6333}
       src={`https://www.youtube.com/embed/${videoFile.externalId}`}
       title="YouTube video player"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
@@ -27,8 +28,8 @@
     <iframe
       id="player"
       type="text/html"
-      width={screenWidth >= 540 ? '456' : screenWidth >= 425 ? '342' : '247'}
-      height={screenWidth >= 540 ? '342' : screenWidth >= 425 ? '256' : '200'}
+      width={screenWidth >= 540 ? '456' : screenWidth * 0.8444}
+      height={screenWidth >= 540 ? '342' : screenWidth * 0.6333}
       src={`https://player.vimeo.com/video/${videoFile.externalId}`}
       title="Vimeo video player"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
