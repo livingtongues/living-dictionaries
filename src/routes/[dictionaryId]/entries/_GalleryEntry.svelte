310912fc324690bbd9f8bd7dd44574019347ff99
Diego Córdova Nieto <gatesofsilent@hotmail.com>
2022-01-26 13:36:25 -0600

    new gallery view

--- src/routes/[dictionaryId]/entries/_GalleryEntry.svelte
+++ src/routes/[dictionaryId]/entries/_GalleryEntry.svelte
@@ -5,22 +5,33 @@
   export let canEdit = false;
 </script>
 
-<div
-  class="bg-gray-300 shadow relative rounded overflow-hidden"
-  style="max-width: 500px; max-height: 500px;">
-  <Image square={480} {entry} {canEdit} />
-  <div
-    class="text-dark-shadow text-white font-semibold p-2 absolute top-0
-            left-0">
-    {@html entry._highlightResult ? entry._highlightResult.lx.value : entry.lx}
-  </div>
-  <div
-    class="text-dark-shadow text-white p-2 absolute bottom-0 left-0
-            text-xs">
-    {@html entry._highlightResult
-      ? entry._highlightResult.gl.en.value
-      : entry.gl && entry.gl.en
-      ? entry.gl.en
-      : ''}
+<div class="flex flex-col relative rounded" style="max-width: 500px; max-height: 500px;">
+  <div class="bg-gray-300 shadow">
+    <div class="aspect-square overflow-hidden">
+      <Image square={480} {entry} {canEdit} />
+    </div>
+    <div class="card-content-wrapper">
+      <p class="font-semibold absolute top-0 left-0">
+        {@html entry._highlightResult ? entry._highlightResult.lx.value : entry.lx}
+      </p>
+      <p class="absolute bottom-0 left-0 text-xs">
+        {@html entry._highlightResult
+          ? entry._highlightResult.gl.en.value
+          : entry.gl && entry.gl.en
+          ? entry.gl.en
+          : ''}
+      </p>
+    </div>
   </div>
 </div>
+
+<style>
+  .card-content-wrapper {
+    background: #f3f3f3;
+    padding: 20px 20px 20px 10px;
+  }
+
+  .card-content-wrapper > p {
+    position: relative;
+  }
+</style>
