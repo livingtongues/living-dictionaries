e48ea4f0e3c016bcaf829ce61866324a659c0c83
Diego Córdova Nieto <gatesofsilent@hotmail.com>
2022-02-09 14:46:26 -0600

    separate remove function

--- src/routes/admin/_RolesManagment.svelte
+++ src/routes/admin/_RolesManagment.svelte
@@ -2,12 +2,13 @@
   import BadgeArrayEmit from '$svelteui/data/BadgeArrayEmit.svelte';
   import ShowHide from '$svelteui/functions/ShowHide.svelte';
   import { removeDictionaryManagePermission } from '$lib/helpers/dictionariesManaging';
-
   export let data: any;
   export let dictionary: string;
   let data_strings: string[];
   $: data_strings = data.map((e) => e.name);
-  console.log(data);
+  async function remove(id, dictionary) {
+    removeDictionaryManagePermission(id, dictionary);
+  }
 </script>
 
 <div class="py-3">
@@ -18,9 +19,9 @@
         canEdit
         addMessage="Add"
         on:itemclicked={(e) => console.log('clicked:', data[e.detail.index].id)}
-        on:itemremoved={(e) =>
-          removeDictionaryManagePermission(data[e.detail.index].id, dictionary)}
-        on:additem={toggle} />
+        on:itemremoved={(e) => remove(data[e.detail.index].id, dictionary)}
+        on:additem={toggle}
+      />
       {#if show}
         <!-- {#await import('./_SelectDictionaryModal.svelte') then { default: SelectDictionaryModal }}
           <SelectDictionaryModal {user} on:close={toggle} />
