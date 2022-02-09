d1f49ed4b41f5bbf5924a993077d0e9633f4e327
Diego Córdova Nieto <gatesofsilent@hotmail.com>
2022-02-08 18:52:40 -0600

    display roles to admin dictionaries tab

---
+++ src/routes/admin/_RolesManagment.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import BadgeArrayEmit from '$svelteui/data/BadgeArrayEmit.svelte';
+  import ShowHide from '$svelteui/functions/ShowHide.svelte';
+  import { removeDictionaryManagePermission } from '$lib/helpers/dictionariesManaging';
+
+  export let data: any;
+  export let dictionary: string;
+  let data_strings: string[];
+  $: data_strings = data.map((e) => e.name);
+  console.log(data);
+</script>
+
+<div class="py-3">
+  <div class="text-sm leading-5 font-medium text-gray-900">
+    <ShowHide let:show let:toggle>
+      <BadgeArrayEmit
+        strings={data_strings}
+        canEdit
+        addMessage="Add"
+        on:itemclicked={(e) => console.log('clicked:', data[e.detail.index].id)}
+        on:itemremoved={(e) =>
+          removeDictionaryManagePermission(data[e.detail.index].id, dictionary)}
+        on:additem={toggle} />
+      {#if show}
+        <!-- {#await import('./_SelectDictionaryModal.svelte') then { default: SelectDictionaryModal }}
+          <SelectDictionaryModal {user} on:close={toggle} />
+        {/await} -->good
+      {/if}
+    </ShowHide>
+  </div>
+  <!-- <div class="text-sm leading-5 text-gray-500" /> -->
+</div>
