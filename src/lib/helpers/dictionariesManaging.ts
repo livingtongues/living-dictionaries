48c801146554b514af13986a3ec65dbe23721d71
Diego Córdova Nieto <gatesofsilent@hotmail.com>
2022-02-11 13:49:21 -0600

    add two new functions for remove and add write-in collaborators

--- src/lib/helpers/dictionariesManaging.ts
+++ src/lib/helpers/dictionariesManaging.ts
@@ -1,6 +1,6 @@
 import { deleteDocument, set, update } from '$sveltefire/firestorelite';
 import { arrayRemove, arrayUnion, serverTimestamp } from 'firebase/firestore/lite';
-import type { IUser, IManager } from '$lib/interfaces';
+import type { IUser, IManager, IWriteInCollaborator } from '$lib/interfaces';
 
 export async function addDictionaryManagePermission(userBeingEdited: IUser, dictionaryId: string) {
   const manager: IManager = {
@@ -26,5 +26,29 @@ export async function removeDictionaryManagePermission(
     await update(`users/${userBeingEdited.uid}`, {
       managing: arrayRemove(dictionaryId),
     });
+  }
+}
+
+export async function addDictionaryCollaboratorPermission(
+  userBeingEdited: IUser,
+  dictionaryId: string
+) {
+  const collab: IWriteInCollaborator = {
+    id: userBeingEdited.uid,
+    name: userBeingEdited.displayName,
+  };
+  await set(`dictionaries/${dictionaryId}/writeInCollaborators/${userBeingEdited.uid}`, collab);
+}
+
+export async function removeDictionaryCollaboratorPermission(
+  userBeingEdited: IUser,
+  dictionaryId: string
+) {
+  if (
+    confirm(`Are you sure you want to remove ${userBeingEdited.displayName} from ${dictionaryId}?`)
+  ) {
+    await deleteDocument(
+      `dictionaries/${dictionaryId}/writeInCollaborators/${userBeingEdited.uid}`
+    );
   }
 }
