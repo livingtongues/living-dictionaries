0f365f36ae80f54eccabbcada2e27a6fe12e5bf6
jacobbowdoin <jwrunner7@gmail.com>
2022-02-15 17:48:34 +0800

    refactor out entry media

---
+++ src/routes/[dictionaryId]/entry/_EntryMedia.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import { _ } from 'svelte-i18n';
+  import type { IEntry } from '$lib/interfaces';
+  import Audio from '../entries/_Audio.svelte';
+  import AddImage from '../entries/_AddImage.svelte';
+  import Image from '$lib/components/image/Image.svelte';
+
+  export let entry: IEntry,
+    canEdit = false;
+</script>
+
+{#if entry.pf}
+  <div class="w-full overflow-hidden rounded relative mb-2" style="height: 25vh;">
+    <Image width={400} {entry} {canEdit} />
+  </div>
+{:else if canEdit}
+  <AddImage {entry} class="rounded-md h-20 bg-gray-100 mb-2">
+    <div class="text-xs" slot="text">
+      {$_('entry.upload_photo', { default: 'Upload Photo' })}
+    </div>
+  </AddImage>
+{/if}
+
+{#if entry.sf || canEdit}
+  <Audio {entry} class="h-20 mb-2 rounded-md bg-gray-100 px-3" />
+{/if}
