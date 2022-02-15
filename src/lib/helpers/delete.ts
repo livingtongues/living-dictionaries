f49d1d70cf04b42acb67b0dd181e883eb8047322
jacobbowdoin <jwrunner7@gmail.com>
2022-02-15 17:04:46 +0800

    refactor entry component to keep data from looping and to make it cleaner

--- src/lib/helpers/delete.ts
+++ src/lib/helpers/delete.ts
@@ -3,7 +3,8 @@ import { get } from 'svelte/store';
 import { _ } from 'svelte-i18n';
 
 import type { IEntry } from '$lib/interfaces';
-import { updateOnline } from '$sveltefirets';
+import { updateOnline, deleteDocumentOnline, setOnline } from '$sveltefirets';
+import { serverTimestamp } from 'firebase/firestore/lite';
 
 export async function deleteImage(entry: IEntry) {
   const $_ = get(_);
@@ -30,5 +31,32 @@ export async function deleteAudio(entry: IEntry) {
     );
   } catch (err) {
     alert(`${$_('misc.error', { default: 'Error' })}: ${err}`);
+  }
+}
+
+import { goto } from '$app/navigation';
+export async function deleteEntry(entry: IEntry, dictionaryId: string, algoliaQueryParams: string) {
+  const $_ = get(_);
+  if (
+    confirm(
+      $_('entry.delete_entry', {
+        default: 'Delete entry?',
+      })
+    )
+  ) {
+    try {
+      goto(`/${dictionaryId}/entries/list${algoliaQueryParams}`);
+      const timeStampRemovedEntry = { ...entry };
+      delete timeStampRemovedEntry.ca; // needed b/c error when entry is received by firestore and set by firestore/lite
+      delete timeStampRemovedEntry.ua;
+      await setOnline<IEntry>(`dictionaries/${dictionaryId}/deletedEntries/${entry.id}`, {
+        ...timeStampRemovedEntry,
+        // @ts-ignore
+        deletedAt: serverTimestamp(),
+      });
+      await deleteDocumentOnline(`dictionaries/${dictionaryId}/words/${entry.id}`);
+    } catch (err) {
+      alert(`${$_('misc.error', { default: 'Error' })}: ${err}`);
+    }
   }
 }
---
+++ src/routes/[dictionaryId]/entry/_EntryMeta.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  import { _ } from 'svelte-i18n';
+  import { printGlosses } from '$lib/helpers/glosses';
+  import type { IDictionary, IEntry } from '$lib/interfaces';
+
+  export let entry: IEntry, dictionary: IDictionary;
+  let shareImage = `https://i2.wp.com/livingtongues.org/wp-content/uploads/2015/03/LT-logo-1.png?w=987&ssl=1`;
+
+  $: title = `${entry.lx} (${dictionary.name} Living Dictionary)`;
+  $: description = printGlosses(entry.gl).join(', ');
+  $: url = `https://livingdictionaries.app/${dictionary.id}/entry/${entry.id}`;
+  $: if (entry.pf && entry.pf.gcs) {
+    shareImage = `https://lh3.googleusercontent.com/${entry.pf.gcs}=w1200`;
+  }
+</script>
+
+<svelte:head>
+  <title>{title}</title>
+  <meta name="title" content={title} />
+  <meta name="description" content={description} />
+
+  <meta property="og:title" content={title} />
+  <meta property="og:description" content={description} />
+  <meta property="og:image" content={shareImage} />
+  <meta property="og:url" content={url} />
+  <meta property="og:site_name" content={$_('misc.LD', { default: 'Living Dictionaries' })} />
+
+  <meta name="twitter:card" content="summary_large_image" />
+  <meta property="twitter:url" content={url} />
+  <meta property="twitter:title" content={title} />
+  <meta property="twitter:description" content={description} />
+  <meta property="twitter:image" content={shareImage} />
+  <meta name="twitter:image:alt" content={title} />
+</svelte:head>
