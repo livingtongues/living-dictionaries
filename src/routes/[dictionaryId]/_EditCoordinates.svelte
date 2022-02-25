047e4fe52a99df43a3ed78236bc4794876ab10e8
Diego Córdova Nieto <gatesofsilent@hotmail.com>
2022-02-25 15:08:06 -0600

    add coordinates to edit

---
+++ src/routes/[dictionaryId]/_EditCoordinates.svelte
@@ -0,0 +1,68 @@
+<script lang="ts">
+  import { _ } from 'svelte-i18n';
+  import type { IDictionary } from '$lib/interfaces';
+  import { updateOnline } from '$sveltefirets';
+  import { GeoPoint } from 'firebase/firestore/lite';
+  import Button from '$svelteui/ui/Button.svelte';
+
+  export let dictionary: IDictionary;
+  let lat = dictionary.coordinates.latitude;
+  let lng = dictionary.coordinates.longitude;
+  let modal: 'coordinates' = null;
+
+  async function save() {
+    try {
+      await updateOnline(`dictionaries/${dictionary.id}`, { coordinates: new GeoPoint(lat, lng) });
+    } catch (err) {
+      alert(`${$_('misc.error', { default: 'Error' })}: ${err}`);
+    }
+  }
+
+  async function showCoordinatesComponent() {
+    // @ts-ignore
+    dictionary.coordinates = dictionary.coordinates.latitude
+      ? { latitude: lat, longitude: lng }
+      : {};
+    modal = 'coordinates';
+  }
+</script>
+
+<form class="mt-4" on:submit|preventDefault={save}>
+  <div class="mt-6">
+    <!-- svelte-ignore a11y-label-has-associated-control -->
+    <label class="block text-sm font-medium leading-5 text-gray-700">
+      {$_('create.where_spoken', {
+        default: 'Where is this language spoken?',
+      })}*
+    </label>
+  </div>
+  <div class="mt-1">
+    <Button onclick={() => showCoordinatesComponent()}>
+      {#if lat && lng}
+        {lat}°
+        {lat < 0 ? 'S' : 'N'},
+        {lng}°
+        {lng < 0 ? 'W' : 'E'}
+      {:else}
+        <i class="fas fa-globe-americas mr-1" />
+        {$_('create.select_coordinates', { default: 'Select Coordinates' })}
+      {/if}
+    </Button>
+  </div>
+</form>
+
+{#if modal === 'coordinates'}
+  {#await import('$lib/components/modals/Coordinates.svelte') then { default: Coordinates }}
+    <Coordinates
+      on:close={() => {
+        modal = null;
+      }}
+      on:save={(event) => {
+        (lat = event.detail.lat), (lng = event.detail.lng);
+        save();
+      }}
+      on:remove={() => {
+        (lat = lat), (lng = lng);
+      }} />
+  {/await}
+{/if}
