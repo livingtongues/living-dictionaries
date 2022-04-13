4026cd9ca5b67ac79562b6ee65b419c7fcfc7b14
Diego Córdova Nieto <gatesofsilent@hotmail.com>
2022-04-13 11:57:15 -0500

    fix ready for monorepo

--- packages/site/src/lib/components/table/Cell.svelte
+++ packages/site/src/lib/components/table/Cell.svelte
@@ -55,6 +55,7 @@
       {canEdit}
       field={`gl.${column.field}`}
       value={entry.gl && entry.gl[column.field]}
+      display={column.display}
       {updatedValue}
       htmlValue={(entry._highlightResult &&
         entry._highlightResult.gl &&
@@ -67,6 +68,7 @@
       {canEdit}
       field={`xs.${column.field}`}
       value={entry.xs && entry.xs[column.field]}
+      display={column.display}
       {updatedValue}
       htmlValue={(entry._highlightResult &&
         entry._highlightResult.xs &&
@@ -79,6 +81,7 @@
       {canEdit}
       field={column.field}
       value={entry[column.field]}
+      display={column.display}
       {updatedValue}
       htmlValue={(entry._highlightResult &&
         entry._highlightResult[column.field] &&
