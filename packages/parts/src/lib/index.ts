a0a548a055ca3f709a56783b85f225b37482de31
jacobbowdoin <jwrunner7@gmail.com>
2022-04-18 07:23:37 +0800

    add LatLngDisplay component

--- packages/parts/src/lib/index.ts
+++ packages/parts/src/lib/index.ts
@@ -2,3 +2,5 @@ export { default as MediaStream } from './record/MediaStream.svelte';
 export { default as Recorder } from './record/Recorder.svelte';
 
 export { default as Invitation } from './contributors/Invitation.svelte';
+
+export { default as LatLngDisplay } from './maps/LatLngDisplay.svelte';
