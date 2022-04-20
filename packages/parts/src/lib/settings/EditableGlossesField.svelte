6d18fc8b5fc27c2d0d3cfeded33119a424778c7c
jacobbowdoin <jwrunner7@gmail.com>
2022-04-21 07:56:36 +0800

    add page for editable glosses field

--- packages/parts/src/lib/settings/EditableGlossesField.svelte
+++ packages/parts/src/lib/settings/EditableGlossesField.svelte
@@ -1,35 +1,35 @@
 <script lang="ts">
   import type { Readable } from 'svelte/store';
   export let t: Readable<any> = undefined;
-  import type { IDictionary, IGlossLanguages } from '@ld/types';
+
+  import type { IGlossLanguages } from '@ld/types';
   import Button from 'svelte-pieces/ui/Button.svelte';
   import ShowHide from 'svelte-pieces/functions/ShowHide.svelte';
   import BadgeArrayEmit from 'svelte-pieces/data/BadgeArrayEmit.svelte';
   import Modal from 'svelte-pieces/ui/Modal.svelte';
   import Filter from '../helpers/Filter.svelte';
-  //import { admin } from '$lib/stores';
   export let admin = false;
   export let glossingLanguages: IGlossLanguages;
-  export let dictionary: IDictionary;
+  export let glossLanguages: string[];
 
-  $: activeGlossingBcps = dictionary.glossLanguages.map((bcp) => $t('gl.' + bcp));
+  $: activeGlossingBcps = glossLanguages.map((bcp) => $t('gl.' + bcp));
   $: remainingGlossingLanguagesAsArray = Object.entries(glossingLanguages)
     .map((e) => ({
       bcp: e[0],
       ...e[1],
     }))
-    .filter((e) => !dictionary.glossLanguages.includes(e.bcp));
+    .filter((e) => !glossLanguages.includes(e.bcp));
 
   import { createEventDispatcher } from 'svelte';
-  const dispatch =
-    createEventDispatcher<{ add: { languageId: string }; remove: { languageId: string } }>();
+  const dispatch = createEventDispatcher<{
+    add: { languageId: string };
+    remove: { languageId: string };
+  }>();
 </script>
 
 <div class="mt-6">
   <label for="glosses" class="block text-xs leading-5 text-gray-700 mb-1">
-    {$t('create.gloss_dictionary_in', {
-      default: 'Make dictionary available in...',
-    })}
+    {t ? $t('create.gloss_dictionary_in') : 'Make dictionary available in...'}
   </label>
 
   <ShowHide let:show let:toggle>
@@ -42,7 +42,7 @@
           if (
             confirm('Remove as admin? Know that regular editors get a message saying "Contact Us"')
           ) {
-            dispatch('remove', { languageId: dictionary.glossLanguages[e.detail.index] });
+            dispatch('remove', { languageId: glossLanguages[e.detail.index] });
           }
         } else {
           alert($t('header.contact_us', { default: 'Contact Us' }));
@@ -52,14 +52,12 @@
     {#if show}
       <Modal on:close={toggle}>
         <span slot="heading">
-          {$t('create.gloss_dictionary_in', {
-            default: 'Make dictionary available in...',
-          })}
+          {t ? $t('create.gloss_dictionary_in') : 'Make dictionary available in...'}
         </span>
         <Filter
           items={remainingGlossingLanguagesAsArray}
           let:filteredItems={filteredLanguages}
-          placeholder={$t('about.search', { default: 'Search' })}>
+          placeholder={t ? $t('about.search') : 'Search'}>
           {#each filteredLanguages as language}
             <Button
               onclick={() => {
@@ -69,17 +67,17 @@
               color="green"
               form="simple"
               class="w-full !text-left">
-              {language.vernacularName || $t('gl.' + language.bcp)}
+              {language.vernacularName || (t && $t('gl.' + language.bcp))}
               {#if language.vernacularAlternate}
                 {language.vernacularAlternate}
               {/if}
-              {#if language.vernacularName}
+              {#if language.vernacularName && t}
                 <small>({$t('gl.' + language.bcp)})</small>
               {/if}
             </Button>
           {/each}
 
-          <div class="modal-footer space-x-1">
+          <div class="modal-footer">
             <Button onclick={toggle} color="black">Cancel</Button>
           </div>
         </Filter>
@@ -88,11 +86,11 @@
   </ShowHide>
 
   <div class="text-xs text-gray-600 mt-1">
-    {$t('create.gloss_dictionary_clarification', {
-      default: 'Language(s) you want to translate entries into',
-    })}
-    {$t('settings.unable-deleting-msg', {
-      default: 'Note: you will not be able to delete these glossing languages later.',
-    })}
+    {t
+      ? $t('create.gloss_dictionary_clarification')
+      : 'Language(s) you want to translate entries into'}
+    {t
+      ? $t('settings.unable-deleting-msg')
+      : 'Note: you will not be able to delete these glossing languages later.'}
   </div>
 </div>
--- packages/parts/src/routes/index.md
+++ packages/parts/src/routes/index.md
@@ -1,3 +1,6 @@
 # Living Dictionaries Kitbook
 
 Documentation home and prototyping tool for the [Living Dictionaries](https://livingdictionaries.app) web app, built using Kitbook.
+
+- hello
+- there
