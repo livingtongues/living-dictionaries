6d3d4e661eafe00c0a88383bfe2d119282178f0b
jacobbowdoin <jwrunner7@gmail.com>
2022-04-29 06:56:35 +0800

    add missing EditableGlossesField features and scenarios

--- packages/parts/src/lib/settings/EditableGlossesField.svelte
+++ packages/parts/src/lib/settings/EditableGlossesField.svelte
@@ -9,13 +9,14 @@
   import Modal from 'svelte-pieces/ui/Modal.svelte';
   import Filter from '../helpers/Filter.svelte';
 
-  export let admin: number | boolean = false;
   export let availableLanguages: IGlossLanguages;
   export let selectedLanguages: string[];
 
-  $: activeGlossingBcps = selectedLanguages.map((bcp) =>
-    t ? $t('gl.' + bcp) : availableLanguages[bcp].vernacularName
-  );
+  export let minimum = 1;
+
+  $: activeGlossingBcps = Array.isArray(selectedLanguages)
+    ? selectedLanguages.map((bcp) => (t ? $t('gl.' + bcp) : availableLanguages[bcp].vernacularName))
+    : [];
   $: remainingGlossingLanguagesAsArray = Object.entries(availableLanguages)
     .map((e) => ({
       bcp: e[0],
@@ -26,74 +27,62 @@
   import { createEventDispatcher } from 'svelte';
   const dispatch = createEventDispatcher<{
     add: { languageId: string };
-    remove: { languageId: string };
+    itemremoved: { value: string; index: number; };
   }>();
 </script>
 
-<div class="mt-6">
-  <label for="glosses" class="block text-xs leading-5 text-gray-700 mb-1">
-    {t ? $t('create.gloss_dictionary_in') : 'Make dictionary available in...'}
-  </label>
+<label for="glosses" class="block text-xs leading-5 text-gray-700 mb-1">
+  {t ? $t('create.gloss_dictionary_in') : 'Make dictionary available in...'}
+</label>
 
-  <ShowHide let:show let:toggle>
-    <BadgeArrayEmit
-      strings={activeGlossingBcps}
-      canEdit
-      addMessage={t ? $t('misc.add', { default: 'Add' }) : 'Add'}
-      on:itemremoved={(e) => {
-        if (admin) {
-          if (
-            confirm('Remove as admin? Know that regular editors get a message saying "Contact Us"')
-          ) {
-            dispatch('remove', { languageId: selectedLanguages[e.detail.index] });
-          }
-        } else {
-          alert(t ? $t('header.contact_us') : 'Contact Us');
-        }
-      }}
-      on:additem={toggle} />
-    {#if show}
-      <Modal on:close={toggle}>
-        <span slot="heading">
-          {t ? $t('create.gloss_dictionary_in') : 'Make dictionary available in...'}
-        </span>
-        <Filter
-          items={remainingGlossingLanguagesAsArray}
-          let:filteredItems={filteredLanguages}
-          placeholder={t ? $t('about.search') : 'Search'}>
-          {#each filteredLanguages as language}
-            <Button
-              onclick={() => {
-                dispatch('add', { languageId: language.bcp });
-                toggle();
-              }}
-              color="green"
-              form="simple"
-              class="w-full !text-left">
-              {language.vernacularName || (t && $t('gl.' + language.bcp))}
-              {#if language.vernacularAlternate}
-                {language.vernacularAlternate}
-              {/if}
-              {#if language.vernacularName && t}
-                <small>({$t('gl.' + language.bcp)})</small>
-              {/if}
-            </Button>
-          {/each}
+<ShowHide let:show let:toggle>
+  <BadgeArrayEmit
+    strings={activeGlossingBcps}
+    {minimum}
+    canEdit
+    addMessage={t ? $t('misc.add', { default: 'Add' }) : 'Add'}
+    on:itemremoved
+    on:additem={toggle} />
+  {#if show}
+    <Modal on:close={toggle}>
+      <span slot="heading">
+        {t ? $t('create.gloss_dictionary_in') : 'Make dictionary available in...'}
+      </span>
+      <Filter
+        items={remainingGlossingLanguagesAsArray}
+        let:filteredItems={filteredLanguages}
+        placeholder={t ? $t('about.search') : 'Search'}>
+        {#each filteredLanguages as language}
+          <Button
+            onclick={() => {
+              dispatch('add', { languageId: language.bcp });
+              toggle();
+            }}
+            color="green"
+            form="simple"
+            class="w-full !text-left">
+            {language.vernacularName || (t && $t('gl.' + language.bcp))}
+            {#if language.vernacularAlternate}
+              {language.vernacularAlternate}
+            {/if}
+            {#if language.vernacularName && t}
+              <small>({$t('gl.' + language.bcp)})</small>
+            {/if}
+          </Button>
+        {/each}
+      </Filter>
+      <div class="modal-footer">
+        <Button onclick={toggle} color="black">Cancel</Button>
+      </div>
+    </Modal>
+  {/if}
+</ShowHide>
 
-          <div class="modal-footer">
-            <Button onclick={toggle} color="black">Cancel</Button>
-          </div>
-        </Filter>
-      </Modal>
-    {/if}
-  </ShowHide>
-
-  <div class="text-xs text-gray-600 mt-1">
-    {t
-      ? $t('create.gloss_dictionary_clarification')
-      : 'Language(s) you want to translate entries into'}
-    {t
-      ? $t('settings.unable-deleting-msg')
-      : 'Note: you will not be able to delete these glossing languages later.'}
-  </div>
+<div class="text-xs text-gray-600 mt-1">
+  {t
+    ? $t('create.gloss_dictionary_clarification')
+    : 'Language(s) you want to translate entries into'}
+  {t
+    ? $t('settings.unable-deleting-msg')
+    : 'Note: you will not be able to delete these glossing languages later.'}
 </div>
