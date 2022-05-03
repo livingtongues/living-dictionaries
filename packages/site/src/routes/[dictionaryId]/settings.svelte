ebebc6e154928ce9fb779895619caa878a060ee4
Diego Córdova Nieto <gatesofsilent@hotmail.com>
2022-05-03 11:11:55 -0500

    add location to edit in settings

--- packages/site/src/routes/[dictionaryId]/settings.svelte
+++ packages/site/src/routes/[dictionaryId]/settings.svelte
@@ -54,7 +54,7 @@
     <EditString
       value={dictionary.name}
       minlength={2}
-      required={true}
+      required
       id="name"
       save={async (name) =>
         await updateOnline(`dictionaries/${$dictionaryStore.id}`, {
@@ -81,6 +81,16 @@
           glottocode,
         })}
       display="Glottocode" />
+    <div class="mb-5" />
+
+    <EditString
+      value={dictionary.location}
+      id="location"
+      save={async (location) =>
+        await updateOnline(`dictionaries/${$dictionaryStore.id}`, {
+          location,
+        })}
+      display={$t('dictionary.location', { default: 'Location' })} />
     <div class="mb-5" />
 
     <EditableGlossesField
