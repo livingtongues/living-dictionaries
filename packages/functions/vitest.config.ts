051fc1a4f1c277efdfcdd49a3d0945d34f9e0ae9
jacobbowdoin <jwrunner7@gmail.com>
2022-05-04 21:56:31 +0800

    convert functions back to commonjs because of too many cloud function errors - not ready for ESM yet

--- packages/functions/vitest.config.ts
+++ packages/functions/vitest.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vitest/config';
 
+const defaultExclude = ['node_modules', 'dist', '.idea', '.git', '.cache'];
 export default defineConfig({
   test: {
     globals: true,
     // includeSource: ['./**/composeMessages.ts'],
+    exclude: [...defaultExclude, 'lib'],
   },
 });
