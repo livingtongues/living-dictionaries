c34fdeca7b512cdf80a700ed03cd801c759b35b7
jacobbowdoin <jwrunner7@gmail.com>
2022-05-04 08:07:21 +0800

    remove spacing

--- packages/parts/src/lib/settings/EditableAlternateNames.svelte
+++ packages/parts/src/lib/settings/EditableAlternateNames.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import BadgeArray from 'svelte-pieces/data/BadgeArray.svelte'
+  import BadgeArray from 'svelte-pieces/data/BadgeArray.svelte';
   import type { Readable } from 'svelte/store';
   export let t: Readable<any> = undefined;
   export let alternateNames: string[];
@@ -7,17 +7,15 @@
   import { createEventDispatcher } from 'svelte';
   const dispatch = createEventDispatcher<{
     update: { alternateNames: string[] };
-  }>()
+  }>();
 </script>
 
-<div class="mt-6">
-  <div class="text-sm font-medium leading-5 text-gray-700 mb-1">
-    { t ? $t('create.alternate_names') : 'Alternate Names'}
-  </div>
-  <BadgeArray
-    strings={alternateNames}
-    canEdit
-    promptMessage={t ? $t('create.enter_alternate_name') : 'Enter Alternate Name'}
-    addMessage={t ? $t('misc.add') : 'Add'}
-    on:valueupdated={e => dispatch('update', {alternateNames: e.detail})} />
+<div class="text-sm font-medium text-gray-700 mb-1">
+  {t ? $t('create.alternate_names') : 'Alternate Names'}
 </div>
+<BadgeArray
+  strings={alternateNames}
+  canEdit
+  promptMessage={t ? $t('create.enter_alternate_name') : 'Enter Alternate Name'}
+  addMessage={t ? $t('misc.add') : 'Add'}
+  on:valueupdated={(e) => dispatch('update', { alternateNames: e.detail })} />
