adbfb0879fc29065ac81dc0d2da4b07df932a68a
jacobbowdoin <jwrunner7@gmail.com>
2022-05-27 20:08:41 +0800

    only add startAt when there is a value to be added, use IVideo interface in place of custom written types in each location

--- packages/site/src/lib/components/video/parseVideoData.test.ts
+++ packages/site/src/lib/components/video/parseVideoData.test.ts
@@ -2,37 +2,75 @@ import { parseVideoData } from './parseVideoData';
 
 test('parseVideoData properly parses YouTube Ids, even w/ query params like timestamp', () => {
   const id = 'GrsknWZpr-k';
-  const expectedWithoutStart = { videoId: id, type: 'youtube', startAt: undefined };
-  const expectedWithStart = {
-    videoId: id,
-    type: 'youtube',
-    startAt: '2113',
-  };
 
-  expect(parseVideoData(`http://www.youtube.com/watch?v=${id}`)).toEqual(expectedWithoutStart);
-  expect(parseVideoData(`http://www.youtube.com/watch?v=${id}?t=2113s`)).toEqual(expectedWithStart);
-  expect(parseVideoData(`http://www.youtube.com/watch?v=${id}&t=2113s`)).toEqual(expectedWithStart);
-  expect(parseVideoData(`http://youtu.be/watch?v=${id}`)).toEqual(expectedWithoutStart);
-  expect(parseVideoData(`https://youtube.googleapis.com/v/${id}`)).toEqual(expectedWithoutStart);
-  expect(parseVideoData(`http://www.youtube.com/watch?v=${id}&t=sahda`)).toEqual(
-    expectedWithoutStart
-  );
+  expect(parseVideoData(`http://www.youtube.com/watch?v=${id}`)).toMatchInlineSnapshot(`
+    {
+      "youtubeId": "GrsknWZpr-k",
+    }
+  `);
+  expect(parseVideoData(`http://www.youtube.com/watch?v=${id}`)).toMatchInlineSnapshot(`
+    {
+      "youtubeId": "GrsknWZpr-k",
+    }
+  `);
+  expect(parseVideoData(`http://www.youtube.com/watch?v=${id}?t=2113s`)).toMatchInlineSnapshot(`
+    {
+      "startAt": 2113,
+      "youtubeId": "GrsknWZpr-k",
+    }
+  `);
+  expect(parseVideoData(`http://www.youtube.com/watch?v=${id}&t=20s`)).toMatchInlineSnapshot(`
+    {
+      "startAt": 20,
+      "youtubeId": "GrsknWZpr-k",
+    }
+  `);
+  expect(parseVideoData(`http://youtu.be/watch?v=${id}`)).toMatchInlineSnapshot(`
+    {
+      "youtubeId": "GrsknWZpr-k",
+    }
+  `);
+  expect(parseVideoData(`https://youtube.googleapis.com/v/${id}`)).toMatchInlineSnapshot(`
+    {
+      "youtubeId": "GrsknWZpr-k",
+    }
+  `);
+  expect(parseVideoData(`http://www.youtube.com/watch?v=${id}&t=sahda`)).toMatchInlineSnapshot(`
+    {
+      "youtubeId": "GrsknWZpr-k",
+    }
+  `);
 });
 
 test('parseVideoData properly parses Vimeo Ids, including relative urls', () => {
   const id = '239862299';
-  const expectedWithoutStart = { videoId: id, type: 'vimeo', startAt: undefined };
-  const expectedWithStart = {
-    videoId: id,
-    type: 'vimeo',
-    startAt: '64',
-  };
-
-  expect(parseVideoData(`http://vimeo.com/${id}`)).toEqual(expectedWithoutStart);
-  expect(parseVideoData(`http://player.vimeo.com/video/${id}`)).toEqual(expectedWithoutStart);
-  expect(parseVideoData(`//player.vimeo.com/video/${id}`)).toEqual(expectedWithoutStart);
-  expect(parseVideoData(`http://player.vimeo.com/video/${id}#t=64`)).toEqual(expectedWithStart);
-  expect(parseVideoData(`http://player.vimeo.com/video/${id}&t=64`)).toEqual(expectedWithStart);
+  expect(parseVideoData(`http://vimeo.com/${id}`)).toMatchInlineSnapshot(`
+    {
+      "vimeoId": "239862299",
+    }
+  `);
+  expect(parseVideoData(`http://player.vimeo.com/video/${id}`)).toMatchInlineSnapshot(`
+    {
+      "vimeoId": "239862299",
+    }
+  `);
+  expect(parseVideoData(`//player.vimeo.com/video/${id}`)).toMatchInlineSnapshot(`
+    {
+      "vimeoId": "239862299",
+    }
+  `);
+  expect(parseVideoData(`http://player.vimeo.com/video/${id}#t=64`)).toMatchInlineSnapshot(`
+    {
+      "startAt": 64,
+      "vimeoId": "239862299",
+    }
+  `);
+  expect(parseVideoData(`http://player.vimeo.com/video/${id}&t=64`)).toMatchInlineSnapshot(`
+    {
+      "startAt": 64,
+      "vimeoId": "239862299",
+    }
+  `);
 });
 
 test('parseVideoData handles returns null for invalid URL', () => {
