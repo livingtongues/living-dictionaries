3f47b1b86f3f9c12887ef2aacc157a6cf4f6b142
jacobbowdoin <jwrunner7@gmail.com>
2022-05-28 13:13:19 +0800

    add SES email sending info (WIP)

--- packages/functions/src/updateDevAdminRole.ts
+++ packages/functions/src/updateDevAdminRole.ts
@@ -1,22 +1,17 @@
 import * as functions from 'firebase-functions';
 const projectId = functions.config().project.key;
+import { db } from './config';
 
-import * as admin from 'firebase-admin';
-admin.initializeApp();
-
-export default async (
-    data: any,
-    context: functions.https.CallableContext
-) => {
-    if (projectId !== 'talking-dictionaries-alpha' && typeof data.role === 'number') {
-        console.log('Set admin role to: ', data.role);
-        await admin.firestore().doc(`users/${context.auth.uid}`).update({
-            roles: {
-                admin: data.role
-            }
-        });
-        return true;
-    } else {
-        return true;
-    }
-}
+export default async (data: any, context: functions.https.CallableContext) => {
+  if (projectId !== 'talking-dictionaries-alpha' && typeof data.role === 'number') {
+    console.log('Set admin role to: ', data.role);
+    await db.doc(`users/${context.auth?.uid}`).update({
+      roles: {
+        admin: data.role,
+      },
+    });
+    return true;
+  } else {
+    return true;
+  }
+};
