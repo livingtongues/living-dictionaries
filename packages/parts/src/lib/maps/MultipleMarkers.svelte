19f9738ff0bcd11189220418df9ef6c678d38dec
Diego Córdova Nieto <gatesofsilent@hotmail.com>
2022-06-21 12:21:31 -0500

    allow single and multiple markers on a map

--- packages/parts/src/lib/maps/MultipleMarkers.svelte
+++ packages/parts/src/lib/maps/MultipleMarkers.svelte
@@ -2,13 +2,25 @@
   import { onMount } from 'svelte';
   import mapboxgl from 'mapbox-gl';
   import Modal from 'svelte-pieces/ui/Modal.svelte';
+  import Button from 'svelte-pieces/ui/Button.svelte';
   import { loadStylesOnce } from './loader';
 
   let map: mapboxgl.Map;
+  let currentMarker: number;
   let markers: mapboxgl.Marker[] = [];
   let marker: mapboxgl.Marker;
   let lng: number;
   let lat: number;
+  //TODO make this a prop
+  const markersConfig = { color: '#0d8529', scale: 1.2, draggable: true };
+  let multipleMarkers = false;
+
+  $: if (multipleMarkers) {
+    marker.remove()
+  } else {
+    markers.forEach(marker => marker.remove()); 
+    markers = []
+  } 
 
   onMount(async () => {
     await loadStylesOnce('https://api.mapbox.com/mapbox-gl-js/v1.8.1/mapbox-gl.css');
@@ -20,12 +32,15 @@
           zoom: 3 // starting zoom
         });
         map.on('click', (event) => {
-          setMarker(event.lngLat.lng, event.lngLat.lat);
-
+          if (multipleMarkers) {
+            addNewMarker(event.lngLat.lng, event.lngLat.lat);
+          } else {
+            setSingleMarker(event.lngLat.lng, event.lngLat.lat);
+          }
         });
   })
 
-  function setMarker(longitude: number, latitude: number) {
+  function convertCoordinates(longitude: number, latitude: number) {
     if (map && longitude && latitude) {
       if (longitude < -180 || longitude > 180 || latitude < -90 || latitude > 90) {
         return;
@@ -35,13 +50,37 @@
       }
       lng = Math.floor(longitude * 1000) / 1000;
       lat = Math.floor(latitude * 1000) / 1000;
-      // record to the 3rd decimal point (111 meters) (4th decimal would be handheld GPS accuracy) // https://gis.stackexchange.com/questions/8650/measuring-accuracy-of-latitude-and-longitude, https://gisjames.wordpress.com/2016/04/27/deciding-how-many-decimal-places-to-include-when-reporting-latitude-and-longitude/
-
-      marker = new mapboxgl.Marker().setLngLat([lng, lat]).addTo(map);
-      map.flyTo({
-        center: [lng, lat],
-      });
     }
+  }
+
+  function addNewMarker(longitude: number, latitude: number) {
+    convertCoordinates(longitude, latitude);
+		const marker = new mapboxgl.Marker(markersConfig).setLngLat([lng, lat]).addTo(map);
+		//TODO make a conditional that prevents to add a new one if it is in the exact same coordinates and is not draggable
+		markers.push(marker);
+
+		const popup = new mapboxgl.Popup({ closeOnClick: false })
+			.setLngLat(marker.getLngLat())
+			.setText(markers.length.toString())
+			.addTo(map);
+		marker.setPopup(popup);
+
+		map.flyTo({
+			center: [lng, lat]
+		});
+		marker.on('dragend', () => {
+			currentMarker = parseInt(marker.getPopup()._container.innerText.replace('\nx', '')) - 1;
+			console.log(marker.getPopup());
+		});
+		console.log('marker created', markers);
+	}
+
+  function setSingleMarker(longitude: number, latitude: number) {
+    convertCoordinates(longitude, latitude);
+    marker = new mapboxgl.Marker().setLngLat([lng, lat]).addTo(map);
+    map.flyTo({
+      center: [lng, lat],
+    });
   }
 
   function toggleStyle() {
@@ -53,15 +92,20 @@
     }
   }
 </script>
-
 <Modal on:close noscroll>
-  <div id="map" class="relative w-full" style="height: 50vh;">
-    <button
-      on:click={toggleStyle}
-      type="button"
-      class="px-2 py-1 absolute rounded shadow bg-white"
-      style="bottom: 40px; left: 8px; z-index: 1;">
-      <i class="far fa-globe-asia" />
-    </button>
-  </div>
+  <form>
+    <div class="mb-2">
+      <input id="multiple-markers" type="checkbox" bind:checked={multipleMarkers} placeholder="Multiple markers" />
+      <label for="multiple-markers" class="text-sm font-medium text-gray-700">Multiple markers</label>
+    </div>
+    <div id="map" class="relative w-full" style="height: 50vh;">
+      <button
+        on:click={toggleStyle}
+        type="button"
+        class="px-2 py-1 absolute rounded shadow bg-white"
+        style="bottom: 40px; left: 8px; z-index: 1;">
+        <i class="far fa-globe-asia" />
+      </button>
+    </div>
+  </form>
 </Modal>
