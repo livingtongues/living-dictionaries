2e1a3dc7b9bc79c861c9f866f25b77b79e794f2d
jacobbowdoin <jwrunner7@gmail.com>
2022-07-01 16:33:27 +0800

    remove unneeded sveltefirets folder (because using installed package)

--- packages/site/src/sveltefirets/helpers/uid.ts
+++
@@ -1,7 +0,0 @@
-import { get } from 'svelte/store';
-import { authState } from '../user';
-
-export const getUid = () => {
-  const u = get(authState);
-  return (u && u.uid) || 'anonymous'; // 'anonymous' allows support messages to be saved by non-logged-in users
-};
