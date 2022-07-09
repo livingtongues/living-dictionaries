ca6bc5e423ef7eb6cc5c528d489f2f16b3fccfe3
jacobbowdoin <jwrunner7@gmail.com>
2022-07-09 08:59:36 +0800

    add semanticDomain value to test

--- packages/site/src/lib/export/formatEntries.test.ts
+++ packages/site/src/lib/export/formatEntries.test.ts
@@ -1,4 +1,4 @@
-import type { IDictionary, IEntry, ISpeaker } from '@living-dictionaries/types';
+import type { IDictionary, IEntry, ISemanticDomain, ISpeaker } from '@living-dictionaries/types';
 import { formatEntriesForCSV } from './formatEntries';
 
 test('formatEntriesForCSV basic example to smoke test', () => {
@@ -10,7 +10,7 @@ test('formatEntriesForCSV basic example to smoke test', () => {
       ph: 'banana',
       gl: { es: 'platano' },
       ps: '1',
-      sdn: ['2.1.2.3'],
+      sdn: ['2.1', '2.2', '2.3'],
       di: 'dialect x',
       nt: 'This is an example of a note, here we can write whatever we want.',
       sr: ['A book', 'www.mybook.com'],
@@ -22,5 +22,6 @@ test('formatEntriesForCSV basic example to smoke test', () => {
   ];
   const dictionary: IDictionary = { name: 'test', glossLanguages: ['ar', 'en'], entryCount: 0 };
   const speakers: ISpeaker[] = [{ displayName: 'John Smith', id: '123' }];
-  expect(formatEntriesForCSV(entriesArray, dictionary, speakers, [], [])).toMatchSnapshot();
+  const semanticDomains: ISemanticDomain[] = [{ key: '2.1', name: 'Plant Test Domain' }];
+  expect(formatEntriesForCSV(entriesArray, dictionary, speakers, semanticDomains, [])).toMatchSnapshot();
 });
