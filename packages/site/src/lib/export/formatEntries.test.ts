e8b19fff93b1005b13c2ba9bfcd2331e9236c281
jacobbowdoin <jwrunner7@gmail.com>
2022-07-09 09:24:20 +0800

    expand test to cover more fields

--- packages/site/src/lib/export/formatEntries.test.ts
+++ packages/site/src/lib/export/formatEntries.test.ts
@@ -1,27 +1,34 @@
-import type { IDictionary, IEntry, ISemanticDomain, ISpeaker } from '@living-dictionaries/types';
+import type { IDictionary, IEntry, IPartOfSpeech, ISemanticDomain, ISpeaker } from '@living-dictionaries/types';
 import { formatEntriesForCSV } from './formatEntries';
 
 test('formatEntriesForCSV basic example to smoke test', () => {
   const entriesArray: IEntry[] = [
     {
-      id: '12ar',
-      lx: 'banana',
+      id: '12345qwerty',
+      lx: 'xiangjiao',
       lo: 'کیلا',
-      ph: 'banana',
-      gl: { es: 'platano' },
-      ps: '1',
+      in: 'n',
+      mr: 'bar',
+      nc: '5',
+      ph: 'xiangjiao',
+      gl: { es: 'platano', ar: 'foo', en: 'banana' },
+      ps: 'n',
+      pl: 'shuang xiangjiao',
       sdn: ['2.1', '2.2', '2.3'],
       di: 'dialect x',
       nt: 'This is an example of a note, here we can write whatever we want.',
       sr: ['A book', 'www.mybook.com'],
-      xs: { en: 'This is a banana' },
-      sf: { path: 'https://example.com', sp: 'aa34' },
-      pf: { gcs: 'https://example.com', path: 'https://example.com' },
+      xs: { en: 'This is a banana', vn: '我很喜歡吃香蕉' },
+      sf: { path: 'https://database.com/sound.mp3', sp: '123' },
+      pf: { gcs: 'not_needed_here', path: 'https://database.com/image.mp3' },
+      xv: '',
     },
     { id: '34qw', lx: 'tree', gl: { es: 'arbol' } },
   ];
   const dictionary: IDictionary = { name: 'test', glossLanguages: ['ar', 'en'], entryCount: 0 };
-  const speakers: ISpeaker[] = [{ displayName: 'John Smith', id: '123' }];
+  const speakers: ISpeaker[] = [{ displayName: 'John Smith', id: '123', birthplace: 'Whoville', decade: 4, gender: 'm' }];
   const semanticDomains: ISemanticDomain[] = [{ key: '2.1', name: 'Plant Test Domain' }];
-  expect(formatEntriesForCSV(entriesArray, dictionary, speakers, semanticDomains, [])).toMatchSnapshot();
+  const partsOfSpeech: IPartOfSpeech[] = [{ enAbbrev: 'n',
+    enName: 'noun', }];
+  expect(formatEntriesForCSV(entriesArray, dictionary, speakers, semanticDomains, partsOfSpeech)).toMatchSnapshot();
 });
