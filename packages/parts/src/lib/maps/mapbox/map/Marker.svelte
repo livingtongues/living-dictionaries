b5b112bb6cf5a4fe61f17d061a7db26a9e9655f3
jacobbowdoin <jwrunner7@gmail.com>
2022-07-31 07:20:25 +0800

    make markers addable and moveable

--- packages/parts/src/lib/maps/mapbox/map/Marker.svelte
+++ packages/parts/src/lib/maps/mapbox/map/Marker.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
-  import { onMount, getContext } from 'svelte';
+  import { onMount, getContext, createEventDispatcher } from 'svelte';
   import { contextKey } from '../contextKey';
-  import type { Map, Marker } from 'mapbox-gl';
+  import type { LngLat, Map, Marker } from 'mapbox-gl';
 
   const { getMap, getMapbox } = getContext(contextKey);
   const map: Map = getMap();
@@ -11,39 +11,47 @@
     return Math.round(Math.random() * 255);
   }
 
-  function move(lng: number, lat: number) {
-    marker.setLngLat({ lng, lat });
-  }
-
   export let lat: number;
   export let lng: number;
   export let label = 'Marker';
   export let popupClassName = 'beyonk-mapbox-popup';
-  export let markerOffset = [0, 0];
+  export let markerOffset: [number, number] = [0, 0];
   export let popupOffset = 10;
   export let color = randomColour();
-  export let popup = false;
+  export let popup = true;
   export let popupOptions = {};
   export let markerOptions = {};
+  export let draggable = true;
 
   let marker: Marker;
-  let element: HTMLDivElement;
-  let elementPopup: HTMLDivElement;
+  let element: HTMLDivElement; // if main slot used
+  let elementPopup: HTMLDivElement; // if popup slot used
 
-  $: marker && move(lng, lat);
+  $: marker?.setLngLat({ lng, lat });
+
+  const dispatch = createEventDispatcher<{ dragend: LngLat }>();
 
   onMount(() => {
-    const namedParams = Object.assign(
-      {
-        offset: markerOffset,
-      },
-      element.hasChildNodes() ? { element } : { color }
-    );
-    marker = new mapbox.Marker(Object.assign(namedParams, markerOptions));
+    const customMarker = element.hasChildNodes();
+    const elementOrColor: { element } | { color } = customMarker ? { element } : { color };
+    marker = new mapbox.Marker({
+      ...elementOrColor,
+      ...markerOptions,
+      offset: markerOffset,
+      draggable,
+    });
+
+    marker.getElement().addEventListener('click', (e) => {
+      console.log('clicked!');
+      // e.stopPropagation();
+    });
 
     if (popup) {
-      const namedPopupParams = { offset: popupOffset, className: popupClassName };
-      const popupEl = new mapbox.Popup(Object.assign(namedPopupParams, popupOptions));
+      const popupEl = new mapbox.Popup({
+        ...popupOptions,
+        offset: popupOffset,
+        className: popupClassName,
+      });
       if (elementPopup.hasChildNodes()) {
         popupEl.setDOMContent(elementPopup);
       } else {
@@ -54,8 +62,12 @@
     }
 
     marker.setLngLat({ lng, lat }).addTo(map);
+    marker.on('dragend', () => dispatch('dragend', marker.getLngLat()));
 
-    return () => marker.remove();
+    return () => {
+      marker.off('dragend', () => dispatch('dragend', marker.getLngLat()));
+      marker.remove();
+    };
   });
 
   export function getMarker() {
