c333ae861b9a0a750b5f939eb20caf976520ead8
jacobbowdoin <jwrunner7@gmail.com>
2022-07-30 15:45:22 +0800

    improve events on map component

--- packages/parts/src/lib/maps/mapbox/map/Map.svelte
+++ packages/parts/src/lib/maps/mapbox/map/Map.svelte
@@ -1,58 +1,78 @@
 <script lang="ts">
   // from https://github.com/beyonk-adventures/svelte-mapbox
-  import { setContext, onDestroy, createEventDispatcher } from 'svelte';
+  import { setContext, onDestroy, createEventDispatcher, onMount, tick } from 'svelte';
   import { contextKey } from '../contextKey';
-  import action from './map-action';
+  import { loadScriptOnce, loadStylesOnce } from '../asset-loader';
   import { EventQueue } from '../queue';
-  import type { LngLatLike, MapboxOptions, Map } from 'mapbox-gl';
+  import type { LngLatLike, MapboxOptions, Map, LngLat, ErrorEvent, EventData } from 'mapbox-gl';
 
   export let map: Map = null;
   export let version = 'v2.9.2';
+  export let customStylesheetUrl: string = undefined;
+  export let accessToken = import.meta.env.VITE_mapboxAccessToken as string;
+
+  export let options: Partial<MapboxOptions> = {};
   export let center: LngLatLike = [-95, 38.907]; // USA
   export let zoom = 2;
-  export let zoomRate = 1;
-  export let wheelZoomRate = 1;
-  export let options: Partial<MapboxOptions> = {};
-  export let accessToken = import.meta.env.VITE_mapboxAccessToken as string;
-  export let customStylesheetUrl: string = undefined;
   export let style = 'mapbox://styles/mapbox/streets-v11?optimize=true'; // light-v8, light-v9, light-v10, dark-v10, satellite-v9, streets-v11
 
-  const dispatch = createEventDispatcher();
+  const dispatch = createEventDispatcher<{
+    ready: null;
+    dragend: LngLat;
+    moveend: LngLat;
+    click: LngLat;
+    zoomend: number;
+    error: ErrorEvent & EventData;
+  }>();
 
   setContext(contextKey, {
     getMap: () => map,
     getMapbox: () => mapbox,
   });
 
-  let container: HTMLElement;
+  let container: HTMLDivElement;
   let mapbox: typeof import('mapbox-gl');
-
-  const optionsWithDefaults = {
-    ...options,
-    accessToken,
-    container,
-    style,
-    center,
-    zoom,
-    zoomRate,
-    wheelZoomRate,
-    version,
-    customStylesheetUrl,
-    map,
-  };
-
   const queue = new EventQueue();
+  let ready = false;
 
-  function init({ detail }) {
-    map = detail.map;
-    mapbox = detail.mapbox;
+  onMount(async () => {
+    await loadScriptOnce(`//api.mapbox.com/mapbox-gl-js/${version}/mapbox-gl.js`);
+    await loadStylesOnce(`//api.mapbox.com/mapbox-gl-js/${version}/mapbox-gl.css`);
+    customStylesheetUrl && (await loadStylesOnce(customStylesheetUrl));
+
+    window.mapboxgl.accessToken = accessToken;
+    map = new window.mapboxgl.Map({
+      ...options,
+      container,
+      style,
+      center,
+      zoom,
+    });
+    mapbox = window.mapboxgl;
+
+    // More events at https://docs.mapbox.com/mapbox-gl-js/api/map/#map-events
+    // map.on('drag', () => dispatch('drag', map.getCenter()))
+    map.on('dragend', () => dispatch('dragend', map.getCenter()));
+    map.on('moveend', () => dispatch('moveend', map.getCenter()));
+    map.on('click', ({ lngLat }) => dispatch('click', lngLat));
+    map.on('zoomend', () => dispatch('zoomend', map.getZoom()));
+    map.on('error', (e) => dispatch('error', e));
+
     queue.start(map);
     dispatch('ready');
-  }
+    ready = true;
+  });
 
-  onDestroy(() => {
+  onDestroy(async () => {
+    map?.off('dragend', () => dispatch('dragend', map.getCenter()));
+    map?.off('moveend', () => dispatch('moveend', map.getCenter()));
+    map?.off('click', ({ lngLat }) => dispatch('click', lngLat));
+    map?.off('zoomend', () => dispatch('zoomend', map.getZoom()));
+    map?.off('error', (e) => dispatch('error', e));
+
     queue.stop();
-    map = undefined;
+    await tick(); // allow controls to remove themselves from the map
+    map?.remove && map.remove();
   });
 
   export function fitBounds(bbox, data = {}) {
@@ -90,17 +110,8 @@
   $: zoom && setZoom(zoom);
 </script>
 
-<div
-  use:action={optionsWithDefaults}
-  on:ready={init}
-  on:recentre
-  on:dragend
-  on:click
-  on:zoomstart
-  on:zoom
-  on:zoomend
-  on:drag>
-  {#if map}
+<div bind:this={container}>
+  {#if ready}
     <slot />
   {/if}
 </div>
