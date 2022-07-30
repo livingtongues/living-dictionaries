625095dd4d2af9a340a2d05eeb4520f91b82e6f6
jacobbowdoin <jwrunner7@gmail.com>
2022-07-30 09:33:50 +0800

    add mapbox event typing

---
+++ packages/parts/src/lib/maps/mapbox/mapbox.d.ts
@@ -0,0 +1,20 @@
+declare namespace svelte.JSX {
+  interface HTMLAttributes<T> {
+    onready?: (event: CustomEvent<any> & { detail: any }) => any;
+    // Map.svelte
+    onrecentre?: (event: CustomEvent<any> & { target: EventTarget & T }) => any;
+    onzoomstart?: (event: CustomEvent<any> & { target: EventTarget & T }) => any;
+    onzoom?: (event: CustomEvent<any> & { target: EventTarget & T }) => any;
+    onzoomend?: (event: CustomEvent<any> & { target: EventTarget & T }) => any;
+    // Geocoder.svelte
+    onresults?: (event: CustomEvent<any> & { target: EventTarget & T }) => any;
+    onresult?: (event: CustomEvent<any> & { target: EventTarget & T }) => any;
+    onloading?: (event: CustomEvent<any> & { target: EventTarget & T }) => any;
+    onclear?: (event: CustomEvent<any> & { target: EventTarget & T }) => any;
+    // GeolocateControl.svelte
+    ongeolocate?: (event: CustomEvent<any> & { target: EventTarget & T }) => any;
+    onoutofmaxbounds?: (event: CustomEvent<any> & { target: EventTarget & T }) => any;
+    ontrackuserlocationend?: (event: CustomEvent<any> & { target: EventTarget & T }) => any;
+    ontrackuserlocationstart?: (event: CustomEvent<any> & { target: EventTarget & T }) => any;
+  }
+}
