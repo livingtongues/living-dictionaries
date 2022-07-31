febf17029e0295dee8131c86aa1610bbcde61999
jacobbowdoin <jwrunner7@gmail.com>
2022-07-31 08:11:02 +0800

    open popup w/o creating another marker

--- packages/parts/src/lib/maps/mapbox/map/Popup.svelte
+++ packages/parts/src/lib/maps/mapbox/map/Popup.svelte
@@ -1,18 +1,18 @@
 <script lang="ts">
-  import { onMount, getContext, createEventDispatcher } from 'svelte';
+  import { onMount, getContext } from 'svelte';
   import { contextKey } from '../contextKey';
   import type { Popup, PopupOptions, Marker } from 'mapbox-gl';
 
   const { getMapbox } = getContext(contextKey);
   const mapbox: typeof import('mapbox-gl') = getMapbox();
 
-  export let closeOnClick = false;
+  export let closeOnClick = true;
   export let closeOnMove = true;
   export let options: PopupOptions = {};
-  export let offset = 10;
+  export let offset = 15;
   export let label = 'Marker';
-  export let open = false;
   export let marker: Marker;
+  // export let startOpen = false;
 
   let popup: Popup;
   let container: HTMLDivElement;
@@ -32,28 +32,11 @@
     }
 
     marker.setPopup(popup);
-    // const markerElement = marker.getElement();
-    // markerElement.style.cursor = 'pointer';
-
-    popup.on('open', () => (open = true));
-    popup.on('close', () => (open = false));
 
     return () => {
-      popup.off('open', () => (open = true));
-      popup.off('close', () => (open = false));
       marker.setPopup(null);
-      // const markerElement = marker.getElement();
-      // if (markerElement) {
-      //   markerElement.style.cursor = null;
-      // }
     };
   });
-
-  $: if (popup) {
-    if (open !== popup.isOpen()) {
-      marker.togglePopup();
-    }
-  }
 </script>
 
 <div bind:this={container}>
