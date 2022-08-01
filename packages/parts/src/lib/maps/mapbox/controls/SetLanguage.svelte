8bb28009a18c25604afda80c6180ddb4bde40954
jacobbowdoin <jwrunner7@gmail.com>
2022-08-02 07:03:42 +0800

    add change map language scratch notes

---
+++ packages/parts/src/lib/maps/mapbox/controls/SetLanguage.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import { getContext } from 'svelte';
+  import { contextKey } from '../contextKey';
+  import type { Map } from 'mapbox-gl';
+
+  const { getMap } = getContext(contextKey);
+  const map: Map = getMap();
+
+  const supportedLanguages = [
+    'ar',
+    'en',
+    'es',
+    'fr',
+    'de',
+    'it',
+    'pt',
+    'ru',
+    'zh-Hans',
+    'zh-Hant',
+    'ja',
+    'ko',
+    'vi',
+  ] as const;
+
+  type SupportedLanguages = typeof supportedLanguages[number]
+  export let bcp: SupportedLanguages = 'en';
+
+  $: if (bcp !== 'en') {
+    map.setLayoutProperty('country-label', 'text-field', ['get', `name_${bcp}`]);
+  }
+</script>
