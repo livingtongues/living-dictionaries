cb9e6752e010de171d271ad669165614bf09719f
jacobbowdoin <jwrunner7@gmail.com>
2022-08-04 08:44:15 +0800

    rework CoordinatesModal to use new Mapbox components

--- packages/parts/src/lib/maps/CoordinatesModal.svelte
+++ packages/parts/src/lib/maps/CoordinatesModal.svelte
@@ -2,122 +2,50 @@
   import type { Readable } from 'svelte/store';
   export let t: Readable<any> = undefined;
 
-  import { onMount } from 'svelte';
-  import mapboxgl from 'mapbox-gl';
+  import { onMount, createEventDispatcher } from 'svelte';
   import Modal from 'svelte-pieces/ui/Modal.svelte';
   import Button from 'svelte-pieces/ui/Button.svelte';
-  import { loadScriptOnce, loadStylesOnce } from 'sveltefirets/client/loader';
-
   import { startCoordinates } from './map.class';
-  let map: mapboxgl.Map;
-  let marker: mapboxgl.Marker;
+  import Map from './mapbox/map/Map.svelte';
+  import Geocoder from './mapbox/geocoder/Geocoder.svelte';
+  import Marker from './mapbox/map/Marker.svelte';
+  import ToggleStyle from './mapbox/controls/ToggleStyle.svelte';
 
   export let lng: number;
   export let lat: number;
-  let center: [number, number];
-  let zoom = 2;
-
-  $: if (map) {
-    setMarker(lng, lat);
-  }
+  let center: [number, number] = lng && lat ? [lng, lat] : startCoordinates.DC;
+  let zoom = lng && lat ? 6 : 2;
 
   function setMarker(longitude: number, latitude: number) {
-    if (map && longitude && latitude) {
-      if (longitude < -180 || longitude > 180 || latitude < -90 || latitude > 90) {
-        return;
-      }
-      if (marker) {
-        marker.remove();
-      }
-      lng = Math.floor(longitude * 1000) / 1000;
-      lat = Math.floor(latitude * 1000) / 1000;
-      // record to the 3rd decimal point (111 meters) (4th decimal would be handheld GPS accuracy) // https://gis.stackexchange.com/questions/8650/measuring-accuracy-of-latitude-and-longitude, https://gisjames.wordpress.com/2016/04/27/deciding-how-many-decimal-places-to-include-when-reporting-latitude-and-longitude/
+    if (!(longitude && latitude)) return;
+    if (longitude < -180 || longitude > 180 || latitude < -90 || latitude > 90) {
+      return;
+    }
+    lng = Math.floor(longitude * 10000) / 10000;
+    lat = Math.floor(latitude * 10000) / 10000;
+    // record to the 4th decimal point as handheld GPS accuracy (3rd decimal point is what we used previously: 111 meters) // https://gis.stackexchange.com/questions/8650/measuring-accuracy-of-latitude-and-longitude, https://gisjames.wordpress.com/2016/04/27/deciding-how-many-decimal-places-to-include-when-reporting-latitude-and-longitude/
 
-      marker = new mapboxgl.Marker().setLngLat([lng, lat]).addTo(map);
-      map.flyTo({
-        center: [lng, lat],
-      });
+    // map.flyTo({
+    //   center: [lng, lat],
+    // });
+  }
+
+  function handleGeocoderResult({ detail }) {
+    if (detail?.user_coordinates?.[0]) {
+      setMarker(detail.user_coordinates[0], detail.user_coordinates[1]);
+    } else {
+      setMarker(detail.center[0], detail.center[1]);
     }
   }
 
   onMount(async () => {
-    if (lng && lat) {
-      center = [lng, lat];
-      zoom = 6;
-    } else {
-      center = startCoordinates.DC;
-      zoom = 4;
-      if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition((position) => {
-          center = [position.coords.longitude, position.coords.latitude];
-          if (map) {
-            map.flyTo({
-              center,
-            });
-          }
-        });
-      }
+    if (!(lng && lat) && navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        center = [position.coords.longitude, position.coords.latitude];
+      });
     }
-
-    await loadStylesOnce('https://api.mapbox.com/mapbox-gl-js/v1.8.1/mapbox-gl.css');
-
-    mapboxgl.accessToken = import.meta.env.VITE_mapboxAccessToken as string;
-
-    map = new mapboxgl.Map({
-      container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v11?optimize=true', // outdoors-v9
-      center,
-      zoom,
-    });
-
-    setMarker(lng, lat);
-
-    map.on('click', (event) => {
-      setMarker(event.lngLat.lng, event.lngLat.lat);
-      // const coordinates = [event.lngLat.lng, event.lngLat.lat];
-      // const newMarker = new GeoJson(coordinates, { message: "test message" });
-    });
-
-    map.on('load', async () => {
-      function setMarkerOnSearchedCoordinates(item) {
-        setMarker(item.geometry.coordinates[0], item.geometry.coordinates[1]);
-        return item.place_name;
-      }
-
-      await loadStylesOnce(
-        'https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v4.5.1/mapbox-gl-geocoder.css'
-      );
-      await loadScriptOnce(
-        'https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v4.5.1/mapbox-gl-geocoder.min.js'
-      );
-      map.addControl(
-        //@ts-ignore
-        new MapboxGeocoder({
-          accessToken: mapboxgl.accessToken,
-          mapboxgl,
-          getItemValue: setMarkerOnSearchedCoordinates,
-          placeholder: t ? $t('about.search') : 'Search',
-        })
-      );
-
-      map.addControl(new mapboxgl.NavigationControl());
-    });
-
-    return () => {
-      map.remove();
-    };
   });
 
-  function toggleStyle() {
-    const style = map.getStyle();
-    if (style.name === 'Mapbox Streets') {
-      map.setStyle('mapbox://styles/mapbox/satellite-streets-v11?optimize=true');
-    } else {
-      map.setStyle('mapbox://styles/mapbox/streets-v11?optimize=true'); // style.name === "Mapbox Satellite Streets"
-    }
-  }
-
-  import { createEventDispatcher } from 'svelte';
   const dispatch = createEventDispatcher<{
     update: { lat: number; lng: number };
     remove: boolean;
@@ -151,7 +79,7 @@
           </div>
           <input
             type="number"
-            step=".001"
+            step=".0001"
             required
             max="90"
             min="-90"
@@ -169,7 +97,7 @@
           </div>
           <input
             type="number"
-            step=".001"
+            step=".0001"
             required
             max="180"
             min="-180"
@@ -180,15 +108,19 @@
       </div>
     </div>
 
-    <div id="map" class="relative w-full" style="height: 50vh;">
-      <button
-        on:click={toggleStyle}
-        type="button"
-        class="px-2 py-1 absolute rounded shadow bg-white"
-        style="bottom: 40px; left: 8px; z-index: 1;">
-        <i class="far fa-globe-asia" />
-      </button>
-    </div>
+    <form on:submit={(e) => e.preventDefault()} style="height: 50vh;">
+      <Map {center} {zoom} on:click={({ detail }) => setMarker(detail.lng, detail.lat)}>
+        <Geocoder
+          options={{ marker: false }}
+          placeholder={t ? $t('about.search') : 'Search'}
+          on:result={handleGeocoderResult}
+          on:error={(e) => console.log(e.detail)} />
+        {#if lng && lat}
+          <Marker {lng} {lat} />
+        {/if}
+        <ToggleStyle />
+      </Map>
+    </form>
 
     <div class="modal-footer">
       <Button onclick={remove} form="simple" color="red">
