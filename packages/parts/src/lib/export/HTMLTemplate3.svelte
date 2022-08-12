cab9ab336565b7c4966533ba60b7d883c007a716
Diego Córdova Nieto <diego@livingtongues.org>
2022-08-12 17:32:22 -0500

    redo select field checkboxes

--- packages/parts/src/lib/export/HTMLTemplate3.svelte
+++ packages/parts/src/lib/export/HTMLTemplate3.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
-  import type { IEntry } from "@living-dictionaries/types";
+  import type { IEntry, EntryForPDF } from "@living-dictionaries/types";
+
 
   export let entries: IEntry[];
+  export let selectedFields;
+
   //TODO import dictionary data
 </script>
 
@@ -9,23 +12,25 @@
   {#each entries as entry}
     <div>
       <strong>{entry.lx}</strong>
-      {entry.ph ? `/${entry.ph}/` : ''}
-      {#each Object.entries(entry.gl) as gloss, index}
-        <strong>{index < Object.entries(entry.gl).length-1 ? `${gloss[1]} - ` : gloss[1]}</strong>
-      {/each}
-      <strong><i>{entry.ps ? entry.ps : ''}</i></strong>
-      {entry.xv ? entry.xv : ''}
-      {#if entry.xs}      
+      {entry.ph && selectedFields.ph ? `/${entry.ph}/` : ''}
+      {#if entry.gl && selectedFields.gl}
+        {#each Object.entries(entry.gl) as gloss, index}
+          <strong>{index < Object.entries(entry.gl).length-1 ? `${gloss[1]} - ` : gloss[1]}</strong>
+        {/each}
+      {/if}
+      <strong><i>{entry.ps && selectedFields.ps ? entry.ps : ''}</i></strong>
+      {entry.xv && selectedFields.xv ? entry.xv : ''}
+      {#if entry.xs && selectedFields.xs}      
         {#each Object.entries(entry.xs) as sentence, index}
           {index < Object.entries(entry.xs).length-1 ? `${sentence[1]}, ` : `${sentence[1]};`}
         {/each}
       {/if}
-      {#if entry.pf}
+      {#if entry.pf && selectedFields.pf}
         <img width="280" src={entry.pf.path} alt={entry.lx} />
         <i>{entry.pf.path};</i>
         <i>{entry.pf.source ? `${entry.pf.source};` : ''}</i>
       {/if}
-      {#if entry.sr}
+      {#if entry.sr && selectedFields.sr}
         {#each entry.sr as source, index}
           <i>{index < entry.sr.length-1 ? `${source}, ` : `${source};`}</i>
         {/each}
