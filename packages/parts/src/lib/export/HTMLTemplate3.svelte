fc1ab6512619c07a57d6ac9a0c5d98ed553132ab
Diego Córdova Nieto <diego@livingtongues.org>
2022-08-17 14:59:38 -0500

    fix layout details

--- packages/parts/src/lib/export/HTMLTemplate3.svelte
+++ packages/parts/src/lib/export/HTMLTemplate3.svelte
@@ -13,29 +13,31 @@
       <!--Essential Fields-->
       <div>
         <!--It might be necessary to style elements inline to eventually use the html-pdf library-->
-        <strong style="font-size: 1.3em;">{entry.lx}</strong>
+        <strong style="font-size: 1.4em;">{entry.lx}</strong>
         {entry.ph && selectedFields.ph ? `/${entry.ph}/` : ''}
         {#if entry.gl && selectedFields.gl}
           {#each Object.entries(entry.gl) as gloss, index}
-            <strong>{gloss[1]}{index < Object.entries(entry.gl).length-1 ? ' - ' : ''}</strong>
+            {gloss[1]}{index < Object.entries(entry.gl).length-1 ? ' - ' : ''}
           {/each}
         {/if}
-        <strong><i>{entry.ps && selectedFields.ps ? entry.ps : ''}</i></strong>
+        <i>{entry.ps && selectedFields.ps ? entry.ps : ''}</i>
         {entry.xv && selectedFields.xv ? entry.xv : ''}
         {#if entry.xs && selectedFields.xs}      
           {#each Object.entries(entry.xs) as sentence, index}
-          {sentence[1]}{index < Object.entries(entry.xs).length-1 ? ', ' : ';'}
+          {sentence[1]}{index < Object.entries(entry.xs).length-1 ? ', ' : ''}
           {/each}
         {/if}
         {#if entry.pf && selectedFields.pf}
-          <img width="280" src={entry.pf.path} alt={entry.lx} />
-          <i>{entry.pf.path};</i>
-          <i>{entry.pf.source ? `${entry.pf.source};` : ''}</i>
+          <div style="font-size: 0.8em;">
+            <img width="280" style="width: -moz-available; width: -webkit-fill-available;" src={entry.pf.path} alt={entry.lx} />
+            <p><i>{entry.pf.path}</i></p>
+            <p><i>{entry.pf.source ? `${entry.pf.source}` : ''}</i></p>
+          </div>
         {/if}
         {#if entry.sr && selectedFields.sr}
           <i><strong>Source(s): </strong></i>
           {#each entry.sr as source, index}
-            <i>{index < entry.sr.length-1 ? `${source}, ` : `${source};`}</i>
+            <i>{index < entry.sr.length-1 ? `${source}, ` : `${source}`}</i>
           {/each}
         {/if}
       </div>
@@ -44,12 +46,12 @@
         {#if entry.sdn && selectedFields.sd}
           <strong>Semantic Domains: </strong>
           {#each entry.sdn as key, index}
-          {semanticDomains.find(sd => sd.key === key).name}{index < entry.sdn.length-1 ? ', ' : ';'}
+          {semanticDomains.find(sd => sd.key === key).name}{index < entry.sdn.length-1 ? ', ' : ''}
           {/each}
         {/if}
         {#each Object.keys(EntryPDFFieldsEnum) as key}
           {#if entry[key] && selectedFields[key]}
-            <span><strong>{EntryPDFFieldsEnum[key]}</strong>: {entry[key]}; </span>
+            <p><strong>{EntryPDFFieldsEnum[key]}</strong>: {entry[key]}</p>
           {/if}
         {/each}
       </div>
@@ -61,6 +63,6 @@
   .container {
     column-count: 2;
     column-gap: 20px;
-    word-break: break-all;
+    word-break: break-word;
   }
 </style>
