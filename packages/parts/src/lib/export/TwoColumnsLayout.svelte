67f8dd7913c814fb7a5ae2f74e4bcbec48646612
Diego Córdova Nieto <diego@livingtongues.org>
2022-08-18 17:43:06 -0500

    add new layout to avoid duplicate code

---
+++ packages/parts/src/lib/export/TwoColumnsLayout.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import type { IEntry } from '@living-dictionaries/types';
+  import Button from 'svelte-pieces/ui/Button.svelte';
+  import HTMLTemplate4 from './HTMLTemplate4.svelte';
+
+  export let entries: IEntry[];
+  export let selectedFields;
+  let element: HTMLElement;
+</script>
+
+<div bind:this={element} style="display: table; clear: both; word-break: break-word; margin: 4rem;">
+  {#each entries as entry, index}
+    {#if index % 2 === 0}
+      <div style="float: left; width:50%;">
+        <HTMLTemplate4 {selectedFields} {entry} />
+      </div>
+    {/if}
+    {#if index % 2 === 1}
+      <div style="float: right; width:50%;">
+        <HTMLTemplate4 {selectedFields} {entry} />
+      </div>
+    {/if}
+  {/each}
+</div>
+
+<Button onclick={() => console.log(element.outerHTML)}>Test HTML</Button>
