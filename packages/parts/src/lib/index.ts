0a68d9d07bb80065e8ce382374fced56605cf570
jacobbowdoin <jwrunner7@gmail.com>
2022-08-24 17:42:50 +0800

    downgrade to working kitbook version

--- packages/parts/src/lib/index.ts
+++ packages/parts/src/lib/index.ts
@@ -16,3 +16,5 @@ export { default as Image } from './entries/media/Image.svelte';
 export { glossingLanguages } from './glosses/glossing-languages';
 export { semanticDomains } from './mappings/semantic-domains';
 export { partsOfSpeech } from './mappings/parts-of-speech';
+
+export { getTimeZoneLongitude } from './maps/getTimeZoneLongitude';
