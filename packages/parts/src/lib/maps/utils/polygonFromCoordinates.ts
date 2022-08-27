2d7e9c027def63fb31522698c04482d1c4ff0403
jacobbowdoin <jwrunner7@gmail.com>
2022-08-27 11:22:37 +0800

    refactor to use lng and lat for clarity

--- packages/parts/src/lib/maps/utils/polygonFromCoordinates.ts
+++ packages/parts/src/lib/maps/utils/polygonFromCoordinates.ts
@@ -1,14 +1,14 @@
 // from https://cs.stackexchange.com/questions/52606/sort-a-list-of-points-to-form-a-non-self-intersecting-polygon/52627
 interface Point {
-  x: number;
-  y: number;
+  lng: number;
+  lat: number;
 }
 
 export function sortPoints(points: Point[]) {
   points = points.splice(0);
-  const p0: Point = { x: 0, y: 0 };
-  p0.y = Math.min(...points.map((p) => p.y));
-  p0.x = Math.max(...points.filter((p) => p.y == p0.y).map((p) => p.x));
+  const p0: Point = { lng: 0, lat: 0 };
+  p0.lat = Math.min(...points.map((p) => p.lat));
+  p0.lng = Math.max(...points.filter((p) => p.lat == p0.lat).map((p) => p.lng));
   points.sort((a, b) => angleCompare(p0, a, b));
   return points;
 }
@@ -20,12 +20,12 @@ function angleCompare(p0: Point, a: Point, b: Point) {
 }
 
 function isLeft(p0: Point, a: Point, b: Point) {
-  return (a.x - p0.x) * (b.y - p0.y) - (b.x - p0.x) * (a.y - p0.y);
+  return (a.lng - p0.lng) * (b.lat - p0.lat) - (b.lng - p0.lng) * (a.lat - p0.lat);
 }
 
 function distCompare(p0: Point, a: Point, b: Point) {
-  const distA = (p0.x - a.x) * (p0.x - a.x) + (p0.y - a.y) * (p0.y - a.y);
-  const distB = (p0.x - b.x) * (p0.x - b.x) + (p0.y - b.y) * (p0.y - b.y);
+  const distA = (p0.lng - a.lng) * (p0.lng - a.lng) + (p0.lat - a.lat) * (p0.lat - a.lat);
+  const distB = (p0.lng - b.lng) * (p0.lng - b.lng) + (p0.lat - b.lat) * (p0.lat - b.lat);
   return distA - distB;
 }
 
@@ -33,55 +33,55 @@ if (import.meta.vitest) {
   test('sortPoints', () => {
     expect(
       sortPoints([
-        { x: 0, y: 0 },
-        { x: 0, y: 1 },
-        { x: 1, y: 1 },
-        { x: 1, y: 0 },
+        { lng: 0, lat: 0 },
+        { lng: 0, lat: 1 },
+        { lng: 1, lat: 1 },
+        { lng: 1, lat: 0 },
       ])
     ).toMatchInlineSnapshot(`
       [
         {
-          "x": 1,
-          "y": 0,
+          "lat": 0,
+          "lng": 1,
         },
         {
-          "x": 0,
-          "y": 0,
+          "lat": 0,
+          "lng": 0,
         },
         {
-          "x": 0,
-          "y": 1,
+          "lat": 1,
+          "lng": 0,
         },
         {
-          "x": 1,
-          "y": 1,
+          "lat": 1,
+          "lng": 1,
         },
       ]
     `);
     expect(
       sortPoints([
-        { x: 0, y: 0 },
-        { x: 1, y: 0 },
-        { x: 1, y: 1 },
-        { x: 0, y: 1 },
+        { lng: 0, lat: 0 },
+        { lng: 1, lat: 0 },
+        { lng: 1, lat: 1 },
+        { lng: 0, lat: 1 },
       ])
     ).toMatchInlineSnapshot(`
       [
         {
-          "x": 1,
-          "y": 0,
+          "lat": 0,
+          "lng": 1,
         },
         {
-          "x": 0,
-          "y": 0,
+          "lat": 0,
+          "lng": 0,
         },
         {
-          "x": 0,
-          "y": 1,
+          "lat": 1,
+          "lng": 0,
         },
         {
-          "x": 1,
-          "y": 1,
+          "lat": 1,
+          "lng": 1,
         },
       ]
     `);
@@ -94,27 +94,26 @@ if (import.meta.vitest) {
       { lng: 0, lat: 0 },
       { lng: 1, lat: 1 },
     ];
-    const sortedPoints = sortPoints(points.map((p) => ({ x: p.lng, y: p.lat })));
-    expect(sortedPoints.map(({ x, y }) => [x, y])).toMatchInlineSnapshot(`
+    expect(sortPoints(points).map(({ lng, lat }) => [lng, lat])).toMatchInlineSnapshot(`
+      [
         [
-          [
-            1,
-            0,
-          ],
-          [
-            0,
-            0,
-          ],
-          [
-            0,
-            1,
-          ],
-          [
-            1,
-            1,
-          ],
-        ]
-      `);
+          1,
+          0,
+        ],
+        [
+          0,
+          0,
+        ],
+        [
+          0,
+          1,
+        ],
+        [
+          1,
+          1,
+        ],
+      ]
+    `);
   });
 
   test('more complicated polygonFromCoordinates test', () => {
@@ -140,30 +139,29 @@ if (import.meta.vitest) {
         lat: 34.45282591700905,
       },
     ];
-    const sortedPoints = sortPoints(points.map((p) => ({ x: p.lng, y: p.lat })));
-    expect(sortedPoints.map(({ x, y }) => [x, y])).toMatchInlineSnapshot(`
+    expect(sortPoints(points).map(({ lng, lat }) => [lng, lat])).toMatchInlineSnapshot(`
+      [
         [
-          [
-            -99.91155986691973,
-            34.45282591700905,
-          ],
-          [
-            -105,
-            35,
-          ],
-          [
-            -105,
-            42,
-          ],
-          [
-            -95,
-            44,
-          ],
-          [
-            -96,
-            37,
-          ],
-        ]
-      `);
+          -99.91155986691973,
+          34.45282591700905,
+        ],
+        [
+          -105,
+          35,
+        ],
+        [
+          -105,
+          42,
+        ],
+        [
+          -95,
+          44,
+        ],
+        [
+          -96,
+          37,
+        ],
+      ]
+    `);
   });
 }
