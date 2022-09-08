97f62c08320affb3faa466540f23d92dcbec80bd
jacobbowdoin <jwrunner7@gmail.com>
2022-09-08 18:21:05 +0800

    remove error note

--- packages/site/windi.config.js
+++ packages/site/windi.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   safelist: ['space-x-1, space-x-3'],
   theme: {
     screens: {
-      print: { raw: 'print' }, // didn't work as described in https://windicss.org/utilities/general/variants.html#raw-media-queries
+      print: { raw: 'print' },
     },
   },
   plugins: [
