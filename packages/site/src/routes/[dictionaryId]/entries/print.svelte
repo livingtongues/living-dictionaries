62a346afc32dd1662e543d13c1ce0bb4d9999e16
jacobbowdoin <jwrunner7@gmail.com>
2022-09-08 18:34:28 +0800

    let user update min column width

--- packages/site/src/routes/[dictionaryId]/entries/print.svelte
+++ packages/site/src/routes/[dictionaryId]/entries/print.svelte
@@ -19,6 +19,7 @@
   import { dictionary } from '$lib/stores';
 
   let columnWidth = 250;
+  $: columnWidthEm = columnWidth / 16;
 </script>
 
 <svelte:head>
@@ -26,7 +27,15 @@
 </svelte:head>
 
 <Hits {search} let:entries>
-  <div class="print-columns" style="--column-width: {columnWidth / 16}em;">
+  <div class="print:hidden">
+    <input type="range" min="150" max="800" bind:value={columnWidth} />
+    <div
+      class="bg-red-50 p-1 border-red-400 border-t border-l border-r overflow-hidden"
+      style="width: {columnWidthEm}em">
+      Minimum column width: {columnWidth}px
+    </div>
+  </div>
+  <div class="print-columns" style="--column-width: {columnWidthEm}em;">
     {#each entries as entry (entry.id)}
       <div>{entry.lx}</div>
       <div class="italic text-sm mb-1 ml-1">{entry.gl?.en}</div>
