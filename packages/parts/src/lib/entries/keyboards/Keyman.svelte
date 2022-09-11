e142bb4f975263c120215e5d31758e869a1aa354
jacobbowdoin <jwrunner7@gmail.com>
2022-09-11 17:54:54 +0800

    add working Keyman component

--- packages/parts/src/lib/entries/keyboards/Keyman.svelte
+++
@@ -1,8 +0,0 @@
-<svelte:head>
-  <script src="https://s.keyman.com/kmw/engine/15.0.18/keymanweb.js"></script>
-  <script src="https://s.keyman.com/kmw/engine/15.0.18/kmwuibutton.js"></script>
-  <!-- float, button, toggle, toolbar -->
-  <!-- https://s.keyman.com/kmw/engine/16.0.59/keymanweb.js -->
-</svelte:head>
-
-Hello
