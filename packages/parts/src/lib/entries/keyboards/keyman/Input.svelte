28eebc8a6e70e63a272f63dd4dc41a8a22f97b9d
jacobbowdoin <jwrunner7@gmail.com>
2022-09-11 18:07:24 +0800

    add child Input and Textarea components

---
+++ packages/parts/src/lib/entries/keyboards/keyman/Input.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  // https://help.keyman.com/DEVELOPER/engine/web/15.0/reference/
+  import { getContext, onMount } from 'svelte';
+  import { glossingLanguages } from '../../../glosses/glossing-languages';
+  import { keymanKey, type keymanKeyContext } from './context';
+
+  export let bcp: string;
+  export let value: string;
+  export let showKeyboard = true;
+
+  const { getKeyman } = getContext<keymanKeyContext>(keymanKey);
+  const kmw = getKeyman();
+
+  let el: HTMLInputElement;
+  onMount(async () => {
+    const internalName = glossingLanguages[bcp] && glossingLanguages[bcp].internalName;
+    const keyboard = (internalName && `${internalName}@${bcp}`) || `@${bcp}`;
+
+    await kmw.addKeyboards(keyboard);
+    if (internalName) {
+      kmw.attachToControl(el);
+      kmw.setKeyboardForControl(el, internalName, bcp);
+    }
+  });
+</script>
+
+<input
+  bind:this={el}
+  class="border shadow px-3 py-1 block mr-1"
+  bind:value
+  class:kmw-disabled={!showKeyboard} />
+<button type="button" on:click={() => (showKeyboard = !showKeyboard)}
+  >Toggle <i class="far fa-keyboard" />
+</button>
