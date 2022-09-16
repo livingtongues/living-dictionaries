dcc6fb36ed5f7fceea490e73b2845fa7f257bfdf
jacobbowdoin <jwrunner7@gmail.com>
2022-09-16 20:25:46 +0800

    switch solely to using svelte-pieces Modal (need to add noscroll={true} to be equivalent)

--- packages/site/src/lib/components/modals/SemanticDomainsModal.svelte
+++ packages/site/src/lib/components/modals/SemanticDomainsModal.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import { _ } from 'svelte-i18n';
-  import Modal from '$lib/components/ui/Modal.svelte';
+  import Modal from 'svelte-pieces/ui/Modal.svelte';
   import { createEventDispatcher } from 'svelte';
   const dispatch = createEventDispatcher();
   const close = () => dispatch('close');
