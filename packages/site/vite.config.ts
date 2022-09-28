ed6d9dbfb5e2e3b5abe9a8ab39e2db1d64552e1e
Diego Córdova Nieto <diego@livingtongues.org>
2022-09-28 12:45:57 -0500

    change dev port to 3041

--- packages/site/vite.config.ts
+++ packages/site/vite.config.ts
@@ -2,12 +2,16 @@ import { sveltekit } from '@sveltejs/kit/vite';
 // import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 // keeps using localhost https://github.com/vitejs/vite/issues/9195
-import dns from 'dns'
-dns.setDefaultResultOrder('verbatim')
+import dns from 'dns';
+dns.setDefaultResultOrder('verbatim');
 
 /** @type {import('vite').UserConfig} */
 const config = {
   plugins: [sveltekit()],
+  server: {
+    port: 3041,
+    strictPort: false,
+  },
   // envDir: '../../',
   build: {
     target: 'es2015',
