3137b7d06da10898d8cb2064907d7dcf659cdedc
jacobbowdoin <jwrunner7@gmail.com>
2022-10-13 11:44:06 +0800

    small styles fixes

--- packages/site/src/routes/+page.svelte
+++ packages/site/src/routes/+page.svelte
@@ -88,7 +88,7 @@
       {#if $admin}
         <ShowHide let:show={hide} let:toggle>
           <CustomControl position="bottom-right">
-            <button class="whitespace-nowrap w-full px-2" on:click={toggle}>Toggle Private</button>
+            <button class="whitespace-nowrap w-90px! px-2" on:click={toggle}>Toggle Private</button>
           </CustomControl>
 
           {#if !hide && privateDictionaries.length}
