d2b14cf9259c20c5eb5c6bb9c1e97024cf17aa3a
Diego Córdova Nieto <diego@livingtongues.org>
2022-10-19 13:01:32 -0500

    remove unused parts of speech

--- packages/parts/src/lib/mappings/parts-of-speech.ts
+++ packages/parts/src/lib/mappings/parts-of-speech.ts
@@ -89,6 +89,8 @@ export const partsOfSpeech: IPartOfSpeech[] = [
   { enAbbrev: 'relpro', enName: 'relative pronoun' },
   { enAbbrev: 'vd', enName: 'ditransitive verb' },
   { enAbbrev: 'va', enName: 'ambitransitive' },
+];
+/* 
   { unofficial: true, enAbbrev: 'interr.pro', enName: 'interrogative pronoun' },
   { unofficial: true, enAbbrev: 'cop', enName: 'copula' },
   { unofficial: true, enAbbrev: 'imp.verb', enName: 'imperative verb' },
@@ -96,4 +98,4 @@ export const partsOfSpeech: IPartOfSpeech[] = [
   { unofficial: true, enAbbrev: 'inal.n', enName: 'inalienable noun' },
   { unofficial: true, enAbbrev: 'rel.n', enName: 'relational noun' },
   { unofficial: true, enAbbrev: 'v.n', enName: 'verbal noun' },
-];
+ */
