4307a12cc95fd8c7c16ebfe3ec920913a2467179
Diego Córdova Nieto <diego@livingtongues.org>
2022-11-07 13:30:17 -0600

    move instant search into dialect modal

--- packages/site/src/lib/components/modals/DialectModal.svelte
+++ packages/site/src/lib/components/modals/DialectModal.svelte
@@ -1,17 +1,34 @@
 <script lang="ts">
   import type { RefinementListItem } from 'instantsearch.js/es/connectors/refinement-list/connectRefinementList';
-  import { onMount } from 'svelte';
   import { connectRefinementList } from 'instantsearch.js/es/connectors';
-  import type { InstantSearch } from 'instantsearch.js';
+  import type { InstantSearch as ISearch} from 'instantsearch.js';
+  import InstantSearch from '$lib/components/search/InstantSearch.svelte';
   import type { Readable } from 'svelte/store';
   export let t: Readable<any> = undefined;
   import Modal from 'svelte-pieces/ui/Modal.svelte';
   import Button from 'svelte-pieces/ui/Button.svelte';
   import DataList from 'svelte-pieces/ui/DataList.svelte';
+  import { dictionary } from '$lib/stores';
+  import { browser } from '$app/environment';
   export let value: string,
     adding = false,
-    search: InstantSearch,
     attribute: 'di';
+
+  let firstTime = true,
+    internalSearch: ISearch;
+  $: if (internalSearch && firstTime) {
+      const customRefinementList = connectRefinementList((params) => {
+        ({ items } = params);
+      });
+      internalSearch.addWidgets([
+        customRefinementList({
+          attribute,
+          limit: 70,
+          showMoreLimit: 20,
+        }),
+      ]);
+      firstTime = false;
+    }
 
   import { createEventDispatcher } from 'svelte';
   const dispatch = createEventDispatcher<{
@@ -25,19 +42,6 @@
 
   let items: RefinementListItem[] = [];
 
-  onMount(async () => {
-    const customRefinementList = connectRefinementList((params) => {
-      ({ items } = params);
-    });
-    search.addWidgets([
-      customRefinementList({
-        attribute,
-        limit: 70,
-        showMoreLimit: 20,
-      }),
-    ]);
-  });
-
   function save() {
     dispatch('valueupdate', {
       field: attribute,
@@ -47,47 +51,51 @@
   }
 </script>
 
-<Modal on:close>
-  <span slot="heading"
-    >{$t('entry.di', {
-      default: 'Dialect',
-    })}
-  </span>
-
-  <form on:submit|preventDefault={save}>
-    <DataList
-      type="search"
-      class="form-input w-full leading-none"
-      allowAny
-      on:selected={(e) => {
-          value = e.detail.display;
-        }}>
-      {#each items as dialect}
-        <option>{dialect.value}</option>
-      {/each}
-    </DataList>
-  
-    <div class="modal-footer">
-      {#if value}       
-        <Button onclick={() => {value = ''; save()} } form="simple" color="red">
-          {t ? $t('misc.remove') : 'Remove'}
-        </Button>
-      {/if}
-      <Button onclick={close} form="simple" color="black">
-        {t ? $t('misc.cancel') : 'Cancel'}
-      </Button>
-      <div class="w-1" />
-      {#if adding}
-        <Button type="submit" form="filled">
-          {t ? $t('misc.next') : 'Next'}
-          <i class="far fa-chevron-right rtl-x-flip" />
-        </Button>
-      {:else}
-        <Button type="submit" form="filled">
-          {t ? $t('misc.save') : 'Save'}
-        </Button>
-      {/if}
-    </div>
-  </form>
-</Modal>
+{#if browser}
+  <InstantSearch dictionaryId={$dictionary.id} let:search>
+    <Modal on:close>
+      <span slot="heading"
+        >{$t('entry.di', {
+          default: 'Dialect',
+        })}
+      </span>
+    
+      <form on:submit|preventDefault={save} on:input={() => {internalSearch = search}}>
+        <DataList
+          type="search"
+          class="form-input w-full leading-none"
+          allowAny
+          on:selected={(e) => {
+              value = e.detail.display;
+            }}>
+          {#each items as dialect}
+            <option>{dialect.value}</option>
+          {/each}
+        </DataList>
+      
+        <div class="modal-footer">
+          {#if value}       
+            <Button onclick={() => {value = ''; save()} } form="simple" color="red">
+              {t ? $t('misc.remove') : 'Remove'}
+            </Button>
+          {/if}
+          <Button onclick={close} form="simple" color="black">
+            {t ? $t('misc.cancel') : 'Cancel'}
+          </Button>
+          <div class="w-1" />
+          {#if adding}
+            <Button type="submit" form="filled">
+              {t ? $t('misc.next') : 'Next'}
+              <i class="far fa-chevron-right rtl-x-flip" />
+            </Button>
+          {:else}
+            <Button type="submit" form="filled">
+              {t ? $t('misc.save') : 'Save'}
+            </Button>
+          {/if}
+        </div>
+      </form>
+    </Modal>
+  </InstantSearch>
+{/if}
 
