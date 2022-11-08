0d223fddf4143c105d06e0b5d3137af9586ef020
Diego Córdova Nieto <diego@livingtongues.org>
2022-11-08 13:55:23 -0600

    remove unused code

--- packages/site/src/lib/components/modals/DialectModal.svelte
+++ packages/site/src/lib/components/modals/DialectModal.svelte
@@ -1,18 +1,16 @@
 <script lang="ts">
   import { onMount } from 'svelte';
-  import InstantSearch from '$lib/components/search/InstantSearch.svelte';
   import type { Readable } from 'svelte/store';
   export let t: Readable<any> = undefined;
   import Modal from 'svelte-pieces/ui/Modal.svelte';
   import Button from 'svelte-pieces/ui/Button.svelte';
   import DataList from 'svelte-pieces/ui/DataList.svelte';
   import { dictionary } from '$lib/stores';
-  import { browser } from '$app/environment';
   export let value: string,
     adding = false,
     attribute: 'di';
 
-  //TODO avoid any type
+  //TODO avoid any type when correct response is ready
   let data: any;
   const getData = async () => {
     try {
@@ -59,53 +57,49 @@
   }
 </script>
 
-{#if browser}
-  <InstantSearch dictionaryId={$dictionary.id} let:search>
-    <Modal on:close>
-      <span slot="heading"
-        >{$t('entry.di', {
-          default: 'Dialect',
-        })}
-      </span>
-    
-      <form on:submit|preventDefault={save}>
-        <DataList
-          type="search"
-          class="form-input w-full leading-none"
-          allowAny
-          on:selected={(e) => {
-              value = e.detail.display;
-            }}>
-          {#if data}
-            {#each data.facetHits as dialect}
-              <option>{dialect.value}</option>
-            {/each}
-          {/if}
-        </DataList>
-      
-        <div class="modal-footer">
-          {#if value}       
-            <Button onclick={() => {value = ''; save()} } form="simple" color="red">
-              {t ? $t('misc.remove') : 'Remove'}
-            </Button>
-          {/if}
-          <Button onclick={close} form="simple" color="black">
-            {t ? $t('misc.cancel') : 'Cancel'}
-          </Button>
-          <div class="w-1" />
-          {#if adding}
-            <Button type="submit" form="filled">
-              {t ? $t('misc.next') : 'Next'}
-              <i class="far fa-chevron-right rtl-x-flip" />
-            </Button>
-          {:else}
-            <Button type="submit" form="filled">
-              {t ? $t('misc.save') : 'Save'}
-            </Button>
-          {/if}
-        </div>
-      </form>
-    </Modal>
-  </InstantSearch>
-{/if}
+<Modal on:close>
+  <span slot="heading"
+    >{$t('entry.di', {
+      default: 'Dialect',
+    })}
+  </span>
+
+  <form on:submit|preventDefault={save}>
+    <DataList
+      type="search"
+      class="form-input w-full leading-none"
+      allowAny
+      on:selected={(e) => {
+          value = e.detail.display;
+        }}>
+      {#if data}
+        {#each data.facetHits as dialect}
+          <option>{dialect.value}</option>
+        {/each}
+      {/if}
+    </DataList>
+  
+    <div class="modal-footer">
+      {#if value}       
+        <Button onclick={() => {value = ''; save()} } form="simple" color="red">
+          {t ? $t('misc.remove') : 'Remove'}
+        </Button>
+      {/if}
+      <Button onclick={close} form="simple" color="black">
+        {t ? $t('misc.cancel') : 'Cancel'}
+      </Button>
+      <div class="w-1" />
+      {#if adding}
+        <Button type="submit" form="filled">
+          {t ? $t('misc.next') : 'Next'}
+          <i class="far fa-chevron-right rtl-x-flip" />
+        </Button>
+      {:else}
+        <Button type="submit" form="filled">
+          {t ? $t('misc.save') : 'Save'}
+        </Button>
+      {/if}
+    </div>
+  </form>
+</Modal>
 
