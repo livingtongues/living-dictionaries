1810a1e14b419e37b3feadf23eae06350abea960
jacob-8 <jwrunner7@gmail.com>
2022-11-17 11:45:01 +0800

    style improvements

--- packages/site/src/lib/components/modals/DialectModal.svelte
+++ packages/site/src/lib/components/modals/DialectModal.svelte
@@ -6,7 +6,6 @@
   import { dictionary } from '$lib/stores';
 
   export let value: string;
-  let attribute: 'di';
   interface IAlgoliaFacetsQuery {
     facetHits: {
       value: string;
@@ -16,7 +15,6 @@
   }
 
   async function fetchDialects(): Promise<IAlgoliaFacetsQuery> {
-    await new Promise((r) => setTimeout(r, 3000));
     const response = await fetch(
       'https://XCVBAYSYXD.algolia.net/1/indexes/entries_prod/facets/di/query',
       {
@@ -44,10 +42,10 @@
 
   const close = () => dispatch('close');
 
-  function save() {
+  function save(newValue: string) {
     dispatch('valueupdate', {
-      field: attribute,
-      newValue: value.trim(),
+      field: 'di',
+      newValue,
     });
     close();
   }
@@ -60,56 +58,48 @@
     })}
   </span>
 
-  <form on:submit|preventDefault={save}>
+  {#await fetchDialects()}
+    <span class="i-gg-spinner animate-spin mb-3 block" />
+  {:then { facetHits }}
+    {#each facetHits as dialect}
+      <Button
+        onclick={() => save(dialect.value)}
+        class="mr-1 mb-1 opacity-100"
+        form={value === dialect.value ? 'filled' : 'simple'}
+        size="sm">{dialect.value}</Button>
+    {/each}
+  {:catch error}
+    <div class="text-red-500 mb-3">
+      Error loading current dialects: {error}
+    </div>
+  {/await}
+
+  <div class="mb-1">
     <Button
       onclick={() => {
-        value = prompt(
+        const value = prompt(
           $t('entry.di', {
             default: 'Dialect',
           })
         );
         if (value) {
-          save();
+          save(value.trim());
         }
       }}
       class="mr-1 mb-1"
       color="orange"
       size="sm"><span class="i-fa-solid-plus" /> {$t('misc.add', { default: 'Add' })}</Button>
+  </div>
 
-    {#await fetchDialects()}
-      <span class="i-gg-spinner animate-spin" />
-    {:then { facetHits }}
-      {#each facetHits as dialect}
-        <Button
-          onclick={() => {
-            value = dialect.value;
-            save();
-          }}
-          class="mr-1 mb-1 opacity-100"
-          disabled={value === dialect.value}
-          form={value === dialect.value ? 'filled' : 'simple'}
-          size="sm">{dialect.value}</Button>
-      {/each}
-    {:catch error}
-      Error loading current dialects: {error}
-    {/await}
-
-    <div class="modal-footer">
-      {#if value}
-        <Button
-          onclick={() => {
-            value = '';
-            save();
-          }}
-          form="simple"
-          color="red">
-          {t ? $t('misc.remove') : 'Remove'}
-        </Button>
-        <div class="w-1" />
-      {/if}
-      <Button onclick={close} form="simple" color="black">
-        {t ? $t('misc.cancel') : 'Cancel'}
+  <div class="modal-footer">
+    {#if value}
+      <Button onclick={() => save(null)} form="simple" color="red">
+        {t ? $t('misc.remove') : 'Remove'}
       </Button>
-    </div>
-  </form>
+      <div class="w-1" />
+    {/if}
+    <Button onclick={close} form="simple" color="black">
+      {t ? $t('misc.cancel') : 'Cancel'}
+    </Button>
+  </div>
 </Modal>
