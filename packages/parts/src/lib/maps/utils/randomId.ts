675742ab24365263836d42ca24980360746306bc
jacob-8 <jwrunner7@gmail.com>
2022-11-22 08:23:05 +0800

    don't register map click when it's on a custom feature

--- packages/parts/src/lib/maps/utils/randomId.ts
+++ packages/parts/src/lib/maps/utils/randomId.ts
@@ -1,6 +1,8 @@
+export const ADDED_FEATURE_ID_PREFIX = '_customFeature_';
+
 /**
  * Math.random should be unique because of its seeding algorithm. Convert it to base 36 (numbers + letters), and grab the first 9 characters after the decimal.
  */
 export function randomId(): string {
-  return '_' + Math.random().toString(36).slice(2, 11);
+  return ADDED_FEATURE_ID_PREFIX + Math.random().toString(36).slice(2, 11);
 }
