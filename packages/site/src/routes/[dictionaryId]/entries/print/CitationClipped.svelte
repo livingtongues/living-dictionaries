bc67d2dea969bbc0f3df4840a226721f3fbf3b4f
Diego Córdova Nieto <diego@livingtongues.org>
2022-11-24 19:16:46 -0600

    add comments

--- packages/site/src/routes/[dictionaryId]/entries/print/CitationClipped.svelte
+++ packages/site/src/routes/[dictionaryId]/entries/print/CitationClipped.svelte
@@ -1,13 +1,15 @@
 <script lang="ts">
   export let citation:string;
   export let citationText:string;
-  const maxLength = 167;
+  const maxLength = 167; //Number based if it looks well on letter format
   let citationClipped: string,
     textsSum: number;
   $: if (citation && citationText) {
     textsSum = citation.length + citationText.length;
     if (textsSum > maxLength) {
+      // When they follow the pattern: [Last name] [First letter from first name].,  
       citationClipped = citation.split('.,')[0];
+      // Otherwise, we only consider the first word
       if (citation === citationClipped) {
         citationClipped = citation.match(/\w+/gm)[0];
       }
