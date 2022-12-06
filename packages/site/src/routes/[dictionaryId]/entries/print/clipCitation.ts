93ed21301f098c1aabe3a72e1e7ba018c1ac764c
Diego Córdova Nieto <diego@livingtongues.org>
2022-12-06 17:21:14 -0600

    dynamic change finally working

--- packages/site/src/routes/[dictionaryId]/entries/print/clipCitation.ts
+++ packages/site/src/routes/[dictionaryId]/entries/print/clipCitation.ts
@@ -1,15 +1,18 @@
+//TODO make test file
 const maxLength = 167; //Number based if it looks well on letter format
 let citationClipped: string;
 let textsSum: number;
-export function clipCitation(citation: string, citationText: string): string {
-  textsSum = citation.length + citationText.length;
+export function clipCitation(authors: string, citationText: string): string {
+  textsSum = authors.length + citationText.length;
   if (textsSum > maxLength) {
     // When they follow the pattern: [Last name] [First letter from first name].,
-    citationClipped = citation.split('.,')[0];
+    citationClipped = authors.split('.,')[0];
     // Otherwise, we only consider the first word
-    if (citation === citationClipped) {
-      citationClipped = citation.match(/\w+/gm)[0];
+    if (authors === citationClipped) {
+      citationClipped = authors.match(/\w+/gm)[0] + '.';
     }
+  } else {
+    citationClipped = authors;
   }
   return citationClipped;
 }
