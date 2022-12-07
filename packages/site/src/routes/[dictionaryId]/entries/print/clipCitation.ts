b71addbdfdade7053d068470305fd9023b29fa47
Diego Córdova Nieto <diego@livingtongues.org>
2022-12-07 13:57:58 -0600

    use xregexp dependency to allow any unicode characters in authors names

--- packages/site/src/routes/[dictionaryId]/entries/print/clipCitation.ts
+++ packages/site/src/routes/[dictionaryId]/entries/print/clipCitation.ts
@@ -1,11 +1,14 @@
-//TODO make test file
+import XRegExp from 'xregexp';
+
+// pL regex supports every Unicode grapheme. See: https://www.regular-expressions.info/unicode.html#prop:~:text=your%20regular%20expression.-,Unicode%20Categories,-In%20addition%20to
+const matchFirstAuthor = XRegExp("^((\\pL'?)+ ?(\\w\\.? )*)?");
 const maxLength = 167; //Number based if it looks well on letter format
 let citationClipped: string;
 let textsSum: number;
 export function clipCitation(authors: string, citationText: string): string {
   textsSum = authors.length + citationText.length;
   if (textsSum > maxLength) {
-    citationClipped = authors.match(/\w+/gm)[0] + ' et al.,';
+    citationClipped = authors.match(matchFirstAuthor)[0] + ' et al.,';
   } else {
     citationClipped = authors;
   }
