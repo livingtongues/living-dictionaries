a31af1f32866945bfe15c0968c0a4e15ced19267
Diego Córdova Nieto <diego@livingtongues.org>
2022-12-07 14:17:19 -0600

    add comments to explain why I did that

--- packages/site/src/routes/[dictionaryId]/entries/print/clipCitation.ts
+++ packages/site/src/routes/[dictionaryId]/entries/print/clipCitation.ts
@@ -1,14 +1,15 @@
 import XRegExp from 'xregexp';
 
 // pL regex supports every Unicode grapheme. See: https://www.regular-expressions.info/unicode.html#prop:~:text=your%20regular%20expression.-,Unicode%20Categories,-In%20addition%20to
+// use of xregexp is because native regex doesn't support pL and other modern syntax. See: https://stackoverflow.com/questions/50178498/no-pl-for-javascript-regex-use-unicode-in-js-regex & https://www.npmjs.com/package/xregexp
 const matchFirstAuthor = XRegExp("^((\\pL'?)+ ?(\\w\\.? )*)?");
-const maxLength = 167; //Number based if it looks well on letter format
+const maxLength = 167; // number based on whether it looks good in letter format
 let citationClipped: string;
 let textsSum: number;
 export function clipCitation(authors: string, citationText: string): string {
   textsSum = authors.length + citationText.length;
   if (textsSum > maxLength) {
-    citationClipped = authors.match(matchFirstAuthor)[0] + ' et al.,';
+    citationClipped = authors.match(matchFirstAuthor)[0].trim() + ' et al.,';
   } else {
     citationClipped = authors;
   }
