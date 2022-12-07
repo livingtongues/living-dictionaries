bf9f95de41361e45b5c415db49557022ce7d8920
Diego Córdova Nieto <diego@livingtongues.org>
2022-12-07 14:17:53 -0600

    add more tests

--- packages/site/src/routes/[dictionaryId]/entries/print/clipCitation.test.ts
+++ packages/site/src/routes/[dictionaryId]/entries/print/clipCitation.test.ts
@@ -8,6 +8,14 @@ test('One author in citation', () =>
     )
   ).toBe('Anderson G.'));
 
+test('One author in citation with apostrophe in her/his name', () =>
+  expect(
+    clipCitation(
+      "O'Neal S.",
+      '2022. Test 004 Living Dictionary. Living Tongues Institute for Endangered Languages. https://livingdictionaries.app/test-004'
+    )
+  ).toBe("O'Neal S."));
+
 test('Two authors in citation but still below the max size', () =>
   expect(
     clipCitation(
@@ -22,4 +30,12 @@ test('Authors in citation exceding max size', () =>
       'Derwing T. Summo A. O. Davids M G. & Timmo E. R.',
       '2022. Test 004 Living Dictionary. Living Tongues Institute for Endangered Languages. https://livingdictionaries.app/test-004'
     )
-  ).toBe('Derwing et al.,'));
+  ).toBe('Derwing T. et al.,'));
+
+test('Authors in citation exceding max size and first author with accent', () =>
+  expect(
+    clipCitation(
+      'Córdova D. Summo A. O. Davids M G. & Timmo E. R.',
+      '2022. Test 004 Living Dictionary. Living Tongues Institute for Endangered Languages. https://livingdictionaries.app/test-004'
+    )
+  ).toBe('Córdova D. et al.,'));
