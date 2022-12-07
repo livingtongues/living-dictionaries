61c13ae6aad527912bb9e2a5bde1a7bc8e04442d
Diego Córdova Nieto <diego@livingtongues.org>
2022-12-06 18:32:35 -0600

    add tests

--- packages/site/src/routes/[dictionaryId]/entries/print/clipCitation.ts
+++ packages/site/src/routes/[dictionaryId]/entries/print/clipCitation.ts
@@ -5,12 +5,7 @@ let textsSum: number;
 export function clipCitation(authors: string, citationText: string): string {
   textsSum = authors.length + citationText.length;
   if (textsSum > maxLength) {
-    // When they follow the pattern: [Last name] [First letter from first name].,
-    citationClipped = authors.split('.,')[0];
-    // Otherwise, we only consider the first word
-    if (authors === citationClipped) {
-      citationClipped = authors.match(/\w+/gm)[0] + '.';
-    }
+    citationClipped = authors.match(/\w+/gm)[0] + ' et al.,';
   } else {
     citationClipped = authors;
   }
