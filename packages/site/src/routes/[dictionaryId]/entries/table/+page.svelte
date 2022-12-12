fb098de2dc78ad8514568d2a1c14230ece9d19b8
Diego Córdova Nieto <diego@livingtongues.org>
2022-12-12 15:58:05 -0600

    add SEO meta tags to entries views

--- packages/site/src/routes/[dictionaryId]/entries/table/+page.svelte
+++ packages/site/src/routes/[dictionaryId]/entries/table/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { _ } from 'svelte-i18n';
   import { getContext } from 'svelte';
   import type { InstantSearch } from 'instantsearch.js';
   const search: InstantSearch = getContext('search');
@@ -6,9 +7,11 @@
 
   import Pagination from '$lib/components/search/Pagination.svelte';
   import EntriesTable from '$lib/components/table/EntriesTable.svelte';
+  import SeoMetaTags from '$lib/components/SeoMetaTags.svelte';
 
   import { configure } from 'instantsearch.js/es/widgets/index.js';
   import { onMount } from 'svelte';
+  import { dictionary } from '$lib/stores'
   onMount(() => {
     search.addWidgets([
       configure({
@@ -17,6 +20,12 @@
     ]);
   });
 </script>
+
+<SeoMetaTags 
+  title={$_('', { default: 'Entries Table' })}
+  dictionaryName={$dictionary.name}
+  description={$_('', { default: 'The entries in this Living Dictionary are displayed in a comprehensive table (spreadsheet) that visitors can easily browse by using the page tabs at the bottom of the screen, or search by using the powerful search bar located at the top of the page. Visitors may also filter and display specific content from this Living Dictionary by activating filters for parts of speech, semantic domains, custom tags, speaker information and other metadata.' })}
+  keywords="Endangered Languages, Language Documentation, Language Revitalization, Build a Dictionary, Online Dictionary, Digital Dictionary, Dictionary Software, Free Software, Online Dictionary Builder, Living Dictionaries, Living Dictionary, Edit a dictionary, Search a dictionary, Browse a dictionary, Explore a Dictionary" />
 
 <Hits {search} let:entries>
   <EntriesTable {entries} />
