5d5e88d48b1590bb276290d4bf7b7ba41b232cfc
jacob-8 <jwrunner7@gmail.com>
2023-01-02 17:46:17 +0800

    init i18n in kitbook

---
+++ packages/site/src/kitbook/+layout.ts
@@ -0,0 +1,12 @@
+import { layoutLoad } from 'kitbook';
+export const load = layoutLoad({ initFunction })
+
+import { init, getLocaleFromNavigator } from 'svelte-i18n';
+async function initFunction(): Promise<void> {
+  // register('en', () => import('./en.json'));
+  await init({
+    fallbackLocale: 'en',
+    initialLocale: getLocaleFromNavigator(),
+  });
+}
+
