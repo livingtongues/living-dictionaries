988a73a980dd267235963ef74a791820abb23518
Diego Córdova Nieto <diego@livingtongues.org>
2023-01-06 13:53:30 -0600

    vernacular sentence first and the rest sorted

--- packages/site/src/lib/helpers/exampleSentences.ts
+++ packages/site/src/lib/helpers/exampleSentences.ts
@@ -5,25 +5,61 @@ export function printExampleSentences(
   t: (id: string) => string,
   { shorten = false } = {}
 ) {
-  return Object.keys(exampleSentences)
-    .filter((bcp) => exampleSentences[bcp])
-    .sort()
-    .map((bcp) => {
-      const exampleSentence = exampleSentences[bcp];
-      if (shorten) return exampleSentence;
-      return `${t('gl.' + bcp)}: ${exampleSentence}`;
+  let exampleSentencesCopy = { ...exampleSentences };
+  exampleSentencesCopy = sortVernacularSentence(exampleSentencesCopy);
+  const sortedExampleSentences = Object.keys(exampleSentencesCopy)
+    .filter((bcp) => exampleSentencesCopy[bcp])
+    .sort((a, b) => {
+      if (a === b) return 0;
+      if (a === 'vn') return -1;
+      if (b === 'vn') return 1;
+
+      if (a < b) return -1;
+      if (a > b) return 1;
+      return 0;
     });
+  return sortedExampleSentences.map((bcp) => {
+    const exampleSentence = exampleSentencesCopy[bcp];
+    if (shorten) return exampleSentence;
+    return `${t('gl.' + bcp)}: ${exampleSentence}`;
+  });
+}
+
+function sortVernacularSentence(exampleSentences: IExampleSentence) {
+  if (exampleSentences.vn) {
+    const vernacularSentence = { vn: exampleSentences.vn };
+    delete exampleSentences.vn;
+    exampleSentences = Object.assign(vernacularSentence, exampleSentences);
+  }
+  return exampleSentences;
 }
 
 if (import.meta.vitest) {
-  const t = (id) => (id === 'gl.en' ? 'English' : 'Spanish');
+  const t = (id) => {
+    switch (id) {
+      case 'gl.de':
+        return 'German';
+      case 'gl.en':
+        return 'English';
+      case 'gl.es':
+        return 'Spanish';
+      case 'gl.vn':
+        return 'Vernacular';
+      default:
+        return 'undefined';
+    }
+  };
   test('printExampleSentences', () => {
     const exampleSentence = {
-      en: 'The dog is walking',
       es: 'El perro está caminando',
+      vn: '我很喜歡吃香蕉',
+      de: 'der hund geht spazieren',
+      en: 'The dog is walking',
     };
     expect(printExampleSentences(exampleSentence, t, { shorten: true })).toMatchInlineSnapshot(`
       [
+        "我很喜歡吃香蕉",
+        "der hund geht spazieren",
         "The dog is walking",
         "El perro está caminando",
       ]
@@ -31,13 +67,17 @@ if (import.meta.vitest) {
 
     expect(printExampleSentences(exampleSentence, t)).toMatchInlineSnapshot(`
       [
+        "Vernacular: 我很喜歡吃香蕉",
+        "German: der hund geht spazieren",
         "English: The dog is walking",
         "Spanish: El perro está caminando",
       ]
     `);
 
-    expect(printExampleSentences(exampleSentence, t, { shorten: true }).join(', ')).toMatchInlineSnapshot(
-      '"The dog is walking, El perro está caminando"'
+    expect(
+      printExampleSentences(exampleSentence, t, { shorten: true }).join(', ')
+    ).toMatchInlineSnapshot(
+      '"我很喜歡吃香蕉, der hund geht spazieren, The dog is walking, El perro está caminando"'
     );
 
     expect(printExampleSentences({}, t)).toMatchInlineSnapshot('[]');
