d4412dc5eb244d1a5279f952b75b856c90b6e28e
jacob-8 <jwrunner7@gmail.com>
2023-01-16 21:18:51 +0800

    break SeoMetaTags title logic into own file and test

--- packages/site/src/routes/[dictionaryId]/entries/+layout.svelte
+++ packages/site/src/routes/[dictionaryId]/entries/+layout.svelte
@@ -86,7 +86,7 @@
 
 <!-- Workaround while we fix meta tags to correctly work in list, table, gallery and print pages -->
 <SeoMetaTags
-  flipTitle={$admin ? true : false}
+  admin={$admin}
   title="Entries"
   dictionaryName={$dictionary.name}
   description={$_('', { default: 'The entries in this Living Dictionary are displayed in a comprehensive list that visitors can easily browse by using the page tabs at the bottom of the screen, or search by using the powerful search bar located at the top of the page. Visitors may also filter and display specific content from this Living Dictionary by activating filters for parts of speech, semantic domains, custom tags, speaker information and other metadata.' })}
