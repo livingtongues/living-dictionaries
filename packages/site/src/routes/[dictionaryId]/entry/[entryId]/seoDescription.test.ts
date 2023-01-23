9832caf326d239a636309b463d5fb813c66717f9
Diego Córdova Nieto <diego@livingtongues.org>
2023-01-23 14:50:03 -0600

    extract local orthographies display

--- packages/site/src/routes/[dictionaryId]/entry/[entryId]/seoDescription.test.ts
+++ packages/site/src/routes/[dictionaryId]/entry/[entryId]/seoDescription.test.ts
@@ -1,5 +1,6 @@
-import type { IEntry } from "@living-dictionaries/types";
-import { seoDescription } from "./seoDescription";
+import type { IEntry } from '@living-dictionaries/types';
+import { seoDescription, showLocalOrthographies } from './seoDescription';
+import { entriesWithAlternateOrthographies } from '../../entries/print/mock-data';
 
 describe('seoDescription', () => {
   const $t = (id: string) => {
@@ -17,10 +18,23 @@ describe('seoDescription', () => {
     const entry: IEntry = {
       lx: 'hi',
       gl: { en: 'hello', es: 'hola' },
-    }
+    };
     const dictionaryGlossLanguages = ['es'];
 
     const result = seoDescription(entry, dictionaryGlossLanguages, $t);
     expect(result).toMatchInlineSnapshot('"Spanish: hola, English: hello."');
+  });
+});
+
+describe('showLocalOrthographies', () => {
+  const mockEntries: IEntry[] = entriesWithAlternateOrthographies;
+
+  test('showing some local orthographies', () => {
+    expect(showLocalOrthographies(mockEntries[1]).join(', ')).toMatchInlineSnapshot('"さよなら, 안녕, αντίο"');
+  });
+  test('showing all local orthographies', () => {
+    expect(showLocalOrthographies(mockEntries[0]).join(', ')).toMatchInlineSnapshot(
+      '"Nnọọ, Привет, سلام, नमस्ते, שלום"'
+    );
   });
 });
