67d6c8d03c303fbe8194bd227c27d4a62a11da31
jacob-8 <jwrunner7@gmail.com>
2023-01-31 09:03:10 +0800

    add removeLineBreaks function

--- packages/site/src/routes/[dictionaryId]/entry/[entryId]/seoDescription.test.ts
+++ packages/site/src/routes/[dictionaryId]/entry/[entryId]/seoDescription.test.ts
@@ -1,5 +1,5 @@
 import type { IEntry } from '@living-dictionaries/types';
-import { seoDescription } from './seoDescription';
+import { removeLineBreaks, seoDescription } from './seoDescription';
 
 describe('seoDescription', () => {
   const $t = (id: string) => {
@@ -22,5 +22,21 @@ describe('seoDescription', () => {
 
     const result = seoDescription(entry, dictionaryGlossLanguages, $t);
     expect(result).toMatchInlineSnapshot('"Spanish: hola, English: hello."');
+  });
+});
+
+
+describe('removeLineBreaks', () => {
+  test('keeps one space between words when newlines come after a space', () => {
+    const phrase = 'This is \nand \nexample \nphrase.';
+    expect(removeLineBreaks(phrase)).toMatchInlineSnapshot(
+      '"This is and example phrase."'
+    );
+  });
+  test('adds space between words only split by newline', () => {
+    const phrase = 'Here is the first line\nand now the second line';
+    expect(removeLineBreaks(phrase)).toMatchInlineSnapshot(
+      '"Here is the first line and now the second line"'
+    );
   });
 });
