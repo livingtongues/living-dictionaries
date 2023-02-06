462d4e4c8eaaf49eb8c4b81f8025aabcd64d5b34
Diego Córdova Nieto <diego@livingtongues.org>
2023-02-06 17:57:19 -0600

    import missing meta vitest

--- packages/site/src/lib/helpers/entry/get_local_orthagraphies.ts
+++ packages/site/src/lib/helpers/entry/get_local_orthagraphies.ts
@@ -8,30 +8,32 @@ export function get_local_orthographies(entry: Partial<IEntry>): string[] {
   return local_orthographies_fields_used.map((field) => entry[field]);
 }
 
-describe('get_local_orthographies', () => {
-  test('returns array of local orthographies', () => {
-    const entryWith5LocalOrthographies: Partial<IEntry> = {
-      lo: 'Nnọọ',
-      lo2: 'Привет',
-      lo3: 'سلام',
-      lo4: 'नमस्ते',
-      lo5: 'שלום',
-    };
-    expect(get_local_orthographies(entryWith5LocalOrthographies)).toEqual([
-      'Nnọọ',
-      'Привет',
-      'سلام',
-      'नमस्ते',
-      'שלום',
-    ]);
+if (import.meta.vitest) {
+  describe('get_local_orthographies', () => {
+    test('returns array of local orthographies', () => {
+      const entryWith5LocalOrthographies: Partial<IEntry> = {
+        lo: 'Nnọọ',
+        lo2: 'Привет',
+        lo3: 'سلام',
+        lo4: 'नमस्ते',
+        lo5: 'שלום',
+      };
+      expect(get_local_orthographies(entryWith5LocalOrthographies)).toEqual([
+        'Nnọọ',
+        'Привет',
+        'سلام',
+        'नमस्ते',
+        'שלום',
+      ]);
+    });
+    test('does not return field if field is empty or missing', () => {
+      const entryWith3LocalOrthographies: Partial<IEntry> = {
+        lo: 'さよなら',
+        lo2: '안녕',
+        lo3: '',
+        lo4: null,
+      };
+      expect(get_local_orthographies(entryWith3LocalOrthographies)).toEqual(['さよなら', '안녕']);
+    });
   });
-  test('does not return field if field is empty or missing', () => {
-    const entryWith3LocalOrthographies: Partial<IEntry> = {
-      lo: 'さよなら',
-      lo2: '안녕',
-      lo3: '',
-      lo4: null,
-    };
-    expect(get_local_orthographies(entryWith3LocalOrthographies)).toEqual(['さよなら', '안녕']);
-  });
-});
+}
