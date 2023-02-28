f453d4fcd9ba2b566a70988e98f965fd9f00526d
Diego Córdova Nieto <diego@livingtongues.org>
2023-02-28 17:19:08 -0600

    add inverse_semantic_domains_mapping function with tests

---
+++ packages/scripts/refactor/inverse-semantic-domains-mapping.ts
@@ -0,0 +1,62 @@
+import { semanticDomains } from '../../site/src/lib/mappings/semantic-domains';
+
+export function inverse_semantic_domains_mapping(semantic_domains: string[]): string[] {
+  const cleaned_semantic_domains = clean_semantic_domains(semantic_domains);
+  const sdn = cleaned_semantic_domains.map(
+    (semantic_domain) => semanticDomains.find((sd) => sd.name === semantic_domain).key
+  );
+  return sdn;
+}
+
+function clean_semantic_domains(semantic_domains: string[]): string[] {
+  const cleaned_semantic_domains = semantic_domains.map((sd) => {
+    if (sd.includes('-')) {
+      return sd.replace(/ -/g, ',');
+    } else {
+      return sd;
+    }
+  });
+  return cleaned_semantic_domains;
+}
+
+if (import.meta.vitest) {
+  test('cleans semantic domains', () => {
+    const semantic_domains = [
+      'Motion',
+      'Pro-forms',
+      'Spirituality and Religion',
+      'Humans - Social Relations and Organization',
+      'Health - well-being and sickness',
+      'Coordinators - Subordinators - Relativizers - Quotatives',
+    ];
+    expect(clean_semantic_domains(semantic_domains)).toEqual([
+      'Motion',
+      'Pro-forms',
+      'Spirituality and Religion',
+      'Humans, Social Relations and Organization',
+      'Health, well-being and sickness',
+      'Coordinators, Subordinators, Relativizers, Quotatives',
+    ]);
+  });
+
+  test.each([
+    {
+      sdn: ['TAM'],
+      expected: ['10.2'],
+    },
+    {
+      sdn: ['Universe and the natural world', 'Earth, geology and landscape'],
+      expected: ['1', '1.2'],
+    },
+    {
+      sdn: ['Names', 'Emotions', 'Birds', 'Colors', 'Animals'],
+      expected: ['4', '3.2', '1.7', '1.6', '1.5'],
+    },
+    {
+      sdn: ['Health - well-being and sickness', 'Earth - geology and landscape'],
+      expected: ['2.4', '1.2'],
+    },
+  ])('inverse semantic domains mapping', ({ sdn, expected }) => {
+    expect(inverse_semantic_domains_mapping(sdn)).toEqual(expected);
+  });
+}
