c92e3b925de13a9642524e440116d42fbb35cdc2
Diego Córdova Nieto <diego@livingtongues.org>
2023-03-06 14:53:59 -0600

    allow kharia diphthongs

--- packages/scripts/spreadsheet_helpers/integrate_tones.ts
+++ packages/scripts/spreadsheet_helpers/integrate_tones.ts
@@ -4,24 +4,33 @@
 // \u0301 = ´ (Acute accent)
 // \u0302 = ˆ (Circumflex accent)
 
-const phonetics = 'abera';
+const phonetics = 'aobera';
 const accents = '\u030C \u0302 \u0300';
 
 //TODO for dipthongs I think it will be easier to locate all possible of them in the language (in the spreadsheet)
 const kharia_vowels = new Set(['a', 'e', 'i', 'o', 'u', 'ɛ', 'ə', 'ɔ', 'ɨ']);
 
 function add_tones(word: string, accents: string) {
+  let new_word = '';
   let accent_index = 0;
   const splitted_accents = accents.split(' ');
   const spelling_word = word.split('');
   //TODO compares someway number of vowels is equals to number of accents
   spelling_word.forEach((letter, letter_index) => {
+    let new_letter;
     if (kharia_vowels.has(letter)) {
-      spelling_word[letter_index] = `${letter}${splitted_accents[accent_index]}`;
-      accent_index += 1;
+      if (kharia_vowels.has(spelling_word[letter_index - 1])) {
+        new_letter = letter;
+      } else {
+        new_letter = `${letter}${splitted_accents[accent_index]}`;
+        accent_index += 1;
+      }
+    } else {
+      new_letter = letter;
     }
+    new_word += new_letter;
   });
-  return spelling_word;
+  return new_word;
 }
 
 console.log(add_tones(phonetics, accents));
