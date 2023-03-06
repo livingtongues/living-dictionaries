79b9813c91a25d82f7fd36310fcc977a3386b2e6
Diego Córdova Nieto <diego@livingtongues.org>
2023-03-06 16:52:46 -0600

    refactor add_tones function

--- packages/scripts/spreadsheet_helpers/integrate_tones.ts
+++ packages/scripts/spreadsheet_helpers/integrate_tones.ts
@@ -4,27 +4,22 @@
 // \u0301 = ´ (Acute accent)
 // \u0302 = ˆ (Circumflex accent)
 
-const phonetics = 'aobera';
+const phonetics = 'abero';
 const accents = '\u030C \u0302 \u0300';
 
-//TODO for dipthongs I think it will be easier to locate all possible of them in the language (in the spreadsheet)
 const kharia_vowels = new Set(['a', 'e', 'i', 'o', 'u', 'ɛ', 'ə', 'ɔ', 'ɨ']);
 
 function add_tones(word: string, accents: string) {
   let new_word = '';
   let accent_index = 0;
   const splitted_accents = accents.split(' ');
-  const spelling_word = word.split('');
+  const splitted_word = word.split('');
   //TODO compares someway number of vowels is equals to number of accents
-  spelling_word.forEach((letter, letter_index) => {
+  splitted_word.forEach((letter, letter_index) => {
     let new_letter;
-    if (kharia_vowels.has(letter)) {
-      if (kharia_vowels.has(spelling_word[letter_index - 1])) {
-        new_letter = letter;
-      } else {
-        new_letter = `${letter}${splitted_accents[accent_index]}`;
-        accent_index += 1;
-      }
+    if (kharia_vowels.has(letter) && !kharia_vowels.has(splitted_word[letter_index - 1])) {
+      new_letter = `${letter}${splitted_accents[accent_index]}`;
+      accent_index += 1;
     } else {
       new_letter = letter;
     }
