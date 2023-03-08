aac104e67c5178218a72c8b10170e2b8703c84a6
jacob-8 <jwrunner7@gmail.com>
2023-03-08 12:33:46 +0800

    remove test not being used

--- packages/site/src/lib/transformers/condense_entry.test.ts
+++
@@ -1,3 +0,0 @@
-import type { ExpandedEntry, GoalDatabaseEntry } from "@living-dictionaries/types";
-
-// take ExpandedEntry, remove each expanded field and save over condensed field value GoalDatabaseEntry 
