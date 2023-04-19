a03cec88acc4e1594d079cb98aa0ccefa5506571
jacob-8 <jwrunner7@gmail.com>
2023-04-19 08:48:23 +0800

    fix playwright base url

--- packages/site/playwright.config.ts
+++ packages/site/playwright.config.ts
@@ -1,6 +1,9 @@
 import type { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
+	use: {
+		baseURL: process.env.PLAYWRIGHT_BASE_URL || 'http://localhost:4173'
+	},
 	webServer: {
 		reuseExistingServer: !process.env.CI,
 		command: 'npm run build && npm run preview',
