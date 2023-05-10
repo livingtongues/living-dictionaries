cce3934d14c4613b59fe55b9cee6557fe6f64273
jacob-8 <jwrunner7@gmail.com>
2023-05-10 11:11:47 +0800

    use async await rather than sleep

--- packages/scripts/algolia/algolia.ts
+++ packages/scripts/algolia/algolia.ts
@@ -11,31 +11,22 @@ const index = client.initIndex(
   projectId === 'talking-dictionaries-dev' ? 'entries_dev' : 'entries_prod'
 );
 
-const CHUNK_SIZE = 2000
-const TWENTY_SECONDS = 20 * 1000
-const sleep = async (time: number) => await new Promise((resolve) => setTimeout(resolve, time));
+const MAX_CHUNK_SIZE = 3000
+// https://www.algolia.com/doc/api-reference/api-methods/add-objects/#examples
+// if forced to iterate instead of save all at once, take note of the rate limiting at 5000 backlogged requests https://www.algolia.com/doc/faq/indexing/is-there-a-rate-limit/
 
 export async function updateIndex(entries: AlgoliaEntry[]) {
-  for (let startOfChunkIndex = 0; startOfChunkIndex < entries.length; startOfChunkIndex += CHUNK_SIZE) {
-    const endOfChunk = startOfChunkIndex + CHUNK_SIZE
-    const chunk = entries.slice(startOfChunkIndex, endOfChunk);
-    console.log({ startOfChunkIndex, endOfChunk, CHUNK_SIZE, chunkLength: chunk.length });
+  try {
+    for (let startOfChunkIndex = 0; startOfChunkIndex < entries.length; startOfChunkIndex += MAX_CHUNK_SIZE) {
+      const endOfChunk = startOfChunkIndex + MAX_CHUNK_SIZE
+      const chunk = entries.slice(startOfChunkIndex, endOfChunk);
+      console.log({ startOfChunkIndex, endOfChunk, CHUNK_SIZE: MAX_CHUNK_SIZE, chunkLength: chunk.length });
 
-    index
-      .saveObjects(chunk)
-      .then(({ objectIDs }) => {
-        console.log(`Entries indexed: ${objectIDs.length}`);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-
-    if (startOfChunkIndex + CHUNK_SIZE < entries.length) {
-      console.log("Pausing to avoid hitting Algolia rate limit...");
-      await sleep(TWENTY_SECONDS)
+      const { objectIDs } = await index.saveObjects(chunk);
+      console.log(`Entries indexed: ${objectIDs.length}`);
     }
+  } catch (err) {
+    console.log(err);
   }
 
-  // https://www.algolia.com/doc/api-reference/api-methods/add-objects/#examples
-  // if forced to iterate instead of save all at once, take note of the rate limiting at 5000 backlogged requests https://www.algolia.com/doc/faq/indexing/is-there-a-rate-limit/
 }
