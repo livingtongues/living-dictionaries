550d652fde9ad364a0725acd9bdf17c4fe89aae8
Diego Córdova Nieto <diego@livingtongues.org>
2023-05-18 15:32:00 -0600

    show test with variant

--- packages/site/src/lib/export/prepareEntriesForCsv.test.ts
+++ packages/site/src/lib/export/prepareEntriesForCsv.test.ts
@@ -4,323 +4,9 @@ import type {
   ISpeaker,
   ISemanticDomain,
   IPartOfSpeech,
-  IEntry,
 } from '@living-dictionaries/types';
-import {
-  prepareEntriesForCsv,
-  assign_local_orthographies_to_headers,
-  count_maximum_semantic_domains_only_from_first_senses,
-  assign_semantic_domains_to_headers,
-  assign_gloss_languages_to_headers,
-  assign_example_sentences_to_headers,
-  find_part_of_speech,
-  get_first_speaker_from_first_sound_file,
-  display_speaker_gender,
-  display_speaker_age_range,
-  assign_local_orthographies_to_formatted_entry,
-  assign_semantic_domains_to_formatted_entry,
-  assign_gloss_languages_to_formatted_entry,
-  assign_example_sentences_to_formatted_entry,
-  type EntryForCSV,
-} from './prepareEntriesForCsv';
-
-//headers tests
-describe('assign_local_orthographies_to_headers', () => {
-  test('assigns alternate_orthographies if any exists', () => {
-    const headers = {} as EntryForCSV;
-    const alternate_orthographies = ['native-1', 'native-2'];
-    assign_local_orthographies_to_headers(headers, alternate_orthographies);
-    expect(headers).toEqual({
-      local_orthography_1: 'native-1',
-      local_orthography_2: 'native-2',
-    });
-  });
-  test("doesn't assign alternate_orthographies if empty array", () => {
-    const headers = {} as EntryForCSV;
-    const alternate_orthographies = [];
-    assign_local_orthographies_to_headers(headers, alternate_orthographies);
-    expect(headers).toEqual({});
-  });
-  test("doesn't assign alternate_orthographies if null", () => {
-    const headers = {} as EntryForCSV;
-    const alternate_orthographies = null;
-    assign_local_orthographies_to_headers(headers, alternate_orthographies);
-    expect(headers).toEqual({});
-  });
-});
-
-describe('count_maximum_semantic_domains_only_from_first_senses', () => {
-  test('counts the maximum number of the semantic domains in the first sense of each entry', () => {
-    const entries = [
-      {
-        lexeme: 'foo',
-        senses: [{ semantic_domains: ['1.2'] }],
-      },
-      {
-        lexeme: 'bar',
-        senses: [{ semantic_domains: ['2.1', '2.2', '2.3'] }],
-      },
-    ];
-    expect(count_maximum_semantic_domains_only_from_first_senses(entries)).toEqual(3);
-  });
-  test("returns 0 if there's only empty arrays or null values in semantic_doains", () => {
-    const entries = [
-      {
-        lexeme: 'foo',
-        senses: [],
-      },
-      {
-        lexeme: 'bar',
-      },
-    ];
-    expect(count_maximum_semantic_domains_only_from_first_senses(entries)).toEqual(0);
-  });
-});
-
-describe('assign_semantic_domains_to_headers', () => {
-  test('assigns semantic domains if any exists', () => {
-    const headers = {} as EntryForCSV;
-    const max_semantic_domain_number = 3;
-    assign_semantic_domains_to_headers(headers, max_semantic_domain_number);
-    expect(headers).toEqual({
-      semantic_domain_1: 'Semantic domain 1',
-      semantic_domain_2: 'Semantic domain 2',
-      semantic_domain_3: 'Semantic domain 3',
-    });
-  });
-  test("doesn't assign semantic domains if none", () => {
-    const headers = {} as EntryForCSV;
-    const max_semantic_domain_number = 0;
-    assign_semantic_domains_to_headers(headers, max_semantic_domain_number);
-    expect(headers).toEqual({});
-  });
-});
-
-describe('assign_gloss_languages_to_headers', () => {
-  test("assigns gloss languages if any exists or bcp if it doesn't", () => {
-    const headers = {} as EntryForCSV;
-    const gloss_languages = ['en', 'es', 'af'];
-    assign_gloss_languages_to_headers(headers, gloss_languages);
-    expect(headers).toEqual({
-      en_gloss_language: 'English Gloss',
-      es_gloss_language: 'español Gloss',
-      af_gloss_language: 'af Gloss',
-    });
-  });
-  test("Doesn't assign gloss languages if empty array", () => {
-    const headers = {} as EntryForCSV;
-    const gloss_languages = [];
-    assign_gloss_languages_to_headers(headers, gloss_languages);
-    expect(headers).toEqual({});
-  });
-  test("Doesn't assign gloss languages if null", () => {
-    const headers = {} as EntryForCSV;
-    const gloss_languages = null;
-    assign_gloss_languages_to_headers(headers, gloss_languages);
-    expect(headers).toEqual({});
-  });
-});
-
-describe('assign_example_sentences_to_headers', () => {
-  test("assigns vernacular and other example sentences if any exists or bcp if it doesn't", () => {
-    const headers = {} as EntryForCSV;
-    const gloss_languages = ['it', 'af'];
-    const dictionary_name = 'Foo';
-    assign_example_sentences_to_headers(headers, gloss_languages, dictionary_name);
-    expect(headers).toEqual({
-      vernacular_example_sentence: 'Example sentence in Foo',
-      it_example_sentence: 'Example sentence in Italiano',
-      af_example_sentence: 'Example sentence in af',
-    });
-  });
-  test('Assigns only verncaular if empty array', () => {
-    const headers = {} as EntryForCSV;
-    const gloss_languages = [];
-    const dictionary_name = 'Baz';
-    assign_example_sentences_to_headers(headers, gloss_languages, dictionary_name);
-    expect(headers).toEqual({
-      vernacular_example_sentence: 'Example sentence in Baz',
-    });
-  });
-  test("Doesn't assign gloss languages if null", () => {
-    const headers = {} as EntryForCSV;
-    const gloss_languages = null;
-    const dictionary_name = 'Boo';
-    assign_example_sentences_to_headers(headers, gloss_languages, dictionary_name);
-    expect(headers).toEqual({
-      vernacular_example_sentence: 'Example sentence in Boo',
-    });
-  });
-});
-
-describe('find_part_of_speech', () => {
-  const parts_of_speech = [
-    {
-      enAbbrev: 'n',
-      enName: 'noun',
-    },
-  ];
-  test('finds readable part of speech name', () => {
-    const part_of_speech_abbreviation = 'n';
-    expect(find_part_of_speech(parts_of_speech, part_of_speech_abbreviation)).toEqual('noun');
-  });
-
-  test('return empty string if abbreviation does not exist', () => {
-    const part_of_speech_abbreviation = null;
-    expect(find_part_of_speech(parts_of_speech, part_of_speech_abbreviation)).toEqual('');
-  });
-});
-
-describe('get_first_speaker_from_first_sound_file', () => {
-  test('gets speaker', () => {
-    const speakers: ISpeaker[] = [
-      {
-        displayName: 'Arthur Morgan',
-        id: 'rdr2',
-        birthplace: 'New Hanover',
-        decade: 3,
-        gender: 'm',
-      },
-    ];
-    const entry: IEntry = {
-      lexeme: 'zoo',
-      sound_files: [{ fb_storage_path: 'https://database.com/example.mp3', speaker_ids: ['rdr2'] }],
-    };
-    expect(get_first_speaker_from_first_sound_file(entry, speakers)).toEqual(speakers[0]);
-  });
-  test("gets undefined if there's no speaker", () => {
-    const speakers: ISpeaker[] = [];
-    const entry: IEntry = {
-      lexeme: 'zoo',
-      sound_files: [{ fb_storage_path: 'https://database.com/example.mp3', speaker_ids: ['rdr2'] }],
-    };
-    expect(get_first_speaker_from_first_sound_file(entry, speakers)).toEqual(undefined);
-  });
-});
-
-describe('display_speaker_gender', () => {
-  test('displays readable speaker gender', () => {
-    expect(display_speaker_gender('m')).toEqual('male');
-  });
-  test('displays empty string if speaker gender it is an empty string or undefined', () => {
-    expect(display_speaker_gender('')).toEqual('');
-    expect(display_speaker_gender(undefined)).toEqual('');
-  });
-});
-
-describe('display_speaker_age_range', () => {
-  test('displays readable speaker age range', () => {
-    expect(display_speaker_age_range(3)).toEqual('31-40');
-  });
-  test('displays empty string if speaker age range is undefined', () => {
-    expect(display_speaker_age_range(undefined)).toEqual('');
-  });
-});
-
-//formattedEntry tests
-describe('assign_local_orthographies_to_formatted_entry', () => {
-  test('assigns local orthography to formatted entry if value exist or assigns an empty string if does not', () => {
-    const formatted_entry: EntryForCSV = {};
-    const headers: EntryForCSV = {
-      local_orthography_1: 'script_1',
-      local_orthography_2: 'script_2',
-      local_orthography_3: 'script_3',
-    };
-    const entry: ExpandedEntry = {
-      lexeme: 'foo',
-      local_orthography_3: 'example-3',
-      local_orthography_2: 'example-2',
-    };
-    const alternate_orthographies = ['script_1', 'script_2', 'scriprt_3'];
-    const expected = {
-      script_1: '',
-      script_2: 'example-2',
-      script_3: 'example-3',
-    };
-    assign_local_orthographies_to_formatted_entry({
-      formatted_entry,
-      headers,
-      entry,
-      alternate_orthographies,
-    });
-    expect(formatted_entry).toEqual(expected);
-  });
-});
-describe('assign_semantic_domains_to_formatted_entry', () => {
-  test('assigns semantic domain to formatted entry if value exist or assigns an empty string if does not', () => {
-    const formatted_entry: EntryForCSV = {};
-    const entry: ExpandedEntry = {
-      lexeme: 'baz',
-      senses: [{ semantic_domains: ['1.6', '1.7'] }],
-    };
-    const max_semantic_domain_number = 3;
-    const global_semantic_domains: ISemanticDomain[] = [
-      { key: '1.5', name: 'Animals' },
-      { key: '1.6', name: 'Colors' },
-      { key: '1.7', name: 'Birds' },
-    ];
-    const expected = {
-      semantic_domain_1: 'Colors',
-      semantic_domain_2: 'Birds',
-      semantic_domain_3: '',
-    };
-    assign_semantic_domains_to_formatted_entry({
-      formatted_entry,
-      entry,
-      max_semantic_domain_number,
-      global_semantic_domains,
-    });
-    expect(formatted_entry).toEqual(expected);
-  });
-});
-
-describe('assign_gloss_languages_to_formatted_entry', () => {
-  test('assigns gloss languages values to formatted entry if value exist or assigns an empty string if does not', () => {
-    const formatted_entry: EntryForCSV = {};
-    const entry: ExpandedEntry = {
-      lexeme: 'Boo',
-      senses: [{ glosses: { es: 'oso' } }],
-    };
-    const gloss_languages = ['en', 'es'];
-    const expected = {
-      en_gloss_language: '',
-      es_gloss_language: 'oso',
-    };
-    assign_gloss_languages_to_formatted_entry({ formatted_entry, entry, gloss_languages });
-    expect(formatted_entry).toEqual(expected);
-  });
-});
-
-describe('assign_example_sentences_to_formatted_entry', () => {
-  test('assigns example sentences values to formatted entry if value exist or assigns an empty string if does not', () => {
-    const formatted_entry: EntryForCSV = {};
-    const entry: ExpandedEntry = {
-      lexeme: 'Boo',
-      senses: [{ example_sentences: [{ es: 'el oso es enorme' }] }],
-    };
-    const gloss_languages = ['en', 'es'];
-    const expected = {
-      en_example_sentence: '',
-      es_example_sentence: 'el oso es enorme',
-      vernacular_example_sentence: '',
-    };
-    assign_example_sentences_to_formatted_entry({ formatted_entry, entry, gloss_languages });
-    expect(formatted_entry).toEqual(expected);
-  });
-  test('assigns vernacular example sentence value to formatted entry if any gloss languages exist', () => {
-    const formatted_entry: EntryForCSV = {};
-    const entry: ExpandedEntry = {
-      lexeme: 'min',
-      senses: [{ example_sentences: [{ vn: 'native example' }] }],
-    };
-    const gloss_languages = [];
-    const expected = {
-      vernacular_example_sentence: 'native example',
-    };
-    assign_example_sentences_to_formatted_entry({ formatted_entry, entry, gloss_languages });
-    expect(formatted_entry).toEqual(expected);
-  });
-});
+import { prepareEntriesForCsv } from './prepareEntriesForCsv';
+import { variants } from '$lib/components/entry/EditField.variants';
 
 describe('prepareEntriesForCsv', () => {
   const headerRow = {
@@ -355,14 +41,6 @@ describe('prepareEntriesForCsv', () => {
     semantic_domain_1: 'Semantic domain 1',
     semantic_domain_2: 'Semantic domain 2',
   };
-
-  const dictionary: IDictionary = {
-    name: 'TestLang',
-    id: 'test',
-    glossLanguages: ['ar', 'en', 'es'],
-    alternateOrthographies: ['native_script_1', 'native_script_2'],
-    entryCount: 0,
-  };
   const speakers: ISpeaker[] = [
     {
       displayName: 'John Smith',
@@ -378,7 +56,14 @@ describe('prepareEntriesForCsv', () => {
   ];
   const partsOfSpeech: IPartOfSpeech[] = [{ enAbbrev: 'n', enName: 'noun' }];
 
-  test('works', () => {
+  test('basic example to smoke test with ExpandedEntry', () => {
+    const dictionary: IDictionary = {
+      name: 'TestLang',
+      id: 'test',
+      glossLanguages: ['ar', 'en', 'es'],
+      alternateOrthographies: ['native_script_1', 'native_script_2'],
+      entryCount: 2,
+    };
     const expanded_entries: ExpandedEntry[] = [
       {
         id: '12345qwerty',
@@ -480,6 +165,93 @@ describe('prepareEntriesForCsv', () => {
     expect(
       prepareEntriesForCsv(expanded_entries, dictionary, speakers, semanticDomains, partsOfSpeech)
     ).toEqual(expected);
+  });
+  test('super simple example to show variant column', () => {
+    const dictionary: IDictionary = {
+      name: 'Babanki example',
+      id: 'babanki',
+      glossLanguages: [],
+      entryCount: 2,
+    };
+    const expanded_entries: ExpandedEntry[] = [
+      {
+        lexeme: 'foo',
+        variant: 'new variant',
+      },
+      {
+        lexeme: 'baz',
+      },
+    ];
+    expect(
+      prepareEntriesForCsv(expanded_entries, dictionary, speakers, semanticDomains, partsOfSpeech)
+    ).toEqual([
+      {
+        dialects: 'Dialects',
+        id: 'Entry Id',
+        image_filename: 'Image filename',
+        interlinearization: 'Interlinearization',
+        lexeme: 'Lexeme/Word/Phrase',
+        morphology: 'Morphology',
+        notes: 'Notes',
+        noun_class: 'Noun class',
+        parts_of_speech: 'Part of Speech',
+        parts_of_speech_abbreviation: 'Part of Speech abbreviation',
+        phonetic: 'Phonetic (IPA)',
+        plural_form: 'Plural form',
+        sound_filename: 'Audio filename',
+        sources: 'Source(s)',
+        speaker_birthplace: 'Speaker birthplace',
+        speaker_decade: 'Speaker decade',
+        speaker_gender: 'Speaker gender',
+        speaker_name: 'Speaker name',
+        variant: 'Variant',
+        vernacular_example_sentence: 'Example sentence in Babanki example',
+      },
+      {
+        dialects: '',
+        id: '',
+        image_filename: '',
+        interlinearization: '',
+        lexeme: 'foo',
+        morphology: '',
+        notes: '',
+        noun_class: '',
+        parts_of_speech: '',
+        parts_of_speech_abbreviation: '',
+        phonetic: '',
+        plural_form: '',
+        sound_filename: '',
+        sources: '',
+        speaker_birthplace: '',
+        speaker_decade: '',
+        speaker_gender: '',
+        speaker_name: '',
+        variant: 'new variant',
+        vernacular_example_sentence: '',
+      },
+      {
+        dialects: '',
+        id: '',
+        image_filename: '',
+        interlinearization: '',
+        lexeme: 'baz',
+        morphology: '',
+        notes: '',
+        noun_class: '',
+        parts_of_speech: '',
+        parts_of_speech_abbreviation: '',
+        phonetic: '',
+        plural_form: '',
+        sound_filename: '',
+        sources: '',
+        speaker_birthplace: '',
+        speaker_decade: '',
+        speaker_gender: '',
+        speaker_name: '',
+        variant: '',
+        vernacular_example_sentence: '',
+      },
+    ]);
   });
 });
 
--- packages/site/src/lib/export/prepareEntriesForCsv.ts
+++ packages/site/src/lib/export/prepareEntriesForCsv.ts
@@ -6,7 +6,23 @@ import type {
   ISpeaker,
 } from '@living-dictionaries/types';
 import { stripHTMLTags } from './stripHTMLTags';
-import { glossingLanguages } from '$lib/glosses/glossing-languages';
+import {
+  assign_local_orthographies_to_headers,
+  assign_example_sentences_to_headers,
+  assign_gloss_languages_to_headers,
+  assign_semantic_domains_to_headers,
+  count_maximum_semantic_domains_only_from_first_senses,
+} from './assignHeadersForCsv';
+import {
+  find_part_of_speech,
+  get_first_speaker_from_first_sound_file,
+  display_speaker_gender,
+  display_speaker_age_range,
+  assign_local_orthographies_to_formatted_entry,
+  assign_semantic_domains_to_formatted_entry,
+  assign_gloss_languages_to_formatted_entry,
+  assign_example_sentences_to_formatted_entry,
+} from './assignFormattedEntryValuesForCsv';
 
 enum StandardEntryCSVFields {
   id = 'Entry Id',
@@ -28,7 +44,7 @@ enum StandardEntryCSVFields {
   speaker_decade = 'Speaker decade',
   speaker_gender = 'Speaker gender',
 }
-type EntryForCSVKeys = keyof typeof StandardEntryCSVFields;
+export type EntryForCSVKeys = keyof typeof StandardEntryCSVFields;
 type StandardEntryForCSV = {
   [key in EntryForCSVKeys]?: string;
 };
@@ -36,177 +52,6 @@ type StandardEntryForCSV = {
 export interface EntryForCSV extends StandardEntryForCSV {
   vernacular_example_sentence?: string;
   variant?: string; // optional for Babanki & Torwali
-}
-
-export function assign_local_orthographies_to_headers(
-  headers: EntryForCSV,
-  alternate_orthographies: string[]
-): void {
-  if (alternate_orthographies) {
-    alternate_orthographies.forEach((lo, index) => {
-      headers[`local_orthography_${index + 1}`] = lo;
-    });
-  }
-}
-
-export function assign_semantic_domains_to_headers(
-  headers: EntryForCSV,
-  max_semantic_domain_number: number
-): void {
-  if (max_semantic_domain_number > 0) {
-    for (let index = 0; index < max_semantic_domain_number; index++) {
-      headers[`semantic_domain_${index + 1}`] = `Semantic domain ${index + 1}`;
-    }
-  }
-}
-
-export function count_maximum_semantic_domains_only_from_first_senses(
-  entries: ExpandedEntry[]
-): number {
-  const max_semantic_domain_number = Math.max(
-    ...entries.map((entry) => entry.senses?.[0]?.semantic_domains?.length || 0)
-  );
-  return max_semantic_domain_number;
-}
-
-export function assign_gloss_languages_to_headers(
-  headers: EntryForCSV,
-  gloss_languages: string[]
-): void {
-  if (gloss_languages) {
-    gloss_languages.forEach((bcp) => {
-      headers[`${bcp}_gloss_language`] = `${glossingLanguages[bcp].vernacularName || bcp} Gloss`;
-    });
-  }
-}
-
-export function assign_example_sentences_to_headers(
-  headers: EntryForCSV,
-  gloss_languages: string[],
-  dictionary_name: string
-): void {
-  headers.vernacular_example_sentence = `Example sentence in ${dictionary_name}`;
-  if (gloss_languages) {
-    gloss_languages.forEach((bcp) => {
-      headers[`${bcp}_example_sentence`] = `Example sentence in ${
-        glossingLanguages[bcp].vernacularName || bcp
-      }`;
-    });
-  }
-}
-
-export function find_part_of_speech(
-  parts_of_speech: IPartOfSpeech[],
-  part_of_speech_abbreviation: string
-): string {
-  const part_of_speech = parts_of_speech.find(
-    (part_of_speech) => part_of_speech.enAbbrev === part_of_speech_abbreviation
-  );
-  return part_of_speech ? part_of_speech.enName : '';
-}
-
-export function get_first_speaker_from_first_sound_file(
-  entry: ExpandedEntry,
-  speakers: ISpeaker[]
-): ISpeaker {
-  return speakers.find((speaker) => speaker.id === entry.sound_files?.[0].speaker_ids?.[0]);
-}
-
-export function display_speaker_gender(speaker_gender: string): string {
-  if (speaker_gender) {
-    return speaker_gender === 'f' ? 'female' : 'male';
-  }
-  return '';
-}
-
-export function display_speaker_age_range(decade: number) {
-  switch (decade) {
-    case 0:
-      return '0-10';
-    case 1:
-      return '11-20';
-    case 2:
-      return '21-30';
-    case 3:
-      return '31-40';
-    case 4:
-      return '41-50';
-    case 5:
-      return '51-60';
-    case 6:
-      return '61-70';
-    case 7:
-      return '71-80';
-    case 8:
-      return '81-90';
-    case 9:
-      return '91-100';
-    default:
-      return '';
-  }
-}
-type LocalOrthographiesAllocator = {
-  formatted_entry: EntryForCSV;
-  headers: EntryForCSV;
-  entry: ExpandedEntry;
-  alternate_orthographies: string[];
-};
-export function assign_local_orthographies_to_formatted_entry(
-  allocator: LocalOrthographiesAllocator
-): void {
-  const { formatted_entry, headers, entry, alternate_orthographies } = allocator;
-  if (alternate_orthographies) {
-    const local_orthographies_of_headers = Object.keys(headers).filter((key) =>
-      key.startsWith('local_orthography')
-    );
-    local_orthographies_of_headers.forEach((header) => {
-      formatted_entry[headers[header]] = entry[header] || '';
-    });
-  }
-}
-
-type SemanticDomainsAllocator = {
-  formatted_entry: EntryForCSV;
-  entry: ExpandedEntry;
-  max_semantic_domain_number: number;
-  global_semantic_domains: ISemanticDomain[];
-};
-export function assign_semantic_domains_to_formatted_entry(
-  allocator: SemanticDomainsAllocator
-): void {
-  const { formatted_entry, entry, max_semantic_domain_number, global_semantic_domains } = allocator;
-  for (let index = 0; index < max_semantic_domain_number; index++) {
-    formatted_entry[`semantic_domain_${index + 1}`] = '';
-    if (entry.senses?.[0].semantic_domains) {
-      const matching_domain = global_semantic_domains.find(
-        (sd) => sd.key === entry.senses?.[0].semantic_domains[index]
-      );
-      if (matching_domain) {
-        formatted_entry[`semantic_domain_${index + 1}`] = matching_domain.name;
-      }
-    }
-  }
-}
-
-type GlossesAllocator = {
-  formatted_entry: EntryForCSV;
-  entry: ExpandedEntry;
-  gloss_languages: string[];
-};
-export function assign_gloss_languages_to_formatted_entry(allocator: GlossesAllocator): void {
-  const { formatted_entry, entry, gloss_languages } = allocator;
-  gloss_languages.forEach((bcp) => {
-    formatted_entry[`${bcp}_gloss_language`] = entry.senses?.[0].glosses[bcp] || '';
-  });
-}
-
-export function assign_example_sentences_to_formatted_entry(allocator: GlossesAllocator): void {
-  const { formatted_entry, entry, gloss_languages } = allocator;
-  formatted_entry.vernacular_example_sentence = entry.senses?.[0].example_sentences?.[0].vn || '';
-  gloss_languages.forEach((bcp) => {
-    formatted_entry[`${bcp}_example_sentence`] =
-      entry.senses?.[0].example_sentences?.[0][bcp] || '';
-  });
 }
 
 const dictionaries_with_variant = ['babanki', 'torwali'];
