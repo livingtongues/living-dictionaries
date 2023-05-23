558fc91dc1f78a95ef80dbfb7cbccccddc436d34
Diego Córdova Nieto <diego@livingtongues.org>
2023-05-23 12:42:29 -0600

    fix friendly names media data

--- packages/site/src/lib/export/friendlyName.ts
+++ packages/site/src/lib/export/friendlyName.ts
@@ -1,10 +1,17 @@
-import type { IEntry } from '@living-dictionaries/types';
+import type { ExpandedEntry } from '@living-dictionaries/types';
 
-export function friendlyName(entry: Partial<IEntry>, path: string) {
-  const fileTypeSuffix = path.split('.').pop();
-  let gloss = entry.gl ? Object.values(entry.gl)[0] || Object.values(entry.gl)[1] || '' : '';
-  gloss = gloss.replace(/\s+/g, '_').replace(/[^a-zA-Z0-9_]+/g, '');
-  // non-ASCII characters: /[^\x20-\x7E]/g
+export function friendlyName(entry: Partial<ExpandedEntry>, path: string) {
+  if (path) {
+    const fileTypeSuffix = path.split('.').pop();
+    let gloss = entry.senses?.[0].glosses
+      ? Object.values(entry.senses?.[0].glosses)[0] ||
+        Object.values(entry.senses?.[0].glosses)[1] ||
+        ''
+      : '';
+    gloss = gloss.replace(/\s+/g, '_').replace(/[^a-zA-Z0-9_]+/g, '');
+    // non-ASCII characters: /[^\x20-\x7E]/g
 
-  return `${entry.id}_${gloss}.${fileTypeSuffix}`;
+    return `${entry.id}_${gloss}.${fileTypeSuffix}`;
+  }
+  return '';
 }
