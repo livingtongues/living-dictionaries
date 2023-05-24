9b9674852aaa089540902a52622f941922bcd728
Diego Córdova Nieto <diego@livingtongues.org>
2023-05-23 18:53:39 -0600

    pass all import tests

--- packages/scripts/import/convertJsonRowToEntryFormat.test.ts
+++ packages/scripts/import/convertJsonRowToEntryFormat.test.ts
@@ -125,150 +125,196 @@ describe('convertJsonRowToEntryFormat', () => {
     expect(entries).toEqual([
       {
         ca: 10101010,
-        di: 'Modern Parisian French',
-        gl: {
-          en: 'car',
-          es: 'auto',
-        },
+        di: ['Modern Parisian French'],
         ii: 'v4-10101010',
         lx: 'voiture',
         nt: 'small automobile',
         ph: 'vwatyʁ',
-        ps: ['n', 'v'],
-        sd: ['vehicle|cars'],
-        sdn: ['5.15', '5'],
+        sn: [
+          {
+            gl: {
+              en: 'car',
+              es: 'auto',
+            },
+            ps: ['n', 'v'],
+            sd: ['vehicle|cars'],
+            sdn: ['5.15', '5'],
+            xs: [
+              {
+                en: 'I drive my car',
+                es: 'Conduzco mi auto',
+                vn: 'Je conduis ma voiture',
+              },
+            ],
+          },
+        ],
         ua: 10101010,
-        xs: {
-          en: 'I drive my car',
-          es: 'Conduzco mi auto',
-          vn: 'Je conduis ma voiture',
-        },
       },
       {
         ca: 10101010,
-        di: 'Modern Parisian French',
-        gl: {
-          en: 'tree',
-          es: 'árbol',
-        },
+        di: ['Modern Parisian French'],
         ii: 'v4-10101010',
         lx: 'arbre',
         nt: 'generic term for all kinds of trees',
         ph: 'aʁbʁ',
-        ps: ['n', 'adj'],
         scn: ['Acer rubrum'],
-        sdn: ['1.4', '1.2'],
+        sn: [
+          {
+            gl: {
+              en: 'tree',
+              es: 'árbol',
+            },
+            ps: ['n', 'adj'],
+            sdn: ['1.4', '1.2'],
+            xs: [
+              {
+                en: 'The tree gives us shade',
+                es: 'El árbol nos da sombra',
+                vn: "L'arbre nous donne de l'ombre",
+              },
+            ],
+          },
+        ],
         ua: 10101010,
-        xs: {
-          en: 'The tree gives us shade',
-          es: 'El árbol nos da sombra',
-          vn: "L'arbre nous donne de l'ombre",
-        },
       },
       {
         ca: 10101010,
-        di: 'Modern Parisian French',
-        gl: {
-          en: 'tube',
-          es: 'tubo',
-        },
+        di: ['Modern Parisian French'],
         ii: 'v4-10101010',
         lx: 'tube',
         nt: 'a cylindrical device for liquids',
         ph: 'tyb',
         pl: 'tubes',
-        ps: ['n'],
-        sd: ['plumbing'],
-        sdn: ['5.9'],
+        sn: [
+          {
+            gl: {
+              en: 'tube',
+              es: 'tubo',
+            },
+            ps: ['n'],
+            sd: ['plumbing'],
+            sdn: ['5.9'],
+            xs: [
+              {
+                en: 'The water goes through the tubes',
+                es: 'El agua pasa a través de los tubos',
+                vn: "L'eau passe à travers les tubes",
+              },
+            ],
+          },
+        ],
         ua: 10101010,
-        xs: {
-          en: 'The water goes through the tubes',
-          es: 'El agua pasa a través de los tubos',
-          vn: "L'eau passe à travers les tubes",
-        },
       },
       {
         ca: 10101010,
-        di: 'Quebec French',
-        gl: {
-          en: 'car',
-          es: 'auto',
-        },
+        di: ['Quebec French'],
         ii: 'v4-10101010',
         lx: 'voiture',
         nt: 'small automobile',
         ph: 'vwɑtYʁ',
-        ps: ['n'],
-        sd: ['vehicle'],
-        sdn: ['5.15'],
+        sn: [
+          {
+            gl: {
+              en: 'car',
+              es: 'auto',
+            },
+            ps: ['n'],
+            sd: ['vehicle'],
+            sdn: ['5.15'],
+            xs: [
+              {
+                en: 'I drive my car',
+                es: 'Conduzco mi auto',
+                vn: 'Je conduis ma voiture',
+              },
+            ],
+          },
+        ],
         sr: ['testing sources'],
         ua: 10101010,
-        xs: {
-          en: 'I drive my car',
-          es: 'Conduzco mi auto',
-          vn: 'Je conduis ma voiture',
-        },
       },
       {
         ca: 10101010,
-        di: 'Quebec French',
-        gl: {
-          en: 'neutral',
-          es: 'neutro',
-        },
+        di: ['Quebec French'],
         ii: 'v4-10101010',
         lx: 'neutre',
         ph: 'nøʏ̯tʁ̥',
-        ps: ['adj'],
+        sn: [
+          {
+            gl: {
+              en: 'neutral',
+              es: 'neutro',
+            },
+            ps: ['adj'],
+            xs: [
+              {
+                en: 'My room is painted with a neutral color.',
+                es: 'Mi habitación está pintada con un color neutro.',
+                vn: "Ma chambre est peinte d'une couleur neutre.",
+              },
+            ],
+          },
+        ],
         ua: 10101010,
-        xs: {
-          en: 'My room is painted with a neutral color.',
-          es: 'Mi habitación está pintada con un color neutro.',
-          vn: "Ma chambre est peinte d'une couleur neutre.",
-        },
       },
       {
         ca: 10101010,
-        di: 'Quebec French',
-        gl: {
-          en: 'to celebrate',
-          es: 'celebrar',
-        },
+        di: ['Quebec French'],
         ii: 'v4-10101010',
         lx: 'fêter',
         nt: 'to have a party',
         ph: 'fɛɪ̯te',
-        ps: ['v'],
+        sn: [
+          {
+            gl: {
+              en: 'to celebrate',
+              es: 'celebrar',
+            },
+            ps: ['v'],
+            xs: [
+              {
+                en: 'We will really party tonight',
+                es: 'Vamos a celebrar esta noche',
+                vn: 'On va vraiment fêter à soir',
+              },
+            ],
+          },
+        ],
         sr: ['test source', 'with multiples sources, test', 'https://example.com'],
         ua: 10101010,
-        xs: {
-          en: 'We will really party tonight',
-          es: 'Vamos a celebrar esta noche',
-          vn: 'On va vraiment fêter à soir',
-        },
       },
       {
         ca: 10101010,
-        di: 'Central Luganda',
-        gl: {
-          en: 'I will see you',
-          es: 'Voy a verte',
-        },
+        di: ['Central Luganda'],
         ii: 'v4-10101010',
         in: '1SG-Fut-2SG-see-Fin.V',
         lx: 'njakulaba',
         mr: 'n-ja-ku-lab-a',
-        ps: ['vp'],
+        sn: [
+          {
+            gl: {
+              en: 'I will see you',
+              es: 'Voy a verte',
+            },
+            ps: ['vp'],
+            xs: [{}],
+          },
+        ],
         ua: 10101010,
       },
       {
         ca: 10101010,
-        gl: {
-          en: 'bye',
-          es: 'adiós',
-        },
         ii: 'v4-10101010',
         lx: 'vale',
+        sn: [
+          {
+            gl: {
+              en: 'bye',
+              es: 'adiós',
+            },
+            xs: [{}],
+          },
+        ],
         ua: 10101010,
       },
     ]);
