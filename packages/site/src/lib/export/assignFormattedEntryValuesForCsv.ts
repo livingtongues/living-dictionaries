4be87c08b7b0eed9d5b27158a359869d19deacf4
jacob-8 <jwrunner7@gmail.com>
2023-05-26 10:13:36 +0800

    add csv output snippet to verify ordering stays same, remove unnecessary empty string fallbacks

--- packages/site/src/lib/export/assignFormattedEntryValuesForCsv.ts
+++ packages/site/src/lib/export/assignFormattedEntryValuesForCsv.ts
@@ -1,15 +1,13 @@
 import type { ExpandedEntry, ISpeaker, IPartOfSpeech } from '@living-dictionaries/types';
 import type { EntryForCSV } from './prepareEntriesForCsv';
-import { decades } from '$lib/components/media/ages';
 
 export function find_part_of_speech_abbreviation(
   global_parts_of_speech: IPartOfSpeech[],
   part_of_speech: string
 ): string {
-  const part_of_speech_abbreviation = global_parts_of_speech.find(
-    (pos) => pos.enName === part_of_speech
-  );
-  return part_of_speech_abbreviation ? part_of_speech_abbreviation.enAbbrev : '';
+  return global_parts_of_speech.find(
+    ({ enName }) => enName === part_of_speech
+  )?.enAbbrev;
 }
 
 export function get_first_speaker_from_first_sound_file(
@@ -20,14 +18,7 @@ export function get_first_speaker_from_first_sound_file(
 }
 
 export function display_speaker_gender(speaker_gender: string): string {
-  if (speaker_gender) {
-    return speaker_gender === 'f' ? 'female' : 'male';
-  }
-  return '';
-}
-
-export function display_speaker_age_range(decade: number) {
-  return decades[decade] || '';
+  if (speaker_gender) return speaker_gender === 'f' ? 'female' : 'male';
 }
 
 export function format_local_orthographies(
@@ -36,7 +27,7 @@ export function format_local_orthographies(
 ): EntryForCSV {
   const formatted_local_orthographies = {};
   Object.keys(local_orthographies_headers).forEach((header) => {
-    formatted_local_orthographies[local_orthographies_headers[header]] = entry[header] || '';
+    formatted_local_orthographies[local_orthographies_headers[header]] = entry[header];
   });
   return formatted_local_orthographies;
 }
@@ -48,7 +39,7 @@ export function format_semantic_domains(
   const formatted_semantic_domains = {};
   for (let index = 0; index < max_semantic_domain_number; index++) {
     formatted_semantic_domains[`semantic_domain_${index + 1}`] =
-      entry.senses?.[0].semantic_domains?.[index] || '';
+      entry.senses?.[0].semantic_domains?.[index];
   }
   return formatted_semantic_domains;
 }
@@ -59,7 +50,7 @@ export function format_gloss_languages(
 ): EntryForCSV {
   const formatted_gloss_languages = {};
   gloss_languages.forEach((bcp) => {
-    formatted_gloss_languages[`${bcp}_gloss_language`] = entry.senses?.[0].glosses[bcp] || '';
+    formatted_gloss_languages[`${bcp}_gloss_language`] = entry.senses?.[0].glosses[bcp];
   });
   return formatted_gloss_languages;
 }
@@ -70,10 +61,10 @@ export function format_example_sentences(
 ): EntryForCSV {
   const formatted_example_sentences = {};
   formatted_example_sentences['vernacular_example_sentence'] =
-    entry.senses?.[0].example_sentences?.[0].vn || '';
+    entry.senses?.[0].example_sentences?.[0].vn;
   gloss_languages.forEach((bcp) => {
     formatted_example_sentences[`${bcp}_example_sentence`] =
-      entry.senses?.[0].example_sentences?.[0][bcp] || '';
+      entry.senses?.[0].example_sentences?.[0][bcp];
   });
   return formatted_example_sentences;
 }
