5316855c5777d808dd565ca8d02d6e727c39bbaf
Diego Córdova Nieto <diego@livingtongues.org>
2023-05-30 11:44:36 -0600

    allow undefined on coordinates

--- packages/site/src/lib/export/prepareDictionariesForCsv.ts
+++ packages/site/src/lib/export/prepareDictionariesForCsv.ts
@@ -57,12 +57,8 @@ export function prepareDictionariesForCsv(dictionaries: IDictionary[]): Dictiona
       iso6393: dictionary.iso6393,
       glottocode: dictionary.glottocode,
       location: cleanedLocation,
-      latitude: dictionary?.coordinates?.latitude
-        ? dictionary.coordinates.latitude?.toString()
-        : '',
-      longitude: dictionary?.coordinates?.longitude
-        ? dictionary.coordinates.longitude?.toString()
-        : '',
+      latitude: dictionary?.coordinates?.latitude?.toString(),
+      longitude: dictionary?.coordinates?.longitude?.toString(),
       thumbnail: dictionary.thumbnail,
       gloss_languages: dictionary?.glossLanguages?.join(', '),
       alternate_names: dictionary?.alternateNames?.join(', '),
