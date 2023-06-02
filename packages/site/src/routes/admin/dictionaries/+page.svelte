81a488c3ba336297d1530215cdf2da2d7f796c86
jacob-8 <jwrunner7@gmail.com>
2023-06-02 08:21:26 +0800

    fetch dictionary helper collections in script block

--- packages/site/src/routes/admin/dictionaries/+page.svelte
+++ packages/site/src/routes/admin/dictionaries/+page.svelte
@@ -1,107 +1,145 @@
 <script lang="ts">
-  import type { IDictionary } from '@living-dictionaries/types';
-  import { Collection, updateOnline } from 'sveltefirets';
+  import type { IDictionary, IHelper, IInvite } from '@living-dictionaries/types';
+  import { updateOnline, collectionStore } from 'sveltefirets';
   import { arrayRemove, arrayUnion, deleteField, GeoPoint } from 'firebase/firestore/lite';
   import { exportDictionariesAsCSV } from '$lib/export/csv';
   import Filter from '$lib/components/Filter.svelte';
   import { Button, ResponsiveTable, ShowHide } from 'svelte-pieces';
-  import DictionaryRow from '../DictionaryRow.svelte';
-  import SortDictionaries from '../SortDictionaries.svelte';
+  import DictionaryRow from './DictionaryRow.svelte';
+  import SortDictionaries from './SortDictionaries.svelte';
   import { admin } from '$lib/stores';
+  import { where } from 'firebase/firestore';
+  import type { DictionaryWithHelpers } from './dictionaryWithHelpers';
+  import IntersectionObserverShared from './IntersectionObserverShared.svelte';
 
-  let dictionariesType: IDictionary[] = [];
+  const dictionaries = collectionStore<IDictionary>('dictionaries', [], {
+    startWith: [],
+    log: true,
+  });
+
+  let dictionariesAndHelpers: DictionaryWithHelpers[] = [];
+  $: dictionariesAndHelpers = $dictionaries.map((dictionary) => {
+    return {
+      ...dictionary,
+      managers: collectionStore<IHelper>(`dictionaries/${dictionary.id}/managers`, [], {
+        startWith: [],
+        log: true,
+      }),
+      contributors: collectionStore<IHelper>(`dictionaries/${dictionary.id}/contributors`, [], {
+        startWith: [],
+        log: true,
+      }),
+      writeInCollaborators: collectionStore<IHelper>(
+        `dictionaries/${dictionary.id}/writeInCollaborators`,
+        [],
+        {
+          startWith: [],
+          log: true,
+        }
+      ),
+      invites: collectionStore<IInvite>(
+        `dictionaries/${dictionary.id}/invites`,
+        [where('status', 'in', ['queued', 'sent'])],
+        {
+          startWith: [],
+          log: true,
+        }
+      ),
+    };
+  });
 </script>
 
-<ShowHide let:show let:toggle>
-  <div class="mb-2 text-xs text-gray-600">
-    Changed data autosaves after 2 seconds. Green cells = data that's not saved. Use "tab" to
-    quickly move between cells.
-  </div>
+<div class="mb-2 text-xs text-gray-600">
+  Changed data autosaves after 2 seconds. Green cells = data that's not saved. Use "tab" to quickly
+  move between cells.
+</div>
 
-  <Collection path={'dictionaries'} startWith={dictionariesType} let:data={dictionaries}>
-    <div class="sticky top-0 h-[calc(100vh-1.5rem)] z-2 relative flex flex-col">
-      <Filter
-        items={dictionaries}
-        let:filteredItems={filteredDictionaries}
-        placeholder="Search dictionaries">
-        <div slot="right">
-          <Button form="filled" onclick={toggle}>Show {show ? 30 : 'All'}</Button>
-          <Button
-            form="filled"
-            color="black"
-            onclick={() =>
-              exportDictionariesAsCSV(filteredDictionaries, 'living-dictionaries-list', $admin)}>
-            <i class="fas fa-download mr-1" />
-            Download {filteredDictionaries.length} Dictionaries as CSV
-          </Button>
-        </div>
-        <div class="mb-1" />
-        <ResponsiveTable stickyHeading stickyColumn>
-          <SortDictionaries dictionaries={filteredDictionaries} let:sortedDictionaries>
-            {#each sortedDictionaries as dictionary, index (dictionary.id)}
-              {#if index < 30 || show}
-                <DictionaryRow
-                  {dictionary}
-                  on:toggleprivacy={() => {
-                    try {
-                      updateOnline(`dictionaries/${dictionary.id}`, {
-                        public: !dictionary.public,
-                      });
-                    } catch (err) {
-                      alert(err);
-                    }
-                  }}
-                  on:togglevideoaccess={() => {
-                    try {
-                      updateOnline(`dictionaries/${dictionary.id}`, {
-                        videoAccess: !dictionary.videoAccess,
-                      });
-                    } catch (err) {
-                      alert(err);
-                    }
-                  }}
-                  on:addalternatename={(event) => {
-                    try {
-                      updateOnline(`dictionaries/${dictionary.id}`, {
-                        alternateNames: arrayUnion(event.detail),
-                      });
-                    } catch (err) {
-                      alert(err);
-                    }
-                  }}
-                  on:removealternatename={(event) => {
-                    try {
-                      updateOnline(`dictionaries/${dictionary.id}`, {
-                        alternateNames: arrayRemove(event.detail),
-                      });
-                    } catch (err) {
-                      alert(err);
-                    }
-                  }}
-                  on:updatecoordinates={({ detail: { lat, lng } }) => {
-                    try {
-                      const location = new GeoPoint(lat, lng);
-                      updateOnline(`dictionaries/${dictionary.id}`, {
-                        coordinates: location,
-                      });
-                    } catch (err) {
-                      alert(err);
-                    }
-                  }}
-                  on:removecoordinates={() => {
-                    try {
-                      updateOnline(`dictionaries/${dictionary.id}`, {
-                        coordinates: deleteField(),
-                      });
-                    } catch (err) {
-                      alert(err);
-                    }
-                  }} />
-              {/if}
-            {/each}
-          </SortDictionaries>
-        </ResponsiveTable>
-      </Filter>
+<div class="sticky top-0 h-[calc(100vh-1.5rem)] z-2 relative flex flex-col">
+  <Filter
+    items={dictionariesAndHelpers}
+    let:filteredItems={filteredDictionaries}
+    placeholder="Search dictionaries">
+    <div slot="right">
+      <Button
+        form="filled"
+        color="black"
+        onclick={() =>
+          exportDictionariesAsCSV(filteredDictionaries, 'living-dictionaries-list', $admin)}>
+        <i class="fas fa-download mr-1" />
+        Download {filteredDictionaries.length} Dictionaries as CSV
+      </Button>
     </div>
-  </Collection>
-</ShowHide>
+    <div class="mb-1" />
+    <ResponsiveTable stickyHeading stickyColumn>
+      <SortDictionaries dictionaries={filteredDictionaries} let:sortedDictionaries>
+        {#each sortedDictionaries as dictionary (dictionary.id)}
+          <IntersectionObserverShared bottom={2000} let:intersecting once>
+            {#if intersecting}
+              <DictionaryRow
+                {dictionary}
+                on:toggleprivacy={() => {
+                  try {
+                    updateOnline(`dictionaries/${dictionary.id}`, {
+                      public: !dictionary.public,
+                    });
+                  } catch (err) {
+                    alert(err);
+                  }
+                }}
+                on:togglevideoaccess={() => {
+                  try {
+                    updateOnline(`dictionaries/${dictionary.id}`, {
+                      videoAccess: !dictionary.videoAccess,
+                    });
+                  } catch (err) {
+                    alert(err);
+                  }
+                }}
+                on:addalternatename={(event) => {
+                  try {
+                    updateOnline(`dictionaries/${dictionary.id}`, {
+                      alternateNames: arrayUnion(event.detail),
+                    });
+                  } catch (err) {
+                    alert(err);
+                  }
+                }}
+                on:removealternatename={(event) => {
+                  try {
+                    updateOnline(`dictionaries/${dictionary.id}`, {
+                      alternateNames: arrayRemove(event.detail),
+                    });
+                  } catch (err) {
+                    alert(err);
+                  }
+                }}
+                on:updatecoordinates={({ detail: { lat, lng } }) => {
+                  try {
+                    const location = new GeoPoint(lat, lng);
+                    updateOnline(`dictionaries/${dictionary.id}`, {
+                      coordinates: location,
+                    });
+                  } catch (err) {
+                    alert(err);
+                  }
+                }}
+                on:removecoordinates={() => {
+                  try {
+                    updateOnline(`dictionaries/${dictionary.id}`, {
+                      coordinates: deleteField(),
+                    });
+                  } catch (err) {
+                    alert(err);
+                  }
+                }} />
+            {:else}
+              <tr>
+                <td colspan="30"> Loading... </td>
+              </tr>
+            {/if}
+          </IntersectionObserverShared>
+        {/each}
+      </SortDictionaries>
+    </ResponsiveTable>
+  </Filter>
+</div>
