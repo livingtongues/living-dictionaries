022c479b25c3694c83c9aa14deba18a07fcc287e
jacob-8 <jwrunner7@gmail.com>
2023-06-09 13:51:48 +0800

    update to @unocss/svelte-scoped

--- packages/site/svelte.config.js
+++ packages/site/svelte.config.js
@@ -1,13 +1,10 @@
-import preprocess from 'svelte-preprocess';
+import { vitePreprocess } from '@sveltejs/kit/vite'
 import adapter from '@sveltejs/adapter-auto';
-
-import UnoCSS from 'temp-s-p-u';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: [
-    preprocess(),
-    UnoCSS({ options: { classPrefix: 'ld-' } }),
+    vitePreprocess(),
   ],
 
   kit: {
--- packages/site/uno.config.ts
+++ packages/site/uno.config.ts
@@ -1,10 +1,12 @@
 // Regardless of whether config is placed here or inlined in svelte.config.js, the unocss.config.ts file required to make the UnoCSS VSCode extension work (v.0.45.26)
-import { defineConfig, presetIcons, presetUno } from 'unocss'
-// import { presetForms } from '@julr/unocss-preset-forms'
+import { defineConfig, presetIcons, presetTypography, presetUno } from 'unocss'
+import { presetForms } from '@julr/unocss-preset-forms'
 
 export default defineConfig({
   presets: [
     presetUno(),
+    presetForms(),
+    presetTypography(),
     presetIcons({
       prefix: 'i-',
       extraProperties: {
@@ -12,11 +14,13 @@ export default defineConfig({
         'vertical-align': 'middle',
       },
     }),
-    // presetForms(),
   ],
   theme: {
     screens: {
       print: { raw: 'print' },
     },
   },
+  shortcuts: [
+    { 'form-input': 'border-gray-300 rounded-md focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50' },
+  ],
 })
