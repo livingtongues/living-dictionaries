b5d05c54429a06e8bab56675b16c89060e4c817b
jacob-8 <jwrunner7@gmail.com>
2023-06-12 07:45:39 +0800

    first half of moving parts to latest uno, use newer eslint

--- packages/parts/svelte.config.js
+++ packages/parts/svelte.config.js
@@ -1,24 +1,28 @@
 import adapter from '@sveltejs/adapter-auto';
-import preprocess from 'svelte-preprocess';
+import { vitePreprocess } from '@sveltejs/kit/vite'
 
 import { mdsvex } from 'mdsvex';
 import mdsvexConfig from './mdsvex.config.js';
 
-import UnoCSS from 'temp-s-p-u';
+import UnoCSS from '@unocss/svelte-scoped/preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   extensions: ['.svelte', ...mdsvexConfig.extensions],
   preprocess: [
-    preprocess(),
+    vitePreprocess(),
     mdsvex(mdsvexConfig),
-    UnoCSS({ options: { classPrefix: 'ldp-' } }),
+    UnoCSS({
+      options: {
+        classPrefix: 'ldp-',
+      }
+    }),
   ],
 
   kit: {
     adapter: adapter(),
   },
-  
+
   onwarn: (warning, handler) => {
     if (warning.code.startsWith('a11y-')) {
       return;
@@ -27,12 +31,12 @@ const config = {
   },
 
   vitePlugin: {
-		experimental: {
-			inspector: {
-				holdMode: true,
-			}
-		}
-	}
+    experimental: {
+      inspector: {
+        holdMode: true,
+      }
+    }
+  }
 };
 
 export default config;
