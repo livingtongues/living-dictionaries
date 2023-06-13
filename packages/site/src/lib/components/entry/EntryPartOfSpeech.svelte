3c370dd95e3485b55cc359f0ebbe665ca0b58305
jacob-8 <jwrunner7@gmail.com>
2023-06-13 10:32:35 +0800

    add to table

--- packages/site/src/lib/components/entry/EntryPartOfSpeech.svelte
+++ packages/site/src/lib/components/entry/EntryPartOfSpeech.svelte
@@ -20,7 +20,7 @@
 
   const dispatch = createEventDispatcher<{
     valueupdate: {
-      field: 'ps';
+      field: "ps";
       newValue: string[];
     };
   }>();
@@ -30,64 +30,58 @@
   };
 </script>
 
-{#if value?.length || canEdit}
-  <div class="md:px-2" class:order-2={!value.length}>
-    <div class="rounded text-xs text-gray-500 mt-1 mb-2">{$t("entry.ps")}</div>
-    {#if canEdit}
-      <ReactiveSet
-        input={value}
-        let:value={editedParts}
-        let:add
-        let:remove
-        on:modified={({ detail: newValue }) => {
-          dispatch("valueupdate", {
-            field: "ps",
-            newValue,
-          });
+{#if canEdit}
+  <ReactiveSet
+    input={value}
+    let:value={editedParts}
+    let:add
+    let:remove
+    on:modified={({ detail: newValue }) => {
+      dispatch("valueupdate", {
+        field: "ps",
+        newValue,
+      });
+    }}
+  >
+    <ShowHide let:show let:toggle>
+      <BadgeArrayEmit
+        strings={translateValues(editedParts)}
+        addMessage={$t("misc.add", { default: "Add" })}
+        canEdit
+        on:itemremoved={({ detail: { index } }) => {
+          remove(editedParts[index]);
         }}
-      >
-        <ShowHide let:show let:toggle>
-          <BadgeArrayEmit
-            strings={translateValues(editedParts)}
-            addMessage={$t("misc.add", { default: "Add" })}
-            canEdit
-            on:itemremoved={({ detail: { index } }) => {
-              remove(editedParts[index]);
+        on:additem={toggle}
+      />
+      {#if show}
+        <Modal noscroll on:close={toggle}>
+          <span slot="heading"
+            >{$t("entry.ps", { default: "Part of Speech" })}</span
+          >
+          <DataList
+            type="search"
+            class="form-input w-full leading-none"
+            resetOnSelect
+            on:selected={(e) => {
+              add(e.detail.value);
+              toggle();
             }}
-            on:additem={toggle}
-          />
-          {#if show}
-            <Modal noscroll on:close={toggle}>
-              <span slot="heading"
-                >{$t("entry.ps", { default: "Part of Speech" })}</span
+          >
+            {#if mayanDictionaries.includes(dictionaryId)}
+              {#each mayanPOS as pos}
+                <option data-value={pos}>{pos}</option>
+              {/each}
+            {/if}
+            {#each partsOfSpeech as pos}
+              <option data-value={pos.enAbbrev}
+                >{$t("ps." + pos.enAbbrev, { default: pos.enName })}</option
               >
-              <DataList
-                type="search"
-                class="form-input w-full leading-none"
-                resetOnSelect
-                on:selected={(e) => {
-                  add(e.detail.value);
-                  toggle();
-                }}
-              >
-                {#if mayanDictionaries.includes(dictionaryId)}
-                  {#each mayanPOS as pos}
-                    <option data-value={pos}>{pos}</option>
-                  {/each}
-                {/if}
-                {#each partsOfSpeech as pos}
-                  <option data-value={pos.enAbbrev}
-                    >{$t("ps." + pos.enAbbrev, { default: pos.enName })}</option
-                  >
-                {/each}
-              </DataList>
-            </Modal>
-          {/if}
-        </ShowHide>
-      </ReactiveSet>
-    {:else}
-      <BadgeArrayEmit strings={value} />
-    {/if}
-    <div class="border-b-2 pb-1 mb-2" />
-  </div>
+            {/each}
+          </DataList>
+        </Modal>
+      {/if}
+    </ShowHide>
+  </ReactiveSet>
+{:else}
+  <BadgeArrayEmit strings={value} />
 {/if}
