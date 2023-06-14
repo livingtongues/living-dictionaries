41351a0849b1a7ac0d4644162991076abb19026f
Diego Córdova Nieto <diego@livingtongues.org>
2023-06-14 14:11:13 -0600

    after knowing entry count is less than one, confirm that words collection is an empty array

--- packages/site/src/lib/components/settings/DeleteDictionary.svelte
+++ packages/site/src/lib/components/settings/DeleteDictionary.svelte
@@ -2,16 +2,22 @@
   import { t } from 'svelte-i18n';
   import { Button } from 'svelte-pieces';
   import type { IDictionary } from '@living-dictionaries/types';
+  import { getCollection } from 'sveltefirets';
 
   export let dictionary: IDictionary;
 
-  function delete_dictionary_if_empty(dictionary: IDictionary) {
-    if (dictionary.entryCount === 0) {
-      const confirm = prompt('If you are sure about this, type the dictionary ID to confirm the complete deletion');
-      if (confirm === dictionary.id) {
-        alert('Deleted');
+  async function delete_dictionary_if_empty(dictionary: IDictionary) {
+    if (dictionary.entryCount < 1) {
+      const words_collection = await getCollection(`dictionaries/${dictionary.id}/words`);
+      if (words_collection.length === 0) {
+        const confirm = prompt('If you are sure about this, type the dictionary ID to confirm the complete deletion');
+        if (confirm === dictionary.id) {
+          alert('Deleted');
+        } else {
+          alert('Sorry the dictionary ID does not match what you typed');
+        }
       } else {
-        alert('Sorry the dictionary ID does not match what you typed');
+        alert('You must delete every entry in the dictionary first');
       }
     } else {
       alert('You must delete every entry in the dictionary first');
