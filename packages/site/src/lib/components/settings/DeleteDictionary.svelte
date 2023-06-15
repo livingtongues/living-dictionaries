26147d30aada368e16ca527173e0ce0dc2aefe9f
Diego Córdova Nieto <diego@livingtongues.org>
2023-06-15 17:00:18 -0600

    create removeDictionary function

--- packages/site/src/lib/components/settings/DeleteDictionary.svelte
+++ packages/site/src/lib/components/settings/DeleteDictionary.svelte
@@ -3,6 +3,7 @@
   import { Button } from 'svelte-pieces';
   import type { IDictionary } from '@living-dictionaries/types';
   import { getCollection } from 'sveltefirets';
+  import { removeDictionary } from '$lib/helpers/dictionariesManaging';
 
   export let dictionary: IDictionary;
 
@@ -10,12 +11,7 @@
     if (!dictionary?.entryCount || dictionary?.entryCount < 1) {
       const words_collection = await getCollection(`dictionaries/${dictionary.id}/words`);
       if (words_collection.length === 0) {
-        const confirm = prompt('If you are sure about this, type the dictionary ID to confirm the complete deletion');
-        if (confirm === dictionary.id) {
-          alert('Deleted');
-        } else if (confirm?.length > 0) {
-          alert('Sorry the dictionary ID does not match what you typed');
-        }
+        removeDictionary(dictionary.id);
       } else {
         alert('You must delete every entry in the dictionary first');
       }
