460e4b6b4b3a280c82b2b21f65cc6b8597e56c3a
Diego Córdova Nieto <diego@livingtongues.org>
2023-07-03 18:28:58 -0600

    improve subjects for i18n translations support

---
+++ packages/site/src/lib/components/modals/subjects.ts
@@ -0,0 +1,19 @@
+//This probably should disappear, instead create a new file that automatically updates when running updateLocales 
+export const subjects = [
+  {
+    keyName: 'delete-dictionary',
+    title: 'Delete dicionary'
+  },
+  {
+    keyName: 'batch-import',
+    title: 'Information about batch import'
+  },
+  {
+    keyName: 'data-fields',
+    title: 'Optional data fields'
+  },
+  {
+    keyName: 'other',
+    title: 'Other'
+  },
+]
