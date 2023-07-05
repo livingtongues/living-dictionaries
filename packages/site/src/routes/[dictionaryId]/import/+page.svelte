538cfb31c2091eec413e888055ec18cb994bdcfd
Diego Córdova Nieto <diego@livingtongues.org>
2023-07-05 10:02:19 -0600

    add text written by Anna

--- packages/site/src/routes/[dictionaryId]/import/+page.svelte
+++ packages/site/src/routes/[dictionaryId]/import/+page.svelte
@@ -20,10 +20,14 @@
     </h3>
 
     <p class="mb-3">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus convallis erat eu diam consectetur, vel bibendum ipsum mattis. Proin sed neque pellentesque leo scelerisque tempor in iaculis eros. Phasellus id urna quis tortor finibus feugiat. Nam vitae nisl hendrerit, porttitor enim quis, malesuada risus. Sed eu cursus nibh, at euismod orci. Pellentesque iaculis massa non ante sodales bibendum. Etiam ultricies ante eget bibendum tincidunt. Nunc fermentum porta purus. Nullam nec eros condimentum, ornare neque sed, aliquet erat. Sed vulputate quam ac venenatis volutpat. Aenean eget erat finibus, condimentum nibh quis, vestibulum sem. Proin dapibus laoreet fringilla. Proin vulputate leo lacus, quis dapibus ex bibendum finibus. Nunc sodales arcu nec dignissim scelerisque.
+      If you already have linguistic data that you want to import into your Living Dictionary, please follow the steps below.
     </p>
+    <ol class="py-3 px-5">
+      <li class="list-decimal">Click on the blue “Link to Template” button below. Download the template as an Excel file, or create a copy on Google Docs. Edit the template according to your project’s needs. Copy-paste your data into the spreadsheet.</li>
+      <li class="list-decimal">Use the “Contact Us” below to contact us. Let us know that you have a new spreadsheet that is ready for import. Ask us any questions you have. We will respond to you by email, and then you can send us your file.</li>
+    </ol>
     <p class="mb-3">
-      Vestibulum suscipit, orci et convallis pharetra, ex augue eleifend sem, sit amet viverra nisl elit nec nulla. Proin vulputate laoreet vestibulum. Nulla malesuada massa in ligula luctus ultrices. Aliquam sagittis sapien eget ante sodales suscipit. Nulla a neque eget mauris posuere tincidunt sed sed diam. Vestibulum sit amet lobortis justo. Vestibulum at ex a libero eleifend ullamcorper nec at leo. Etiam ac mattis nisl. Praesent vulputate cursus blandit. Praesent pharetra congue ex, in volutpat purus feugiat ac. Duis id sodales lacus. Vestibulum egestas rhoncus auctor. Nunc sed ante ex. Nulla sollicitudin cursus vestibulum.
+      To send us data exported from dictionary softwares like FLEx, Lexique Pro, Shoebox, please use the “Contact Us” button below and tell us the details of your project.
     </p>
   </div>
   <div class="flex justify-between">
