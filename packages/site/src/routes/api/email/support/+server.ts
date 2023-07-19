a600f4103e162fadbea63bc437867a58addc64ed
Diego Córdova Nieto <diego@livingtongues.org>
2023-07-18 18:20:09 -0600

    simplify POST request handler

--- packages/site/src/routes/api/email/support/+server.ts
+++ packages/site/src/routes/api/email/support/+server.ts
@@ -17,7 +17,7 @@ export const POST: RequestHandler = async ({ request, fetch }) => {
   const { email, message, name, url, subject, to } = await request.json() as SupportRequestBody;
   const recipients: Address[] = getSupportMessageRecipients({ dev })
   const emailParts: EmailParts = {
-    to: to ? to.concat(recipients) : recipients,
+    to: recipients.concat(to),
     reply_to: { email },
     subject: subject || 'Living Dictionaries Support Request',
     type: 'text/plain',
