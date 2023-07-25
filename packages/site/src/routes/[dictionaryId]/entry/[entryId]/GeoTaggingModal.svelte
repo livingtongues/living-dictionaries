d0ec7f386742549afea456f379aa57b5a65c0529
Diego Córdova Nieto <diego@livingtongues.org>
2023-07-25 15:47:05 -0600

    add modal buttons

--- packages/site/src/routes/[dictionaryId]/entry/[entryId]/GeoTaggingModal.svelte
+++ packages/site/src/routes/[dictionaryId]/entry/[entryId]/GeoTaggingModal.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
-  import { Modal } from 'svelte-pieces';
+  import { Modal, Button } from 'svelte-pieces';
   import Map from '@living-dictionaries/parts/src/lib/maps/mapbox/map/Map.svelte';
-  // import { createEventDispatcher } from 'svelte';
   import type { Readable } from 'svelte/store';
-  import { onMount } from 'svelte';
+  import { onMount, createEventDispatcher } from 'svelte';
   export let t: Readable<any> = undefined;
 
   let centerLng:number;
   let centerLat:number;
+  export let canRemove = true;
 
   onMount(() => {
     if (navigator.geolocation) {
@@ -17,10 +17,32 @@
       });
     }
   });
+
+  const dispatch = createEventDispatcher<{
+    remove: boolean;
+    close: boolean;
+  }>();
+  function remove() {
+    dispatch('remove');
+    dispatch('close');
+  }
 </script>
 
 <Modal on:close noscroll>
-  <form style="height: 50vh;">
+  <form on:submit|preventDefault style="height: 50vh;">
     <Map lng={centerLng} lat={centerLat} />
   </form>
+  <div class="modal-footer">
+    <Button onclick={() => dispatch('close')} form="simple" color="black">
+      {t ? $t('misc.cancel') : 'Cancel'}
+    </Button>
+    {#if canRemove}
+      <Button onclick={remove} form="simple" color="red">
+        {t ? $t('misc.remove') : 'Remove'}
+      </Button>
+    {/if}
+    <Button type="submit" form="filled">
+      {t ? $t('misc.save') : 'Save'}
+    </Button>
+  </div>
 </Modal>
