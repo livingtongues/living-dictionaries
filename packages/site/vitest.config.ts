d5c42a95540b3d459d65236ad57e5b8b95e517f6
jacob-8 <jwrunner7@gmail.com>
2023-07-25 08:19:33 +0800

    pull vitest config into own file

---
+++ packages/site/vitest.config.ts
@@ -0,0 +1,26 @@
+import { defineProject } from 'vitest/config'
+import path from 'node:path'
+// import { resolve } from 'node:path'
+// import { fileURLToPath } from 'node:url'
+// const projectDir = fileURLToPath(new URL('.', import.meta.url))
+
+// import { svelte } from '@sveltejs/vite-plugin-svelte';
+
+export default defineProject({
+  test: {
+    alias: {
+      $lib: path.join(__dirname, './src/lib'),
+      // $lib: resolve(projectDir, './src/lib'),
+      // $lib: new URL('./src/lib', import.meta.url).pathname,
+    },
+    name: 'site:unit',
+    globals: true,
+    includeSource: ['src/**/*.ts'],
+    // plugins: [
+    //   svelte({
+    //     hot: false,
+    //     configFile: path.join(__dirname, 'svelte.config.js')
+    //   })
+    // ],
+  },
+})
