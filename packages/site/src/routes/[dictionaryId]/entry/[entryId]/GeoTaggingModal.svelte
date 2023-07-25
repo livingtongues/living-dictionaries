ecb28b7eb363b1e4cb6d465c14c53625070783b5
Diego Córdova Nieto <diego@livingtongues.org>
2023-07-25 15:18:34 -0600

    add automatic coordinates

--- packages/site/src/routes/[dictionaryId]/entry/[entryId]/GeoTaggingModal.svelte
+++ packages/site/src/routes/[dictionaryId]/entry/[entryId]/GeoTaggingModal.svelte
@@ -1,12 +1,26 @@
 <script lang="ts">
-  // import { t } from 'svelte-i18n';
   import { Modal } from 'svelte-pieces';
   import Map from '@living-dictionaries/parts/src/lib/maps/mapbox/map/Map.svelte';
-// import { createEventDispatcher } from 'svelte';
+  // import { createEventDispatcher } from 'svelte';
+  import type { Readable } from 'svelte/store';
+  import { onMount } from 'svelte';
+  export let t: Readable<any> = undefined;
+
+  let centerLng:number;
+  let centerLat:number;
+
+  onMount(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        centerLng = position.coords.longitude;
+        centerLat = position.coords.latitude;
+      });
+    }
+  });
 </script>
 
 <Modal on:close noscroll>
   <form style="height: 50vh;">
-    <Map />
+    <Map lng={centerLng} lat={centerLat} />
   </form>
 </Modal>
