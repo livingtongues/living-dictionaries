a384d048010de8f7cdc3737d30a908fdf8035512
Diego Córdova Nieto <diego@livingtongues.org>
2023-07-31 20:18:43 -0600

    allow adding one marker

--- packages/site/src/routes/[dictionaryId]/entry/[entryId]/GeoTaggingModal.svelte
+++ packages/site/src/routes/[dictionaryId]/entry/[entryId]/GeoTaggingModal.svelte
@@ -3,6 +3,7 @@
   import Map from '@living-dictionaries/parts/src/lib/maps/mapbox/map/Map.svelte';
   import Geocoder from '@living-dictionaries/parts/src/lib/maps/mapbox/geocoder/Geocoder.svelte';
   import NavigationControl from '@living-dictionaries/parts/src/lib/maps/mapbox/controls/NavigationControl.svelte';
+  import Marker from '@living-dictionaries/parts/src/lib/maps/mapbox/map/Marker.svelte';
   import type { Readable } from 'svelte/store';
   import type { IEntry } from '@living-dictionaries/types';
   import { onMount, createEventDispatcher } from 'svelte';
@@ -22,11 +23,31 @@
   let pitch = 0;
   let bearing = 0;
   const high_density = true
+  let marker_lng:number;
+  let marker_lat:number;
+  // let color:string = 'black'
   export let canRemove = true;
+
+  function add_marker_to_static_image() {
+    return `pin-s+000000(${marker_lng},${marker_lat})`;
+  }
+
+  function setMarker(longitude: number, latitude: number) {
+    if (!(longitude && latitude)) return;
+    if (longitude < -180 || longitude > 180 || latitude < -90 || latitude > 90)
+      return;
+
+    marker_lng = +longitude.toFixed(4);
+    marker_lat = +latitude.toFixed(4);
+  }
 
   async function saveStaticImage() {
     await updateOnline<IEntry>(`dictionaries/${$dictionary.id}/words/${entry.id}`, { gt: static_image_link }, { abbreviate: true })
   }
+
+  $: if (marker_lat && marker_lng)
+    overlay = add_marker_to_static_image();
+
 
   $: static_image_link = `https://api.mapbox.com/styles/v1/${username}/${style_id}/static/${overlay ? overlay + '/' : ''}${lng},${lat},${zoom},${bearing},${pitch}/${width}x${height}${high_density ? '@2x' : ''}?access_token=`;
 
@@ -55,13 +76,21 @@
       <Map {lng} {lat}
         on:pitchend={({ detail }) => ([pitch, bearing] = detail)}
         on:dragend={({ detail }) => ({ lng, lat } = detail)}
-        on:zoomend={({ detail }) => zoom = detail}>
+        on:zoomend={({ detail }) => zoom = detail}
+        on:click={({ detail }) => setMarker(detail.lng, detail.lat)}>
         <NavigationControl />
         <Geocoder
           options={{ marker: false }}
           placeholder={t ? $t('about.search') : 'Search'}
           on:result={({ detail }) => ([lng, lat] = detail.center)}
           on:error={(e) => console.error(e.detail)} />
+        {#if marker_lng && marker_lat}
+          <Marker
+            draggable
+            on:dragend={({ detail }) => setMarker(detail.lng, detail.lat)}
+            lng={marker_lng}
+            lat={marker_lat} />
+        {/if}
       </Map>
     </div>
 
