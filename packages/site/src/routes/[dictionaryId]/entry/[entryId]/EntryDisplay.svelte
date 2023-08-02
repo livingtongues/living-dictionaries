7be44ed803a047149547d4c02fd14752b86044f7
jacob-8 <jwrunner7@gmail.com>
2023-08-02 08:26:41 +0800

    don't show sense denotation until ready to use

--- packages/site/src/routes/[dictionaryId]/entry/[entryId]/EntryDisplay.svelte
+++ packages/site/src/routes/[dictionaryId]/entry/[entryId]/EntryDisplay.svelte
@@ -6,7 +6,8 @@
   import EntrySemanticDomains from './EntrySemanticDomains.svelte';
   import { createEventDispatcher } from 'svelte';
   import EntryDialect from '$lib/components/entry/EntryDialect.svelte';
-  import { BadgeArray, Button } from 'svelte-pieces';
+  // import { BadgeArray, Button } from 'svelte-pieces';
+  import { BadgeArray } from 'svelte-pieces';
   import { order_entry_and_dictionary_gloss_languages } from '$lib/helpers/glosses';
   import { DICTIONARIES_WITH_VARIANTS } from '$lib/constants';
   import EntryMedia from './EntryMedia.svelte';
@@ -59,93 +60,94 @@
 
     <EntryField value={entry.phonetic} field="ph" {canEdit} display={$t('entry.ph')} on:valueupdate />
 
-    {#each entry.senses as sense, index}
+    {#each entry.senses as sense}
+      <!-- {#each entry.senses as sense, index} -->
       {@const glossingLanguages = order_entry_and_dictionary_gloss_languages(sense.glosses, dictionary.glossLanguages)}
-      <div class="p-2 hover:bg-gray-50 rounded">
+      <!-- <div class="p-2 hover:bg-gray-50 rounded"> -->
 
-        {#if entry.senses.length > 1 || canEdit}
-          <div class="font-semibold mb-2 flex">
-            <div class="font-semibold">
-              Sense {index + 1}
-            </div>
-            <div class="mx-auto" />
-            {#if canEdit}
-              {#if index > 0}
-                <Button size="sm" form="menu" onclick={() => alert('Re-ordering not ready yet.')}><span class="i-fa-chevron-up -mt-1" /></Button>
-              {/if}
-              {#if index < entry.senses.length - 1}
-                <Button size="sm" form="menu" onclick={() => alert('Re-ordering not ready yet.')}><span class="i-fa-chevron-down -mt-1" /></Button>
-              {/if}
-              {#if entry.senses.length > 1}
-                <Button size="sm" form="menu" onclick={() => alert('Delete sense feature not ready yet.')}><span class="i-fa-solid-times -mt-1" /></Button>
-              {/if}
-              <Button size="sm" form="menu" onclick={() => alert('Ability to add additional senses coming soon.')}><span class="i-fa-solid-plus -mt-1" /></Button>
-            {/if}
+      <!-- {#if entry.senses.length > 1 || canEdit}
+        <div class="font-semibold mb-2 flex">
+          <div class="font-semibold">
+            Sense {index + 1}
           </div>
-        {/if}
-
-        <div class="flex flex-col border-l-2">
-          {#each glossingLanguages as bcp}
-            <EntryField
-              value={sense.glosses?.[bcp]}
-              field={`gl.${bcp}`}
-              {canEdit}
-              display={`${$t(`gl.${bcp}`)}: ${$t('entry.gloss', {
-                default: 'Gloss',
-              })}`}
-              on:valueupdate />
-          {/each}
-
-          <!-- Only in Bahasa Lani (id: jaRhn6MAZim4Blvr1iEv) -->
-          {#if sense.definition_english}
-            <EntryField
-              value={sense.definition_english}
-              field="de"
-              {canEdit}
-              display="Definition (deprecated)"
-              on:valueupdate />
+          <div class="mx-auto" />
+          {#if canEdit}
+            {#if index > 0}
+              <Button size="sm" form="menu" onclick={() => alert('Re-ordering not ready yet.')}><span class="i-fa-chevron-up -mt-1" /></Button>
+            {/if}
+            {#if index < entry.senses.length - 1}
+              <Button size="sm" form="menu" onclick={() => alert('Re-ordering not ready yet.')}><span class="i-fa-chevron-down -mt-1" /></Button>
+            {/if}
+            {#if entry.senses.length > 1}
+              <Button size="sm" form="menu" onclick={() => alert('Delete sense feature not ready yet.')}><span class="i-fa-solid-times -mt-1" /></Button>
+            {/if}
+            <Button size="sm" form="menu" onclick={() => alert('Ability to add additional senses coming soon.')}><span class="i-fa-solid-plus -mt-1" /></Button>
           {/if}
-
-          {#if sense.translated_parts_of_speech?.length || canEdit}
-            <div class="md:px-2" class:order-2={!sense.translated_parts_of_speech?.length}>
-              <div class="rounded text-xs text-gray-500 mt-1 mb-2">{$t('entry.ps')}</div>
-              <EntryPartOfSpeech value={sense.translated_parts_of_speech} {canEdit} on:valueupdate />
-              <div class="border-b-2 pb-1 mb-2" />
-            </div>
-          {/if}
-
-          <EntrySemanticDomains {sense} {canEdit}
-            on:update={({detail}) => dispatch('valueupdate', { field: 'sdn', newValue: detail })}
-            on:removeCustomDomain={() => dispatch('valueupdate', { field: 'sd', newValue: null })} />
-
-          <EntryField
-            value={sense.noun_class}
-            field="nc"
-            {canEdit}
-            display={$t('entry.nc')}
-            on:valueupdate />
-
-          {#each sense.example_sentences || [{}] as sentence}
-            <EntryField
-              value={sentence.vn}
-              field="xs.vn"
-              {canEdit}
-              display={$t('entry.example_sentence', { default: 'Example Sentence' })}
-              on:valueupdate />
-
-            {#each glossingLanguages as bcp}
-              <EntryField
-                value={sentence[bcp]}
-                field={`xs.${bcp}`}
-                {canEdit}
-                display={`${$t(`gl.${bcp}`)}: ${$t('entry.example_sentence', {
-                  default: 'Example Sentence',
-                })}`}
-                on:valueupdate />
-            {/each}
-          {/each}
         </div>
-      </div>
+      {/if} -->
+
+      <!-- <div class="flex flex-col border-l-2"> -->
+      {#each glossingLanguages as bcp}
+        <EntryField
+          value={sense.glosses?.[bcp]}
+          field={`gl.${bcp}`}
+          {canEdit}
+          display={`${$t(`gl.${bcp}`)}: ${$t('entry.gloss', {
+            default: 'Gloss',
+          })}`}
+          on:valueupdate />
+      {/each}
+
+      <!-- Only in Bahasa Lani (id: jaRhn6MAZim4Blvr1iEv) -->
+      {#if sense.definition_english}
+        <EntryField
+          value={sense.definition_english}
+          field="de"
+          {canEdit}
+          display="Definition (deprecated)"
+          on:valueupdate />
+      {/if}
+
+      {#if sense.translated_parts_of_speech?.length || canEdit}
+        <div class="md:px-2" class:order-2={!sense.translated_parts_of_speech?.length}>
+          <div class="rounded text-xs text-gray-500 mt-1 mb-2">{$t('entry.ps')}</div>
+          <EntryPartOfSpeech value={sense.translated_parts_of_speech} {canEdit} on:valueupdate />
+          <div class="border-b-2 pb-1 mb-2" />
+        </div>
+      {/if}
+
+      <EntrySemanticDomains {sense} {canEdit}
+        on:update={({detail}) => dispatch('valueupdate', { field: 'sdn', newValue: detail })}
+        on:removeCustomDomain={() => dispatch('valueupdate', { field: 'sd', newValue: null })} />
+
+      <EntryField
+        value={sense.noun_class}
+        field="nc"
+        {canEdit}
+        display={$t('entry.nc')}
+        on:valueupdate />
+
+      {#each sense.example_sentences || [{}] as sentence}
+        <EntryField
+          value={sentence.vn}
+          field="xs.vn"
+          {canEdit}
+          display={$t('entry.example_sentence', { default: 'Example Sentence' })}
+          on:valueupdate />
+
+        {#each glossingLanguages as bcp}
+          <EntryField
+            value={sentence[bcp]}
+            field={`xs.${bcp}`}
+            {canEdit}
+            display={`${$t(`gl.${bcp}`)}: ${$t('entry.example_sentence', {
+              default: 'Example Sentence',
+            })}`}
+            on:valueupdate />
+        {/each}
+      {/each}
+      <!-- </div> -->
+      <!-- </div> -->
     {/each}
 
     {#if entry.dialects?.length || canEdit}
