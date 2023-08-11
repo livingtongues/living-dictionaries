712579ef12a12b8727bedd2a2a187a5af871b78d
jacob-8 <jwrunner7@gmail.com>
2023-08-11 19:12:38 +0800

    update PrintEntry shape

--- packages/site/src/routes/[dictionaryId]/entries/print/PrintEntry.svelte
+++ packages/site/src/routes/[dictionaryId]/entries/print/PrintEntry.svelte
@@ -24,53 +24,57 @@
   <b style="font-size: {headwordSize}pt;">{entry.lexeme}</b>
 
   {#if selectedFields.alternateOrthographies}
-    <b>{get_local_orthographies(entry).sort().join(' ')}</b>
+    {#each get_local_orthographies(entry) as orthography}
+      <b>{orthography}, </b>
+    {/each}
   {/if}
 
   {#if selectedFields.ph && entry.phonetic}
     [${entry.phonetic}]
   {/if}
 
-  {#if selectedFields.ps && entry.senses?.[0].parts_of_speech_keys}<i>{add_periods_and_comma_separate_parts_of_speech(entry.senses?.[0].parts_of_speech_keys)}</i>{/if}
+  {#each entry.senses || [] as sense}
+    {#if selectedFields.ps && sense.parts_of_speech_keys}<i>{add_periods_and_comma_separate_parts_of_speech(sense.parts_of_speech_keys)}</i>{/if}
 
-  {#if selectedFields.gloss && entry.senses?.[0].glosses}
-    <span>
-      {@html sanitize(order_glosses({
-        glosses: entry.senses?.[0].glosses,
-        dictionary_gloss_languages: dictionary.glossLanguages,
-        $t,
-      }).join(' - '))}
-    </span>
-  {/if}
+    {#if selectedFields.gloss && sense.glosses}
+      <span>
+        {@html sanitize(order_glosses({
+          glosses: sense.glosses,
+          dictionary_gloss_languages: dictionary.glossLanguages,
+          $t,
+        }).join(', '))}
+      </span>
+    {/if}
 
-  {#if selectedFields.example_sentence}
-    {@const example_sentences = entry.senses?.[0].example_sentences}
-    {spaceSeparateSentences(example_sentences)}
-  {/if}
+    {#if selectedFields.example_sentence}
+      {spaceSeparateSentences(sense.example_sentences)}
+    {/if}
 
-  {#if selectedFields.sr && entry.sources}
-    <div>
-      {#if showLabels}
-        <span class="italic text-[80%]">{$t('entry.sr', { default: 'Source' })}: </span>
-      {/if}
-      {entry.sources.join(', ')}
-    </div>
-  {/if}
-
-  <div>
     {#if selectedFields.sdn}
-      {@const semantic_domains = [...entry.senses?.[0].translated_ld_semantic_domains || [], ...entry.senses?.[0].write_in_semantic_domains || []]}
+      {@const semantic_domains = [...sense.translated_ld_semantic_domains || [], ...sense.write_in_semantic_domains || []]}
       {#if semantic_domains.length}
-        {#if showLabels}
-          <span class="italic text-[80%]">
-            {$t('entry.sdn', { default: 'Semantic Domains' })}:
-          </span>
-        {/if}
-        {semantic_domains.join(', ')}
+        <div>
+          {#if showLabels}
+            <span class="italic text-[80%]">
+              {$t('entry.sdn', { default: 'Semantic Domains' })}:
+            </span>
+          {/if}
+          {semantic_domains.join(', ')}
+        </div>
       {/if}
     {/if}
 
-    <!-- TODO -->
+    {#if selectedFields.noun_class && sense.noun_class}
+      <p>
+        {#if showLabels}
+          <span class="italic text-[80%]">{$t('entry.nc', { default: 'Noun Class' })}: </span>
+        {/if}
+        {sense.noun_class}
+      </p>
+    {/if}
+  {/each}
+
+  <div>
     {#each Object.keys(StandardPrintFields) as key}
       {#if entry[key] && selectedFields[key]}
         <p>
@@ -88,6 +92,15 @@
     {/each}
   </div>
 
+  {#if selectedFields.sr && entry.sources}
+    <div>
+      {#if showLabels}
+        <span class="italic text-[80%]">{$t('entry.sr', { default: 'Source' })}: </span>
+      {/if}
+      {entry.sources.join(', ')}
+    </div>
+  {/if}
+
   <!-- TODO: get speaker names from speaker_ids -->
   {#if selectedFields.speaker && entry.sound_files?.[0].speakerName}
     <div>
@@ -99,7 +112,7 @@
   {/if}
 </div>
 
-{#if selectedFields.image && entry.senses?.[0].photo_files?.[0]}
+{#if selectedFields.image && entry.senses?.[0]?.photo_files?.[0]}
   <!-- max-height keeps tall images from spilling onto 2nd page when printing single column w/ images at 100% width; -->
   <img
     class="block mb-1 mt-1px"
@@ -109,7 +122,7 @@
 {/if}
 
 {#if showQrCode}
-  {#await tick()}
+  {#await tick() then _}
     <QrCode
       pixelsPerModule={2}
       value={`livingdictionaries.app/${dictionary.id}/entry/${entry.id}`} />
