11e499ea7d0b165fb818ee24030b4df3db8cfbad
jacob-8 <jwrunner7@gmail.com>
2023-08-13 12:59:56 +0800

    move table components next to table page

---
+++ packages/site/src/routes/[dictionaryId]/entries/table/cells/SemanticDomains.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+  // import { t } from 'svelte-i18n';
+  import type { ExpandedEntry } from '@living-dictionaries/types';
+  import { ShowHide } from 'svelte-pieces';
+
+  export let entry: ExpandedEntry;
+  export let canEdit = false;
+</script>
+
+<ShowHide let:show let:toggle let:set>
+
+  <div
+    class:cursor-pointer={canEdit}
+    class="h-full"
+    style="padding: 0.1em 0.25em"
+    on:click={() => set(canEdit)}>
+    SD_TODO
+    <!-- {#if entry.sdn && entry.sdn.length}
+      {#each entry.sdn as domain, i}
+        {$_('sd.' + domain, { default: domain })}{#if i != entry.sdn.length - 1}
+          <span class="mr-1">,</span>
+        {/if}
+      {/each}
+    {/if}
+    {#if entry.sdn && entry.sdn.length && entry.sd}|{/if}
+    {#if entry.sd}<i class="mr-1">{entry.sd}</i>{/if} -->
+    &nbsp;
+  </div>
+
+  {#if show}
+    {#await import('$lib/components/entry/SemanticDomainsModal.svelte') then { default: SemanticDomainsModal }}
+      <SemanticDomainsModal
+        {entry}
+        on:valueupdate
+        on:close={toggle} />
+    {/await}
+  {/if}
+</ShowHide>
--- packages/site/src/routes/[dictionaryId]/entries/table/cells/Textbox.svelte
+++ packages/site/src/routes/[dictionaryId]/entries/table/cells/Textbox.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-  import { ShowHide } from "svelte-pieces";
-  import sanitize from "xss";
+  import { ShowHide } from 'svelte-pieces';
+  import sanitize from 'xss';
 
   export let value: string;
   export let htmlValue: string = undefined;
@@ -8,13 +8,13 @@
   export let canEdit = false;
   export let display: string;
 
-  $: html = sanitize(htmlValue || value) || "";
+  $: html = sanitize(htmlValue || value) || '';
 </script>
 
 <ShowHide let:show let:toggle let:set>
   <div
     class:cursor-pointer={canEdit}
-    class:italic={field === "scn" && !value?.includes("<i>")}
+    class:italic={field === 'scn' && !value?.includes('<i>')}
     class="h-full"
     style="padding: 0.1em 0.25em"
     on:click={() => set(canEdit)}
@@ -24,7 +24,7 @@
   </div>
 
   {#if show}
-    {#await import("$lib/components/entry/EditFieldModal.svelte") then { default: EditFieldModal }}
+    {#await import('$lib/components/entry/EditFieldModal.svelte') then { default: EditFieldModal }}
       <EditFieldModal
         on:valueupdate
         {value}
