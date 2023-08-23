2d19cf3354702b09d71e67139863f37b6a84a1fc
jacob-8 <jwrunner7@gmail.com>
2023-08-23 11:15:27 +0800

    finish geotagging feature

--- packages/site/src/routes/[dictionaryId]/entry/[entryId]/GeoTaggingModal.svelte
+++ packages/site/src/routes/[dictionaryId]/entry/[entryId]/GeoTaggingModal.svelte
@@ -1,14 +1,16 @@
 <script lang="ts">
   import { t } from 'svelte-i18n';
-  import { Modal, Button } from 'svelte-pieces';
+  import { Modal, Button, ShowHide } from 'svelte-pieces';
   import Map from '@living-dictionaries/parts/src/lib/maps/mapbox/map/Map.svelte';
-  import Geocoder from '@living-dictionaries/parts/src/lib/maps/mapbox/geocoder/Geocoder.svelte';
   import NavigationControl from '@living-dictionaries/parts/src/lib/maps/mapbox/controls/NavigationControl.svelte';
-  import Marker from '@living-dictionaries/parts/src/lib/maps/mapbox/map/Marker.svelte';
-  import type { Coordinates } from '@living-dictionaries/types';
+  import type { Coordinates, IPoint, IRegion } from '@living-dictionaries/types';
   import { onMount, createEventDispatcher } from 'svelte';
-  import { setMarker } from '@living-dictionaries/parts/src/lib/maps/utils/setCoordinatesToMarker';
   import ToggleStyle from '@living-dictionaries/parts/src/lib/maps/mapbox/controls/ToggleStyle.svelte';
+  import Marker from '@living-dictionaries/parts/src/lib/maps/mapbox/map/Marker.svelte';
+  import Popup from '@living-dictionaries/parts/src/lib/maps/mapbox/map/Popup.svelte';
+  import CoordinatesModal from '@living-dictionaries/parts/src/lib/maps/CoordinatesModal.svelte';
+  import RegionModal from '@living-dictionaries/parts/src/lib/maps/RegionModal.svelte';
+  import Region from '@living-dictionaries/parts/src/lib/maps/mapbox/map/Region.svelte';
 
   export let coordinates: Coordinates;
 
@@ -21,19 +23,21 @@
     valueupdate: { field: string; newValue: Coordinates };
   }>();
 
-  function save() {
-    dispatch('valueupdate', { field: 'co', newValue: { ...coordinates, points: [{coordinates: {latitude: lat, longitude: lng}}]}})
-    dispatch('close');
+  function savePoints(points: IPoint[]) {
+    dispatch('valueupdate', { field: 'co', newValue: { ...coordinates, points}})
   }
 
-  function remove() {
-    dispatch('valueupdate', { field: 'co', newValue: null})
-    dispatch('close');
+  function saveRegions(regions: IRegion[]) {
+    dispatch('valueupdate', { field: 'co', newValue: { ...coordinates, regions}})
   }
 
   onMount(() => {
     if (coordinates?.points?.[0]) {
       const [{ coordinates: { longitude, latitude } }] = coordinates.points;
+      lng = longitude
+      lat = latitude
+    } else if (coordinates?.regions?.[0]) {
+      const [{ coordinates: [{ longitude, latitude }] }] = coordinates.regions;
       lng = longitude
       lat = latitude
     } else if (navigator.geolocation) {
@@ -47,36 +51,110 @@
 
 <Modal on:close noscroll>
   <div class="h-sm">
-    <Map style={style_id} {lng} {lat}
-      on:click={({ detail }) => ({lng, lat} = setMarker(detail.lng, detail.lat))}>
+    <Map style={style_id} {lng} {lat} zoom={6}>
       <NavigationControl />
-      <Geocoder
-        options={{ marker: false }}
-        placeholder={$t('about.search')}
-        on:resultCoordinates={({detail: { longitude, latitude }}) => ({lng, lat} = setMarker(longitude, latitude))}
-        on:error={(e) => console.error(e.detail)} />
-      {#if lng && lat}
-        <Marker
-          draggable
-          on:dragend={({ detail }) => ({lng, lat} = setMarker(detail.lng, detail.lat))}
-          {lng}
-          {lat} />
-      {/if}
+      {#each coordinates?.points || [] as point, index (point)}
+        <Marker lat={point.coordinates.latitude} lng={point.coordinates.longitude}>
+          <Popup>
+            <ShowHide let:show let:toggle>
+              <Button form="simple" size="sm" onclick={toggle}>
+                <span class="i-octicon-pencil" />
+              </Button>
+              {#if show}
+                <CoordinatesModal
+                  {t}
+                  lng={point.coordinates.longitude}
+                  lat={point.coordinates.latitude}
+                  on:update={({ detail }) => {
+                    const { points } = coordinates;
+                    points[index] = {
+                      coordinates: { longitude: detail.lng, latitude: detail.lat },
+                    };
+                    savePoints(points)
+                  }}
+                  on:remove={() => {
+                    const {points} = coordinates;
+                    points.splice(index, 1);
+                    savePoints(points)
+                  }}
+                  on:close={toggle}>
+                </CoordinatesModal>
+              {/if}
+            </ShowHide>
+          </Popup>
+        </Marker>
+      {/each}
+
+      {#each coordinates?.regions || [] as region, index (region)}
+        <Region {region}>
+          <ShowHide let:show let:toggle>
+            <Button form="simple" size="sm" onclick={toggle}>
+              <span class="i-octicon-pencil" />
+            </Button>
+            {#if show}
+              <RegionModal
+                {t}
+                {region}
+                on:update={({ detail }) => {
+                  const {regions} = coordinates;
+                  regions[index] = detail;
+                  saveRegions(regions)
+                }}
+                on:remove={() => {
+                  const {regions} = coordinates;
+                  regions.splice(index, 1);
+                  saveRegions(regions)
+                }}
+                on:close={toggle}>
+              </RegionModal>
+            {/if}
+          </ShowHide>
+        </Region>
+      {/each}
+
       <ToggleStyle />
     </Map>
+
+    <div class="mt-1">
+      <ShowHide let:show let:toggle>
+        <Button
+          onclick={toggle}
+          color="black"
+          size="sm">
+          <span class="i-mdi-map-marker-plus mr-1" style="margin-top: -3px;" />
+          {$t('create.select_coordinates', { default: 'Select Coordinates' })}
+        </Button>
+        {#if show}
+          <CoordinatesModal {t} lng={lng} lat={lat} on:update={({ detail }) => {
+            const newPoint = { coordinates: { longitude: detail.lng, latitude: detail.lat }}
+            const points = [...(coordinates.points || []), newPoint];
+            savePoints(points)
+          }} on:close={toggle} />
+        {/if}
+      </ShowHide>
+
+      <ShowHide let:show let:toggle>
+        <Button onclick={toggle} color="black" size="sm">
+          <span class="i-mdi-map-marker-path mr-1" style="margin-top: -2px;" />
+          {$t('create.select_region', { default: 'Select Region' })}
+        </Button>
+        {#if show}
+          <RegionModal
+            {t}
+            region={null}
+            on:update={({ detail }) => {
+              const regions = [...(coordinates.regions || []), detail];
+              saveRegions(regions)
+            }}
+            on:close={toggle} />
+        {/if}
+      </ShowHide>
+    </div>
   </div>
 
   <div class="modal-footer">
     <Button onclick={() => dispatch('close')} form="simple" color="black">
-      {$t('misc.cancel')}
-    </Button>
-    {#if coordinates}
-      <Button onclick={remove} form="simple" color="red">
-        {$t('misc.remove')}
-      </Button>
-    {/if}
-    <Button onclick={save} form="filled">
-      {$t('misc.save')}
+      {$t('misc.close')}
     </Button>
   </div>
 </Modal>
