2b7f3c724871f1bf4957b83123b53092c2100cf0
jacob-8 <jwrunner7@gmail.com>
2023-09-27 10:05:25 +0800

    save backUrl entry page mount

--- packages/site/src/routes/[dictionaryId]/entry/[entryId]/+page.svelte
+++ packages/site/src/routes/[dictionaryId]/entry/[entryId]/+page.svelte
@@ -9,37 +9,34 @@
   import { seo_description } from './seo_description';
   import { convert_and_expand_entry } from '$lib/transformers/convert_and_expand_entry';
   import { navigating } from '$app/stores';
-  import { goto } from '$app/navigation';
+  import { onMount } from 'svelte';
 
   export let data;
-  $: ({ admin,
+  $: ({
+    admin,
     algoliaQueryParams,
     canEdit,
     dictionary,
     isContributor,
     isManager,
     user,
-    initialEntry } = data);
+    initialEntry,
+  } = data);
 
   $: entry = $locale && convert_and_expand_entry($initialEntry); // adding locale triggers update of translated semantic domains and parts of speech
-  if ($navigating?.from.url.href)
-    localStorage.setItem('last_href_visited', $navigating.from.url.href);
-
+  let backUrl: string;
+  onMount(() => {
+    backUrl = $navigating?.from?.url.href
+      ? `${$navigating.from.url.href}${$algoliaQueryParams}`
+      : `/${$dictionary.id}/entries/list${$algoliaQueryParams}`;
+  });
 </script>
 
 <div
   class="flex justify-between items-center mb-3 md:top-12 sticky top-0 z-30
-    bg-white pt-1 -mt-1">
-  <Button
-    class="-ml-2 !px-2"
-    color="black"
-    form="simple"
-    onclick={() => {
-      if (localStorage.getItem('last_href_visited'))
-        goto(localStorage.getItem('last_href_visited'));
-      else
-        goto(`/${$dictionary.id}/entries/list${$algoliaQueryParams}`);
-    }}>
+    bg-white pt-1 -mt-1"
+>
+  <Button class="-ml-2 !px-2" color="black" form="simple" href={backUrl}>
     <i class="fas fa-arrow-left rtl-x-flip" />
     {$t('misc.back', { default: 'Back' })}
   </Button>
@@ -52,7 +49,9 @@
       <Button
         color="red"
         form="simple"
-        onclick={() => deleteEntry($initialEntry, $dictionary.id, $algoliaQueryParams)}>
+        onclick={() =>
+          deleteEntry($initialEntry, $dictionary.id, $algoliaQueryParams)}
+      >
         <span class="hidden md:inline">
           {$t('misc.delete', { default: 'Delete' })}
         </span>
@@ -73,7 +72,14 @@
   canEdit={$canEdit}
   on:deleteImage={() => deleteImage(entry, $dictionary.id)}
   on:deleteVideo={() => deleteVideo(entry, $dictionary.id)}
-  on:valueupdate={({detail: { field, newValue}}) => saveUpdateToFirestore({field, value: newValue, entryId: entry.id, dictionaryId: $dictionary.id})} />
+  on:valueupdate={({ detail: { field, newValue } }) =>
+    saveUpdateToFirestore({
+      field,
+      value: newValue,
+      entryId: entry.id,
+      dictionaryId: $dictionary.id,
+    })}
+/>
 
 <SeoMetaTags
   imageTitle={entry.lx}
@@ -83,4 +89,5 @@
   lng={$dictionary.coordinates?.longitude}
   url="https://livingdictionaries.app/{$dictionary.id}/entry/{entry.id}"
   gcsPath={entry.senses?.[0]?.photo_files?.[0]?.specifiable_image_url}
-  keywords="Minority Languages, Indigenous Languages, Language Documentation, Dictionary, Minority Community, Language Analysis, Language Education, Endangered Languages, Language Revitalization, Linguistics, Word Lists, Linguistic Analysis, Dictionaries, Living Dictionaries, Living Tongues, Under-represented Languages, Tech Resources, Language Sustainability, Language Resources, Diaspora Languages, Elicitation, Language Archives, Ancient Languages, World Languages, Obscure Languages, Little Known languages, Digital Dictionary, Dictionary Software, Free Software, Online Dictionary Builder" />
+  keywords="Minority Languages, Indigenous Languages, Language Documentation, Dictionary, Minority Community, Language Analysis, Language Education, Endangered Languages, Language Revitalization, Linguistics, Word Lists, Linguistic Analysis, Dictionaries, Living Dictionaries, Living Tongues, Under-represented Languages, Tech Resources, Language Sustainability, Language Resources, Diaspora Languages, Elicitation, Language Archives, Ancient Languages, World Languages, Obscure Languages, Little Known languages, Digital Dictionary, Dictionary Software, Free Software, Online Dictionary Builder"
+/>
