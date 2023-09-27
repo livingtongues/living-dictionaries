26e6d87a1d421732550ca527048d8f045665423b
jacob-8 <jwrunner7@gmail.com>
2023-09-27 16:48:48 +0800

    turn off kitbook to test build

--- packages/site/svelte.config.js
+++ packages/site/svelte.config.js
@@ -1,13 +1,13 @@
 // @ts-check
 import { vitePreprocess } from '@sveltejs/kit/vite';
 import adapter from '@sveltejs/adapter-auto';
-import { mdsvex, MDSVEX_EXTENSIONS, KITBOOK_MDSVEX_CONFIG } from 'kitbook/plugins/vite';
+// import { mdsvex, MDSVEX_EXTENSIONS, KITBOOK_MDSVEX_CONFIG } from 'kitbook/plugins/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  extensions: ['.svelte', ...MDSVEX_EXTENSIONS],
+  // extensions: ['.svelte', ...MDSVEX_EXTENSIONS],
   preprocess: [
-    mdsvex(KITBOOK_MDSVEX_CONFIG),
+    // mdsvex(KITBOOK_MDSVEX_CONFIG),
     vitePreprocess(),
   ],
 
