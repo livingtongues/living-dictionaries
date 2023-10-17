e37f3e7696f7ac7312ca1a8e669d59ed56fe0486
jacob-8 <jwrunner7@gmail.com>
2023-10-17 19:47:35 +0800

    restore part

--- packages/site/src/routes/Analytics.svelte
+++ packages/site/src/routes/Analytics.svelte
@@ -1,1 +1,9 @@
-Hi
+<script lang="ts">
+  import { page } from '$app/stores';
+</script>
+
+{#if $page.url.host.includes('livingdictionaries.app')}
+  {#await import('$lib/components/shell/LogRocket.svelte') then { default: LogRocket }}
+    <LogRocket />
+  {/await}
+{/if}
