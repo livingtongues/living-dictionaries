0139aaf35836ea97b87e7f52550b124dba8acf9e
jacob-8 <jwrunner7@gmail.com>
2023-10-17 17:31:46 +0800

    turn on kitbook

--- packages/site/vite.config.ts
+++ packages/site/vite.config.ts
@@ -1,20 +1,20 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import UnoCSS from '@unocss/svelte-scoped/vite';
-// import { kitbook } from 'kitbook/plugins/vite';
+import { kitbook } from 'kitbook/plugins/vite';
 
 export default defineConfig({
   plugins: [
-    // kitbook({
-    //   title: 'Living Dictionaries',
-    //   description: 'Svelte Component Documentation and Prototyping Workbench built for Living Dictionaries using Kitbook',
-    //   githubURL: 'https://github.com/livingtongues/living-dictionaries/tree/main/packages/site',
-    //   expandTree: true,
-    //   viewports: [
-    //     { width: 320, height: 640 },
-    //     { width: 768, height: 800 },
-    //   ]
-    // }),
+    kitbook({
+      title: 'Living Dictionaries',
+      description: 'Svelte Component Documentation and Prototyping Workbench built for Living Dictionaries using Kitbook',
+      githubURL: 'https://github.com/livingtongues/living-dictionaries/tree/main/packages/site',
+      expandTree: true,
+      viewports: [
+        { name: 'mobile', width: 375, height: 667 },
+        { name: 'desktop', width: 768, height: 800 },
+      ]
+    }),
     UnoCSS({
       injectReset: '@unocss/reset/tailwind.css',
     }),
