d5af76f12a29fe106284482e5030ae3cd4490c95
jacob-8 <jwrunner7@gmail.com>
2023-10-17 17:45:14 +0800

    try again

--- packages/site/src/routes/Analytics.svelte
+++ packages/site/src/routes/Analytics.svelte
@@ -1,13 +1,1 @@
-<script lang="ts">
-  import { page } from '$app/stores';
-
-  const analyticsId = import.meta.env.VERCEL_ANALYTICS_ID as string;
-  console.info({analyticsId})
-
-</script>
-
-{#if $page.url.host.includes('livingdictionaries.app')}
-  {#await import('$lib/components/shell/LogRocket.svelte') then { default: LogRocket }}
-    <LogRocket />
-  {/await}
-{/if}
+Hi
