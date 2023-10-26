fee380814e14483f1ff05952c75bddb0f505044c
Diego Córdova Nieto <diego@livingtongues.org>
2023-10-26 15:50:18 -0600

    public updated message

--- packages/site/src/routes/[dictionaryId]/entry/[entryId]/EntryHistory.svelte
+++ packages/site/src/routes/[dictionaryId]/entry/[entryId]/EntryHistory.svelte
@@ -1,13 +1,19 @@
 <script lang="ts">
+  import { canEdit } from '$lib/stores';
   import type { History } from '@living-dictionaries/types';
   export let history: History[];
 </script>
 
 <div class="{$$props.class} text-gray-500">
-  <strong>Lexeme history:</strong>
-  {#each history as record}
-    {#if window.location.pathname.match(record.editedLexeme)}
-      <p class="m-3">{record.editor} edited this entry on {record.updatedAt.toDate().toLocaleString()}</p>
-    {/if}
-  {/each}
+  {#if canEdit}
+    <strong>Lexeme history:</strong>
+    {#each history as record}
+      <!-- TODO it could be better to query only lexeme records-->
+      {#if window.location.pathname.match(record.editedLexeme)}
+        <p class="m-3">{record.editor} edited this entry on {record.updatedAt.toDate().toLocaleString()}</p>
+      {/if}
+    {/each}
+  {:else}
+    <p class="m-3">Last edited on {history[history.length-1].updatedAt.toDate().toLocaleString()}</p>
+  {/if}
 </div>
