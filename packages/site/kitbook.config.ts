abff564af22436e5b700bcb3f4aab847121bb1f4
jacob-8 <jwrunner7@gmail.com>
2023-10-30 17:57:02 +0800

    less languages, more time

--- packages/site/kitbook.config.ts
+++ packages/site/kitbook.config.ts
@@ -1,5 +1,23 @@
 import { defineConfig } from 'kitbook/defineConfig'
-import { ReadyLocales } from './src/locales/languages.interface'
+// import { ReadyLocales } from './src/locales/languages.interface'
+
+enum ReadyLocales {
+  en = 'English',
+  es = 'Español',
+  fr = 'Français',
+  zh = '中文',
+  // sw = 'Kiswahili',
+  // ru = 'русский',
+  he = 'עברית',
+  // pt = 'Portuguese',
+  // id = 'Bahasa Indonesia',
+  // ms = 'Malay',
+  // bn = 'বাংলা', // Bengali,
+  // as = 'Assamese / অসমীয়া',
+  // hi = 'हिन्दी',
+  // vi = 'Vietnamese',
+}
+
 
 export default defineConfig({
   importModuleGlobs: ['/src/**/*.{md,svelte,variants.ts,composition}', '/README.md'],
