ee8976320f41fb3fbca42df4f9549f68c909a616
Diego Córdova Nieto <diego@livingtongues.org>
2023-10-30 14:02:20 -0600

    create history variant

---
+++ packages/site/src/routes/[dictionaryId]/history/_page.variants.ts
@@ -0,0 +1,48 @@
+import type { Variant } from 'kitbook';
+import { Timestamp } from 'firebase/firestore';
+import type Component from './+page.svelte';
+
+export const variants: Variant<Component>[] = [
+  {
+    name: 'Normal',
+    props: {
+      data: {
+        dictionary: {
+          name: 'test',
+          glossLanguages: []
+        },
+        user: null,
+        history: [
+          {
+            editor: 'Diego Córdova',
+            editedLexeme: 'EntryDisplay',
+            editedDictionaryId: 'Banange',
+            action: 'created',
+            updatedAt: Timestamp.fromDate(new Date(2023, 7, 11, 15, 35, 54))
+          },
+          {
+            editor: 'Anna Luisa',
+            editedLexeme: '002',
+            editedDictionaryId: 'Banange',
+            action: 'created',
+            updatedAt: Timestamp.fromDate(new Date(2023, 8, 24, 15, 36, 54))
+          },
+          {
+            editor: 'Diego Córdova',
+            editedLexeme: 'EntryDisplay',
+            editedDictionaryId: 'Banange',
+            action: 'edited',
+            updatedAt: Timestamp.fromDate(new Date(2023, 9, 12, 15, 35, 59))
+          },
+          {
+            editor: 'Anna Luisa',
+            editedLexeme: 'EntryDisplay',
+            editedDictionaryId: 'Banange',
+            action: 'edited',
+            updatedAt: Timestamp.fromDate(new Date(2023, 9, 12, 15, 39, 2))
+          },
+        ]
+      }
+    }
+  }
+]
