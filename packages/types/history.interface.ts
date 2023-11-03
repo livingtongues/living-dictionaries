7e5e476581fffd735dc7aff8f213b0f8e4e15ce3
Diego Córdova Nieto <diego@livingtongues.org>
2023-11-02 21:46:47 -0600

    prepare new interface

--- packages/types/history.interface.ts
+++ packages/types/history.interface.ts
@@ -9,3 +9,15 @@ export interface History {
   // field?: string, by request
   updatedAt: Timestamp;
 }
+
+// export interface Change {
+//   updatedBy: string;
+//   updatedName: string;
+//   entryId: string;
+//   dictionaryId: string;
+//   dictionaryName: string;
+//   previousValue: string;
+//   currentValue: string;
+//   field: string; //by request
+//   updatedAtMs: number;
+// }
