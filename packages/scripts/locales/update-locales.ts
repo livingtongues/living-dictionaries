7e302e4895dfee5e1f2879d648ce8d98449b39ab
jacob-8 <jwrunner7@gmail.com>
2023-11-07 13:16:44 +0800

    move translations to js and update (part 1)

--- packages/scripts/locales/update-locales.ts
+++ packages/scripts/locales/update-locales.ts
@@ -2,44 +2,41 @@ import fetch from 'node-fetch';
 import csv from 'csvtojson';
 import dot from 'dot-object';
 import { promises as fs } from 'fs';
-import { ReadyLocales, UnpublishedLocales } from '../../site/src/locales/languages.interface.js';
+import { Locales, UnpublishedLocales } from '../../site/src/lib/i18n/locales.js';
 import { type IGlossLanguage } from '@living-dictionaries/types';
-const languages = [...Object.keys(ReadyLocales), ...Object.keys(UnpublishedLocales)];
+const languages = [...Object.keys(Locales), ...Object.keys(UnpublishedLocales)];
+
+const I18N_GOOGLE_SHEET_ID = '1SqtfUvYYAEQSFTaTPoAJq6k-wlbuAgWCkswE_kiUhLs';
+const LOCALES_DIRECTORY = '../site/src/lib/i18n/locales';
 
 export async function generateFilesFromSpreadsheet() {
-  const i18nGoogleSheetId = '1SqtfUvYYAEQSFTaTPoAJq6k-wlbuAgWCkswE_kiUhLs';
-  const localesDir = '../site/src/locales';
-  try {
-    const rows = await jsonFromCsvUrl(googleSheetCsvUrl(i18nGoogleSheetId, 'App-Translations'));
-    const translations = await generateTranslationsFromSpreadsheet(rows, { nesting: 'deep' });
-    await writeLocaleFiles(translations, localesDir);
+  const rows = await jsonFromCsvUrl(getGoogleSheetCsvUrl(I18N_GOOGLE_SHEET_ID, 'App-Translations'));
+  const translations = generateTranslationsFromSpreadsheet(rows, { nesting: 'deep' });
+  await writeLocaleFiles(translations, LOCALES_DIRECTORY);
 
-    const rows_sd = await jsonFromCsvUrl(googleSheetCsvUrl(i18nGoogleSheetId, 'Semantic-Domains'));
-    const translations_sd = await generateTranslationsFromSpreadsheet(rows_sd, { prefix: 'sd' });
-    await writeLocaleFiles(translations_sd, localesDir + '/sd');
+  const rows_sd = await jsonFromCsvUrl(getGoogleSheetCsvUrl(I18N_GOOGLE_SHEET_ID, 'Semantic-Domains'));
+  const translations_sd = generateTranslationsFromSpreadsheet(rows_sd, { prefix: 'sd' });
+  await writeLocaleFiles(translations_sd, LOCALES_DIRECTORY + '/sd');
 
-    const rows_ps = await jsonFromCsvUrl(googleSheetCsvUrl(i18nGoogleSheetId, 'Parts-of-Speech'));
-    const translations_ps = await generateTranslationsFromSpreadsheet(rows_ps, { prefix: 'ps' });
-    await writeLocaleFiles(translations_ps,  localesDir + '/ps');
-    const translations_psAbbrev = await generateTranslationsFromSpreadsheet(rows_ps, {
-      prefix: 'psAbbrev',
-    });
-    await writeLocaleFiles(translations_psAbbrev, localesDir + '/psAbbrev');
+  const rows_ps = await jsonFromCsvUrl(getGoogleSheetCsvUrl(I18N_GOOGLE_SHEET_ID, 'Parts-of-Speech'));
+  const translations_ps = generateTranslationsFromSpreadsheet(rows_ps, { prefix: 'ps' });
+  await writeLocaleFiles(translations_ps, LOCALES_DIRECTORY + '/ps');
+  const translations_psAbbrev = generateTranslationsFromSpreadsheet(rows_ps, {
+    prefix: 'psAbbrev',
+  });
+  await writeLocaleFiles(translations_psAbbrev, LOCALES_DIRECTORY + '/psAbbrev');
 
-    const rows_gl = await jsonFromCsvUrl(
-      googleSheetCsvUrl(i18nGoogleSheetId, 'Glossing-Languages')
-    );
-    const translations_gl = await generateTranslationsFromSpreadsheet(rows_gl, { prefix: 'gl' });
-    await writeLocaleFiles(translations_gl, localesDir + '/gl');
-    const glossingLanguages = await generateGlossingLanguages(rows_gl);
-    await fs.writeFile(
-      `../site/src/lib/glosses/glossing-languages-list.json`,
-      JSON.stringify(glossingLanguages, null, 2) + '\r\n'
-    );
-    console.log('glossing-languages-list.json file written');
-  } catch (error) {
-    throw new Error(error.message);
-  }
+  const rows_gl = await jsonFromCsvUrl(
+    getGoogleSheetCsvUrl(I18N_GOOGLE_SHEET_ID, 'Glossing-Languages')
+  );
+  const translations_gl = generateTranslationsFromSpreadsheet(rows_gl, { prefix: 'gl' });
+  await writeLocaleFiles(translations_gl, LOCALES_DIRECTORY + '/gl');
+  const glossingLanguages = await generateGlossingLanguages(rows_gl);
+  await fs.writeFile(
+    `../site/src/lib/glosses/glossing-languages-list.json`,
+    JSON.stringify(glossingLanguages, null, 2) + '\r\n'
+  );
+  console.log('glossing-languages-list.json file written');
 }
 generateFilesFromSpreadsheet();
 
@@ -50,13 +47,13 @@ export const generateTranslationsFromSpreadsheet: (
     prefix?: string;
   }
 ) => Record<string, unknown> = (rows, options = {}) => {
-  const translations = {};
+  const translations: Record<string, any> = {};
 
   languages.forEach((lang) => {
     translations[lang] = {};
 
     rows.forEach((row) => {
-      const {key} = row;
+      const { key } = row;
       const langColumn = options.prefix === 'psAbbrev' ? lang + 'Abbrev' : lang;
       const value = row[langColumn];
       if (key && value) {
@@ -103,14 +100,17 @@ async function jsonFromCsvUrl(url: string) {
   return await csv().fromString(csvString);
 }
 
-function googleSheetCsvUrl(spreadsheetId: string, sheetName: string) {
+function getGoogleSheetCsvUrl(spreadsheetId: string, sheetName: string) {
   return `https://docs.google.com/spreadsheets/d/${spreadsheetId}/gviz/tq?tqx=out:csv&sheet=${sheetName}`;
 }
 
 async function writeLocaleFiles(translations: Record<string, any>, directory: string) {
   const languagesToWrite = languages.map(async (lang) => {
-    const path = `${directory}/${lang}.json`;
-    return fs.writeFile(path, JSON.stringify(translations[lang], null, 2) + '\r\n');
+    const path = `${directory}/${lang}.js`;
+    const content = `/** @type {typeof import('./en.js').default} */
+export default ${JSON.stringify(translations[lang], null, 2)}
+`
+    return fs.writeFile(path, content);
   });
   await Promise.all(languagesToWrite);
   console.log('locale files written to ' + directory);
---
+++ packages/site/src/lib/i18n/locales/psAbbrev/he.js
@@ -0,0 +1,11 @@
+/** @type {typeof import('./en.js').default} */
+export default {
+  "psAbbrev": {
+    "n": "שם",
+    "v": "פ",
+    "adj": "ש״ת",
+    "adv": "תה\"פ",
+    "prep": "מ״י",
+    "conj": "מ״ק"
+  }
+}
