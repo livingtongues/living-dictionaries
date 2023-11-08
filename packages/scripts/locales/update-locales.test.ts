6cf6bcc85438227b04aea72de59b8bd4145e18e2
jacob-8 <jwrunner7@gmail.com>
2023-11-08 14:58:00 +0800

    improve locale update function tests and types

--- packages/scripts/locales/update-locales.test.ts
+++ packages/scripts/locales/update-locales.test.ts
@@ -1,32 +1,44 @@
-import { generateTranslationsFromSpreadsheet, jsonFromLocalCsv } from './update-locales';
+import { prepareMainTranslationsFromSpreadsheet, jsonFromLocalCsv, getSectionTranslationsFromSpreadsheet } from './update-locales';
 import path from 'node:path'
 
-test('generateTranslationsFromSpreadsheet deep nests app translations', async () => {
+test(prepareMainTranslationsFromSpreadsheet, async () => {
   const rows = await jsonFromLocalCsv(path.join(__dirname, './App-Translations.csv'));
-  const translations = generateTranslationsFromSpreadsheet(rows, { nesting: 'deep' });
-  expect(translations.en.direction).toBe('ltr');
-  expect(translations.he.direction).toBe('rtl');
+  const translations = prepareMainTranslationsFromSpreadsheet(rows);
+  expect(translations.en.page.direction).toBe('ltr');
+  expect(translations.he.page.direction).toBe('rtl');
   expect(translations.en.about.about_LD).toBe('About Living Dictionaries');
   expect(translations.es.about.about_LD).toBe('Acerca de Diccionarios Vivos');
 });
 
-test('generateTranslationsFromSpreadsheet prefixes glossing languages', async () => {
-  const rows = await jsonFromLocalCsv(path.join(__dirname, './Glossing-Languages.csv'));
-  const translations = generateTranslationsFromSpreadsheet(rows, { section: 'gl' });
-  expect(translations.en.gl.aa).toBe('Afar');
-  expect(translations.es.gl.aa).toBe('Lejos');
-});
+describe(getSectionTranslationsFromSpreadsheet, () => {
+  test('prefixes glossing languages', async () => {
+    const rows = await jsonFromLocalCsv(path.join(__dirname, './Glossing-Languages.csv'));
+    const translations = getSectionTranslationsFromSpreadsheet(rows, { section: 'gl' });
+    expect(translations.en.gl.aa).toBe('Afar');
+    expect(translations.es.gl.aa).toBe('Lejos');
+  });
+  
+  test('prefixes parts of speech', async () => {
+    const rows = await jsonFromLocalCsv(path.join(__dirname, './Parts-of-Speech.csv'));
+    const translations = getSectionTranslationsFromSpreadsheet(rows, { section: 'ps' });
+    expect(translations.en.ps.n).toBe('noun');
+    expect(translations.es.ps.n).toBe('sustantivo');
+  });
+  
+  test('prefixes parts of speech abbreviations', async () => {
+    const rows = await jsonFromLocalCsv(path.join(__dirname, './Parts-of-Speech.csv'));
+    const translations = getSectionTranslationsFromSpreadsheet(rows, { section: 'psAbbrev' });
+    expect(translations.en.psAbbrev.n).toBe('n');
+    expect(translations.es.psAbbrev.n).toBe('s');
+  });
 
-test('generateTranslationsFromSpreadsheet prefixes parts of speech', async () => {
-  const rows = await jsonFromLocalCsv(path.join(__dirname, './Parts-of-Speech.csv'));
-  const translations = generateTranslationsFromSpreadsheet(rows, { section: 'ps' });
-  expect(translations.en.ps.n).toBe('noun');
-  expect(translations.es.ps.n).toBe('sustantivo');
-});
+  test('prefixes semantic domains', async () => {
+    const rows = await jsonFromLocalCsv(path.join(__dirname, './Semantic-Domains.csv'));
+    const translations = getSectionTranslationsFromSpreadsheet(rows, { section: 'sd' });
+    expect(translations.en.sd['1.0']).toBe('Universe and the natural world');
+    expect(translations.es.sd['1.0']).toBe('El universo y el mundo natural');
+  });
+})
 
-test('generateTranslationsFromSpreadsheet prefixes parts of speech abbreviations', async () => {
-  const rows = await jsonFromLocalCsv(path.join(__dirname, './Parts-of-Speech.csv'));
-  const translations = generateTranslationsFromSpreadsheet(rows, { section: 'psAbbrev' });
-  expect(translations.en.psAbbrev.n).toBe('n');
-  expect(translations.es.psAbbrev.n).toBe('s');
-});
+
+
