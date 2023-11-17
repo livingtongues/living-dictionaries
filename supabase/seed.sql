a84905311f4d02bf92747b00dbb716b90897b290
jacob-8 <jwrunner7@gmail.com>
2023-11-17 09:42:04 +0800

    get entry_updates working

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -1,20 +1,143 @@
-INSERT INTO
-  public.senses (
-    id,
-    entry_id,
-    created_by,
-    updated_by,
-    glosses,
-    noun_class,
-    definition_english_deprecated
-  )
-VALUES
-  ( 
-    'a1b2c3d4-e5f6-a7b8-c9d0-e1f2a3b4c5d6',
-    '1',
-    'user-1',
-    'user-1',
-    '{"en": "cat"}',
-    '8',
-    'A small domesticated carnivorous mammal with soft fur, a short snout, and retractable claws.'
-  )
+-- text field, first entry, first sense
+INSERT INTO entry_updates (
+  id, 
+  user_id, 
+  dictionary_id, 
+  entry_id, 
+  "table", 
+  "row", 
+  "column", 
+  new_value, 
+  old_value
+ )
+VALUES (
+  '00000000-bbbb-cccc-dddd-eeeeeefff001',
+  'user123',
+  'dictionary123',
+  'entry123',
+  'senses',
+  'aaaaaaaa-bbbb-cccc-dddd-eeeeeefff001',
+  'noun_class',
+  '2',
+  '1'
+);
+
+-- text[] field, same entry, same sense
+INSERT INTO entry_updates (
+  id, 
+  user_id, 
+  dictionary_id, 
+  entry_id, 
+  "table", 
+  "row", 
+  "column", 
+  new_value, 
+  old_value
+ )
+VALUES (
+  '00000000-bbbb-cccc-dddd-eeeeeefff002',
+  'user123',
+  'dictionary123',
+  'entry123',
+  'senses',
+  'aaaaaaaa-bbbb-cccc-dddd-eeeeeefff001',
+  'parts_of_speech',
+  '{n, v}',
+  null
+);
+
+-- jsonb field, same entry, second sense
+INSERT INTO entry_updates (
+  id, 
+  user_id, 
+  dictionary_id, 
+  entry_id, 
+  "table", 
+  "row", 
+  "column", 
+  new_value, 
+  old_value
+ )
+VALUES (
+  '00000000-bbbb-cccc-dddd-eeeeeefff003',
+  'user123',
+  'dictionary123',
+  'entry123',
+  'senses',
+  'aaaaaaaa-bbbb-cccc-dddd-eeeeeefff002',
+  'glosses',
+  '{"en":"Hi","es":"Hola"}',
+  null
+);
+
+-- jsonb field, same entry, third sense (dup data to have something to delete)
+INSERT INTO entry_updates (
+  id, 
+  user_id, 
+  dictionary_id, 
+  entry_id, 
+  "table", 
+  "row", 
+  "column", 
+  new_value, 
+  old_value
+ )
+VALUES (
+  '00000000-bbbb-cccc-dddd-eeeeeefff004',
+  'user123',
+  'dictionary123',
+  'entry123',
+  'senses',
+  'aaaaaaaa-bbbb-cccc-dddd-eeeeeefff003',
+  'glosses',
+  '{"en":"Hi","es":"Hola"}',
+  null
+);
+
+-- timestamp field, same entry, third sense to delete
+INSERT INTO entry_updates (
+  id, 
+  user_id, 
+  dictionary_id, 
+  entry_id, 
+  "table", 
+  "row", 
+  "column", 
+  new_value, 
+  old_value
+ )
+VALUES (
+  '00000000-bbbb-cccc-dddd-eeeeeefff005',
+  'user123',
+  'dictionary123',
+  'entry123',
+  'senses',
+  'aaaaaaaa-bbbb-cccc-dddd-eeeeeefff003',
+  'deleted',
+  '2023-11-16T07:13:48.267Z',
+  null
+);
+
+-- second entry, first sense
+INSERT INTO entry_updates (
+  id, 
+  user_id, 
+  dictionary_id, 
+  entry_id, 
+  "table", 
+  "row", 
+  "column", 
+  new_value, 
+  old_value
+ )
+VALUES (
+  '00000000-bbbb-cccc-dddd-eeeeeefff006',
+  'user123',
+  'dictionary123',
+  'entry124',
+  'senses',
+  'aaaaaaaa-bbbb-cccc-dddd-eeeeeefff011',
+  'noun_class',
+  'animals',
+  'colors'
+);
