0cdc73deb1e49dde60ffe79a8fa39453ce8a5802
jacob-8 <jwrunner7@gmail.com>
2023-11-17 09:27:59 +0800

    don't throw i18n error on missing translations, just log error

--- packages/site/src/routes/+layout.ts
+++ packages/site/src/routes/+layout.ts
@@ -1,11 +1,10 @@
 import type { LayoutLoad } from './$types';
 import { getTranslator } from '$lib/i18n'
 import { getSupportedLocale } from '$lib/i18n/locales'
-import { dev } from '$app/environment';
 
 export const load: LayoutLoad = async ({  url: { searchParams }, data: { serverLocale, user } }) => {
   const urlLocale = searchParams.get('lang')
   const locale = getSupportedLocale(urlLocale || serverLocale) || 'en'
-  const t = await getTranslator(locale, { errorOnMissingBase: dev})
+  const t = await getTranslator(locale)
   return { locale, t, user }
 };
