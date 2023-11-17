db68155bd2a553bb46f9ad33365448a2f28c5b3b
jacob-8 <jwrunner7@gmail.com>
2023-11-17 09:40:36 +0800

    add notes

--- supabase/ideas/unused-change-tracking.sql
+++ supabase/ideas/unused-change-tracking.sql
@@ -1,65 +1,3 @@
-CREATE TYPE user_role AS ENUM ('data-admin', 'dev-admin');
-
-CREATE TABLE user_profiles (
-  id uuid references auth.users (id) primary key NOT NULL,
-  name text NOT NULL,
-  role user_role,
-  agree_to_terms timestamp with time zone,
-  unsubscribe_from_announcements timestamp with time zone,
-);
-
--- TODO: add editors table to store managers and contributors
-
-CREATE TABLE dictionaries (
-  id uuid text unique primary key NOT NULL,
-  CONSTRAINT proper_dictionary_id CHECK (id ~ '^[a-zA-Z0-9_-]+$'),
-  CONSTRAINT dictionary_id_length CHECK (char_length(id) > 2 and char_length(id) <= 20),
-);
-
-CREATE TABLE dictionary_updates (
-
-);
-
-
-
-CREATE TABLE entry_updates (
-
-);
-
-CREATE TABLE senses (
-  id bigint generated always as identity primary key,
-  -- entry_id uuid REFERENCES entries (id) NOT NULL, -- will be turned on once Firebase data migrated
-  created_at timestamp with time zone DEFAULT now() NOT NULL,
-  created_by uuid REFERENCES auth.users (id) NOT NULL,
-  updated_at timestamp with time zone DEFAULT now() NOT NULL,
-  updated_by uuid REFERENCES auth.users (id) NOT NULL,
-  glosses jsonb,
-  noun_class character varying,
-  write_in_semantic_domains jsonb,
-  definition_english_deprecated text,
-);
-
-
-
-CREATE TABLE videos (
-
-);
-
-CREATE TABLE photos (
-
-);
-
-CREATE TABLE audio (
-
-);
-
-CREATE TABLE audio_updates (
-
-);
--- referenced by videos table (junction many-many)
--- referenced by photos table (junction many-many)
-
-
 -- functions for created_at and updated_at from https://code.build/p/MpCtTTkYvzRGSRqQFzxTuY/supabase-date-protection-on-postgresql https://github.com/orgs/supabase/discussions/6741
 CREATE FUNCTION insert_dates()
 RETURNS TRIGGER AS $$
@@ -89,13 +27,10 @@ FOR EACH ROW EXECUTE FUNCTION insert_dates();
 
 
 -- change history logging
-
-
 CREATE TRIGGER create_senses_change
 AFTER INSERT OR UPDATE ON senses
 FOR EACH ROW
 EXECUTE FUNCTION create_senses_change_function();
-
 
 -- https://www.thegnar.com/blog/history-tracking-with-postgres and https://dev.to/mistval/database-architecture-history-over-state-3m8o
 CREATE FUNCTION make_archive_of_changes() RETURNS TRIGGER AS $$
@@ -150,7 +85,3 @@ $$ LANGUAGE plpgsql;
 CREATE TRIGGER trg_make_archive_of_changes_for_entries
 AFTER INSERT OR DELETE OR UPDATE ON entries
 FOR EACH ROW EXECUTE FUNCTION make_archive_of_changes('Entries');
-
-CREATE TRIGGER trg_make_archive_of_changes_for_senses
-AFTER INSERT OR DELETE OR UPDATE ON senses
-FOR EACH ROW EXECUTE FUNCTION make_archive_of_changes('Senses');
