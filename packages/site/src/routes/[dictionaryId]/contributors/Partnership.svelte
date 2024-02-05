c0cd8988333af1fb6801b33cf2acde69c24a2fb1
Diego Córdova Nieto <diego@livingtongues.org>
2024-02-05 13:45:08 -0600

    Partner logo image-upload working

--- packages/site/src/routes/[dictionaryId]/contributors/Partnership.svelte
+++ packages/site/src/routes/[dictionaryId]/contributors/Partnership.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
   import { page } from '$app/stores';
   import { admin } from '$lib/stores';
-  import { add, deleteDocumentOnline, Collection } from 'sveltefirets';
+  import { add, updateOnline, deleteDocumentOnline, Collection } from 'sveltefirets';
   import { Button } from 'svelte-pieces';
   import type { IPartnership, IDictionary } from '@living-dictionaries/types';
-  // import Image from '$lib/components/image/Image.svelte';
+  import Image from '$lib/components/image/Image.svelte';
+  import ImageDropZone from '$lib/components/image/ImageDropZone.svelte';
 
   export let dictionary: IDictionary;
   let partnershipType: IPartnership[];
@@ -14,69 +15,79 @@
     if (name)
       add(`dictionaries/${dictionary.id}/partnerships`, { name });
   }
+
+  async function updatePartner(change: Partial<IPartnership>, partner_id: string) {
+    try {
+      await updateOnline<IPartnership>(`dictionaries/${dictionary.id}/partnerships/${partner_id}`, change)
+    } catch (err) {
+      alert(`${$page.data.t('misc.error')}: ${err}`);
+    }
+  }
 </script>
 
 <h3 class="font-semibold text-lg mb-1 mt-3">
   Partnerships<!-- {$page.data.t('')} -->
 </h3>
 
-<Collection
-  path={`dictionaries/${dictionary.id}/partnerships`}
-  startWith={partnershipType}
-  let:data={partnerships}>
-  {#each partnerships as partner}
-    <div class="py-3 flex flex-wrap items-center">
-      <div class="text-sm leading-5 font-medium text-gray-900">
-        {partner.name}
+<div>
+  <Collection
+    path={`dictionaries/${dictionary.id}/partnerships`}
+    startWith={partnershipType}
+    let:data={partnerships}>
+    {#each partnerships as partner}
+      <div class="py-3 flex flex-wrap items-center">
+        <div class="text-sm leading-5 font-medium text-gray-900">
+          {partner.name}
+        </div>
+        {#if $admin}
+          <div class="w-1" />
+          <Button
+            color="red"
+            size="sm"
+            onclick={() => {
+              if (confirm($page.data.t('misc.delete') + '?')) {
+                deleteDocumentOnline(
+                  `dictionaries/${dictionary.id}/partnerships/${partner.id}`
+                );
+              }
+            }}>{$page.data.t('misc.delete')}
+            <i class="fas fa-times" /></Button>
+        {/if}
       </div>
-      {#if $admin}
-        <div class="w-1" />
-        <Button
-          color="red"
-          size="sm"
-          onclick={() => {
-            if (confirm($page.data.t('misc.delete') + '?')) {
-              deleteDocumentOnline(
-                `dictionaries/${dictionary.id}/partnerships/${partner.id}`
-              );
-            }
-          }}>{$page.data.t('misc.delete')}
-          <i class="fas fa-times" /></Button>
+      {#if partner.logo}
+        <Image
+          canEdit
+          height={300}
+          title="{partner.name} Logo"
+          gcs={partner.logo.specifiable_image_url}
+          on:deleteImage={async () => await updatePartner({ logo: null }, partner.id)} />
+      {:else}
+        <ImageDropZone let:file class="p-3 rounded">
+          <span slot="label">{$page.data.t('misc.upload')}</span>
+          {#if file}
+            {#await import('$lib/components/image/UploadImage.svelte') then { default: UploadImage }}
+              <div class="flex flex-col min-h-100px">
+                <UploadImage
+                  {file}
+                  fileLocationPrefix={`${dictionary.id}/partnerships/${partner.id}/logo/`}
+                  on:uploaded={async ({detail: {fb_storage_path, specifiable_image_url}}) => await updatePartner({
+                    logo: {
+                      fb_storage_path,
+                      specifiable_image_url,
+                      uid_added_by: 'test',
+                      timestamp: new Date(),
+                    }
+                  }, partner.id)} />
+              </div>
+            {/await}
+          {/if}
+        </ImageDropZone>
       {/if}
-    </div>
-  {/each}
-</Collection>
-<!-- {#if partner.logo}
-    <Image
-      canEdit
-      height={300}
-      title="{partner.name} Featured Image"
-      gcs={dictionary.featuredImage.specifiable_image_url}
-      on:deleteImage={async () => await updateDictionary({ featuredImage: null })} />
-  {:else}
-    <ImageDropZone let:file class="p-3 rounded">
-      <span slot="label">{$page.data.t('misc.upload')}</span>
-      {#if file}
-        {#await import('$lib/components/image/UploadImage.svelte') then { default: UploadImage }}
-          <div class="flex flex-col min-h-100px">
-            <UploadImage
-              {file}
-              fileLocationPrefix={`${dictionary.id}/featured_images/`}
-              on:uploaded={async ({detail: {fb_storage_path, specifiable_image_url}}) => await updateDictionary({
-                featuredImage: {
-                  fb_storage_path,
-                  specifiable_image_url,
-                  uid_added_by: $user.uid,
-                  timestamp: new Date(),
-                }
-              })} />
-          </div>
-        {/await}
-      {/if}
-    </ImageDropZone>
-  {/if} -->
-<Button onclick={writeIn} form="filled">
-  <i class="far fa-pencil" />
-  Write in Partner<!-- {$page.data.t('')} -->
-</Button>
+    {/each}
+  </Collection>
+  <Button class="mt-2" onclick={writeIn} form="filled">
+    <i class="far fa-pencil" />
+    Write in Partner<!-- {$page.data.t('')} -->
+  </Button>
+</div>
 
