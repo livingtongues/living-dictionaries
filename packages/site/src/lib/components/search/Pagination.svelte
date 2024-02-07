886902d2a36e0fe679f03d33ca7ba391d2c00a82
jacob-8 <jwrunner7@gmail.com>
2024-02-07 16:13:02 +0800

    separate pagination from instantsearch and make it easier to do large jumps

--- packages/site/src/lib/components/search/Pagination.svelte
+++ packages/site/src/lib/components/search/Pagination.svelte
@@ -3,103 +3,30 @@
   import { connectPagination } from 'instantsearch.js/es/connectors';
   import { onMount } from 'svelte';
   import { canEdit } from '$lib/stores';
-  import { Button } from 'svelte-pieces';
+  import PaginationButtons from './PaginationButtons.svelte';
 
   export let search: InstantSearch;
   export let showAdd = true;
-  let pages: number[] = [];
   let currentRefinement: number;
   let nbPages: number;
-  let isFirstPage: boolean;
-  let isLastPage: boolean;
   let refine: (page: number) => void;
-  let goToPage: string;
 
   onMount(() => {
     const customPagination = connectPagination((params) => {
-      ({ pages, currentRefinement, nbPages, isFirstPage, isLastPage, refine } = params);
+      ({ currentRefinement, nbPages, refine } = params);
     });
 
     search.addWidgets([customPagination({})]);
   });
+
+  function go_to_page(one_based_page: number) {
+    refine(one_based_page - 1);
+    window.scrollTo({ top: 0 });
+  }
 </script>
 
 <nav class="sticky md:bottom-0 bg-white pt-2 pb-1 flex items-center print:hidden">
-  {#if !isFirstPage}
-    <button
-      type="button"
-      on:click={() => {
-        refine(0);
-        window.scrollTo({ top: 0 });
-      }}
-      class="hidden sm:block rounded py-2 px-3 mr-1 text-sm leading-5 font-medium text-gray-500 hover:text-gray-700 hover:bg-gray-100 focus:text-gray-800 focus:outline-none transition ease-in-out duration-150">
-      <i class="far fa-angle-double-left rtl-x-flip" /></button>
-    <button
-      type="button"
-      on:click={() => {
-        refine(currentRefinement - 1);
-        window.scrollTo({ top: 0 });
-      }}
-      class="rounded py-2 px-3 mr-1 sm:text-sm leading-5 font-medium text-gray-500 hover:text-gray-700 hover:bg-gray-100 focus:text-gray-800 focus:outline-none transition ease-in-out duration-150">
-      <i class="far fa-angle-left rtl-x-flip">
-        <!-- <span class="sm:hidden ml-1">Previous</span> -->
-      </i></button>
-  {/if}
-
-  <div class="hidden md:flex">
-    {#each pages as page}
-      <button
-        type="button"
-        on:click={() => {
-          refine(page);
-          window.scrollTo({ top: 0 });
-        }}
-        class="{currentRefinement === page
-          ? 'bg-blue-100 text-blue-700 focus:bg-blue-200 focus:text-blue-800'
-          : 'text-gray-500 hover:text-gray-700 hover:bg-gray-100 focus:text-gray-800'} rounded py-2 px-3 mr-1 inline-flex items-center text-sm leading-5 font-medium focus:outline-none  transition ease-in-out duration-150">
-        {page + 1}
-      </button>
-    {/each}
-  </div>
-
-  {#if !isLastPage}
-    <button
-      type="button"
-      on:click={() => {
-        refine(currentRefinement + 1);
-        window.scrollTo({ top: 0 });
-      }}
-      class="rounded py-2 px-3 mr-1 sm:text-sm leading-5 font-medium text-gray-500 hover:text-gray-700 hover:bg-gray-100 focus:text-gray-800 focus:outline-none transition ease-in-out duration-150">
-      <!-- <span class="sm:hidden mr-1">Next</span> -->
-      <i class="far fa-angle-right rtl-x-flip" /></button>
-    <button
-      type="button"
-      on:click={() => {
-        refine(nbPages - 1);
-        window.scrollTo({ top: 0 });
-      }}
-      class="hidden sm:block rounded py-2 px-3 mr-1 text-sm leading-5 font-medium text-gray-500 hover:text-gray-700 hover:bg-gray-100 focus:text-gray-800 focus:outline-none transition ease-in-out duration-150">
-      <i class="far fa-angle-double-right rtl-x-flip" /></button>
-  {/if}
-
-  <form on:submit={() => refine(parseInt(goToPage) - 1)} class="flex">
-    <input
-      bind:value={goToPage}
-      type="text"
-      id="name"
-      name="name"
-      maxlength="4"
-      size="1"
-      class="form-input w-full" />
-    <Button
-      type="submit"
-      form="filled"
-      size="sm">
-      Go to
-    </Button>
-  </form>
-
-  <div class="flex-grow" />
+  <PaginationButtons pages={nbPages} current_page={currentRefinement + 1} {go_to_page} />
 
   {#if $canEdit && showAdd}
     <span class="px-3 md:px-0 fixed md:sticky bottom-3 ltr:right-0 rtl:left-0 z-10">
