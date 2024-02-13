f9408e9842281799aeebfddb818b9304e00252e8
Diego Córdova Nieto <diego@livingtongues.org>
2024-02-13 15:20:57 -0600

    temporal message after hit Algolia's limit

--- packages/site/src/routes/+layout.svelte
+++ packages/site/src/routes/+layout.svelte
@@ -3,11 +3,18 @@
   import { navigating, page } from '$app/stores';
   import { browser } from '$app/environment';
   import LoadingIndicator from './LoadingIndicator.svelte';
+
+  let hideMessage = false;
 </script>
 
 {#if browser && $navigating}
   <LoadingIndicator />
 {/if}
+
+<div class:hidden={hideMessage} class="bg-amber-400 pl-4">
+  <span class="cursor-pointer" on:click={() => hideMessage = true}>&times;</span>
+  <strong>Attention:</strong> our site is temporarily down but will be restored on Feb 14, 2024. Our sincere apologies.
+</div>
 
 <div id="direction" dir={$page.data.t('page.direction')}>
   <slot />
