639a79556052db104ed46fb91d08d8b2eec37e97
Diego Córdova Nieto <diego@livingtongues.org>
2024-02-14 12:11:02 -0600

    another attention message since edits are not stil visible

--- packages/site/src/routes/+layout.svelte
+++ packages/site/src/routes/+layout.svelte
@@ -3,11 +3,18 @@
   import { navigating, page } from '$app/stores';
   import { browser } from '$app/environment';
   import LoadingIndicator from './LoadingIndicator.svelte';
+
+  let hideMessage = false;
 </script>
 
 {#if browser && $navigating}
   <LoadingIndicator />
 {/if}
+
+<div class:hidden={hideMessage} class="bg-amber-400 pl-4 text-sm">
+  <span class="cursor-pointer" on:click={() => hideMessage = true}>&times;</span>
+  <strong>Attention:</strong> New dictionary entries are not being displayed correctly at this time. This will be resolved on February 15, 2024. We apologize for the inconvenience.
+</div>
 
 <div id="direction" dir={$page.data.t('page.direction')}>
   <slot />
