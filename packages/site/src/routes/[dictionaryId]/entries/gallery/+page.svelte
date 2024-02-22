59a90953c266c7d658d0b2dfe19687e413577a0d
Diego Córdova Nieto <diego@livingtongues.org>
2024-02-21 18:47:29 -0600

    add same functionality in Gallery page

--- packages/site/src/routes/[dictionaryId]/entries/gallery/+page.svelte
+++ packages/site/src/routes/[dictionaryId]/entries/gallery/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   // import SeoMetaTags from '$lib/components/SeoMetaTags.svelte';
-  import { onMount, getContext } from 'svelte';
+  import { onMount, getContext, onDestroy, tick } from 'svelte';
   import Hits from '$lib/components/search/Hits.svelte';
   import Pagination from '$lib/components/search/Pagination.svelte';
   import { configure } from 'instantsearch.js/es/widgets/index.js';
@@ -11,8 +11,11 @@
   import { convert_and_expand_entry } from '$lib/transformers/convert_and_expand_entry';
   import type { InstantSearch } from 'instantsearch.js';
   import { page } from '$app/stores';
+  import { lastEntriesUrl } from '$lib/stores/lastEntriesUrl';
+  import { sliceUrl, getScrollPointFromLocalStorage } from '$lib/helpers/scrollPoint';
 
   const search: InstantSearch = getContext('search');
+  let lastScrollPoint = 0;
 
   onMount(() => {
     let refine: (value?: { isRefined: boolean }) => void;
@@ -31,13 +34,25 @@
       }),
     ]);
 
+    lastScrollPoint = sliceUrl($lastEntriesUrl) === `/${$dictionary.id}/entry` ? getScrollPointFromLocalStorage('gallery_scroll_point') : 0;
     refine({ isRefined: false });
 
     return () => {
       refine({ isRefined: true });
     };
   });
+
+  async function handleMounted() {
+    await tick();
+    window.scrollTo({top: lastScrollPoint});
+  }
+
+  onDestroy(() => {
+    localStorage.setItem('gallery_scroll_point', JSON.stringify(lastScrollPoint))
+  });
 </script>
+
+<svelte:window on:scroll={() => lastScrollPoint = window.scrollY} />
 
 <Hits {search} let:entries>
   <div class="gallery">
@@ -45,6 +60,7 @@
       {#each entries as algoliaEntry (algoliaEntry.id)}
         {#if algoliaEntry.pf}
           <Doc
+            on:ref={handleMounted}
             path="dictionaries/{$dictionary.id}/words/{algoliaEntry.id}"
             startWith={algoliaEntry}
             let:data={entry}>
