b4347a9d9fb0e832773c8465b7318421230f912f
Diego Córdova Nieto <diego@livingtongues.org>
2024-02-27 14:04:10 -0600

    Revert "add no image and no audio examples"
    This reverts commit d83f9b9c62bedc60b5c7857e3c3b85e1bbd57c5a.

--- packages/site/src/routes/[dictionaryId]/entries/EntryFilters.svelte
+++ packages/site/src/routes/[dictionaryId]/entries/EntryFilters.svelte
@@ -48,25 +48,11 @@
           label={$page.data.t('entry.has_exists') +
             ' ' +
             $page.data.t('entry.image')} />
-        <ToggleRefinement
-          {search}
-          attribute="NoImage"
-          on={false}
-          label={'No' +
-            ' ' +
-            $page.data.t('entry.image')} />
       {/if}
       <ToggleRefinement
         {search}
         attribute="hasAudio"
         label={$page.data.t('entry.has_exists') +
-          ' ' +
-          $page.data.t('entry_field.audio')} />
-      <ToggleRefinement
-        {search}
-        attribute="NoAudio"
-        on={false}
-        label={'No' +
           ' ' +
           $page.data.t('entry_field.audio')} />
       <ToggleRefinement
