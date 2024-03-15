a387a29afa8c8335aceb3064a47a17ea62a63239
Diego Córdova Nieto <diego@livingtongues.org>
2024-03-14 20:17:31 -0600

    create a new speaker-refactor script

---
+++ packages/scripts/refactor/speaker-refactor.ts
@@ -0,0 +1,93 @@
+import { ActualDatabaseEntry, ISpeaker } from '@living-dictionaries/types';
+import { db } from '../config';
+import { program } from 'commander';
+// import { CollectionReference } from 'firebase/firestore;
+program
+  //   .version('0.0.1')
+  .option('--id <value>', 'Dictionary Id')
+  .option('--live', 'If not included, only log values')
+  .parse(process.argv);
+
+const dictionaryId = program.opts().id;
+const {live} = program.opts();
+
+async function speakerRefactor() {
+  try {
+    if (dictionaryId) {
+      console.log(`---Refactoring: ${dictionaryId}`);
+      await fetchEntries(dictionaryId);
+    } else {
+      const snapshot = await db.collection('dictionaries').get();
+      for (const dictionarySnap of snapshot.docs) {
+        // If setting limits on refactoring, you can skip dictionaries beginning with letters that have already been processed:
+        const done = /^[abcdefghijklmn].*/;
+        if (!done.test(dictionarySnap.id.toLowerCase())) {
+          console.log(`---Refactoring: ${dictionarySnap.id}`);
+          await fetchEntries(dictionarySnap.id);
+        }
+      }
+    }
+  } catch (error) {
+    console.error('Refactor failed!');
+    console.error(error);
+  }
+}
+
+async function fetchEntries(dictionaryId: string) {
+  type unique_speakers = Record<string, string>;
+  const different_speakers: unique_speakers[] = [];
+  const speakerRef = db.collection('speakers');
+  const dictionarySpeakerSnapshot = await speakerRef.where('contributingTo', 'array-contains', dictionaryId).get();
+  dictionarySpeakerSnapshot.docs.forEach((snap) => different_speakers.push({ [snap.data().displayName]: snap.id }));
+  const snapshot = await db.collection(`dictionaries/${dictionaryId}/words`).get();
+  for (const snap of snapshot.docs) {
+    const entry: ActualDatabaseEntry = { id: snap.id, ...(snap.data() as ActualDatabaseEntry) };
+    addSpeakerIdToEntry(dictionaryId, entry, {birthplace: 'USA', displayName: ''}, speakerRef);
+  }
+}
+
+const addSpeaker = async (speakerData: ISpeaker, speakerRef: any) => {
+
+  const speakerId = speakerRef.doc().id;
+  console.log(`Saving speaker... speaker id: ${speakerId}`)
+  if (!live) return speakerId
+  await speakerRef.doc(speakerId).set(speakerData);
+  return speakerId;
+}
+
+const addSpeakerIdToEntry = async (dictionaryId: string, entry: ActualDatabaseEntry, speakerData: ISpeaker, speakerRef: any) => {
+  const sfBefore = entry.sf;
+
+  if (entry.sf?.speakerName) {
+    const speakerId = await addSpeaker({
+      ...speakerData,
+      displayName: entry.sf.speakerName,
+      contributingTo: [dictionaryId]
+    }, speakerRef);
+
+    console.log(`Before: sf-${JSON.stringify(sfBefore)} sfs-${JSON.stringify(entry?.sfs)}`);
+    entry.sfs = [{
+      sp: [speakerId],
+      path: entry.sf.path,
+      // TODO complete all other fields
+    }]
+    delete entry.sf;
+    console.log(`After: sf-${JSON.stringify(entry?.sf)} sfs-${JSON.stringify(entry.sfs)}`);
+  }
+  if (!live) return;
+  await db.collection(`dictionaries/${dictionaryId}/words`).doc(entry.id).set(entry);
+}
+
+speakerRefactor();
+
+// Single Dictionary
+// `pnpm entryRefactor --id babanki` to log refactor in dev
+// `pnpm entryRefactor --id babanki --live` to do refactor in dev
+// `pnpm entryRefactor --id babanki -e prod` to log refactor in prod
+// `pnpm entryRefactor --id babanki --live -e prod` to do refactor in prod
+
+// All dictionaries
+// `pnpm entryRefactor` to log refactor in dev
+// `pnpm entryRefactor --live` to do refactor in dev
+// `pnpm entryRefactor -e prod` to log refactor in prod
+// `pnpm entryRefactor --live -e prod` to do refactor in prod
