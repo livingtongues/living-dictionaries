d29e1be44a561ea073c22a3628147c189fbba1f4
Diego Córdova Nieto <diego@livingtongues.org>
2024-04-10 17:35:33 -0600

    uncomment adjustments in semnatic domains

--- packages/ids-import/Code.ts
+++ packages/ids-import/Code.ts
@@ -28,6 +28,6 @@ function triggerManually() {
   const semanticDomainsAdjustmentsSheet = spreadsheet.getSheetByName('IDS Adjustments for semantic domains');
   const idsDataSheet = spreadsheet.getSheetByName('IDS Data');
   const testTsvSheet = spreadsheet.getSheetByName('test_tsv');
-  // adjustSemanticDomains({ objectSheet: semanticDomainsAdjustmentsSheet, tsvSheet: testTsvSheet }); //TODO change testTsvSheet for real tsv sheets
+  adjustSemanticDomains({ objectSheet: semanticDomainsAdjustmentsSheet, tsvSheet: testTsvSheet }); //TODO change testTsvSheet for real tsv sheets
   repairWrongTranslations({ objectSheet: idsDataSheet, tsvSheet: testTsvSheet }); //TODO change testTsvSheet for real tsv sheets
 }
