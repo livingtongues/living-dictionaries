5654303f757a38fbb86ce364ebe6eb5e802090f2
Diego Córdova Nieto <diego@livingtongues.org>
2024-04-17 19:00:34 -0600

    prepare to adjust senses import

--- packages/scripts/import/convertJsonRowToEntryFormat.ts
+++ packages/scripts/import/convertJsonRowToEntryFormat.ts
@@ -1,124 +1,123 @@
-import type { ActualDatabaseEntry } from '@living-dictionaries/types';
-import type { Timestamp } from 'firebase/firestore';
-import { randomUUID } from 'crypto';
-import { supabase } from '../config-supabase';
+import { randomUUID } from 'node:crypto'
+import type { ActualDatabaseEntry } from '@living-dictionaries/types'
+import type { Timestamp } from 'firebase/firestore'
+import { supabase } from '../config-supabase'
 
 interface StandardData {
-  row: Record<string, string>;
-  dateStamp?: number;
-  // eslint-disable-next-line no-undef
-  timestamp?: FirebaseFirestore.FieldValue;
+  row: Record<string, string>
+  dateStamp?: number
+
+  timestamp?: FirebaseFirestore.FieldValue
 }
 
 interface SenseData {
-  entry_id: string;
-  dictionary_id: string;
+  entry_id: string
+  dictionary_id: string
 }
 
 export function convertJsonRowToEntryFormat(
   standard: StandardData,
-  senseData?: SenseData
+  senseData?: SenseData,
 ): ActualDatabaseEntry {
-  const { row, dateStamp, timestamp } = standard;
-  const entry: ActualDatabaseEntry = { lx: row.lexeme, gl: {}, xs: {} };
-  const sense_regex = /^s\d+_/;
-  let glossObject:Record<string, string> = {};
-  let row_id = randomUUID();
-  let old_key = 2;
+  const { row, dateStamp, timestamp } = standard
+  const entry: ActualDatabaseEntry = { lx: row.lexeme, gl: {}, xs: {} }
+  const sense_regex = /^s\d+_/
+  let glossObject: Record<string, string> = {}
+  let row_id = randomUUID()
+  let old_key = 2
 
-  if (row.phonetic) entry.ph = row.phonetic;
-  if (row.morphology) entry.mr = row.morphology;
-  if (row.interlinearization) entry.in = row.interlinearization;
-  if (row.partOfSpeech) entry.ps = returnArrayFromCommaSeparatedItems(row.partOfSpeech);
-  if (row.dialects) entry.di = row.dialects.split(',').map(dialect => dialect.trim());
-  if (row.variant) entry.va = row.variant;
-  if (row.nounClass) entry.nc = row.nounClass;
-  if (row.source) entry.sr = row.source.split('|');
-  if (row.pluralForm) entry.pl = row.pluralForm;
-  if (row.scientificName) entry.scn = [row.scientificName];
-  if (row.semanticDomain_custom) entry.sd = [row.semanticDomain_custom];
-  if (row.ID) entry.ei = row.ID;
+  if (row.phonetic) entry.ph = row.phonetic
+  if (row.morphology) entry.mr = row.morphology
+  if (row.interlinearization) entry.in = row.interlinearization
+  if (row.partOfSpeech) entry.ps = returnArrayFromCommaSeparatedItems(row.partOfSpeech)
+  if (row.dialects) entry.di = row.dialects.split(',').map(dialect => dialect.trim())
+  if (row.variant) entry.va = row.variant
+  if (row.nounClass) entry.nc = row.nounClass
+  if (row.source) entry.sr = row.source.split('|')
+  if (row.pluralForm) entry.pl = row.pluralForm
+  if (row.scientificName) entry.scn = [row.scientificName]
+  if (row.semanticDomain_custom) entry.sd = [row.semanticDomain_custom]
+  if (row.ID) entry.ei = row.ID
 
-  if (row.localOrthography) entry.lo1 = row.localOrthography;
-  if (row.localOrthography2) entry.lo2 = row.localOrthography2;
-  if (row.localOrthography3) entry.lo3 = row.localOrthography3;
-  if (row.localOrthography4) entry.lo4 = row.localOrthography4;
-  if (row.localOrthography5) entry.lo5 = row.localOrthography5;
+  if (row.localOrthography) entry.lo1 = row.localOrthography
+  if (row.localOrthography2) entry.lo2 = row.localOrthography2
+  if (row.localOrthography3) entry.lo3 = row.localOrthography3
+  if (row.localOrthography4) entry.lo4 = row.localOrthography4
+  if (row.localOrthography5) entry.lo5 = row.localOrthography5
 
-  if (row.notes) entry.nt = row.notes;
+  if (row.notes) entry.nt = row.notes
 
   for (const [key, value] of Object.entries(row)) {
-    if (!value) continue;
+    if (!value) continue
 
     // gloss fields are labeled using bcp47 language codes followed by '_gloss' (e.g. es_gloss, tpi_gloss)
     if (key.includes('_gloss') && !sense_regex.test(key)) {
-      const [language] = key.split('_gloss');
-      entry.gl[language] = value;
+      const [language] = key.split('_gloss')
+      entry.gl[language] = value
     }
 
     if (key.includes('vernacular_exampleSentence')) {
-      entry.xs.vn = value;
-      continue; // to keep next block from also adding
+      entry.xs.vn = value
+      continue // to keep next block from also adding
     }
 
     // example sentence fields are codes followed by '_exampleSentence'
     if (key.includes('_exampleSentence')) {
-      const [language] = key.split('_exampleSentence');
-      entry.xs[language] = value;
+      const [language] = key.split('_exampleSentence')
+      entry.xs[language] = value
     }
 
     if (senseData) {
-      const { entry_id, dictionary_id } = senseData;
+      const { entry_id, dictionary_id } = senseData
       if (sense_regex.test(key)) {
         if (key.includes('_gloss')) {
-          let language_key = key.replace(sense_regex, '');
-          language_key = language_key.replace('_gloss', '');
+          let language_key = key.replace(sense_regex, '')
+          language_key = language_key.replace('_gloss', '')
 
           if (key === `s${old_key}_${language_key}_gloss`) {
-            glossObject[language_key] = row[key];
+            glossObject[language_key] = row[key]
           } else {
-            old_key++;
-            row_id = randomUUID();
-            glossObject = {};
-            glossObject[language_key] = row[key];
+            old_key++
+            row_id = randomUUID()
+            glossObject = {}
+            glossObject[language_key] = row[key]
           }
-          addAdditionalSensesToSupabase(entry_id, dictionary_id, glossObject, 'glosses', row_id);
+          addAdditionalSensesToSupabase(entry_id, dictionary_id, glossObject, 'glosses', row_id)
         }
 
-        if (key.includes('_partOfSpeech'))
-          addAdditionalSensesToSupabase(entry_id, dictionary_id, stringifyArray([row[key]]), 'parts_of_speech', row_id);
+        // if (key.includes('_partOfSpeech'))
+        //   addAdditionalSensesToSupabase(entry_id, dictionary_id, stringifyArray([row[key]]), 'parts_of_speech', row_id)
 
-        if (key.includes('_semanticDomains'))
-          addAdditionalSensesToSupabase(entry_id, dictionary_id, stringifyArray([row[key]]), 'semantic_domains', row_id);
+        // if (key.includes('_semanticDomains'))
+        //   addAdditionalSensesToSupabase(entry_id, dictionary_id, stringifyArray([row[key]]), 'semantic_domains', row_id)
 
         if (key.includes('_nounClass'))
-          addAdditionalSensesToSupabase(entry_id, dictionary_id, row[key], 'noun_class', row_id);
+          addAdditionalSensesToSupabase(entry_id, dictionary_id, row[key], 'noun_class', row_id)
       }
     }
 
-    const semanticDomain_FOLLOWED_BY_OPTIONAL_DIGIT = /^semanticDomain\d*$/; // semanticDomain, semanticDomain2, semanticDomain<#>, but not semanticDomain_custom
+    const semanticDomain_FOLLOWED_BY_OPTIONAL_DIGIT = /^semanticDomain\d*$/ // semanticDomain, semanticDomain2, semanticDomain<#>, but not semanticDomain_custom
     if (semanticDomain_FOLLOWED_BY_OPTIONAL_DIGIT.test(key)) {
-      if (!entry.sdn) entry.sdn = [];
+      if (!entry.sdn) entry.sdn = []
 
-      entry.sdn.push(value.toString());
+      entry.sdn.push(value.toString())
     }
   }
 
   if (Object.keys(entry.xs).length === 0)
-    delete entry.xs;
+    delete entry.xs
 
+  if (!dateStamp) return entry
 
-  if (!dateStamp) return entry;
+  entry.ii = `v4-${dateStamp}`
+  entry.ca = timestamp as Timestamp
+  entry.ua = timestamp as Timestamp
 
-  entry.ii = `v4-${dateStamp}`;
-  entry.ca = timestamp as Timestamp;
-  entry.ua = timestamp as Timestamp;
-
-  return entry;
+  return entry
 }
 
 export function returnArrayFromCommaSeparatedItems(string: string): string[] {
-  return string?.split(',').map((item) => item.trim()) || [];
+  return string?.split(',').map(item => item.trim()) || []
 }
 
 export async function addAdditionalSensesToSupabase(entry_id: string, dictionary_id: string, value: any, column: SenseColumns, row: string) {
@@ -137,15 +136,15 @@ export async function addAdditionalSensesToSupabase(entry_id: string, dictionary
           new_value: value,
         },
       ])
-      .select();
+      .select()
 
     if (error) {
-      console.error('Error inserting into Supabase: ', error);
-      throw error;
+      console.error('Error inserting into Supabase: ', error)
+      throw error
     }
 
-    return data;
+    return data
   } catch (error) {
-    console.error('Error: ', error);
+    console.error('Error: ', error)
   }
 }
