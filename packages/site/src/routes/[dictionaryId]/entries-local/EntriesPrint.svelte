68516624f528e858f0097e088e0927ba46035c0c
Diego Córdova Nieto <diego@livingtongues.org>
2024-04-25 14:30:50 -0600

    remove flex for elements to be printed

--- packages/site/src/routes/[dictionaryId]/entries-local/EntriesPrint.svelte
+++ packages/site/src/routes/[dictionaryId]/entries-local/EntriesPrint.svelte
@@ -102,9 +102,9 @@
     {$page.data.t('misc.LD_singular')}
   </div>
 
-  <div class="flex overflow-x-hidden">
+  <div class="overflow-x-hidden">
     <div
-      class="print-columns pr-4 print:pr-9 max-w-full flex-grow"
+      class="print-columns pr-4 print:pr-9 max-w-full"
       style="--column-count: {$columnCount}">
       {#each entries as entry (entry.id)}
         <PrintEntry
