ec8522889ae2482fab9e0a3243ca9a80a84a2ff9
Diego Córdova Nieto <43384963+Danble@users.noreply.github.com>
2024-04-24 20:28:44 -0600

    Ids modification patches (#443)
    * add conditionals for gloss columns that do not exist
    
    * uncomment adjustments in semantic domains
    
    * prepare everything for real dictionaries
    
    
    ---------
    
    Co-authored-by: jacob-8 <jwrunner7@gmail.com>

---
+++ packages/ids-import/cleanupTasks.ts
@@ -0,0 +1,104 @@
+function adjustSemanticDomains(sheet_info: SheetData): void {
+  const { objectSheet: semanticDomainsAdjustmentsSheet, tsvSheet } = sheet_info
+  const data = tsvSheet.getRange('A1:1').getValues() // Assuming the header is in the first row
+  const tsv_semantic_domains_label_column_index = data[0].indexOf('semanticDomain') + 2 //* +2 because we need the second column: the one with the labels instead of the keys.
+  const [
+    english_gloss_sd_column_values,
+    semantic_domains_key_column_values,
+    semantic_domains_label_column_values,
+    english_gloss_tsv_column_values,
+    semantic_domains_tsv_column_values,
+    semantic_domains_tsv_column_range,
+  ] = getValuesFromColumns([
+    {
+      from_sheet: semanticDomainsAdjustmentsSheet,
+      columns: ['en_gloss', 'Semantic Domain Key', 'Semantic Domain Label'],
+    },
+    {
+      from_sheet: tsvSheet,
+      columns: ['en_gloss', 'semanticDomain'],
+    },
+    {
+      from_sheet: tsvSheet,
+      columns: [tsv_semantic_domains_label_column_index],
+      are_columns_numbers: true,
+      is_range: true,
+    },
+  ])
+
+  let current_semantic_domain_index: number
+  (english_gloss_tsv_column_values as any[]).forEach((cell, i) => {
+    const matchIndex = (english_gloss_sd_column_values as any[]).findIndex((value) => {
+      if (cell[0] === value[0]) {
+        current_semantic_domain_index = i
+        // @ts-expect-error - GoogleApsScript magic
+        Logger.log(`value: ${value}`)
+        return value
+      }
+      return false
+    })
+    if (matchIndex > -1 && (String(semantic_domains_tsv_column_values[current_semantic_domain_index]) !== String(semantic_domains_key_column_values[matchIndex]))) {
+      const newValue = semantic_domains_label_column_values[matchIndex]
+      // @ts-expect-error - GoogleApsScript magic
+      Logger.log(`New semantic domain value: ${newValue}`)
+      // Update the tsvSheet with the new value
+      semantic_domains_tsv_column_range.getCell(current_semantic_domain_index + 1, 1).setValue(newValue) //* tsvSemanticDomainIndex + 1 because the semanticDomain is a merged header and the labels are in its second column
+    }
+  })
+}
+
+const WRONG_TRANSLATIONS = ['often', 'always', 'ready', 'cease, stop', 'finish', 'end (temporal)', 'last, endure', 'begin, beginning', 'retard, delay', 'hasten, hurry', 'slow', 'swift, fast, quick', 'immediately', 'now', 'late', 'early', 'old', 'young', 'age', 'new', 'time', 'three times', 'third', 'two times', 'pair', 'second', 'last', 'first', 'alone, only', 'half', 'part, piece', 'empty', 'full', 'multitude, crowd']
+function repairWrongTranslations(sheet_info: SheetData): void {
+  const { objectSheet: idsDataSheet, tsvSheet } = sheet_info
+  const [
+    english_values,
+    spanish_values,
+    french_values,
+    portuguese_values,
+    russian_values,
+    english_gloss_values,
+    spanish_column_range,
+    french_column_range,
+    portuguese_column_range,
+    russian_column_range,
+  ] = getValuesFromColumns([
+    {
+      from_sheet: idsDataSheet,
+      columns: ['ENGLISH', 'SPANISH', 'FRENCH', 'PORTUGUESE', 'RUSSIAN'],
+    },
+    {
+      from_sheet: tsvSheet,
+      columns: ['en_gloss'],
+    },
+    {
+      from_sheet: tsvSheet,
+      columns: ['es_gloss', 'fr_gloss', 'pt_gloss', 'ru_gloss'],
+      is_range: true,
+    },
+  ])
+
+  let current_row_index: number
+  english_gloss_values.forEach((cell, i) => {
+    const matchIndex = english_values.findIndex((value) => {
+      if ((cell[0] === value[0]) && WRONG_TRANSLATIONS.includes(value[0])) {
+        // @ts-expect-error - GoogleApsScript magic
+        Logger.log(`English value: ${value}`)
+        current_row_index = i
+        return value
+      }
+      return false
+    })
+    if (matchIndex > -1) {
+      // get the new values
+      const new_spanish_value = spanish_values[matchIndex]
+      const new_french_value = french_values[matchIndex]
+      const new_portuguese_value = portuguese_values[matchIndex]
+      const new_russian_value = russian_values[matchIndex]
+      // set the new values to the target columns
+      spanish_column_range?.getCell(current_row_index + 1, 1).setValue(new_spanish_value)
+      french_column_range?.getCell(current_row_index + 1, 1).setValue(new_french_value)
+      portuguese_column_range?.getCell(current_row_index + 1, 1).setValue(new_portuguese_value)
+      russian_column_range?.getCell(current_row_index + 1, 1).setValue(new_russian_value)
+    }
+  })
+}
--- packages/ids-import/helpers.ts
+++ packages/ids-import/helpers.ts
@@ -1,50 +1,56 @@
+// @ts-expect-error - GoogleAppsScript magic
 function get_header_values(sheet: GoogleAppsScript.Spreadsheet.Sheet): string[] {
-  return sheet.getRange(1, 1, 1, sheet.getLastColumn()).getValues()[0];
+  return sheet.getRange(1, 1, 1, sheet.getLastColumn()).getValues()[0]
 }
 
 function get_first_empty_column(header_values: string[]): number {
-  return header_values.length + 1;
+  return header_values.length + 1
 }
 
+// @ts-expect-error - GoogleAppsScript magic
 function getValuesFromColumns(values_from_columns: ValuesFromColumns[]): string[][][] | GoogleAppsScript.Spreadsheet.Range[] {
   const values = []
   values_from_columns.forEach((element) => {
-    const {from_sheet, columns, are_columns_numbers, is_range} = element;
-    const header_values = get_header_values(from_sheet);
+    const { from_sheet, columns, are_columns_numbers, is_range } = element
+    const header_values = get_header_values(from_sheet)
     columns.forEach((column) => {
-      const ranges = from_sheet.getRange(
-        2,
-        are_columns_numbers ? column : header_values.indexOf(column) + 1,
-        from_sheet.getLastRow() - 1,
-        1);
-      if(is_range)
-        values.push(ranges);
-      else
-        values.push(ranges.getValues());
-    });
-  });
-  return values;
+      const columnIndex = are_columns_numbers ? column : header_values.indexOf(column) + 1
+      if (columnIndex > 0) {
+        const ranges = from_sheet.getRange(
+          2,
+          columnIndex,
+          from_sheet.getLastRow() - 1,
+          1,
+        )
+        if (is_range)
+          values.push(ranges)
+        else
+          values.push(ranges.getValues())
+      }
+    })
+  })
+  return values
 }
 
 function create_unique_ids(chapter_id_column_values: any[], entry_id_column_values: any[]): any[][] {
-  const concatenated_data_with_suffixes = [];
-  const concatenated_freq = {};
+  const concatenated_data_with_suffixes = []
+  const concatenated_freq = {}
 
   chapter_id_column_values.forEach((cell, i) => {
-    const prefix = Number(cell) <= 9 ? '0' : '';
-    const concatenated = `${prefix}${cell}.${entry_id_column_values[i]}`;
+    const prefix = Number(cell) <= 9 ? '0' : ''
+    const concatenated = `${prefix}${cell}.${entry_id_column_values[i]}`
 
     if (concatenated_freq[concatenated] === undefined) {
-      concatenated_freq[concatenated] = 1;
-      concatenated_data_with_suffixes.push([concatenated]);
+      concatenated_freq[concatenated] = 1
+      concatenated_data_with_suffixes.push([concatenated])
     } else {
-      concatenated_freq[concatenated]++;
-      const new_concatenated = concatenated + '-' + concatenated_freq[concatenated];
-      concatenated_data_with_suffixes.push([new_concatenated]);
+      concatenated_freq[concatenated]++
+      const new_concatenated = `${concatenated}-${concatenated_freq[concatenated]}`
+      concatenated_data_with_suffixes.push([new_concatenated])
     }
-  });
+  })
 
-  return concatenated_data_with_suffixes;
+  return concatenated_data_with_suffixes
 }
 
 if (import.meta.vitest) {
@@ -54,19 +60,19 @@ if (import.meta.vitest) {
       getRange: (row: number, col: number, numRows: number, numCols: number) => ({
         getValues: () => [['header1', 'header2']],
       }),
-    };
+    }
     expect(get_header_values(mockSheet)).toEqual(
       [
         'header1',
-        'header2'
-      ]
-    );
-  });
+        'header2',
+      ],
+    )
+  })
 
   test(get_first_empty_column, () => {
-    const header_values = ['chapter_id', 'entry_id', 'meaning', 'Example_Phonemic', 'comment'];
-    expect(get_first_empty_column(header_values)).toEqual(6);
-  });
+    const header_values = ['chapter_id', 'entry_id', 'meaning', 'Example_Phonemic', 'comment']
+    expect(get_first_empty_column(header_values)).toEqual(6)
+  })
 
   test(getValuesFromColumns, () => {
     const mockSheet = {
@@ -77,23 +83,23 @@ if (import.meta.vitest) {
           [['column1-value1'], ['column1-value2']],
           [['column2-value1'], ['column2-value2']],
           [['column3-range1'], ['column3-range2']],
-        ];
+        ]
         return {
-          getValues: () => columnValues[col - 1]
+          getValues: () => columnValues[col - 1],
         }
       },
-    };
+    }
     expect(getValuesFromColumns([
       {
         from_sheet: mockSheet,
         columns: [1, 2],
-        are_columns_numbers: true
+        are_columns_numbers: true,
       },
       {
         from_sheet: mockSheet,
         columns: [3],
         are_columns_numbers: true,
-        is_range: true
+        is_range: true,
       },
     ])).toEqual(
       [
@@ -116,9 +122,9 @@ if (import.meta.vitest) {
         {
           getValues: expect.any(Function),
         },
-      ]
-    );
-  });
+      ],
+    )
+  })
 
   test(create_unique_ids, () => {
     expect(create_unique_ids(['1', '9', '13', '1'], ['123', '234', '345', '123'])).toEqual(
@@ -135,7 +141,7 @@ if (import.meta.vitest) {
         [
           '01.123-2',
         ],
-      ]
-    );
-  });
+      ],
+    )
+  })
 }
