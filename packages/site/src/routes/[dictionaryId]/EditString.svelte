fc3c9c6a3fd62f38ae9930dd602c847f15dd8ca4
Diego Córdova Nieto <diego@livingtongues.org>
2024-05-27 19:29:32 -0600

    non editable fields

--- packages/site/src/routes/[dictionaryId]/EditString.svelte
+++ packages/site/src/routes/[dictionaryId]/EditString.svelte
@@ -8,30 +8,40 @@
   export let minlength = 0
   export let maxlength = 30
   export let required = false
+  export let can_edit = false
 
   export let save: (value: string) => Promise<void>
 </script>
 
-<Form let:loading onsubmit={async () => await save(value.trim())}>
-  <label for={id} class="text-sm font-medium text-gray-700 mb-2">
+{#if can_edit}
+  <Form let:loading onsubmit={async () => await save(value.trim())}>
+    <label for={id} class="text-sm font-medium text-gray-700 mb-2">
+      {display}
+    </label>
+    <div class="flex">
+      <input
+        {id}
+        type="text"
+        autocomplete="off"
+        autocorrect="off"
+        spellcheck={false}
+        {minlength}
+        {maxlength}
+        {required}
+        bind:value
+        class="form-input w-full"
+        placeholder={display} />
+      <div class="w-1" />
+      <Button class="shrink-0" {loading} type="submit">
+        {$page.data.t('misc.save')}
+      </Button>
+    </div>
+  </Form>
+{:else}
+  <div class="text-sm font-medium text-gray-700 mb-2">
     {display}
-  </label>
-  <div class="flex">
-    <input
-      {id}
-      type="text"
-      autocomplete="off"
-      autocorrect="off"
-      spellcheck={false}
-      {minlength}
-      {maxlength}
-      {required}
-      bind:value
-      class="form-input w-full"
-      placeholder={display} />
-    <div class="w-1" />
-    <Button class="shrink-0" {loading} type="submit">
-      {$page.data.t('misc.save')}
-    </Button>
   </div>
-</Form>
+  <p>
+    {value}
+  </p>
+{/if}
--- packages/site/src/routes/[dictionaryId]/settings/+page.svelte
+++ packages/site/src/routes/[dictionaryId]/settings/+page.svelte
@@ -4,6 +4,8 @@
   import { page } from '$app/stores'
   import EditableGlossesField from '$lib/components/settings/EditableGlossesField.svelte'
   import WhereSpoken from '$lib/components/settings/WhereSpoken.svelte'
+  import Map from '$lib/components/maps/mapbox/map/Map.svelte'
+  import Marker from '$lib/components/maps/mapbox/map/Marker.svelte'
   import EditableAlternateNames from '$lib/components/settings/EditableAlternateNames.svelte'
   import PublicCheckbox from '$lib/components/settings/PublicCheckbox.svelte' // only used here - perhaps colocate
   import PrintAccessCheckbox from '$lib/components/settings/PrintAccessCheckbox.svelte' // only used here - perhaps colocate
@@ -25,31 +27,26 @@
       minlength={2}
       required
       id="name"
+      can_edit={$can_edit}
       save={async name => await updateDictionary({ name })}
       display={$page.data.t('settings.edit_dict_name')} />
     <div class="mb-5" />
   {/if}
 
-  {#if $can_edit}
-    <EditString
-      value={$dictionary.iso6393}
-      id="iso6393"
-      save={async iso6393 => await updateDictionary({ iso6393 })}
-      display="ISO 639-3" />
-  {:else}
-    <p>{$dictionary.iso6393 || ''}</p>
-  {/if}
+  <EditString
+    value={$dictionary.iso6393}
+    id="iso6393"
+    can_edit={$can_edit}
+    save={async iso6393 => await updateDictionary({ iso6393 })}
+    display="ISO 639-3" />
   <div class="mb-5" />
 
-  {#if $can_edit}
-    <EditString
-      value={$dictionary.glottocode}
-      id="glottocode"
-      save={async glottocode => await updateDictionary({ glottocode })}
-      display="Glottocode" />
-  {:else}
-    <p>{$dictionary.glottocode || ''}</p>
-  {/if}
+  <EditString
+    value={$dictionary.glottocode}
+    id="glottocode"
+    can_edit={$can_edit}
+    save={async glottocode => await updateDictionary({ glottocode })}
+    display="Glottocode" />
   <div class="mb-5" />
 
   {#if $can_edit}
@@ -83,18 +80,33 @@
   {/if}
   <div class="mb-5" />
 
-  <WhereSpoken
-    dictionary={$dictionary}
-    on_update_coordinates={async coordinates => await updateDictionary({ coordinates })}
-    on_remove_coordinates={async () => await updateDictionary({ coordinates: null })}
-    on_update_points={async points => await updateDictionary({ points })}
-    on_update_regions={async regions => await updateDictionary({ regions })} />
+  {#if $can_edit}
+    <WhereSpoken
+      dictionary={$dictionary}
+      on_update_coordinates={async coordinates => await updateDictionary({ coordinates })}
+      on_remove_coordinates={async () => await updateDictionary({ coordinates: null })}
+      on_update_points={async points => await updateDictionary({ points })}
+      on_update_regions={async regions => await updateDictionary({ regions })} />
+  {:else}
+    <div class="text-sm font-medium text-gray-700 mb-1">Map</div> <!-- TODO Translate -->
+    <div class="h-240px">
+      <Map
+        lat={$dictionary.coordinates.latitude}
+        lng={$dictionary.coordinates.longitude}>
+        <Marker
+          lat={$dictionary.coordinates.latitude}
+          lng={$dictionary.coordinates.longitude}
+          color="red" />
+      </Map>
+    </div>
+  {/if}
   <div class="mb-5" />
 
   <EditString
     value={$dictionary.location}
     maxlength={100}
     id="location"
+    can_edit={$can_edit}
     save={async location => await updateDictionary({ location })}
     display={$page.data.t('dictionary.location')} />
   <div class="mb-5" />
@@ -104,7 +116,7 @@
   </div>
   {#if $dictionary.featuredImage}
     <Image
-      can_edit
+      can_edit={$can_edit}
       height={300}
       title="{$dictionary.name} Featured Image"
       gcs={$dictionary.featuredImage.specifiable_image_url}
@@ -116,24 +128,26 @@
   {/if}
   <div class="mb-5" />
 
-  <PrintAccessCheckbox
-    checked={$dictionary.printAccess}
-    on:changed={async ({ detail: { checked } }) => await updateDictionary({ printAccess: checked })} />
-  <div class="mb-5" />
+  {#if $can_edit}
+    <PrintAccessCheckbox
+      checked={$dictionary.printAccess}
+      on:changed={async ({ detail: { checked } }) => await updateDictionary({ printAccess: checked })} />
+    <div class="mb-5" />
 
-  <PublicCheckbox
-    checked={$dictionary.public}
-    on:changed={async ({ detail: { checked } }) => {
-      if (!checked) {
-        await updateDictionary({ public: false })
-      } else if ($admin) {
-        await updateDictionary({ public: true })
-      } else {
-        const communityAllowsOnline = confirm($page.data.t('settings.community_permission'))
-        if (communityAllowsOnline) alert($page.data.t('header.contact_us'))
-      }
-    }} />
-  <div class="mb-5" />
+    <PublicCheckbox
+      checked={$dictionary.public}
+      on:changed={async ({ detail: { checked } }) => {
+        if (!checked) {
+          await updateDictionary({ public: false })
+        } else if ($admin) {
+          await updateDictionary({ public: true })
+        } else {
+          const communityAllowsOnline = confirm($page.data.t('settings.community_permission'))
+          if (communityAllowsOnline) alert($page.data.t('header.contact_us'))
+        }
+      }} />
+    <div class="mb-5" />
+  {/if}
 
   {#if $is_manager}
     <div>
