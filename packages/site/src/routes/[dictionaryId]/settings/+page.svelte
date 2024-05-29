bf63a730934b886f6f88737be8dff8f4a153c778
Diego Córdova Nieto <diego@livingtongues.org>
2024-05-28 19:18:36 -0600

    handle empty values correctly

--- packages/site/src/routes/[dictionaryId]/settings/+page.svelte
+++ packages/site/src/routes/[dictionaryId]/settings/+page.svelte
@@ -33,100 +33,110 @@
     <div class="mb-5" />
   {/if}
 
-  <EditString
-    value={$dictionary.iso6393}
-    id="iso6393"
-    can_edit={$can_edit}
-    save={async iso6393 => await updateDictionary({ iso6393 })}
-    display="ISO 639-3" />
-  <div class="mb-5" />
-
-  <EditString
-    value={$dictionary.glottocode}
-    id="glottocode"
-    can_edit={$can_edit}
-    save={async glottocode => await updateDictionary({ glottocode })}
-    display="Glottocode" />
-  <div class="mb-5" />
-
-  {#if $can_edit}
-    <EditableGlossesField
-      minimum={1}
-      availableLanguages={glossingLanguages}
-      selectedLanguages={$dictionary.glossLanguages}
-      add_language={async languageId => await add_gloss_language(languageId)}
-      remove_language={async languageId => await remove_gloss_language(languageId)} />
-  {:else}
-    <div class="text-sm font-medium text-gray-700 mb-1">{$page.data.t('entry_field.gloss')}</div>
-    {#if $dictionary.glossLanguages?.length > 0}
-      {#each $dictionary.glossLanguages as gloss}
-        <p>{glossingLanguages[gloss].vernacularName}</p>
-      {/each}
-    {/if}
-  {/if}
-  <div class="mb-5" />
-
-  {#if $can_edit}
-    <EditableAlternateNames
-      alternateNames={$dictionary.alternateNames}
-      on_update={async new_value => await updateDictionary({ alternateNames: new_value })} />
-  {:else}
-    <div class="text-sm font-medium text-gray-700 mb-1">{$page.data.t('create.alternate_names')}</div>
-    {#if $dictionary.alternateNames?.length > 0}
-      {#each $dictionary.alternateNames as alternate_names}
-        <p>{alternate_names}</p>
-      {/each}
-    {/if}
-  {/if}
-  <div class="mb-5" />
-
-  {#if $can_edit}
-    <WhereSpoken
-      dictionary={$dictionary}
-      on_update_coordinates={async coordinates => await updateDictionary({ coordinates })}
-      on_remove_coordinates={async () => await updateDictionary({ coordinates: null })}
-      on_update_points={async points => await updateDictionary({ points })}
-      on_update_regions={async regions => await updateDictionary({ regions })} />
-  {:else}
-    <div class="text-sm font-medium text-gray-700 mb-1">Map</div> <!-- TODO Translate -->
-    <div class="h-240px">
-      <Map
-        lat={$dictionary.coordinates.latitude}
-        lng={$dictionary.coordinates.longitude}>
-        <Marker
-          lat={$dictionary.coordinates.latitude}
-          lng={$dictionary.coordinates.longitude}
-          color="red" />
-      </Map>
-    </div>
-  {/if}
-  <div class="mb-5" />
-
-  <EditString
-    value={$dictionary.location}
-    maxlength={100}
-    id="location"
-    can_edit={$can_edit}
-    save={async location => await updateDictionary({ location })}
-    display={$page.data.t('dictionary.location')} />
-  <div class="mb-5" />
-
-  <div class="text-sm font-medium text-gray-700 mb-2">
-    {$page.data.t('settings.featured_image')}
-  </div>
-  {#if $dictionary.featuredImage}
-    <Image
+  {#if $dictionary.iso6393}
+    <EditString
+      value={$dictionary.iso6393}
+      id="iso6393"
       can_edit={$can_edit}
-      height={300}
-      title="{$dictionary.name} Featured Image"
-      gcs={$dictionary.featuredImage.specifiable_image_url}
-      on_delete_image={async () => await updateDictionary({ featuredImage: null })} />
-  {:else}
-    <div class="hover:bg-gray-100 min-h-150px flex flex-col">
-      <AddImage border upload_image={add_featured_image} />
-    </div>
+      save={async iso6393 => await updateDictionary({ iso6393 })}
+      display="ISO 639-3" />
+    <div class="mb-5" />
   {/if}
-  <div class="mb-5" />
+
+  {#if $dictionary.glottocode}
+    <EditString
+      value={$dictionary.glottocode}
+      id="glottocode"
+      can_edit={$can_edit}
+      save={async glottocode => await updateDictionary({ glottocode })}
+      display="Glottocode" />
+    <div class="mb-5" />
+  {/if}
+
+  {#if $dictionary.glossLanguages?.length > 0}
+    {#if $can_edit} <!-- TODO put can_edit inside the component -->
+      <EditableGlossesField
+        minimum={1}
+        availableLanguages={glossingLanguages}
+        selectedLanguages={$dictionary.glossLanguages}
+        add_language={async languageId => await add_gloss_language(languageId)}
+        remove_language={async languageId => await remove_gloss_language(languageId)} />
+    {:else}
+      <div class="text-sm font-medium text-gray-700 mb-1">{$page.data.t('entry_field.gloss')}</div>
+      {#if $dictionary.glossLanguages?.length > 0}
+        {$dictionary.glossLanguages.map(gl => glossingLanguages[gl].vernacularName).join(', ')}
+      {/if}
+    {/if}
+    <div class="mb-5" />
+  {/if}
+
+  {#if $dictionary.alternateNames?.length > 0}
+    {#if $can_edit} <!-- TODO put can_edit inside the component -->
+      <EditableAlternateNames
+        alternateNames={$dictionary.alternateNames}
+        on_update={async new_value => await updateDictionary({ alternateNames: new_value })} />
+    {:else}
+      <div class="text-sm font-medium text-gray-700 mb-1">{$page.data.t('create.alternate_names')}</div>
+      {#if $dictionary.alternateNames?.length > 0}
+        {$dictionary.alternateNames.join(', ')}
+      {/if}
+    {/if}
+    <div class="mb-5" />
+  {/if}
+
+  {#if $dictionary.coordinates}
+    {#if $can_edit}
+      <WhereSpoken
+        dictionary={$dictionary}
+        on_update_coordinates={async coordinates => await updateDictionary({ coordinates })}
+        on_remove_coordinates={async () => await updateDictionary({ coordinates: null })}
+        on_update_points={async points => await updateDictionary({ points })}
+        on_update_regions={async regions => await updateDictionary({ regions })} />
+    {:else}
+      <div class="text-sm font-medium text-gray-700 mb-1">Map</div> <!-- TODO Translate -->
+      <div class="h-240px">
+        <Map
+          lat={$dictionary.coordinates?.latitude}
+          lng={$dictionary.coordinates?.longitude}>
+          <Marker
+            lat={$dictionary.coordinates?.latitude}
+            lng={$dictionary.coordinates?.longitude}
+            color="red" />
+        </Map>
+      </div>
+    {/if}
+    <div class="mb-5" />
+  {/if}
+
+  {#if $dictionary.location}
+    <EditString
+      value={$dictionary.location}
+      maxlength={100}
+      id="location"
+      can_edit={$can_edit}
+      save={async location => await updateDictionary({ location })}
+      display={$page.data.t('dictionary.location')} />
+    <div class="mb-5" />
+  {/if}
+
+  {#if $dictionary.featuredImage}
+    <div class="text-sm font-medium text-gray-700 mb-2">
+      {$page.data.t('settings.featured_image')}
+    </div>
+    {#if $dictionary.featuredImage}
+      <Image
+        can_edit={$can_edit}
+        height={300}
+        title="{$dictionary.name} Featured Image"
+        gcs={$dictionary.featuredImage.specifiable_image_url}
+        on_delete_image={async () => await updateDictionary({ featuredImage: null })} />
+    {:else}
+      <div class="hover:bg-gray-100 min-h-150px flex flex-col">
+        <AddImage border upload_image={add_featured_image} />
+      </div>
+    {/if}
+    <div class="mb-5" />
+  {/if}
 
   {#if $can_edit}
     <PrintAccessCheckbox
