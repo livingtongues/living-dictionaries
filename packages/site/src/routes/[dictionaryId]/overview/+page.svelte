f6c4f9698c03ee0cf8c0c557025fef70ca7fbbc2
Diego Córdova Nieto <diego@livingtongues.org>
2024-06-04 13:49:58 -0600

    add featured image and SEO tags component

--- packages/site/src/routes/[dictionaryId]/overview/+page.svelte
+++ packages/site/src/routes/[dictionaryId]/overview/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
   import DisplayString from './DisplayString.svelte'
   import VisualMap from './VisualMap.svelte'
+  import Image from '$lib/components/image/Image.svelte'
+  import SeoMetaTags from '$lib/components/SeoMetaTags.svelte'
   import { page } from '$app/stores'
   import { glossingLanguages } from '$lib/glosses/glossing-languages'
 
@@ -23,4 +25,22 @@
   <DisplayString display={$page.data.t('dictionary.location')} value={$dictionary.location} />
   <VisualMap coordinates={$dictionary.coordinates} />
   <div class="mb-5" />
+  {#if $dictionary.featuredImage}
+    <div class="text-sm font-medium text-gray-700 mb-2">
+      {$page.data.t('settings.featured_image')}
+    </div>
+    <Image
+      can_edit={false}
+      height={300}
+      title="{$dictionary.name} Featured Image"
+      gcs={$dictionary.featuredImage.specifiable_image_url}
+      on_delete_image={null} />
+  {/if}
+  <div class="mb-5" />
 </div>
+
+<SeoMetaTags
+  title="Overview"
+  dictionaryName={$dictionary.name}
+  description="Overview Description."
+  keywords="Parameters, ISO 639-3, Glottocde, glossing languages, alternate names, GPS, language medata, public dictionary, private dictionary, Endangered Languages, Language Documentation, Language Revitalization, Build a Dictionary, Online Dictionary, Digital Dictionary, Dictionary Software, Free Software, Online Dictionary Builder, Living Dictionaries, Living Dictionary, Edit a dictionary, Search a dictionary, Browse a dictionary, Explore a Dictionary" />
