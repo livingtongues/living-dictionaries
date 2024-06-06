9d73b883e3ac7f807b158badfd8b897173d39e63
Diego Córdova Nieto <diego@livingtongues.org>
2024-06-05 18:35:35 -0600

    synopsis page visible  only for non editors

--- packages/site/src/routes/[dictionaryId]/SideMenu.svelte
+++ packages/site/src/routes/[dictionaryId]/SideMenu.svelte
@@ -74,14 +74,16 @@
       {$page.data.t('dictionary.grammar')}
     </span>
   </a>
-  <a
-    href={`/${dictionary.id}/synopsis`}
-    class:active={$page.url.pathname.includes('synopsis')}>
-    <i class="fal fa-file-alt fa-fw" />
-    <span class="font-medium mx-2">
-      {$page.data.t('synopsis.name')}
-    </span>
-  </a>
+  {#if !is_manager}
+    <a
+      href={`/${dictionary.id}/synopsis`}
+      class:active={$page.url.pathname.includes('synopsis')}>
+      <i class="fal fa-file-alt fa-fw" />
+      <span class="font-medium mx-2">
+        {$page.data.t('synopsis.name')}
+      </span>
+    </a>
+  {/if}
   {#if is_manager}
     <a
       href={`/${dictionary.id}/import`}
