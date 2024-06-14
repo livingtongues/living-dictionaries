1a00355bad39cb240b758f7f83eb1aab117d2059
Diego Córdova Nieto <43384963+Danble@users.noreply.github.com>
2024-06-14 14:01:18 -0600

    7000 languages contact (#463)
    * Send email to 7000 Languages for new learning materials option in Contact form

--- packages/site/src/routes/api/email/addresses.ts
+++ packages/site/src/routes/api/email/addresses.ts
@@ -1,46 +1,63 @@
-import { dev } from '$app/environment';
-import type { Address } from './send/mail-channels.interface';
-import { firebaseConfig } from 'sveltefirets';
+import { firebaseConfig } from 'sveltefirets'
+import type { Address } from './send/mail-channels.interface'
+import { dev } from '$app/environment'
 
 export const noReplyAddress = {
   email: 'no-reply@livingdictionaries.app',
   name: 'Living Tongues Institute for Endangered Languages',
-};
+}
 
-export const officialAddress = { email: 'dictionaries@livingtongues.org' };
-const gregAddress = { email: 'livingtongues@gmail.com' };
+export const officialAddress = { email: 'dictionaries@livingtongues.org' }
+const gregAddress = { email: 'livingtongues@gmail.com' }
+const languages7000 = { email: 'info@7000.org' }
 
 export function getAdminRecipients(initiatorEmail: string): Address[] {
   if (initiatorEmail === 'jacob@livingtongues.org'
     || initiatorEmail === 'diego@livingtongues.org')
-    return [{ email: initiatorEmail }];
+    return [{ email: initiatorEmail }]
 
   const recipients: Address[] = [
     { email: 'jacob@livingtongues.org' },
     { email: 'diego@livingtongues.org' },
-  ];
+  ]
 
   if (dev || firebaseConfig.projectId === 'talking-dictionaries-dev')
-    return recipients;
+    return recipients
 
   return [
     ...recipients,
     officialAddress,
     gregAddress,
-  ];
+  ]
 }
 
 export function getSupportMessageRecipients({ dev }: { dev: boolean }): Address[] {
   const recipients: Address[] = [
     { email: 'jacob@livingtongues.org' },
     { email: 'diego@livingtongues.org' },
-  ];
+  ]
 
   if (dev || firebaseConfig.projectId === 'talking-dictionaries-dev')
-    return recipients;
+    return recipients
 
   return [
     ...recipients,
     officialAddress,
-  ];
+  ]
+}
+
+export function getLanguageLearningMaterialsRecipients({ dev }: { dev: boolean }): Address[] {
+  const recipients: Address[] = [
+    { email: 'jacob@livingtongues.org' },
+    { email: 'diego@livingtongues.org' },
+  ]
+
+  if (dev || firebaseConfig.projectId === 'talking-dictionaries-dev')
+    return recipients
+
+  return [
+    ...recipients,
+    officialAddress,
+    languages7000,
+  ]
 }
