2353c14dfc14549b05ba9f11074e6dea60adcb0e
Diego Córdova Nieto <diego@livingtongues.org>
2024-07-17 11:35:23 -0600

    create specific types for Full_Sense and Full_Sentence types

--- packages/scripts/import/row.type.ts
+++ packages/scripts/import/row.type.ts
@@ -1,14 +1,14 @@
 import type { Glossing_Languages } from '@living-dictionaries/site/src/lib/glosses/glossing-languages'
 
 export type Row = {
-  [key in (Entry_Fields | Sense_Fields | Sentence_Fields)]?: string;
+  [key in (Full_Entry | Full_Sense | Full_Sentence)]?: string;
 }
 // 's3.es_gloss': 'hi',
 // 'semanticDomain4': '2.3',
 // 's2.fr_exampleSentence.3': 'Bonjour docteur',
 // 's4.default_vernacular_exampleSentence': 'foo bar',
 
-type Entry_Fields = `${String_Fields | Multiple_Fields | Translation_Fields}`
+type Full_Entry = `${String_Fields | Multiple_Fields | Translation_Fields}`
 
 type String_Fields = 'lexeme' | 'dialects' | 'ID' | 'soundFile' | 'speakerName' | 'scientificName' | 'speakerHometown' | 'speakerAge' | 'speakerGender' | 'notes' | 'source' | 'morphology' | 'interlinearization' | 'photoFile' | 'vernacular_exampleSentence' | 'pluralForm' | 'nounClass' | 'variant' | 'phonetic' | 'semanticDomain_custom'
 type Fields_That_Can_Have_Multiple = 'localOrthography' | 'partOfSpeech' | 'semanticDomain'
@@ -16,8 +16,10 @@ type Multiple_Fields = `${Fields_That_Can_Have_Multiple}${Number_Suffix}`
 type Number_Suffix = '' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9'
 type Translation_Fields = `${Glossing_Languages}_gloss` | `${Glossing_Languages}_exampleSentence` | `${Writing_Systems}_vernacular_exampleSentence`
 
-type Sense_Fields = `${Sense_Prefix}.${Entry_Fields}` // TODO: too broad
+type Sense_Fields = `${Glossing_Languages}_gloss` | `partOfSpeech${Number_Suffix}` | `semanticDomain${Number_Suffix}` | 'nounClass'
+type Full_Sense = `${Sense_Prefix}.${Sense_Fields}`
 type Sense_Prefix = 's2' | 's3' | 's4' | 's5' | 's6' | 's7' | 's8' | 's9'
 
-type Sentence_Fields = `${Sense_Prefix}.${Entry_Fields}.${Number_Suffix}` // TODO: too broad
+type Sentence_Fields = `${Glossing_Languages}_exampleSentence` | `${Writing_Systems}_vernacular_exampleSentence`
+type Full_Sentence = `${Sense_Prefix}.${Sentence_Fields}` | `${Sense_Prefix}.${Sentence_Fields}.${Number_Suffix}`
 type Writing_Systems = 'default' // TODO improve Writing Systems field
