bec81e8e5d43bb496f65c150838bcb2936c2534c
Diego Córdova Nieto <43384963+Danble@users.noreply.github.com>
2024-07-26 11:33:01 -0600

    create specific types for Full_Sense and Full_Sentence types (#470)
    * separate all row types into entry, sense, sentence, media
    
    ---------
    
    Co-authored-by: Jacob Bowdoin <7559478+jacob-8@users.noreply.github.com>

--- packages/scripts/import/row.type.ts
+++ packages/scripts/import/row.type.ts
@@ -1,23 +1,24 @@
 import type { Glossing_Languages } from '@living-dictionaries/site/src/lib/glosses/glossing-languages'
 
 export type Row = {
-  [key in (Entry_Fields | Sense_Fields | Sentence_Fields)]?: string;
+  [key in (Entry | Sense | Sentence | Media)]?: string;
 }
 // 's3.es_gloss': 'hi',
 // 'semanticDomain4': '2.3',
 // 's2.fr_exampleSentence.3': 'Bonjour docteur',
 // 's4.default_vernacular_exampleSentence': 'foo bar',
 
-type Entry_Fields = `${String_Fields | Multiple_Fields | Translation_Fields}`
+type Sense_Prefix = '' | 's2.' | 's3.' | 's4.' | 's5.' | 's6.' | 's7.' | 's8.' | 's9.'
+type Number_Suffix = '' | '.2' | '.3' | '.4' | '.5' | '.6' | '.7' | '.8' | '.9'
 
-type String_Fields = 'lexeme' | 'dialects' | 'ID' | 'soundFile' | 'speakerName' | 'scientificName' | 'speakerHometown' | 'speakerAge' | 'speakerGender' | 'notes' | 'source' | 'morphology' | 'interlinearization' | 'photoFile' | 'vernacular_exampleSentence' | 'pluralForm' | 'nounClass' | 'variant' | 'phonetic' | 'semanticDomain_custom'
-type Fields_That_Can_Have_Multiple = 'localOrthography' | 'partOfSpeech' | 'semanticDomain'
-type Multiple_Fields = `${Fields_That_Can_Have_Multiple}${Number_Suffix}`
-type Number_Suffix = '' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9'
-type Translation_Fields = `${Glossing_Languages}_gloss` | `${Glossing_Languages}_exampleSentence` | `${Writing_Systems}_vernacular_exampleSentence`
+type Entry = 'lexeme' | `localOrthography${Number_Suffix}` | 'phonetic' | 'dialects' | 'ID' | 'notes' | 'source' | 'morphology' | 'interlinearization' | 'scientificName' | 'pluralForm' | 'variant'
 
-type Sense_Fields = `${Sense_Prefix}.${Entry_Fields}` // TODO: too broad
-type Sense_Prefix = 's2' | 's3' | 's4' | 's5' | 's6' | 's7' | 's8' | 's9'
+type Sense = `${Sense_Prefix}${Sense_Fields}`
+type Sense_Fields = `${Glossing_Languages}_gloss` | `partOfSpeech${Number_Suffix}` | `semanticDomain${Number_Suffix}` | 'semanticDomain_custom' | 'nounClass' // en_gloss, s2.en_gloss, nounClass, s2.nounClass
 
-type Sentence_Fields = `${Sense_Prefix}.${Entry_Fields}.${Number_Suffix}` // TODO: too broad
-type Writing_Systems = 'default' // TODO improve Writing Systems field
+type Sentence = `${Sense_Prefix}${Sentence_Fields}` | `${Sense_Prefix}${Sentence_Fields}${Number_Suffix}`
+type Sentence_Fields = `${Glossing_Languages}_exampleSentence` | `default_vernacular_exampleSentence` | 'vernacular_exampleSentence'
+
+type Media = 'photoFile' | 'soundFile' | 'speakerName' | 'speakerHometown' | 'speakerAge' | 'speakerGender'
+
+// type Writing_Systems = 'default' // TODO improve Writing Systems field
