6fcb5d55389ced2bfbddcce9516381d5c5d72332
Diego Córdova Nieto <43384963+Danble@users.noreply.github.com>
2024-08-12 20:58:16 -0600

    Revert table view speakers (#477)

--- packages/site/src/routes/[dictionaryId]/entries/table/cells/SelectSpeakerCell.svelte
+++ packages/site/src/routes/[dictionaryId]/entries/table/cells/SelectSpeakerCell.svelte
@@ -1,20 +1,10 @@
 <script lang="ts">
-  import type { ExpandedEntry, ISpeaker } from '@living-dictionaries/types'
+  import type { ExpandedEntry } from '@living-dictionaries/types'
 
   export let entry: ExpandedEntry
   export let can_edit = false
-  export let speakers: ISpeaker[]
-  let displayed_speaker_name: string
 
-  $: first_sound_file = entry?.sound_files?.[0]
-  $: if (first_sound_file?.speaker_ids?.length) {
-    displayed_speaker_name = speakers.find(speaker => speaker.uid === first_sound_file.speaker_ids[0])?.displayName
-    if (!displayed_speaker_name) {
-      console.error(`Missing speaker ID: ${first_sound_file.speaker_ids[0]}`)
-    }
-  } else {
-    displayed_speaker_name = first_sound_file?.speakerName
-  }
+  $: first_sound_file = entry.sound_files?.[0]
 </script>
 
 <div
@@ -32,6 +22,12 @@
       }
     }
   }}>
-  {displayed_speaker_name || ''}
+  {#if first_sound_file}
+    {#if first_sound_file.speaker_ids?.length}
+      {first_sound_file.speaker_ids[0]}
+    {:else}
+      {first_sound_file.speakerName || ''}
+    {/if}
+  {/if}
   &nbsp;
 </div>
