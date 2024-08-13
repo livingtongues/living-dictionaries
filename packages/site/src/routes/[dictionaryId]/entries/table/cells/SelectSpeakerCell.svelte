dc0901d08798d05db152036ed20b80f4010b58c1
Diego Córdova Nieto <43384963+Danble@users.noreply.github.com>
2024-08-13 13:29:10 -0600

    Table view speakers (#478)
    * fix speaker name display instead of id in table view

--- packages/site/src/routes/[dictionaryId]/entries/table/cells/SelectSpeakerCell.svelte
+++ packages/site/src/routes/[dictionaryId]/entries/table/cells/SelectSpeakerCell.svelte
@@ -1,10 +1,20 @@
 <script lang="ts">
-  import type { ExpandedEntry } from '@living-dictionaries/types'
+  import type { ExpandedEntry, ISpeaker } from '@living-dictionaries/types'
 
   export let entry: ExpandedEntry
   export let can_edit = false
+  export let speakers: ISpeaker[]
+  let displayed_speaker_name: string
 
-  $: first_sound_file = entry.sound_files?.[0]
+  $: first_sound_file = entry?.sound_files?.[0]
+  $: if (speakers?.length && first_sound_file?.speaker_ids?.length) {
+    displayed_speaker_name = speakers.find(speaker => speaker.id === first_sound_file.speaker_ids[0])?.displayName
+    if (!displayed_speaker_name) {
+      console.warn(`Missing speaker ID: ${first_sound_file.speaker_ids[0]}`)
+    }
+  } else {
+    displayed_speaker_name = first_sound_file?.speakerName
+  }
 </script>
 
 <div
@@ -22,12 +32,6 @@
       }
     }
   }}>
-  {#if first_sound_file}
-    {#if first_sound_file.speaker_ids?.length}
-      {first_sound_file.speaker_ids[0]}
-    {:else}
-      {first_sound_file.speakerName || ''}
-    {/if}
-  {/if}
+  {displayed_speaker_name || ''}
   &nbsp;
 </div>
