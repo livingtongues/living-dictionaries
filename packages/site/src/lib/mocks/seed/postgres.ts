f7684d2ad5c8ea753b51ed6bdc57050c83dd9056
jacob-8 <jwrunner7@gmail.com>
2024-10-21 16:35:32 +0800

    get db tests passing

--- packages/site/src/lib/mocks/seed/postgres.ts
+++ packages/site/src/lib/mocks/seed/postgres.ts
@@ -1,24 +1,24 @@
 import PG from 'pg' // import all because library is not written for ESM yet
 
-export async function execute_sql_query_on_db(query: string) {
-  const client = new PG.Client({
-    user: 'postgres',
-    host: '127.0.0.1',
-    database: 'postgres',
-    password: 'postgres',
-    port: 54322,
-  })
-  try {
-    await client.connect()
-    await client.query(query)
-  } catch (error) {
-    console.error('Error in connection/executing query:', error)
-  } finally {
-    await client.end().catch((error) => {
-      console.error('Error ending client connection:', error)
-    })
-  }
-}
+// export async function execute_sql_query_on_db(query: string) {
+//   const client = new PG.Client({
+//     user: 'postgres',
+//     host: '127.0.0.1',
+//     database: 'postgres',
+//     password: 'postgres',
+//     port: 54322,
+//   })
+//   try {
+//     await client.connect()
+//     await client.query(query)
+//   } catch (error) {
+//     console.error('Error in connection/executing query:', error)
+//   } finally {
+//     await client.end().catch((error) => {
+//       console.error('Error ending client connection:', error)
+//     })
+//   }
+// }
 
 class DB {
   private pool: PG.Pool
--- packages/site/src/lib/mocks/seed/write-seed-and-reset-db.ts
+++ packages/site/src/lib/mocks/seed/write-seed-and-reset-db.ts
@@ -1,7 +1,7 @@
 import { writeFileSync } from 'node:fs'
-import { execute_sql_query_on_db } from './postgres'
 import { sql_file_string } from './to-sql-string'
 import { seed_dictionaries, seed_entries, users } from './tables'
+import { postgres } from '$lib/mocks/seed/postgres'
 
 function sql_string_for_all_seeded_tables() {
   return `${sql_file_string('auth.users', users)}
@@ -13,11 +13,11 @@ function sql_string_for_all_seeded_tables() {
 export async function reset_db() {
   console.info('reseting db')
 
-  await execute_sql_query_on_db(`truncate table auth.users cascade;`)
-  await execute_sql_query_on_db('truncate table senses cascade;')
+  await postgres.execute_query(`truncate table auth.users cascade;`)
+  await postgres.execute_query('truncate table senses cascade;')
 
   const seed_sql = sql_string_for_all_seeded_tables()
-  await execute_sql_query_on_db(seed_sql)
+  await postgres.execute_query(seed_sql)
 }
 
 export function write_seed() {
