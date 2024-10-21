75fac7a73a33546baae7f14a97154f9ed49a86d8
jacob-8 <jwrunner7@gmail.com>
2024-10-21 09:12:54 +0800

    make banner float above page

--- packages/site/src/routes/Banner.svelte
+++ packages/site/src/routes/Banner.svelte
@@ -1,9 +1,16 @@
 <script lang='ts'>
+  import { Button, ShowHide } from 'svelte-pieces'
   import { page } from '$app/stores'
-
-  let hide_message = false
 </script>
 
-<div class:hidden={hide_message} class='bg-amber-400 pl-4 sticky top-0 z-20 md:text-sm text-xs w-full py-1'>
-  <span class="cursor-pointer" on:click={() => hide_message = true}>&times;</span>
-  {$page.data.t('banner.attention')}: <strong>{$page.data.t('banner.message')}</strong></div>
+<ShowHide let:show={hide} let:toggle>
+  {#if !hide}
+    <div class='bg-amber-400 fixed top-3 left-3 right-3 rounded z-20 p-2 flex'>
+      <div>
+        <b>{$page.data.t('banner.attention')}</b>: {$page.data.t('banner.message')}
+      </div>
+      <div class="w-2"></div>
+      <Button form="simple" color="black" onclick={toggle}><span class="i-fa-solid-times" /></Button>
+    </div>
+  {/if}
+</ShowHide>
