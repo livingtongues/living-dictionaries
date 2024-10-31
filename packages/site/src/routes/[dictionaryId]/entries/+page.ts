51bd9a2e7ad9b1ec90320656d42a0af264c01bd6
Jacob Bowdoin <7559478+jacob-8@users.noreply.github.com>
2024-10-31 17:59:43 +0800

    Search typo tolerance (#502)
    * add default of 1 typo tolerance to search, make it adjustable, and sort by relevance once a search term is entered
    
    * fix semantic domains search and parts of speech with periods

--- packages/site/src/routes/[dictionaryId]/entries/+page.ts
+++ packages/site/src/routes/[dictionaryId]/entries/+page.ts
@@ -1,7 +1,7 @@
 import { createQueryParamStore } from 'svelte-pieces'
 import type { QueryParams } from '$lib/search/types'
 
-export function load({ params: { dictionaryId } }) {
+export function load() {
   const default_params: QueryParams = {
     page: 1,
     query: '',
@@ -9,7 +9,7 @@ export function load({ params: { dictionaryId } }) {
   const search_params = createQueryParamStore({
     key: 'q',
     startWith: default_params,
-    storagePrefix: `${dictionaryId}_`,
+    // storagePrefix: `${dictionaryId}_`,
     cleanFalseValues: true,
     // replaceState: false,
   })
