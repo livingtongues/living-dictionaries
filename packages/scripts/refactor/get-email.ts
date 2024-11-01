1a2cedac418a8703fd5cfa0c027ed98ca1aa12c3
jacob-8 <jwrunner7@gmail.com>
2024-11-01 17:52:18 +0800

    get all emails

--- packages/scripts/refactor/get-email.ts
+++ packages/scripts/refactor/get-email.ts
@@ -1,25 +1,36 @@
+import type { IUser } from '@living-dictionaries/types'
 import { db } from '../config-firebase'
 
 const dictionary_ids = ['shauki', 'nongtrai', 'eyak', 'early-assamese', 'werikyana', 'nambya', 'garifuna', 'arvanitika', 'awakateko', 'marma', 'birhor', 'kihunde', 'ngiwa---popoloca', 'memoni', 'judeo-iraqi-arabic', 'namtrik-de-totoro', 'belarusian', 'seniwan', 'zapotec']
 
-get_emails(dictionary_ids)
+// get_dictionary_emails(dictionary_ids)
 
-async function get_emails(dictionary_ids: string[]) {
+async function get_dictionary_emails(dictionary_ids: string[]) {
   const emails = new Set<string>()
   for (const dictionary_id of dictionary_ids) {
     const snapshot = await db.collection(`dictionaries/${dictionary_id}/managers`).get()
     for (const snap of snapshot.docs) {
-      const user = (await db.collection('users').doc(snap.id).get()).data()
+      const user = (await db.collection('users').doc(snap.id).get()).data() as IUser
       console.log({ user })
       emails.add(user.email)
     }
 
     const snapshot2 = await db.collection(`dictionaries/${dictionary_id}/contributors`).get()
     for (const snap of snapshot2.docs) {
-      const user = (await db.collection('users').doc(snap.id).get()).data()
+      const user = (await db.collection('users').doc(snap.id).get()).data() as IUser
       console.log({ user })
       emails.add(user.email)
     }
   }
   console.log(Array.from(emails))
 }
+
+async function get_all_emails() {
+  const userSnapshots = await db.collection('users').get()
+  const users = userSnapshots.docs.map(doc => doc.data()) as IUser[]
+  for (const user of users) {
+    console.log(user.email)
+  }
+}
+
+get_all_emails()
