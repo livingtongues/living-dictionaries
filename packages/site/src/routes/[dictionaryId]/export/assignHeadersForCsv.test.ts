4f1f83057b5e2e5591af31276cd42720e7581ce4
Diego Córdova Nieto <diego@livingtongues.org>
2024-11-06 17:42:30 -0600

    improve getting headers and reactivate their tests

--- packages/site/src/routes/[dictionaryId]/export/assignHeadersForCsv.test.ts
+++ packages/site/src/routes/[dictionaryId]/export/assignHeadersForCsv.test.ts
@@ -1,80 +1,84 @@
 // import type { ExpandedEntry } from '@living-dictionaries/types'
-// import {
-//   get_example_sentence_headers,
-//   get_gloss_language_headers,
-//   get_local_orthography_headers,
-//   get_semantic_domain_headers,
-// } from './assignHeadersForCsv'
+import {
+  // get_example_sentence_headers,
+  get_gloss_language_headers,
+  get_local_orthography_headers,
+  get_semantic_domain_headers,
+} from './assignHeadersForCsv'
 
-// describe(get_local_orthography_headers, () => {
-//   test('assigns alternate_orthographies if any exists', () => {
-//     const alternate_orthographies = ['native-1', 'native-2']
-//     expect(get_local_orthography_headers(alternate_orthographies)).toEqual({
-//       local_orthography_1: 'native-1',
-//       local_orthography_2: 'native-2',
-//     })
-//   })
-//   test('doesn\'t assign alternate_orthographies if empty array', () => {
-//     const alternate_orthographies = []
-//     expect(get_local_orthography_headers(alternate_orthographies)).toEqual({})
-//   })
-//   test('doesn\'t assign alternate_orthographies if null', () => {
-//     const alternate_orthographies = null
-//     expect(get_local_orthography_headers(alternate_orthographies)).toEqual({})
-//   })
-// })
+describe(get_local_orthography_headers, () => {
+  test('assigns alternate_orthographies if any exists', () => {
+    const alternate_orthographies = ['native-1', 'native-2', 'native-3']
+    expect(get_local_orthography_headers(alternate_orthographies)).toEqual({
+      'localOrthography': 'native-1',
+      'localOrthography.2': 'native-2',
+      'localOrthography.3': 'native-3',
+    })
+  })
+  test('doesn\'t assign alternate_orthographies if empty array', () => {
+    const alternate_orthographies = []
+    expect(get_local_orthography_headers(alternate_orthographies)).toEqual({})
+  })
+  test('doesn\'t assign alternate_orthographies if null', () => {
+    const alternate_orthographies = null
+    expect(get_local_orthography_headers(alternate_orthographies)).toEqual({})
+  })
+})
 
-// describe(get_semantic_domain_headers, () => {
-//   test('adds semantic domain headers if any exists', () => {
-//     const entries: ExpandedEntry[] = [
-//       {
-//         lexeme: 'foo',
-//         senses: [{ translated_ld_semantic_domains: ['NA'] }],
-//       },
-//       {
-//         lexeme: 'bar',
-//         senses: [{ translated_ld_semantic_domains: ['NA', 'NA', 'NA'] }],
-//       },
-//     ]
-//     expect(get_semantic_domain_headers(entries)).toEqual({
-//       semantic_domain_1: 'Semantic domain 1',
-//       semantic_domain_2: 'Semantic domain 2',
-//       semantic_domain_3: 'Semantic domain 3',
-//     })
-//   })
+describe(get_semantic_domain_headers, () => {
+  test('adds semantic domain headers if any exists', () => {
+    const semantic_domains = ['NA', 'NA', 'NA']
+    const first_sense_index = 0
+    expect(get_semantic_domain_headers(semantic_domains, first_sense_index)).toEqual({
+      'semanticDomain': 'Semantic domain 1',
+      'semanticDomain.2': 'Semantic domain 2',
+      'semanticDomain.3': 'Semantic domain 3',
+    })
+  })
 
-//   test('does not add semantic domain headers if none exist', () => {
-//     const entries = [
-//       {
-//         lexeme: 'foo',
-//         senses: [],
-//       },
-//       {
-//         lexeme: 'bar',
-//       },
-//     ]
-//     expect(get_semantic_domain_headers(entries)).toEqual({})
-//   })
-// })
+  test('adds semantic domain to third sense', () => {
+    const third_sense_index = 2
+    const semantic_domains = ['NA', 'NA']
+    expect(get_semantic_domain_headers(semantic_domains, third_sense_index)).toEqual({
+      's3.semanticDomain': 'Semantic domain 1',
+      's3.semanticDomain.2': 'Semantic domain 2',
+    })
+  })
 
-// describe(get_gloss_language_headers, () => {
-//   test('uses full name or bcp if it no name exists', () => {
-//     const gloss_languages = ['en', 'es', 'af']
-//     expect(get_gloss_language_headers(gloss_languages)).toEqual({
-//       en_gloss_language: 'English Gloss',
-//       es_gloss_language: 'español Gloss',
-//       af_gloss_language: 'Afrikaans Gloss',
-//     })
-//   })
-//   test('doesn\'t assign gloss languages if empty array', () => {
-//     const gloss_languages = []
-//     expect(get_gloss_language_headers(gloss_languages)).toEqual({})
-//   })
-//   test('doesn\'t assign gloss languages if null', () => {
-//     const gloss_languages = null
-//     expect(get_gloss_language_headers(gloss_languages)).toEqual({})
-//   })
-// })
+  test('does not add semantic domain headers if none exist', () => {
+    const first_sense_index = 0
+    const semantic_domains = []
+    expect(get_semantic_domain_headers(semantic_domains, first_sense_index)).toEqual({})
+  })
+})
+
+describe(get_gloss_language_headers, () => {
+  const first_sense_index = 0
+  test('uses full name or bcp if it no name exists', () => {
+    const glosses = { ar: 'فغي', en: 'red', es: 'rojo' }
+    expect(get_gloss_language_headers(glosses, first_sense_index)).toEqual({
+      ar_gloss: 'العَرَبِيَّة‎ Gloss',
+      en_gloss: 'English Gloss',
+      es_gloss: 'español Gloss',
+    })
+  })
+  test('uses glosses in second sense', () => {
+    const second_sense_index = 1
+    const glosses = { en: 'blue', es: 'azul' }
+    expect(get_gloss_language_headers(glosses, second_sense_index)).toEqual({
+      's2.en_gloss': 'English Gloss',
+      's2.es_gloss': 'español Gloss',
+    })
+  })
+  test('doesn\'t assign gloss languages if empty array', () => {
+    const glosses = {}
+    expect(get_gloss_language_headers(glosses, first_sense_index)).toEqual({})
+  })
+  test('doesn\'t assign gloss languages if null', () => {
+    const glosses = null
+    expect(get_gloss_language_headers(glosses, first_sense_index)).toEqual({})
+  })
+})
 
 // describe(get_example_sentence_headers, () => {
 //   test('assigns vernacular and other example sentences if any exists or bcp if it doesn\'t', () => {
--- packages/site/src/routes/[dictionaryId]/export/assignHeadersForCsv.ts
+++ packages/site/src/routes/[dictionaryId]/export/assignHeadersForCsv.ts
@@ -1,4 +1,4 @@
-import type { EntryView, SenseWithSentences } from '@living-dictionaries/types'
+import type { EntryView, MultiString } from '@living-dictionaries/types'
 import type { EntryForCSV } from './prepareEntriesForCsv'
 import { glossingLanguages } from '$lib/glosses/glossing-languages'
 
@@ -22,62 +22,52 @@ export function get_local_orthography_headers(
 export function get_sense_headers(entries: EntryView[]) {
   let headers: EntryForCSV = {}
 
-  // Using for of loop for a slightly better performance
+  // Using for of loops for a slightly better performance
   for (const entry of entries) {
-    console.info(`Entry: ${JSON.stringify(entry)}`)
-    headers = { ...headers, ...get_semantic_domain_headers(entry.senses) }
-    // for (const sense of entry.senses) {
-    //   console.log(`sense: ${sense}`)
-    // }
-  }
-
-  // if (max_senses_in_dictionary > 0) {
-  //   for (let index = 0; index <= max_senses_in_dictionary; index++) {
-  //     headers = { ...headers, ...get_semantic_domain_headers(entries[index].senses, index) }
-  //   }
-  // }
-
-  return headers
-}
-
-export function get_semantic_domain_headers(senses: SenseWithSentences[]) {
-  const headers: EntryForCSV = {}
-
-  for (const [sense_index, sense] of Array.from(senses).entries()) {
-    if (sense.semantic_domains) {
-      // max_semantic_domains = Math.max(max_semantic_domains, sense.semantic_domains.length)
-      for (let index = 0; index < sense.semantic_domains.length; index++) {
-        headers[`${sense_index > 0 ? `s${sense_index + 1}.` : ''}semanticDomain${index > 0 ? `.${index + 1}` : ''}`] = `Semantic domain ${index + 1}`
-      }
+    for (const [sense_index, sense] of Array.from(entry.senses).entries()) {
+      //* get glosses headers
+      headers = { ...headers, ...get_gloss_language_headers(sense.glosses, sense_index) }
+      //* get semantic domains headers
+      headers = { ...headers, ...get_semantic_domain_headers(sense.semantic_domains, sense_index) }
     }
   }
 
   return headers
 }
 
-export function get_gloss_language_headers(gloss_languages: string[]) {
+export function get_gloss_language_headers(glosses: MultiString, sense_index: number) {
   const headers: EntryForCSV = {}
-  if (gloss_languages) {
-    gloss_languages.forEach((bcp) => {
-      headers[`${bcp}_gloss_language`] = `${glossingLanguages[bcp].vernacularName || bcp} Gloss`
+  if (glosses) {
+    Object.keys(glosses).forEach((bcp) => {
+      headers[`${sense_index > 0 ? `s${sense_index + 1}.` : ''}${bcp}_gloss`] = `${glossingLanguages[bcp].vernacularName || bcp} Gloss`
     })
   }
   return headers
 }
 
-export function get_example_sentence_headers(
-  gloss_languages: string[],
-  dictionary_name: string,
-) {
+export function get_semantic_domain_headers(semantic_domains: string[], sense_index: number) {
   const headers: EntryForCSV = {}
-  // TODO, the vernacular is now sentence.text?.default and the languages are at sentence.translation[bcp]
-  const _vernacular_example_sentence_header = `Example sentence in ${dictionary_name}`
-  if (gloss_languages) {
-    gloss_languages.forEach((bcp) => {
-      headers[`${bcp}_example_sentence`] = `Example sentence in ${
-        glossingLanguages[bcp].vernacularName || bcp
-      }`
-    })
+  if (semantic_domains) {
+    for (let index = 0; index < semantic_domains.length; index++) {
+      headers[`${sense_index > 0 ? `s${sense_index + 1}.` : ''}semanticDomain${index > 0 ? `.${index + 1}` : ''}`] = `Semantic domain ${index + 1}`
+    }
   }
   return headers
 }
+
+// export function get_example_sentence_headers(
+//   gloss_languages: string[],
+//   dictionary_name: string,
+// ) {
+//   const headers: EntryForCSV = {}
+//   // TODO, the vernacular is now sentence.text?.default and the languages are at sentence.translation[bcp]
+//   const _vernacular_example_sentence_header = `Example sentence in ${dictionary_name}`
+//   if (gloss_languages) {
+//     gloss_languages.forEach((bcp) => {
+//       headers[`${bcp}_example_sentence`] = `Example sentence in ${
+//         glossingLanguages[bcp].vernacularName || bcp
+//       }`
+//     })
+//   }
+//   return headers
+// }
