87cc4f696fbead551d2abe31ee5cb1204575e120
jacob-8 <jwrunner7@gmail.com>
2024-11-14 11:12:13 +0800

    remove unused firebase functions code

--- packages/functions/src/db.ts
+++
@@ -1,5 +0,0 @@
-import { initializeApp } from 'firebase-admin/app';
-import { getFirestore } from 'firebase-admin/firestore';
-
-initializeApp();
-export const db = getFirestore();
--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -4,6 +4,5 @@ export default defineWorkspace([
   'packages/site/vitest.config.ts',
   'packages/types/vitest.config.ts',
   'packages/scripts/vitest.config.ts',
-  'packages/functions/vitest.config.ts',
   'packages/ids-import/vitest.config.ts',
 ])
