b6a2da44a00811f79a9cc77e8d1e5bb6d962f665
Diego Córdova Nieto <diego@livingtongues.org>
2024-11-15 13:34:27 -0600

    improve informational headers

--- packages/site/src/routes/[dictionaryId]/export/assignHeadersForCsv.ts
+++ packages/site/src/routes/[dictionaryId]/export/assignHeadersForCsv.ts
@@ -45,7 +45,7 @@ export function get_gloss_language_headers(glosses: MultiString, sense_index: nu
   const headers: EntryForCSV = {}
   if (glosses) {
     Object.keys(glosses).forEach((bcp) => {
-      headers[`${sense_index > 0 ? `s${sense_index + 1}.` : ''}${bcp}_gloss`] = `${glossingLanguages[bcp].vernacularName || bcp} Gloss`
+      headers[`${sense_index > 0 ? `s${sense_index + 1}.` : ''}${bcp}_gloss`] = `${sense_index > 0 ? `Sense ${sense_index + 1}: ` : ''}${glossingLanguages[bcp].vernacularName || bcp} Gloss`
     })
   }
   return headers
@@ -55,7 +55,7 @@ export function get_semantic_domain_headers(semantic_domains: string[], sense_in
   const headers: EntryForCSV = {}
   if (semantic_domains) {
     for (let index = 0; index < semantic_domains.length; index++) {
-      headers[`${sense_index > 0 ? `s${sense_index + 1}.` : ''}semanticDomain${index > 0 ? `.${index + 1}` : ''}`] = `Semantic domain ${index + 1}`
+      headers[`${sense_index > 0 ? `s${sense_index + 1}.` : ''}semanticDomain${index > 0 ? `.${index + 1}` : ''}`] = `${sense_index > 0 ? `Sense ${sense_index + 1}: ` : ''}Semantic domain ${index + 1}`
     }
   }
   return headers
@@ -65,8 +65,8 @@ export function get_parts_of_speech_headers(parts_of_speech_abbreviations: strin
   const headers: EntryForCSV = {}
   if (parts_of_speech) {
     for (let index = 0; index < parts_of_speech_abbreviations.length; index++) {
-      headers[`${sense_index > 0 ? `s${sense_index + 1}.` : ''}partOfSpeech${index > 0 ? `.${index + 1}` : ''}`] = `Part of speech ${index + 1} (abbreviation)`
-      headers[`${sense_index > 0 ? `s${sense_index + 1}.` : ''}partOfSpeech fullname${index > 0 ? `.${index + 1}` : ''}`] = `Part of speech ${index + 1}`
+      headers[`${sense_index > 0 ? `s${sense_index + 1}.` : ''}partOfSpeech${index > 0 ? `.${index + 1}` : ''}`] = `${sense_index > 0 ? `Sense ${sense_index + 1}: ` : ''}Part of speech ${index + 1} (abbreviation)`
+      headers[`${sense_index > 0 ? `s${sense_index + 1}.` : ''}partOfSpeech fullname${index > 0 ? `.${index + 1}` : ''}`] = `${sense_index > 0 ? `Sense ${sense_index + 1}: ` : ''}Part of speech ${index + 1}`
     }
   }
   return headers
@@ -78,11 +78,11 @@ export function get_example_sentence_headers(
 ) {
   const headers: EntryForCSV = {}
   if (sentence?.text) {
-    headers[`${sense_index > 0 ? `s${sense_index + 1}.` : ''}vernacular_exampleSentence`] = `Example sentence in ${sentence?.dictionary_id}`
+    headers[`${sense_index > 0 ? `s${sense_index + 1}.` : ''}vernacular_exampleSentence`] = `${sense_index > 0 ? `Sense ${sense_index + 1}: ` : ''}Example sentence in ${sentence?.dictionary_id}`
   }
   if (sentence?.translation) {
     Object.keys(sentence?.translation).forEach((bcp) => {
-      headers[`${sense_index > 0 ? `s${sense_index + 1}.` : ''}${bcp}_exampleSentence`] = `Example sentence in ${glossingLanguages[bcp].vernacularName || bcp}`
+      headers[`${sense_index > 0 ? `s${sense_index + 1}.` : ''}${bcp}_exampleSentence`] = `${sense_index > 0 ? `Sense ${sense_index + 1}: ` : ''}Example sentence in ${glossingLanguages[bcp].vernacularName || bcp}`
     })
   }
 
@@ -93,8 +93,8 @@ export function get_image_files_headers(image_storage_path: string, sense_index:
   const headers: EntryForCSV = {}
 
   if (image_storage_path) {
-    headers[`${sense_index > 0 ? `s${sense_index + 1}.` : ''}photoFile`] = 'Image filename'
-    headers[`${sense_index > 0 ? `s${sense_index + 1}.` : ''}photoSource`] = 'Source of image'
+    headers[`${sense_index > 0 ? `s${sense_index + 1}.` : ''}photoFile`] = `${sense_index > 0 ? `Sense ${sense_index + 1}: ` : ''}Image filename`
+    headers[`${sense_index > 0 ? `s${sense_index + 1}.` : ''}photoSource`] = `${sense_index > 0 ? `Sense ${sense_index + 1}: ` : ''}Source of image`
   }
 
   return headers
@@ -103,21 +103,21 @@ export function get_image_files_headers(image_storage_path: string, sense_index:
 export function get_noun_class_headers(noun_class: string, sense_index: number) {
   const headers: EntryForCSV = {}
   if (noun_class) {
-    headers[`${sense_index > 0 ? `s${sense_index + 1}.` : ''}nounClass`] = 'Noun class'
+    headers[`${sense_index > 0 ? `s${sense_index + 1}.` : ''}nounClass`] = `${sense_index > 0 ? `Sense ${sense_index + 1}: ` : ''}Noun class`
   }
   return headers
 }
 export function get_variant_headers(variant: MultiString, sense_index: number) {
   const headers: EntryForCSV = {}
   if (variant?.default) {
-    headers[`${sense_index > 0 ? `s${sense_index + 1}.` : ''}variant`] = 'Variant'
+    headers[`${sense_index > 0 ? `s${sense_index + 1}.` : ''}variant`] = `${sense_index > 0 ? `Sense ${sense_index + 1}: ` : ''}Variant`
   }
   return headers
 }
 export function get_plural_form_headers(plural_form: MultiString, sense_index: number) {
   const headers: EntryForCSV = {}
   if (plural_form?.default) {
-    headers[`${sense_index > 0 ? `s${sense_index + 1}.` : ''}pluralForm`] = 'Plural form'
+    headers[`${sense_index > 0 ? `s${sense_index + 1}.` : ''}pluralForm`] = `${sense_index > 0 ? `Sense ${sense_index + 1}: ` : ''}Plural form`
   }
   return headers
 }
