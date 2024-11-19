0e74419df16c8341324409448ab3e61595959dd2
Diego Córdova Nieto <diego@livingtongues.org>
2024-11-18 19:07:44 -0600

    refactoring headers

--- packages/site/src/routes/[dictionaryId]/export/assignFormattedEntryValuesForCsv.ts
+++ packages/site/src/routes/[dictionaryId]/export/assignFormattedEntryValuesForCsv.ts
@@ -124,33 +124,3 @@ export function format_example_sentence(
 
   return headers
 }
-// export function format_semantic_domains(
-//   entry: ExpandedEntry,
-// ): EntryForCSV {
-//   const domains = {}
-//   for (const [index, domain] of (entry.senses?.[0].translated_ld_semantic_domains || []).entries())
-//     domains[`semantic_domain_${index + 1}`] = domain
-//   return domains
-// }
-
-// export function format_gloss_languages(
-//   entry: ExpandedEntry,
-// ): EntryForCSV {
-//   const gloss_languages = {}
-//   for (const [bcp, value] of Object.entries(entry.senses?.[0].glosses || {}))
-//     gloss_languages[`${bcp}_gloss_language`] = value
-//   return gloss_languages
-// }
-
-// export function format_example_sentences(
-//   entry: ExpandedEntry,
-// ): EntryForCSV {
-//   const example_sentences: EntryForCSV = {}
-//   for (const [bcp, value] of Object.entries(entry.senses?.[0].example_sentences?.[0] || {})) {
-//     if (bcp === 'vn')
-//       example_sentences.vernacular_example_sentence = value
-//     else
-//       example_sentences[`${bcp}_example_sentence`] = value
-//   }
-//   return example_sentences
-// }
--- packages/site/src/routes/[dictionaryId]/export/assignHeadersForCsv.test.ts
+++ packages/site/src/routes/[dictionaryId]/export/assignHeadersForCsv.test.ts
@@ -1,12 +1,13 @@
 import type { Tables } from '@living-dictionaries/types'
 import {
-  get_example_sentence_headers,
-  get_gloss_language_headers,
-  get_image_files_headers,
-  get_local_orthography_headers,
-  get_parts_of_speech_headers,
-  get_semantic_domain_headers,
-} from './assignHeadersForCsv'
+  get_example_sentence,
+  get_glosses,
+  get_image_files,
+  get_parts_of_speech,
+  get_semantic_domain,
+} from './getRows'
+
+import { get_local_orthography_headers } from './assignHeadersForCsv'
 
 describe(get_local_orthography_headers, () => {
   test('assigns alternate_orthographies if any exists', () => {
@@ -27,11 +28,11 @@ describe(get_local_orthography_headers, () => {
   })
 })
 
-describe(get_semantic_domain_headers, () => {
+describe(get_semantic_domain, () => {
   test('adds semantic domain headers if any exists', () => {
     const semantic_domains = ['NA', 'NA', 'NA']
     const first_sense_index = 0
-    expect(get_semantic_domain_headers(semantic_domains, first_sense_index)).toEqual({
+    expect(get_semantic_domain(semantic_domains, { sense_index: first_sense_index, position: 'header' })).toEqual({
       'semanticDomain': 'Semantic domain 1',
       'semanticDomain.2': 'Semantic domain 2',
       'semanticDomain.3': 'Semantic domain 3',
@@ -41,24 +42,24 @@ describe(get_semantic_domain_headers, () => {
   test('adds semantic domain to third sense', () => {
     const third_sense_index = 2
     const semantic_domains = ['NA', 'NA']
-    expect(get_semantic_domain_headers(semantic_domains, third_sense_index)).toEqual({
-      's3.semanticDomain': 'Semantic domain 1',
-      's3.semanticDomain.2': 'Semantic domain 2',
+    expect(get_semantic_domain(semantic_domains, { sense_index: third_sense_index, position: 'header' })).toEqual({
+      's3.semanticDomain': 'Sense 3: Semantic domain 1',
+      's3.semanticDomain.2': 'Sense 3: Semantic domain 2',
     })
   })
 
   test('does not add semantic domain headers if none exist', () => {
     const first_sense_index = 0
     const semantic_domains = []
-    expect(get_semantic_domain_headers(semantic_domains, first_sense_index)).toEqual({})
+    expect(get_semantic_domain(semantic_domains, { sense_index: first_sense_index, position: 'header' })).toEqual({})
   })
 })
-describe(get_parts_of_speech_headers, () => {
+describe(get_parts_of_speech, () => {
   test('adds parts of speech headers if any exists', () => {
     const parts_of_speech_abbreviations = ['n', 'v']
     const parts_of_speech = ['noun', 'verb']
     const first_sense_index = 0
-    expect(get_parts_of_speech_headers(parts_of_speech_abbreviations, parts_of_speech, first_sense_index)).toEqual({
+    expect(get_parts_of_speech(parts_of_speech_abbreviations, parts_of_speech, { sense_index: first_sense_index, position: 'header' })).toEqual({
       'partOfSpeech': 'Part of speech 1 (abbreviation)',
       'partOfSpeech.2': 'Part of speech 2 (abbreviation)',
       'partOfSpeech fullname': 'Part of speech 1',
@@ -70,11 +71,11 @@ describe(get_parts_of_speech_headers, () => {
     const third_sense_index = 2
     const parts_of_speech_abbreviations = ['n', 'adj']
     const parts_of_speech = ['sustantivo', 'adjetivo']
-    expect(get_parts_of_speech_headers(parts_of_speech_abbreviations, parts_of_speech, third_sense_index)).toEqual({
-      's3.partOfSpeech': 'Part of speech 1 (abbreviation)',
-      's3.partOfSpeech.2': 'Part of speech 2 (abbreviation)',
-      's3.partOfSpeech fullname': 'Part of speech 1',
-      's3.partOfSpeech fullname.2': 'Part of speech 2',
+    expect(get_parts_of_speech(parts_of_speech_abbreviations, parts_of_speech, { sense_index: third_sense_index, position: 'header' })).toEqual({
+      's3.partOfSpeech': 'Sense 3: Part of speech 1 (abbreviation)',
+      's3.partOfSpeech.2': 'Sense 3: Part of speech 2 (abbreviation)',
+      's3.partOfSpeech fullname': 'Sense 3: Part of speech 1',
+      's3.partOfSpeech fullname.2': 'Sense 3: Part of speech 2',
     })
   })
 
@@ -82,15 +83,15 @@ describe(get_parts_of_speech_headers, () => {
     const first_sense_index = 0
     const parts_of_speech_abbreviations = []
     const parts_of_speech = []
-    expect(get_parts_of_speech_headers(parts_of_speech_abbreviations, parts_of_speech, first_sense_index)).toEqual({})
+    expect(get_parts_of_speech(parts_of_speech_abbreviations, parts_of_speech, { sense_index: first_sense_index, position: 'header' })).toEqual({})
   })
 })
 
-describe(get_gloss_language_headers, () => {
+describe(get_glosses, () => {
   const first_sense_index = 0
   test('uses full name or bcp if it no name exists', () => {
     const glosses = { ar: 'فغي', en: 'red', es: 'rojo' }
-    expect(get_gloss_language_headers(glosses, first_sense_index)).toEqual({
+    expect(get_glosses(glosses, { sense_index: first_sense_index, position: 'header' })).toEqual({
       ar_gloss: 'العَرَبِيَّة‎ Gloss',
       en_gloss: 'English Gloss',
       es_gloss: 'español Gloss',
@@ -99,27 +100,27 @@ describe(get_gloss_language_headers, () => {
   test('uses glosses in second sense', () => {
     const second_sense_index = 1
     const glosses = { en: 'blue', es: 'azul' }
-    expect(get_gloss_language_headers(glosses, second_sense_index)).toEqual({
-      's2.en_gloss': 'English Gloss',
-      's2.es_gloss': 'español Gloss',
+    expect(get_glosses(glosses, { sense_index: second_sense_index, position: 'header' })).toEqual({
+      's2.en_gloss': 'Sense 2: English Gloss',
+      's2.es_gloss': 'Sense 2: español Gloss',
     })
   })
   test('doesn\'t assign gloss languages if empty array', () => {
     const glosses = {}
-    expect(get_gloss_language_headers(glosses, first_sense_index)).toEqual({})
+    expect(get_glosses(glosses, { sense_index: first_sense_index, position: 'header' })).toEqual({})
   })
   test('doesn\'t assign gloss languages if null', () => {
     const glosses = null
-    expect(get_gloss_language_headers(glosses, first_sense_index)).toEqual({})
+    expect(get_glosses(glosses, { sense_index: first_sense_index, position: 'header' })).toEqual({})
   })
 })
 
-describe(get_example_sentence_headers, () => {
+describe(get_example_sentence, () => {
   const first_sense_index = 0
   test('assigns vernacular and translations', () => {
     // @ts-ignore
     const sentences: Tables<'sentences'> = { dictionary_id: 'example', text: { default: 'vernacular example sentence' }, translation: { en: 'English example sentence', es: 'Oración de ejemplo en español' } }
-    expect(get_example_sentence_headers(sentences, first_sense_index)).toEqual({
+    expect(get_example_sentence(sentences, { sense_index: first_sense_index, position: 'header' })).toEqual({
       vernacular_exampleSentence: 'Example sentence in example',
       en_exampleSentence: 'Example sentence in English',
       es_exampleSentence: 'Example sentence in español',
@@ -129,29 +130,29 @@ describe(get_example_sentence_headers, () => {
     // @ts-ignore
     const sentences: Tables<'sentences'> = { dictionary_id: 'example', text: { default: 'vernacular example sentence' }, translation: { en: 'English example sentence', es: 'Oración de ejemplo en español' } }
     const fourth_sense_index = 3
-    expect(get_example_sentence_headers(sentences, fourth_sense_index)).toEqual({
-      's4.vernacular_exampleSentence': 'Example sentence in example',
-      's4.en_exampleSentence': 'Example sentence in English',
-      's4.es_exampleSentence': 'Example sentence in español',
+    expect(get_example_sentence(sentences, { sense_index: fourth_sense_index, position: 'header' })).toEqual({
+      's4.vernacular_exampleSentence': 'Sense 4: Example sentence in example',
+      's4.en_exampleSentence': 'Sense 4: Example sentence in English',
+      's4.es_exampleSentence': 'Sense 4: Example sentence in español',
     })
   })
   test('assigns only verncular', () => {
     // @ts-ignore
     const sentences: Tables<'sentences'> = { dictionary_id: 'example', text: { default: 'vernacular example sentence' } }
-    expect(get_example_sentence_headers(sentences, first_sense_index)).toEqual({
+    expect(get_example_sentence(sentences, { sense_index: first_sense_index, position: 'header' })).toEqual({
       vernacular_exampleSentence: 'Example sentence in example',
     })
   })
   test('doesn\'t assign anything if null', () => {
-    expect(get_example_sentence_headers(null, first_sense_index)).toEqual({})
+    expect(get_example_sentence(null, { sense_index: first_sense_index, position: 'header' })).toEqual({})
   })
 })
 
-describe(get_image_files_headers, () => {
+describe(get_image_files, () => {
   test('adds photo filename and source photo', () => {
     const image_id = 'abc'
     const first_sense_index = 0
-    expect(get_image_files_headers(image_id, first_sense_index)).toEqual({
+    expect(get_image_files(image_id, { sense_index: first_sense_index, position: 'header' })).toEqual({
       photoFile: 'Image filename',
       photoSource: 'Source of image',
     })
@@ -159,20 +160,20 @@ describe(get_image_files_headers, () => {
   test('adds photo filename and source photo in second sense', () => {
     const image_id = 'abc'
     const second_sense_index = 1
-    expect(get_image_files_headers(image_id, second_sense_index)).toEqual({
-      's2.photoFile': 'Image filename',
-      's2.photoSource': 'Source of image',
+    expect(get_image_files(image_id, { sense_index: second_sense_index, position: 'header' })).toEqual({
+      's2.photoFile': 'Sense 2: Image filename',
+      's2.photoSource': 'Sense 2: Source of image',
     })
   })
   test('doesn\'t assign anything if empty string', () => {
     const image_id = ''
     const first_sense_index = 0
-    expect(get_image_files_headers(image_id, first_sense_index)).toEqual({})
+    expect(get_image_files(image_id, { sense_index: first_sense_index, position: 'header' })).toEqual({})
   })
   test('doesn\'t assign anything if null', () => {
     const image_id = null
     const first_sense_index = 0
-    expect(get_image_files_headers(image_id, first_sense_index)).toEqual({})
+    expect(get_image_files(image_id, { sense_index: first_sense_index, position: 'header' })).toEqual({})
   })
 })
-// TODO add last test for get_sense_headers
+// TODO add last test for get_sense
