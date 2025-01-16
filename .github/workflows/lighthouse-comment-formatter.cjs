5fd075259976e9a284a5ae03f318f32cf3130b72
Jacob Bowdoin <7559478+jacob-8@users.noreply.github.com>
2025-01-16 10:23:13 -0600

    Revert lighthouse-comment-formatter.cjs

--- .github/workflows/lighthouse-comment-formatter.cjs
+++ .github/workflows/lighthouse-comment-formatter.cjs
@@ -45,12 +45,6 @@ const scoreSimple = (
 function makeComment(lighthouseOutputs, targetPlatform) {
   let comment = `## ⚡️Lighthouse report`;
 
-  if (targetPlatform === 'pr') {
-    comment += `
-<details><summary>Average score across ${lighthouseOutputs.manifest.length} targets: <b>${computeAverageScore(lighthouseOutputs)}%</b></summary>
-`
-  }
-
   for (const manifest of lighthouseOutputs.manifest) {
     const { url: testedUrl, summary } = manifest;
     const reportUrl = lighthouseOutputs.links[testedUrl];
@@ -75,28 +69,7 @@ ${scoreRow('PWA', summary.pwa)}
     }
   }
 
-  if (targetPlatform === 'pr') {
-    comment += `
-</details>`
-  }
-
   return comment;
-}
-
-/**
- * @param {LighthouseOutputs} lighthouseOutputs
- * @return {string}
- */
-function computeAverageScore(lighthouseOutputs) {
-  let totalScore = 0;
-  for (const manifest of lighthouseOutputs.manifest) {
-    const { summary } = manifest;
-    totalScore += summary.performance / 4;
-    totalScore += summary['best-practices'] / 4;
-    totalScore += summary.seo / 4;
-    totalScore += summary.pwa / 4;
-  }
-  return ((totalScore / lighthouseOutputs.manifest.length) * 100).toFixed(1);
 }
 
 module.exports = ({ lighthouseOutputs, targetPlatform }) => {
