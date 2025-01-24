ff75470e479647db40c8e56379863ab1872b5c19
Diego Córdova Nieto <diego@livingtongues.org>
2025-01-23 18:12:01 -0600

    improve css and add scrollbar to admin users table as well

--- packages/site/src/routes/admin/users/+page.svelte
+++ packages/site/src/routes/admin/users/+page.svelte
@@ -1,28 +1,28 @@
 <script lang="ts">
-  import type { IUser } from '@living-dictionaries/types';
-  import { Collection } from 'sveltefirets';
-  import Filter from '$lib/components/Filter.svelte';
-  import { downloadObjectsAsCSV } from '$lib/export/csv';
-  import { Button, ResponsiveTable } from 'svelte-pieces';
-  import UserRow from './UserRow.svelte';
-  import SortUsers from './SortUsers.svelte';
+  import type { IUser } from '@living-dictionaries/types'
+  import { Collection } from 'sveltefirets'
+  import { Button, ResponsiveTable } from 'svelte-pieces'
+  import UserRow from './UserRow.svelte'
+  import SortUsers from './SortUsers.svelte'
+  import { downloadObjectsAsCSV } from '$lib/export/csv'
+  import Filter from '$lib/components/Filter.svelte'
 
-  const usersType: IUser[] = [];
+  const usersType: IUser[] = []
 
   function exportUsersAsCSV(users: IUser[]) {
     const headers = {
       displayName: 'Name',
       email: 'Email',
-    };
+    }
 
     const formattedUsers = users.map((user) => {
       return {
         displayName: user.displayName?.replace(/,/, ''),
         email: user.email,
-      };
-    });
+      }
+    })
 
-    downloadObjectsAsCSV(headers, formattedUsers, 'livingdictionary-users');
+    downloadObjectsAsCSV(headers, formattedUsers, 'livingdictionary-users')
   }
 </script>
 
@@ -36,7 +36,7 @@
         </Button>
       </div>
       <div class="mb-1" />
-      <ResponsiveTable stickyColumn stickyHeading>
+      <ResponsiveTable class="!overflow-unset" stickyColumn stickyHeading>
         <SortUsers users={filteredUsers} let:sortedUsers>
           {#each sortedUsers as user (user.uid)}
             <UserRow {user} />
