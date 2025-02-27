236501e41cd64666baba4e8b8c33d5b4afc26364
Diego Córdova Nieto <43384963+Danble@users.noreply.github.com>
2025-02-27 11:23:27 -0600

    add conlangs path to create dictionary form (#532)
    * add conlangs path to create dictionary form
    
    * finalize create dictionary page
    
    ---------
    
    Co-authored-by: jacob-8 <jwrunner7@gmail.com>

--- packages/site/src/routes/create-dictionary/+page.svelte
+++ packages/site/src/routes/create-dictionary/+page.svelte
@@ -5,13 +5,13 @@
   import { convertToFriendlyUrl } from './convertToFriendlyUrl'
   import { page } from '$app/stores'
   import Header from '$lib/components/shell/Header.svelte'
+  import EditableGlossesField from '$lib/components/settings/EditableGlossesField.svelte'
+  import WhereSpoken from '$lib/components/settings/WhereSpoken.svelte'
+  import EditableAlternateNames from '$lib/components/settings/EditableAlternateNames.svelte'
+  import { glossingLanguages } from '$lib/glosses/glossing-languages'
   import SeoMetaTags from '$lib/components/SeoMetaTags.svelte'
   import { debounce } from '$lib/helpers/debounce'
   import { browser, dev } from '$app/environment'
-  import EditableGlossesField from '$lib/components/settings/EditableGlossesField.svelte'
-  import EditableAlternateNames from '$lib/components/settings/EditableAlternateNames.svelte'
-  import WhereSpoken from '$lib/components/settings/WhereSpoken.svelte'
-  import { glossingLanguages } from '$lib/glosses/glossing-languages'
 
   export let data
   $: ({ user } = data)
@@ -27,10 +27,11 @@
   let regions: IRegion[] = []
   let iso_639_3 = ''
   let glottocode = ''
-  let language_used_by_community: boolean
+  let conlang: boolean
   let community_permission: 'yes' | 'no' | 'unknown'
   let author_connection = ''
-  let con_language_description = ''
+  let conlang_source = ''
+  let conlang_use = ''
 
   $: urlFromName = convertToFriendlyUrl(name, MAX_URL_LENGTH)
   let customUrl: string
@@ -54,106 +55,179 @@
   onMount(() => {
     if (dev && browser) {
       name = `Test${Date.now()}`
-      language_used_by_community = false
       community_permission = 'no'
-      author_connection = 'a'.repeat(100)
+      author_connection = 'aaaaa '.repeat(10)
     }
   })
 </script>
 
 <Header>{$page.data.t('create.create_new_dictionary')}</Header>
 
-{#if !dev}
-  <div class="flex justify-center my-[50px] mx-6 md:mx-auto md:w-[35%] text-justify">
-    <strong>
-      <p>{$page.data.t('misc.out_of_service')}</p>
-      <br />
-      <p>{$page.data.t('misc.transmitter')}</p>
-    </strong>
-  </div>
-{:else}
-  <Form
-    let:loading
-    onsubmit={async () => {
-      if (!$user) return modal = 'auth'
+<Form
+  let:loading
+  onsubmit={async () => {
+    if (!$user) return modal = 'auth'
 
-      await data.create_dictionary({
-        id: urlToUse,
-        name: name.trim().replace(/^./, name[0].toUpperCase()),
-        gloss_languages: Array.from(gloss_languages),
-        alternate_names,
-        coordinates: points || regions ? { points, regions } : null,
-        iso_639_3: iso_639_3.trim(),
-        glottocode: glottocode.trim(),
-        language_used_by_community,
-        community_permission,
-        author_connection,
-        con_language_description,
-      })
-    }}>
-    <div class="flex-col justify-center p-4 max-w-md mx-auto">
-      <label for="name" class="block text-xl font-medium text-gray-700">
-        {$page.data.t('dictionary.name_of_language')}*
-      </label>
-      <div class="mt-2 rounded-md shadow-sm">
+    await data.create_dictionary({
+      id: urlToUse,
+      name: name.trim().replace(/^./, name[0].toUpperCase()),
+      gloss_languages: Array.from(gloss_languages),
+      alternate_names,
+      coordinates: points || regions ? { points, regions } : null,
+      iso_639_3: iso_639_3.trim(),
+      glottocode: glottocode.trim(),
+      community_permission,
+      author_connection,
+      con_language_description: conlang
+        ? `Source: ${conlang_source.trim()}
+
+Use: ${conlang_use.trim()}`
+        : null,
+    })
+  }}>
+  <div class="flex-col justify-center p-4 max-w-md mx-auto">
+    <label for="name" class="block text-xl font-medium text-gray-700">
+      {$page.data.t('dictionary.name_of_language')}*
+    </label>
+    <div class="mt-2 rounded-md shadow-sm">
+      <input
+        id="name"
+        type="text"
+        autocomplete="off"
+        autocorrect="off"
+        spellcheck={false}
+        autofocus
+        minlength={data.MIN_URL_LENGTH}
+        required
+        bind:value={name}
+        class="form-input w-full" />
+    </div>
+    <div class="text-xs text-gray-600 mt-1">
+      {$page.data.t('create.name_clarification')}
+    </div>
+    <div class="mb-6" />
+
+    {#if name.length > 2}
+      {#if dev}
+        <Button type="submit" class="mb-5 w-full" color="red">
+          Dev: Add Test Dictionary Immediately
+        </Button>
+      {/if}
+      <div class="flex justify-between items-center" style="direction: ltr">
+        <label for="url" class="text-sm font-medium text-gray-700"> URL </label>
+      </div>
+
+      <div class="mt-1 flex rounded-md shadow-sm" style="direction: ltr">
+        <span
+          class="inline-flex items-center px-2 rounded-l-md border border-r-0
+            border-gray-300 bg-gray-50 text-gray-500 text-sm">
+          livingdictionaries.app/
+        </span>
         <input
-          id="name"
-          type="text"
+          id="url"
+          value={customUrl || urlFromName}
+          on:keyup={handleUrlKeyup}
+          required
+          minlength={data.MIN_URL_LENGTH}
+          maxlength={MAX_URL_LENGTH}
           autocomplete="off"
           autocorrect="off"
           spellcheck={false}
-          autofocus
-          minlength={data.MIN_URL_LENGTH}
-          required
-          bind:value={name}
-          class="form-input w-full" />
+          class="form-input flex-1 block w-full px-2 sm:px-3 py-2 rounded-none
+            rounded-r-md sm:text-sm border"
+          placeholder="url" />
       </div>
       <div class="text-xs text-gray-600 mt-1">
-        {$page.data.t('create.name_clarification')}
+        {$page.data.t('create.permanent_url_msg')}
+        {$page.data.t('create.only_letters_numbers')}
       </div>
+      {#if urlToUse.length >= data.MIN_URL_LENGTH && !isUniqueURL}
+        <div class="text-xs text-red-600 mt-1">
+          {$page.data.t('create.choose_different_url')}
+        </div>
+      {/if}
       <div class="mb-6" />
 
-      {#if name.length > 2}
-        {#if dev}
-          <Button type="submit" class="mb-5 w-full" color="red">
-            Dev: Add Test Dictionary Immediately
-          </Button>
-        {/if}
-        <div class="flex justify-between items-center" style="direction: ltr">
-          <label for="url" class="text-sm font-medium text-gray-700"> URL </label>
-        </div>
+      <div class="mb-2 text-sm font-medium text-gray-700">
+        Is this a constructed language or an artificial language?
+      </div>
 
-        <div class="mt-1 flex rounded-md shadow-sm" style="direction: ltr">
-          <span
-            class="inline-flex items-center px-2 rounded-l-md border border-r-0
-              border-gray-300 bg-gray-50 text-gray-500 text-sm">
-            livingdictionaries.app/
-          </span>
-          <input
-            id="url"
-            value={customUrl || urlFromName}
-            on:keyup={handleUrlKeyup}
-            required
-            minlength={data.MIN_URL_LENGTH}
-            maxlength={MAX_URL_LENGTH}
-            autocomplete="off"
-            autocorrect="off"
-            spellcheck={false}
-            class="form-input flex-1 block w-full px-2 sm:px-3 py-2 rounded-none
-              rounded-r-md sm:text-sm border"
-            placeholder="url" />
+      <label class="block">
+        <input
+          type="radio"
+          name="conlang"
+          bind:group={conlang}
+          value={true}
+          required />
+        {$page.data.t('misc.assertion')}
+      </label>
+
+      <label class="block">
+        <input
+          type="radio"
+          name="conlang"
+          bind:group={conlang}
+          value={false} />
+        {$page.data.t('misc.negation')}
+      </label>
+      <div class="mb-6" />
+
+      {#if conlang === true}
+        <div class="mb-3 font-italic">
+          Since our mission focuses on documenting endangered and minority natural human languages, we do not offer technical support for constructed languages (conlangs) or artificial languages. Due to our small team and limited hours of operation, we do not offer data imports for conlangs.
         </div>
-        <div class="text-xs text-gray-600 mt-1">
-          {$page.data.t('create.permanent_url_msg')}
-          {$page.data.t('create.only_letters_numbers')}
+        <div class="mb-3 font-italic">
+          Living Dictionaries for constructed languages will not be made public on our website or available on our homepage map. If we see derogatory content in this Living Dictionary, we also reserve the right to delete it.
         </div>
-        {#if urlToUse.length >= data.MIN_URL_LENGTH && !isUniqueURL}
-          <div class="text-xs text-red-600 mt-1">
-            {$page.data.t('create.choose_different_url')}
-          </div>
-        {/if}
+        <div class="mb-6" />
+        <div>
+          <input type="checkbox" id="agreement" name="agreement" required />
+          <label for="agreement">I agree to the above.</label>
+        </div>
+        <div>
+          <input type="checkbox" id="citeAgreement" name="citeAgreement" required />
+          <label for="citeAgreement">I agree to cite any relevant published works within dictionary entries.</label>
+        </div>
+        <div>
+          <input type="checkbox" id="non-commercialAgreement" name="non-commercialAgreement" required />
+          <label for="non-commercialAgreement">I agree that this work is non-commercial.</label>
+        </div>
         <div class="mb-6" />
 
+        <label class="block mb-2 text-sm font-medium text-gray-700" for="conlangInfo">
+          Where did the data in this dictionary come from? Please describe if you collected the data yourself. If you used any published reference sources, please list them here.
+        </label>
+        <textarea
+          name="conlangInfo"
+          required
+          rows="5"
+          minlength="100"
+          maxlength="2500"
+          bind:value={conlang_source}
+          class="form-input w-full" />
+        <div class="flex text-xs">
+          <div class="text-gray-500 ml-auto">{conlang_source.length}/2500</div>
+        </div>
+        <div class="mb-6" />
+
+        <label class="block mb-2 text-sm font-medium text-gray-700" for="conlangUse">
+          Who will be using this dictionary? Will it be used in any educational or media projects?
+        </label>
+        <textarea
+          name="conlangUse"
+          required
+          rows="5"
+          minlength="100"
+          maxlength="2500"
+          bind:value={conlang_use}
+          class="form-input w-full" />
+        <div class="flex text-xs">
+          <div class="text-gray-500 ml-auto">{conlang_use.length}/2500</div>
+        </div>
+        <div class="mb-6" />
+      {/if}
+
+      {#if conlang != null}
         <EditableGlossesField
           minimum={1}
           availableLanguages={glossingLanguages}
@@ -173,6 +247,32 @@
           on_update={new_value => alternate_names = new_value} />
         <div class="mb-6" />
 
+        <!-- <div class="mb-2 text-sm font-medium text-gray-700">
+          {$page.data.t('create.language_used_by_community')}*
+        </div>
+
+        <label class="block">
+          <input
+            type="radio"
+            name="languageUsedByCommunity"
+            bind:group={language_used_by_community}
+            value={true}
+            required />
+          {$page.data.t('misc.assertion')}
+        </label>
+
+        <label class="block">
+          <input
+            type="radio"
+            name="languageUsedByCommunity"
+            bind:group={language_used_by_community}
+            value={false} />
+          {$page.data.t('misc.negation')}
+        </label>
+        <div class="mb-6" /> -->
+      {/if}
+
+      {#if conlang === false}
         <WhereSpoken
           dictionary={{ coordinates: { points, regions } }}
           on_update_points={new_points => points = new_points}
@@ -233,30 +333,6 @@
         <div class="mb-6" />
 
         <div class="mb-2 text-sm font-medium text-gray-700">
-          {$page.data.t('create.language_used_by_community')}*
-        </div>
-
-        <label class="block">
-          <input
-            type="radio"
-            name="languageUsedByCommunity"
-            bind:group={language_used_by_community}
-            value={true}
-            required />
-          {$page.data.t('misc.assertion')}
-        </label>
-
-        <label class="block">
-          <input
-            type="radio"
-            name="languageUsedByCommunity"
-            bind:group={language_used_by_community}
-            value={false} />
-          {$page.data.t('misc.negation')}
-        </label>
-        <div class="mb-6" />
-
-        <div class="mb-2 text-sm font-medium text-gray-700">
           {$page.data.t('create.community_permission')}*
         </div>
         <label class="block">
@@ -304,34 +380,31 @@
         </div>
         <div class="mb-6" />
 
-        <label class="block mb-2 text-sm font-medium text-gray-700" for="conLangDescription">
-          {$page.data.t('create.con_lang_description')}
-        </label>
-        <textarea
-          name="conLangDescription"
-          rows="3"
-          minlength="1"
-          maxlength="1000"
-          bind:value={con_language_description}
-          class="form-input w-full" />
-        <div class="flex text-xs">
-          <div class="text-gray-500 ml-auto">{con_language_description.length}/1000</div>
+        <div>
+          <input type="checkbox" id="citeAgreement" name="citeAgreement" required />
+          <label for="citeAgreement">I agree to cite any relevant published works within dictionary entries.</label>
         </div>
-        <div class="mb-6" />
-
-        <Button type="submit" class="w-full" form="filled" {loading}>
-          {$page.data.t('create.create_dictionary')}
-        </Button>
-
-        <div class="mt-2 text-sm text-gray-600">
-          {$page.data.t('terms.agree_by_submit')}
-          <a href="/terms" class="underline" target="_blank">{$page.data.t('dictionary.terms_of_use')}</a>.
+        <div>
+          <input type="checkbox" id="non-commercialAgreement" name="non-commercialAgreement" required />
+          <label for="non-commercialAgreement">I agree that this work is non-commercial.</label>
         </div>
         <div class="mb-6" />
       {/if}
-    </div>
-  </Form>
-{/if}
+
+      <!-- {#if (conlang && agreement && cite_agreement && non_commercial_agreement) || (!conlang && cite_agreement && non_commercial_agreement)} -->
+      <Button type="submit" class="w-full" form="filled" {loading}>
+        {$page.data.t('create.create_dictionary')}
+      </Button>
+
+      <div class="mt-2 text-sm text-gray-600">
+        {$page.data.t('terms.agree_by_submit')}
+        <a href="/terms" class="underline" target="_blank">{$page.data.t('dictionary.terms_of_use')}</a>.
+      </div>
+      <div class="mb-6" />
+    {/if}
+    <!-- {/if} -->
+  </div>
+</Form>
 
 {#if modal === 'auth'}
   {#await import('$lib/components/shell/AuthModal.svelte') then { default: AuthModal }}
