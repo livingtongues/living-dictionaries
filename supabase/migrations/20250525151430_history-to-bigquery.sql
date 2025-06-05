7133ba78e14cd96291383224cff2c6f491db6848
jacob-8 <jwrunner7@gmail.com>
2025-06-05 15:32:02 -0700

    WIP

--- supabase/migrations/20250525151430_history-to-bigquery.sql
+++ supabase/migrations/20250525151430_history-to-bigquery.sql
@@ -1,3 +1,48 @@
+CREATE extension IF NOT EXISTS wrappers WITH SCHEMA extensions;
+
+create foreign data wrapper bigquery_wrapper
+  handler big_query_fdw_handler
+  validator big_query_fdw_validator;
+
+-- Save BigQuery service account json in Vault and retrieve the created `key_id`
+select vault.create_secret(
+  '
+    {
+      "type": "service_account",
+      "project_id": "your_gcp_project_id",
+      "private_key_id": "your_private_key_id",
+      "private_key": "-----BEGIN PRIVATE KEY-----\n...\n-----END PRIVATE KEY-----\n",
+      ...
+    }
+  ',
+  'bigquery',
+  'BigQuery service account json for Wrappers'
+);
+
+create server bigquery_server
+  foreign data wrapper bigquery_wrapper
+  options (
+    sa_key_id '<key_ID>', -- The Key ID from above.
+    project_id 'your_gcp_project_id',
+    dataset_id 'your_gcp_dataset_id'
+  );
+
+create schema if not exists bigquery;
+
+create foreign table bigquery.my_bigquery_table (
+  id bigint,
+  name text,
+  ts timestamp
+)
+  server bigquery_server
+  options (
+    table 'people',
+    location 'EU'
+  );
+
+
+------------------------------------------------------------
+
 CREATE TRIGGER set_created_by_trigger_senses_in_sentences
 BEFORE UPDATE ON senses_in_sentences
 FOR EACH ROW
@@ -62,3 +107,7 @@ CREATE TRIGGER set_created_by_trigger_api_keys
 BEFORE UPDATE ON api_keys
 FOR EACH ROW
 EXECUTE FUNCTION set_created_by();
+
+
+
+
