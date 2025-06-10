78dd5958e98b50ceb087512187ebe74cd12b3251
Diego Córdova Nieto <43384963+Danble@users.noreply.github.com>
2025-06-10 10:32:16 -0600

    Right side menu (#578)
    * change responsive icons to right side menu
    
    * improve right side menu style

--- packages/site/src/lib/components/shell/Header.svelte
+++ packages/site/src/lib/components/shell/Header.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { Button, ShowHide } from 'svelte-pieces'
+  import { Button, ResponsiveSlideover, ShowHide } from 'svelte-pieces'
   import User from './User.svelte'
   import { page } from '$app/stores'
   import { mode } from '$lib/supabase'
@@ -32,18 +32,18 @@
 
   <div class="print:hidden flex items-center whitespace-nowrap">
     <Button
-      class="!md:inline !px-1 md:!px-4"
+      class="!hidden !md:inline"
       form="text"
       href="https://www.flipcause.com/secure/cause_pdetails/NTQ3NDQ"
       target="_blank">
       <i class="far fa-donate" />
       <span class="ml-1 hidden lg:inline">{$page.data.t('header.donate')}</span>
     </Button>
-    <Button href="/about" form="text" class="!lg:inline !px-1 md:!px-4">
+    <Button href="/about" form="text" class="!hidden !lg:inline" target="_blank">
       <i class="far fa-info-circle" />
       <span class="ml-1 hidden lg:inline">{$page.data.t('header.about')}</span>
     </Button>
-    <Button href="/tutorials" form="text" class="!md:inline !px-1 md:!px-4">
+    <Button href="/tutorials" form="text" class="!hidden !md:inline" target="_blank">
       <span class="i-fluent-learning-app-24-regular -mt-2px" />
       <span class="ml-1 hidden lg:inline">{$page.data.t('header.tutorials')}</span>
     </Button>
@@ -51,19 +51,22 @@
       form="text"
       href="https://docs.google.com/document/d/1MZGkBbnCiAch3tWjBOHRYPpjX1MVd7f6x5uVuwbxM-Q/edit?usp=sharing"
       target="_blank"
-      class="!p-3 text-gray-600 hover:text-black !lg:block !px-1 md:!px-4">
+      class="!p-3 text-gray-600 hover:text-black !hidden !lg:block">
       <i class="far fa-question-circle" />
-      <span class="ml-1 hidden lg:inline">
+      <span class="ml-1">
         FAQ
       </span>
     </Button>
 
     <ShowHide let:show let:toggle>
-      <Button form="text" onclick={toggle} class="!px-1 md:!px-4">
-        <span class="lg:inline">
+      <Button form="text" onclick={toggle}>
+        <span class="inline">
           <i class="far fa-comment" />
         </span>
-        <span class="ml-1 hidden lg:inline">
+        <!-- <span class="lg:hidden">
+          <i class="far fa-question-circle" />
+        </span> -->
+        <span class="ml-1 hidden sm:inline">
           {$page.data.t('header.contact_us')}
         </span>
       </Button>
@@ -75,7 +78,7 @@
     </ShowHide>
 
     <ShowHide let:show let:toggle>
-      <Button form="text" onclick={toggle} class="!px-1 md:!px-4">
+      <Button form="text" onclick={toggle}>
         <i class="far fa-language" />
         <span class="ml-1 hidden lg:inline">
           {$page.data.t('header.language')}
@@ -86,6 +89,58 @@
           <SelectLanguage on:close={toggle} />
         {/await}
       {/if}
+    </ShowHide>
+
+    <ShowHide let:show let:toggle let:set>
+      <button type="button" class="p-3 md:hidden print:p-0" on:click={toggle}>
+        <i class="far fa-bars print:hidden" />
+      </button>
+      <ResponsiveSlideover
+        side={$page.data.t('page.direction') === 'ltr' ? 'right' : 'left'}
+        showWidth={show ? 'md' : null}
+        widthRem={9}
+        on_close={() => set(false)}
+        open={show}>
+        <div class="print:hidden h-full flex flex-col">
+          <header>
+            <div class="block p-3 text-lg font-semibold mb-3 border-b">{$page.data.t('header.menu')}</div>
+          </header>
+          <div>
+            <Button
+              form="text"
+              href="https://www.flipcause.com/secure/cause_pdetails/NTQ3NDQ"
+              target="_blank">
+              <i class="far fa-donate" />
+              <span class="ml-1">{$page.data.t('header.donate')}</span>
+            </Button>
+            <Button href="/about" form="text" target="_blank">
+              <i class="far fa-info-circle" />
+              <span class="ml-1">{$page.data.t('header.about')}</span>
+            </Button>
+            <Button href="/tutorials" form="text" target="_blank">
+              <span class="i-fluent-learning-app-24-regular -mt-2px" />
+              <span class="ml-1">{$page.data.t('header.tutorials')}</span>
+            </Button>
+            <Button
+              form="text"
+              href="https://docs.google.com/document/d/1MZGkBbnCiAch3tWjBOHRYPpjX1MVd7f6x5uVuwbxM-Q/edit?usp=sharing"
+              target="_blank"
+              class="text-gray-600 hover:text-black !lg:block">
+              <i class="far fa-question-circle" />
+              <span class="ml-1">
+                FAQ
+              </span>
+            </Button>
+          </div>
+          <div class="mt-auto">
+            <hr class="md:hidden" />
+            <Button form="menu" class="text-left !md:hidden" onclick={toggle}>
+              <i class="far fa-times fa-lg fa-fw" />
+              {$page.data.t('misc.close')}
+            </Button>
+          </div>
+        </div>
+      </ResponsiveSlideover>
     </ShowHide>
 
     <User />
