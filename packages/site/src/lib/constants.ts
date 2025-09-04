0a0f00db2085022cf40bfd90471538ccf2717c06
Diego Córdova Nieto <43384963+Danble@users.noreply.github.com>
2025-09-03 18:22:35 -0600

    Hotfix multiple sentences import (#599)
    * add missing suffix to multiple example sentences import
    
    * add variant field to Guwar

--- packages/site/src/lib/constants.ts
+++ packages/site/src/lib/constants.ts
@@ -1,4 +1,4 @@
-export const DICTIONARIES_WITH_VARIANTS = ['babanki', 'torwali', 'ksingmul', 'tutelo-saponi', 'tseltal', 'namtrik-de-totoro', 'werikyana', 'woleaia']
+export const DICTIONARIES_WITH_VARIANTS = ['babanki', 'torwali', 'ksingmul', 'tutelo-saponi', 'tseltal', 'namtrik-de-totoro', 'werikyana', 'woleaia', 'guwar']
 
 export enum ResponseCodes {
   OK = 200,
